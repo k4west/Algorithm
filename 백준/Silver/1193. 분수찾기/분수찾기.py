775fbfdbb0fda47e4200b7c0a911a7d83ad7d5cb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-18 15:34:43 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,14 +1,8 @@
-def s(i):
-    return (i*(i+1))//2
 n=int(input())
 i=int((2*n)**0.5)
-while True:
-    if s(i-1)< n <= s(i):
-        d = n - s(i-1) - 1
-        if i % 2 == 0:
-            print(f"{d+1}/{i-d}")
-            break
-        else:
-            print(f"{i-d}/{d+1}")
-            break
-    i += 1
+i+=int(n>(i*(i+1))//2)
+d = n - (i*(i-1))//2 - 1
+if i % 2 == 0:
+    print(f"{d+1}/{i-d}")
+else:
+    print(f"{i-d}/{d+1}")
