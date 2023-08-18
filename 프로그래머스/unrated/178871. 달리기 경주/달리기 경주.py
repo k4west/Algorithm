c8283860f46ba46f7ba722dc8dee9896623def46
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-18 21:28:25 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,7 +1,10 @@
+from collections import deque
+
 def solution(players, callings):
     p = {name:i for i, name in enumerate(players)}
+    players = deque(players)
     for name in callings:
         i = p[name]
         p[name], p[players[i-1]] = i-1, i
         players[i], players[i-1] = players[i-1], players[i]
-    return players
+    return list(players)
