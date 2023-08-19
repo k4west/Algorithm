39e722e2ac3e4f49eafcb912143929c86951ff40
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-19 14:30:13 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -8,3 +8,11 @@ def solution(ingredient):
             for _ in range(4):
                 s.pop()
     return count
+
+# def solution(ingredient):
+#     s = "".join(map(str,ingredient))
+#     n = len(ingredient)
+#     while '1231' in s:
+#         s = s.replace('1231','',1)
+#     n -= len(s)
+#     return n // 4
