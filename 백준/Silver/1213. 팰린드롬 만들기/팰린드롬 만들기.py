514ea2d6f1a7f00797c1f85952e2ea860827dacb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-22 19:03:22 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,16 +1,13 @@
 s = input()
 d = {a:s.count(a) for a in set(s)}
-c = 0
+c, u = 0, ""
 for k, v in d.items():
     if v % 2 != 0:
         c += 1
         u = k
 if c > 1:
     r = "I'm Sorry Hansoo"
-elif c == 1:
-    r = sorted([a*(d[a]//2) for a in set(s)])
-    r = "".join(r) + u  + "".join(reversed(r))
 else:
     r = sorted([a*(d[a]//2) for a in set(s)])
-    r = "".join(r) + "".join(reversed(r))
+    r = "".join(r) + u + "".join(reversed(r))
 print(r)
