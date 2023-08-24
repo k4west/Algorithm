a90b4d48c2eb7b63cd3944f4174ed360196824aa
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-24 21:10:11 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 488 ms, Memory: 70048 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -1,15 +1,8 @@
 import sys
 n, m = map(int, sys.stdin.readline().split())
-D = [[0]*m for _ in range(n)]
-
-for i in range(n):
-    D[i][0] = 1
-
-for j in range(m):
-    D[0][j] = 1
-    
-for i in range(1,n):
-    for j in range(1,m):
-        D[i][j] = (D[i-1][j] + D[i-1][j-1] + D[i][j-1])%(1000000007)
-
-print(D[n-1][m-1])
+D = [[0]*(m+1) for _ in range(n+1)]
+D[0][0] = 1
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        D[i][j] = (D[i-1][j] + D[i-1][j-1] + D[i][j-1])%(10**9+7)
+print(D[-1][-1])
