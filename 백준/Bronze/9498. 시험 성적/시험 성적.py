41e656f4889cb45dd0a649f7700ba27e37d21a4e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-26 20:24:33 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,2 @@
-score = int(input())
-if score > 89:
-    print("A")
-elif score > 79:
-    print("B")
-elif score > 69:
-    print("C")
-elif score > 59:
-    print("D")
-else:
-    print("F")
+s = int(input())//10
+print("FFFFFFDCBAA"[s])
