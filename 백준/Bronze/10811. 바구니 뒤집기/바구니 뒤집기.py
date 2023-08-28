cd1b997075c2b6ffecd2f7aac52e2adbc00de3a4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-28 23:59:26 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,7 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+B = list(range(1,N+1))
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    B = B[:i-1]+B[i-1:j][::-1]+B[j:]
+print(*B)
