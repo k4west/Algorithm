943456cf46f60ad11ac9d434b9b228c09a83fa41
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-28 23:15:15 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,10 +1,5 @@
 import sys
 _ = sys.stdin.readline()
-d = {}
-for i in sys.stdin.readline().split():
-    if i in d:
-        d[i] += 1
-    else:
-        d[i] = 1
+d = list(sys.stdin.readline().split())
 n = sys.stdin.readline().rstrip()
-print(d[n] if n in d else 0)
+print(d.count(n))
