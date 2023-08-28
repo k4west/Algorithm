e03e27bf10c4e62121d92743404710ec1254ada9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-28 23:27:20 +0900

    [Bronze III] Title: 최소, 최대, Time: 364 ms, Memory: 154680 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,4 +1,4 @@
 import sys
-sys.stdin.readline()
-li = sorted(map(int, sys.stdin.readline().split()))
-print(li[0], li[-1])
+_ = sys.stdin.readline()
+li = list(map(int, sys.stdin.readline().split()))
+print(min(li), max(li))
