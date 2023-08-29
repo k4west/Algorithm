a0d75c47323e23a359e56a2928ea103d2aa9a843
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-29 22:37:49 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,9 +1,3 @@
 import sys
 N, B = sys.stdin.readline().split()
-B, s = int(B), 0
-for i, n in enumerate(N[::-1]):
-    if n.isdigit():
-        s += int(n) * (B**i)
-    else:
-        s += (ord(n)-55) * (B**i)
-print(s)
+print(int(N,int(B)))
