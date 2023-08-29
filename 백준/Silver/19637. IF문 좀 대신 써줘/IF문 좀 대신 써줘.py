18edbc8422ba0a7ae9ee708991e2a8b2ffad7cfe
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-29 20:00:48 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 244 ms, Memory: 44104 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,14 +1,11 @@
 import sys
 import bisect
 N, M = map(int, sys.stdin.readline().split())
-
-d = {}
-for _ in range(N):
-    name, power = sys.stdin.readline().split()
-    if int(power) not in d:
-        d[int(power)] = name
-powers = list(d.keys())
-
+names = [0]*N
+powers = [0]*N
+for i in range(N):
+    names[i], power = sys.stdin.readline().split()
+    powers[i] = int(power)
 for _ in range(M):
     power = int(sys.stdin.readline())
-    print(d[powers[bisect.bisect_left(powers, power)]])
+    print(names[bisect.bisect_left(powers, power)])
