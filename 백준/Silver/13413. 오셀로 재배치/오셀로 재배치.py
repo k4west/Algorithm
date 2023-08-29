9e4600343db9c1a0f5027d8eef0485dface1b316
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-29 18:24:47 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 440 ms, Memory: 32728 KB -BaekjoonHub

--- 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
@@ -2,9 +2,9 @@ import sys
 T = int(sys.stdin.readline())
 for _ in range(T):
     sys.stdin.readline()
-    org = [1 if s=="W" else 0 for s in list(sys.stdin.readline().rstrip())]
-    goal = [1 if s=="W" else 0 for s in list(sys.stdin.readline().rstrip())]
+    org = sys.stdin.readline().rstrip()
+    goal = sys.stdin.readline().rstrip()
     diff = sum([1 if o!=g else 0 for o,g in zip(org, goal)])
-    temp = abs(sum(org) - sum(goal))
+    temp = abs(org.count("W") - goal.count("W"))
     count = (diff+temp)//2 
     print(count)
