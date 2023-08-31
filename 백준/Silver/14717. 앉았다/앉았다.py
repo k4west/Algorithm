b1cca04fc7d9fc8ebec006c9f3455a6d27a2f2c1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-08-31 22:38:34 +0900

    [Silver V] Title: 앉았다, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14717. 앉았다/앉았다.py
+++ 백준/Silver/14717. 앉았다/앉았다.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import combinations
 a, b = map(int, sys.stdin.readline().split())
 if a == b:
     print("{:.3f}".format(round(1-(10-a)/153, 3)))
@@ -8,9 +7,9 @@ else:
     li.remove(a)
     li.remove(b)
     a = (a+b)%10
-    cb = combinations(li, 2)
     s = 0
-    for c in cb:
-        if c[0] != c[1] and sum(c)%10 < a:
-            s += 1
+    for i in range(18):
+        for j in range(i+1, 18):
+            if li[i] != li[j] and (li[i]+li[j])%10 < a:
+                s += 1 
     print("{:.3f}".format(round(s/153, 3)))
