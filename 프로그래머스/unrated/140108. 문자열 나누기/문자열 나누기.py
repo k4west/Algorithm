1f1ee991e91e8fd3d971033f95281bcfd97d550e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-04 23:35:52 +0900

    [unrated] Title: 문자열 나누기, Time: 1.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,14 +1,10 @@
 def solution(s):
-    a, b, c, i = 0, 0, 0, 0
-    n, t = len(s), s[i]
-    s += ' '
-    while i < n:
-        if s[i] == t:
+    a, b, c = 0, 0, 0
+    for i in s:
+        if a == b:
+            c += 1
+            t = i
+        if i == t:
             a += 1
         else: b += 1
-        i += 1
-        if a == b and a != 0:
-            a, b = 0, 0
-            c += 1
-            t = s[i]
-    return c+1 if a else c
+    return c
