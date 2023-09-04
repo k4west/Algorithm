45711c6f5505d0098fed9493b6d50456ac4a5f2e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-04 19:54:28 +0900

    [Silver V] Title: 암호해독기, Time: 84 ms, Memory: 37156 KB -BaekjoonHub

--- 백준/Silver/17176. 암호해독기/암호해독기.py
+++ 백준/Silver/17176. 암호해독기/암호해독기.py
@@ -1,18 +1,28 @@
 import sys
-sys.stdin.readline()
-C = map(int, sys.stdin.readline().split())
-P = sys.stdin.readline().strip()
+def f():
+    sys.stdin.readline()
+    C = map(int, sys.stdin.readline().split())
+    P = sys.stdin.readline().strip()
 
-EC = [0] * 53
-for c in C:
-    EC[c] += 1
+    EC = [0] * 53
+    EP = [0] * 53
 
-k = 1
-for p in P:
-    p = ord(p)
-    p -= [32,64,70][int(p>64)+int(p>96)]
-    EC[p] -= 1
-    if EC[p] < 0:
-        k = 0
-        break
-print(['n','y'][k])
+    for c in C:
+        EC[c] += 1
+
+    k = 1
+    for p in P:
+        if p == " ":
+            t = 32
+        elif p.islower():
+            t = 70
+        else: t = 64
+        p = ord(p)
+        p -= t
+        EP[p] += 1
+        
+    for i in range(53):
+        if EC[i] != EP[i]:
+            return 'n'
+    return 'y'
+print(f())
