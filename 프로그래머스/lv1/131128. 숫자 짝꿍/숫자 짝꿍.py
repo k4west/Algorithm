59e5e126f5a02f4c2689073e1feec88c7a844ce1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-05 18:10:43 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,12 +1,8 @@
 def solution(X, Y):
-    x, y = [0]*10, [0]*10
-    for s in "0123456789":
-        x[int(s)] = X.count(s)
-        y[int(s)] = Y.count(s)
     li = []
-    for i, s in enumerate(zip(x,y)):
-        li.extend([i]*min(s))
-    a = "".join(map(str,li[::-1]))
+    for s in "0123456789":
+        li.extend([s]*min(X.count(s), Y.count(s)))
+    a = "".join(li[::-1])
     if not a:
         a = "-1"
     elif a[0] == '0':
