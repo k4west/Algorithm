9563054a8f582de2bd8bd242ec55ef6dce1166c5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-06 18:51:52 +0900

    [Silver V] Title: 멘토와 멘티, Time: 288 ms, Memory: 72128 KB -BaekjoonHub

--- 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.py
+++ 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.py
@@ -3,7 +3,7 @@ def f(N):
     li = [tuple(sys.stdin.readline().split()) for _ in range(N)]
     li.sort(key=lambda x:x[1], reverse=True)
     li.sort(key=lambda x:x[0])
+    li = [" ".join(m) for m in li]
     return li
 li = f(int(sys.stdin.readline()))
-li = [" ".join(m) for m in li]
 print("\n".join(li))
