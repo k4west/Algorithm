c2e554dbc4237e89dd073a00448a1cf17b37f209
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-06 19:54:44 +0900

    [Silver III] Title: 스피카, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/21316. 스피카/스피카.py
+++ 백준/Silver/21316. 스피카/스피카.py
@@ -3,14 +3,12 @@ A = [[0]*13 for _ in range(13)]
 for _ in range(12):
     a, b = map(int, sys.stdin.readline().split())
     A[a][b], A[b][a] = 1, 1
-li = []
 for i in range(1, 13):
-    if sum(A[i]) == 3:
-        li.append(i)
-for p in li:
     s = 0
-    for i, q in enumerate(A[p]):
-        if q:
-            s += sum(A[i])
-    if s == 6:
-        print(p)
+    if sum(A[i]) == 3:
+        for j, p in enumerate(A[i]):
+            if p:
+                s += sum(A[j])
+        if s == 6:
+            print(i)
+            break
