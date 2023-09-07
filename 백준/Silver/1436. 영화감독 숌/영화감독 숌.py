bd542010c7d2cb9dabd57b88b317db270db5f7a2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-08 07:42:32 +0900

    [Silver V] Title: 영화감독 숌, Time: 580 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -2,8 +2,6 @@ import sys
 N = int(sys.stdin.readline())
 s = 666
 for _ in range(N-1):
-    if set(str(s)) == {6}:
-        s += 10*len(str(s))
-    else: s += 1
+    s += 1
     while "666" not in str(s): s += 1
 print(s)
