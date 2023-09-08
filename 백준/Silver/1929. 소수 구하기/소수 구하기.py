539360ef3549d3a03aeee29bcb267ee5f0dafea4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-08 11:08:37 +0900

    [Silver III] Title: 소수 구하기, Time: 188 ms, Memory: 50224 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,10 @@
 import sys
 
 def f(N):
-    li = [False,False,True]+[i%2 for i in range(3,N+1)]
+    li = ["","",True]+[i%2 for i in range(3,N+1)]
     for n in range(3, N+1, 2):
         if li[n]:
-            li[n*n::n] = [False]*(N//n-n+1)
+            li[n*n::n] = [""]*(N//n-n+1)
     return li
 
 M, N = map(int, sys.stdin.readline().split())
