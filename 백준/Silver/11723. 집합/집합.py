1c57621b25238f3cf666a5dd7440f16224b378ca
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-11 22:32:33 +0900

    [Silver V] Title: 집합, Time: 4368 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,22 @@
+import sys
+S = set()
+T = int(sys.stdin.readline())
+for _ in range(T):
+    op, *val = sys.stdin.readline().rstrip().split()
+    if val:
+        x = int(val[0])
+    if op == "add":
+        S.add(x)
+    elif op == "remove":
+        S -= {x}
+    elif op == "check":
+        print(int(x in S))
+    elif op == "toggle":
+        if x in S:
+            S -= {x}
+        else:
+            S.add(x)
+    elif op == "all":
+        S = set(range(1,21))
+    elif op == "empty":
+        S = set()
