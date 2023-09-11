17a405bb642bb998907c910ba9235e018cb0840c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-11 22:41:40 +0900

    [Silver V] Title: 집합, Time: 3820 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,46 +1,26 @@
 import sys
-
-class set_op:
-    def __init__(self):
-        self.S = set()
-    
-    def add(self, x):
-        self.S.add(x)
-    
-    def remove(self, x):
-        self.S.discard(x)
-        
-    def check(self, x):
-        print(int(x in self.S))
-        
-    def toggle(self, x):
-        if x in self.S:
-            self.S.discard(x)
-        else:
-            self.S.add(x)
-    
-    def all(self):
-        self.S = set(range(1, 21))
-    
-    def empty(self):
-        self.S.clear()
-        
-T = int(sys.stdin.readline())
-S = set_op()
-for _ in range(T):
+def f():
+    global S
     op, *val = sys.stdin.readline().rstrip().split()
     if val:
         x = int(val[0])
-        if op == "add":
+    if op == "add":
+        S.add(x)
+    elif op == "remove":
+        S -= {x}
+    elif op == "check":
+        print(int(x in S))
+    elif op == "toggle":
+        if x in S:
+            S -= {x}
+        else:
             S.add(x)
-        elif op == "remove":
-            S.remove(x)
-        elif op == "check":
-            S.check(x)
-        elif op == "toggle":
-            S.toggle(x)
-    else:
-        if op == "all":
-            S.all()
-        elif op == "empty":
-            S.empty()
+    elif op == "all":
+        S = set(range(1,21))
+    elif op == "empty":
+        S = set()
+        
+S = set()
+T = int(sys.stdin.readline())
+for _ in range(T):
+    f()
