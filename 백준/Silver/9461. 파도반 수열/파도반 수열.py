9c6345d14570b6abe7ef82109119fe595edc39f9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-13 08:27:49 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,7 +1,7 @@
-import sys
-T = int(sys.stdin.readline())
-P = [0, 1, 1, 1, 2, 2] + [0] * 95
-for n in range(6, 101):
-            P[n] = P[n-1] + P[n-5]
-for _ in range(T):
-    print(P[int(sys.stdin.readline())])
+a = open(0)
+next(a)
+P = [1, 1, 1, 2, 2]
+for _ in range(95):
+    P.append(P[-1] + P[-5])
+for n in a:
+    print(P[int(n)-1])
