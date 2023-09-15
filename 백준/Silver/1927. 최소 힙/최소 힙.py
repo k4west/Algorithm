de13d00096cbe6a9c20dc88b98397bd7ab45e397
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-15 22:20:58 +0900

    [Silver II] Title: 최소 힙, Time: 96 ms, Memory: 37420 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,14 +1,13 @@
 import heapq
-from sys import stdin
 def f(a):
     next(a)
     li, t = [], []
     for n in a:
-        n = int(n)
-        if n: heapq.heappush(li, n)
-        else:
+        if n=="0\n":
             if li: t.append(str(heapq.heappop(li)))
             else: t.append("0")
+        else:
+            heapq.heappush(li, int(n))
     print(*t, sep="\n")
 a = open(0)
 f(a)
