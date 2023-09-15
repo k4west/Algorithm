40e3faf6af1d7b2e84f4f8a49ed1d4bfad5e46df
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-15 21:12:25 +0900

    [Silver III] Title: Four Squares, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,10 +1,11 @@
 from sys import stdin
+def g(N):
+    return int(N**0.5) == N**0.5
 def f(N):
-    if int(N**0.5) == N**0.5:
+    if g(N):
         return 1
-    s = {i**2 for i in range(1, int(N**0.5)+1)}
-    for i in s:
-        if N-i in s:
+    for i in range(1, int(N**0.5)+1):
+        if g(N-i**2):
             return 2
     while N%4==0:
         N//=4
