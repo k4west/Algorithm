0c9f277cd06fc8a64f6d09ccd03c3bdddd174460
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-19 07:25:56 +0900

    [Silver II] Title: 색종이 만들기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,17 +1,19 @@
 def f(N, A):
+    global wb
     s = sum(sum(A, []))
-    if s == 0: w, b = 1, 0
-    elif s == N**2: w, b = 0, 1
+    if s == 0: wb[0] += 1
+    elif s == N**2: wb[1] += 1
     else:
         M = N//2
-        A0, A1, A2, A3 = [i[:M] for i in A[:M]], [i[M:] for i in A[:M]], [i[:M] for i in A[M:]], [i[M:] for i in A[M:]]
-        t0, t1, t2, t3 = f(M, A0), f(M, A1), f(M, A2), f(M, A3)
-        w, b = [sum(j) for j in zip(t0, t1, t2, t3)]
-    return w, b
+        f(M, [i[:M] for i in A[:M]])
+        f(M, [i[M:] for i in A[:M]])
+        f(M, [i[:M] for i in A[M:]])
+        f(M, [i[M:] for i in A[M:]])
+    return
 
 a = open(0)
 N = int(next(a))
 A = [list(map(int, i.split())) for i in a]
-w, b = f(N, A)
-print(w)
-print(b)
+wb = [0, 0]
+f(N, A)
+print(*wb, sep='\n')
