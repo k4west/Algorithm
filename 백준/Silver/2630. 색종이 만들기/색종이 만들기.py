d4e830e9f433668c2563e84747f3b026afbe7ccb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-19 07:41:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,19 +1,21 @@
-def f(N, A):
+def g(i, j, N):
     global wb
-    s = sum(sum(A, []))
-    if s == 0: wb[0] += 1
-    elif s == N**2: wb[1] += 1
-    else:
-        M = N//2
-        f(M, [i[:M] for i in A[:M]])
-        f(M, [i[M:] for i in A[:M]])
-        f(M, [i[:M] for i in A[M:]])
-        f(M, [i[M:] for i in A[M:]])
-    return
-
+    c = A[i][j]
+    if N == 1: wb[c] += 1; return
+    M = N//2
+    for s in range(i,i+N):
+        for t in range(j,j+N):
+            if A[s][t] != c:
+                g(i,j,M)
+                g(i+M,j,M)
+                g(i,j+M,M)
+                g(i+M,j+M,M)
+                return
+    wb[c] += 1
+    
 import sys
 N = int(sys.stdin.readline())
 A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 wb = [0, 0]
-f(N, A)
+g(0, 0, N)
 print(*wb, sep='\n')
