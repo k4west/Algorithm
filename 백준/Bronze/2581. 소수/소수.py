8f7efdca3bd9e5011bafadbed95f24634059a6a6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-18 10:34:00 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,8 +1,10 @@
+def f(M,N):
+    li = [0,0]+[1]*(N-1)
+    for n in range(2, int(N**.5)+1):
+        if li[n]:
+            for i in range(n**2, N+1, n): li[i] = 0
+    return [i for i in range(M, N+1) if li[i]]
 M, N = map(int, open(0))
-li = [0,0]+[1]*(N-1)
-for n in range(2, int(N**.5)+1):
-    if li[n]:
-        for i in range(n**2, N+1, n): li[i] = 0
-s = [i for i in range(M, N+1) if li[i]]
+s = f(M, N)
 if s: print(sum(s), s[0], sep='\n')
 else: print('-1')
