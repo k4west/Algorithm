89cb9c4a42e5b0ff13a8cb5078bc91f0fc6c1994
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-18 10:31:02 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,9 +1,8 @@
 M, N = map(int, open(0))
-d = {i:1 for i in range(2, N+1)}
+li = [0,0]+[1]*(N-1)
 for n in range(2, int(N**.5)+1):
-    if d[n]:
-        for i in range(n**2, N+1, n): 
-            d[i] = 0
-li = [k for k, v in d.items() if v and k >= M]
-if li: print(sum(li), li[0], sep='\n')
+    if li[n]:
+        for i in range(n**2, N+1, n): li[i] = 0
+s = [i for i in range(M, N+1) if li[i]]
+if s: print(sum(s), s[0], sep='\n')
 else: print('-1')
