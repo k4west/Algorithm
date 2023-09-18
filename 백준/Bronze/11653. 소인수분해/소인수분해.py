41f5764e32558ebb36e70e7459bb357c8c8ac64c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-18 11:30:05 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,8 +1,8 @@
 N = int(input())
 t = []
 for n in range(2, int(N**.5)+1):
-    c = 0
     while not N%n:
-        print(n)
+        t.append(n)
         N //= n
+if t: print(*t, sep='\n')
 if N!=1: print(N)
