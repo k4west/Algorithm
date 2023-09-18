fc4fc375850869968d6ae21eaf6fee9502c3cf1e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-18 10:27:12 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 31544 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,14 +1,9 @@
-import sys
-M = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
-s = []
-t = []
-for n in range(2, N+1):
-    if n not in t:
-        t.extend([i for i in range(n**2, N+1, n)])
-        if n >= M:
-            s.append(n)
-if s:
-    print(sum(s))
-    print(s[0])
+M, N = map(int, open(0))
+d = {i:1 for i in range(2, N+1)}
+for n in range(2, int(N**.5)+1):
+    if d[n]:
+        for i in range(n**2, N+1, n): 
+            d[i] = 0
+li = [k for k, v in d.items() if v and k >= M]
+if li: print(sum(li), li[0], sep='\n')
 else: print('-1')
