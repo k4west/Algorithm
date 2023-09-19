ae87d1fc4b5a506ce691a037b990fe9261825b39
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-19 15:19:22 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 520 ms, Memory: 64944 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -10,17 +10,20 @@ def f(i, v, A):
                 li.append(k)
                 v[k] = True
 
-N, M = map(int, sys.stdin.readline().split())
-A = [[] for _ in range(N+1)]
-for _ in range(M):
-    i, j = map(int, sys.stdin.readline().split())
-    A[i].append(j)
-    A[j].append(i)
+def g(N, M):
+    A = [[] for _ in range(N+1)]
+    for _ in range(M):
+        i, j = map(int, sys.stdin.readline().split())
+        A[i].append(j)
+        A[j].append(i)
     
-c = 0
-v = [False] * (N+1)
-for i in range(1,N+1):
-    if not v[i]:
-        f(i, v, A)
-        c += 1
-print(c)
+    c = 0
+    v = [False] * (N+1)
+    for i in range(1,N+1):
+        if not v[i]:
+            f(i, v, A)
+            c += 1
+    print(c)
+    
+N, M = map(int, sys.stdin.readline().split())
+g(N, M)
