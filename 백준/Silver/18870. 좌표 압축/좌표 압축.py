f17c27211ac8e85c06f4f1f9f62a0bbabfd23450
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-19 22:51:35 +0900

    [Silver II] Title: 좌표 압축, Time: 1064 ms, Memory: 302876 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,6 +1,8 @@
 import sys
+def f(X):
+    d = {x:i for i, x in enumerate(sorted(set(X)))}
+    li = [d[x] for x in X]
+    print(*li)
 sys.stdin.readline()
 X = list(map(int, sys.stdin.readline().split()))
-d = {x:i for i, x in enumerate(sorted(set(X)))}
-li = [d[x] for x in X]
-print(*li)
+f(X)
