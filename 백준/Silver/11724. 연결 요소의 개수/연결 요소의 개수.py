49579aacde0e789cb10eb95e298e422dd94f5a0d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-19 16:55:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,22 +1,26 @@
 import sys
-N, M = map(int, sys.stdin.readline().split())
-c, t = N, 1
-li = [0 for _ in range(N+1)]
-for _ in range(M):
-    if c == 1: break
-    i, j = map(int, sys.stdin.readline().split())
-    if li[i] * li[j] == 0:
-        if li[i] == li[j]:
-            li[i] = li[j] = t
-            t += 1
-        else:
-            li[i] = li[j] = li[i]+li[j]
-    elif li[i] != li[j]:
-        s = min(li[i], li[j])
-        l = max(li[i], li[j])
-        for k in range(1, N+1):
-            if li[k] == l:
-                li[k] = s
-    else: c += 1
-    c -= 1
-print(c)
+def f(N, M):
+    c, t = N, 1
+    li = [0 for _ in range(N+1)]
+    for _ in range(M):
+        if c == 1: break
+        i, j = map(int, sys.stdin.readline().split())
+        if li[i] * li[j] == 0:
+            if li[i] == li[j]:
+                li[i] = li[j] = t
+                t += 1
+            else:
+                li[i] = li[j] = li[i]+li[j]
+        elif li[i] != li[j]:
+            s = min(li[i], li[j])
+            l = max(li[i], li[j])
+            for k in range(1, N+1):
+                if li[k] == l:
+                    li[k] = s
+        else: c += 1
+        c -= 1
+    print(c)
+
+if __name__ == "__main__":
+    N, M = map(int, sys.stdin.readline().split())
+    f(N, M)
