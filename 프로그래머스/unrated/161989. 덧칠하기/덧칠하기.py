b13a460f3e97fc70af4e95aac7365ab87505301a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-20 17:52:16 +0900

    [unrated] Title: 덧칠하기, Time: 4.17 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,7 @@
 def solution(n, m, section):
-    c, t = 0, 0
-    li = [0] * (n+1)
-    if m == 1:
-        return len(section)
-    else:
-        for i in section:
-            li[i] = 1
-        for i in range(1,n+1):
-            if t:
-                t -= 1
-            elif li[i]:
-                c += 1
-                t = m-1
+    c, pre = 1, section[0]
+    for now in section[1:]:
+        if now - pre >= m:
+            c +=1
+            pre = now
     return c
