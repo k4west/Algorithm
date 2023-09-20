570c62352c743ee546e0c7c39563b64894243272
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-20 21:30:57 +0900

    [Bronze II] Title: 커트라인, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.py
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -1,5 +1,7 @@
-import sys 
-if __name__ == "__main__":
-    _, k = map(int, sys.stdin.readline().split())
+import sys
+def f(k): 
     li = sorted(map(int, sys.stdin.readline().split()))
     print(li[-k])
+if __name__ == "__main__":
+    _, k = map(int, sys.stdin.readline().split())
+    f(k)
