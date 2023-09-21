d1ee2b03a90bcea6e8c06a40a215216e01d27938
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-21 14:27:38 +0900

    [Silver I] Title: 회의실 배정, Time: 224 ms, Memory: 52820 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,14 +1,11 @@
 import sys
-def f(N):
-    li = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]    
-    li.sort(key=lambda x: (x[1], x[0]))
-    c = 1
-    e = li[0][1]
-    for a, b in li[1:N]:
+def f():
+    N = int(sys.stdin.readline())
+    li = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(N)], key=lambda x: (x[1], x[0]))
+    c, e = 0, 0
+    for a, b in li:
         if e <= a:
             e = b
             c += 1
     print(c)
-
-N = int(sys.stdin.readline())
-f(N)
+if __name__ == "__main__": f()
