88ce3fa032190aafe5463ea46b78824366e5b559
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-21 21:33:22 +0900

    [Gold V] Title: 토마토, Time: 548 ms, Memory: 79236 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,32 @@
 import sys
+
 def f():
     M, N = map(int, sys.stdin.readline().split())
-    Box =  [[] for _ in range(N)]
+    Box =  []
+    for _ in range(N):
+        Box.extend(sys.stdin.readline().split())
     ripe = []
-    for i in range(N):
-        Box[i] = sys.stdin.readline().split()
-        for j, t in enumerate(Box[i]):
-            if t=='1':
-                ripe.append((i,j))
+    for i, t in enumerate(Box):
+        if t=='1':
+            ripe.append((i))
     day = -1
     while ripe:
         day += 1
         temp = []
         for t in ripe:
-            i, j = t
-            if i > 0 and Box[i-1][j]=='0':
-                Box[i-1][j] = True
-                temp.append((i-1,j))
-            if i < N-1 and Box[i+1][j]=='0':
-                Box[i+1][j] = True
-                temp.append((i+1,j))
-            if j > 0 and Box[i][j-1]=='0':
-                Box[i][j-1] = True
-                temp.append((i,j-1))
-            if j < M-1 and Box[i][j+1]=='0':
-                Box[i][j+1] = True
-                temp.append((i,j+1))
+            if t//M > 0 and Box[t-M]=='0':
+                Box[t-M] = '1'
+                temp.append(t-M)
+            if t//M < N-1 and Box[t+M]=='0':
+                Box[t+M] = '1'
+                temp.append(t+M)
+            if t%M > 0 and Box[t-1]=='0':
+                Box[t-1] = '1'
+                temp.append(t-1)
+            if t%M < M-1 and Box[t+1]=='0':
+                Box[t+1] = '1'
+                temp.append(t+1)
         ripe = temp
-    for a in Box:
-        if '0' in a:
-            print(-1)
-            return
-    print(day)
+    print((day,-1)['0' in Box])
 if  __name__ == "__main__":
     f()
