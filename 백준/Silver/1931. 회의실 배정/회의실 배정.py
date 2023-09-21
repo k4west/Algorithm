3cdc2fae86a7a076a47ed2968ebe57e39a24355d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-21 14:47:17 +0900

    [Silver I] Title: 회의실 배정, Time: 160 ms, Memory: 53684 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,6 +1,5 @@
 def f(c=0):
     a = open(0)
-    next(a)
     *li, = map(int, a.read().split())
-    print(sum((c:=e) and 1 for e, s in sorted(zip(li[1::2], li[::2])) if c <= s))
+    print(sum((c:=e) and 1 for e, s in sorted(zip(li[2::2], li[1::2])) if c <= s))
 if __name__ == "__main__": f()
