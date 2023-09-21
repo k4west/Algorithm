8402b36df65780de4d706bf8689fc77eacae921f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-21 17:28:35 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 134464 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,36 @@
 import sys
 def f():
     M, N = map(int, sys.stdin.readline().split())
-    A =  [[] for _ in range(N)]
-    T = []
+    Box =  [[] for _ in range(N)]
+    ripe = []
     for i in range(N):
-        A[i] = list(map(int, sys.stdin.readline().split()))
-        for j, t in enumerate(A[i]):
-            if t==1:
-                T.append((i,j))
-    c = -1
-    while T:
-        c += 1
+        Box[i] = sys.stdin.readline().split()
+        for j, t in enumerate(Box[i]):
+            if t=='1':
+                ripe.append((i,j))
+    day = -1
+    while ripe:
+        day += 1
         temp = []
-        for t in T:
+        for t in ripe:
             i, j = t
-            if i > 0 and not A[i-1][j]:
-                A[i-1][j] = True
+            if i > 0 and Box[i-1][j]=='0':
+                Box[i-1][j] = True
                 temp.append((i-1,j))
-            if i < N-1 and not A[i+1][j]:
-                A[i+1][j] = True
+            if i < N-1 and Box[i+1][j]=='0':
+                Box[i+1][j] = True
                 temp.append((i+1,j))
-            if j > 0 and not A[i][j-1]:
-                A[i][j-1] = True
+            if j > 0 and Box[i][j-1]=='0':
+                Box[i][j-1] = True
                 temp.append((i,j-1))
-            if j < M-1 and not A[i][j+1]:
-                A[i][j+1] = True
+            if j < M-1 and Box[i][j+1]=='0':
+                Box[i][j+1] = True
                 temp.append((i,j+1))
-        T = temp
-    for a in A:
-        if 0 in a:
+        ripe = temp
+    for a in Box:
+        if '0' in a:
             print(-1)
             return
-    print(c)
+    print(day)
 if  __name__ == "__main__":
     f()
