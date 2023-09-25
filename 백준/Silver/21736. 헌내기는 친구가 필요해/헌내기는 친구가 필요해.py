a4ac59f2aa976f403714b12b90a2da866179e917
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-25 23:20:29 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 1304 ms, Memory: 139140 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,33 +1,35 @@
 import sys
-sys.setrecursionlimit(10**6)
-def f(i, j, A, v, N, M):
+def f(s, A, N, M):
+    v = [[False] * M for _ in range(N)]
+    v[s[0]][s[1]] = True
+    q = [s]
     c = 0
-    if A[i][j] == 'P':
-        c += 1
-    if j<M-1 and A[i][j+1] != 'X' and not v[i][j+1]:
-        v[i][j+1] = True
-        c += f(i, j+1, A, v, N, M)
-    if j>0 and A[i][j-1] != 'X' and not v[i][j-1]:
-        v[i][j-1] = True
-        c += f(i, j-1, A, v, N, M)
-    if i<N-1 and A[i+1][j] != 'X' and not v[i+1][j]:
-        v[i+1][j] = True
-        c += f(i+1, j, A, v, N, M)
-    if i>0 and A[i-1][j] != 'X' and not v[i-1][j]:
-        v[i-1][j] = True
-        c += f(i-1, j, A, v, N, M)
+    while q:
+        i, j = q.pop(0)
+        if A[i][j] == 'P':
+            c += 1
+        if j<M-1 and A[i][j+1] != 'X' and not v[i][j+1]:
+            v[i][j+1] = True
+            q.append((i, j+1))
+        if j>0 and A[i][j-1] != 'X' and not v[i][j-1]:
+            v[i][j-1] = True
+            q.append((i, j-1))
+        if i<N-1 and A[i+1][j] != 'X' and not v[i+1][j]:
+            v[i+1][j] = True
+            q.append((i+1, j))
+        if i>0 and A[i-1][j] != 'X' and not v[i-1][j]:
+            v[i-1][j] = True
+            q.append((i-1, j))
     return c
 def m():
     N, M = map(int, sys.stdin.readline().split())
     A = []
-    v = [[False] * M for _ in range(N)]
     for i in range(N):
         li = list(sys.stdin.readline().rstrip())
         A.append(li)
         if 'I' in li:
-            x, y = (i, li.index('I'))
-    v[x][y] = True
-    n = f(x, y, A, v, N, M)
+            s = (i, li.index('I'))
+    n = f(s, A, N, M)
     print(n if n else 'TT')
 if __name__=="__main__":
     m()
