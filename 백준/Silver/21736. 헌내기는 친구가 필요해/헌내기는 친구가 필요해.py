abae8f78c744ab0fbcfbd8e1b08b8abb64e8b3a4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-25 23:39:31 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 1140 ms, Memory: 39700 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 def m():
     N, M = map(int, sys.stdin.readline().split())
     A = []
@@ -12,16 +11,16 @@ def m():
             x, y = (i, li.index('I'))
     v[x][y] = True
     c = 0
-    queue = deque([(x, y)])
-    while queue:
-        i, j = queue.popleft()
+    q = [(x, y)]
+    while q:
+        i, j = q.pop(0)
         if A[i][j] == 'P':
             c += 1
         for dx, dy in d:
             ni, nj = i + dx, j + dy
             if 0 <= ni < N and 0 <= nj < M and A[ni][nj] != 'X' and not v[ni][nj]:
                 v[ni][nj] = True
-                queue.append((ni, nj))
+                q.append((ni, nj))
     print(c if c else 'TT')
 if __name__ == "__main__":
     m()
