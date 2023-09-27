854bf7c3012b9f268854c0d4823b04dbe0fe7dc6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-09-28 08:36:09 +0900

    [Silver II] Title: 최대 힙, Time: 108 ms, Memory: 37420 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+import heapq
+def f(a):
+    next(a)
+    li, t = [], []
+    for n in a:
+        if n=="0\n":
+            if li: t.append(str(-heapq.heappop(li)))
+            else: t.append("0")
+        else:
+            heapq.heappush(li, -int(n))
+    print(*t, sep="\n")
+a = open(0)
+f(a)
