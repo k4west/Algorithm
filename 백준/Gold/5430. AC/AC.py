a1eb73ed6dfa4757aff33fcc2b75b18cef33b378
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-06 10:49:35 +0900

    [Gold V] Title: AC, Time: 3804 ms, Memory: 42072 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,22 @@
+import sys
+T = range(int(sys.stdin.readline()))
+li = []
+for _ in T:
+    flag, e = 0, False
+    fns = sys.stdin.readline().rstrip()
+    n = int(sys.stdin.readline())
+    arr = sys.stdin.readline().rstrip()[1:-1].split(',')
+    if arr == [""]: arr = []
+    for fn in fns:
+        if fn == "R":
+            flag = (flag + 1)%2
+        elif arr:
+            arr.pop(-flag)
+        else:
+            e = True
+            li.append("error")
+            break
+    if not e:
+        if not flag: flag = -1
+        li.append("["+",".join(arr[::-flag])+"]")
+print(*li, sep="\n")
