49f32a7f4720bae246951199c4a616d5d4bedea4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-10 22:38:24 +0900

    [Gold V] Title: 토마토, Time: 1428 ms, Memory: 48468 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -5,8 +5,9 @@ def main():
     M, N, H = map(int, sys.stdin.readline().split())
     tomato = [[] for _ in range(H)]
     ripen = deque()
+    new_ripen = deque()
     d = ((1, 0, 0), (0, 1, 0), (0, 0, 1), (-1, 0, 0), (0, -1, 0), (0, 0, -1))
-    days = 1
+    days = 0
 
     for i in range(H):
         box = []
@@ -18,24 +19,25 @@ def main():
                     ripen.append((i, j, k))
         tomato[i] = box
     
-    while ripen:
+    while ripen or new_ripen:
+        if not ripen:
+            ripen, new_ripen = new_ripen, ripen
+            days += 1
+
         h, n, m = ripen.popleft()
-        c = tomato[h][n][m] + 1
         for dm, dn, dh in d:
             nh, nn, nm = h + dh, n + dn, m + dm
             if 0 <= nh < H and 0 <= nn < N and 0 <= nm < M:
                 if not tomato[nh][nn][nm]:
-                    ripen.append((nh, nn, nm))
-                    tomato[nh][nn][nm] = c
-                    days = max(days, c)
+                    new_ripen.append((nh, nn, nm))
+                    tomato[nh][nn][nm] = 1
     
     for i in range(H):
         for col in range(N):
             if 0 in tomato[i][col]:
                     print('-1')
-                    return
-            
-    print(days - 1)
+                    return        
+    print(days)
 
 if __name__ == "__main__":
     main()
