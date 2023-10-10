83acd1535eb253f30cc390a075e4abc307e6d076
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-10 10:18:26 +0900

    [Silver V] Title: 셀프 넘버, Time: 120 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,10 +1,13 @@
+nums = [False for _ in range(10001)]
 def gen(n):
     return n+(eval("+".join(str(n))))
 
-nums, gens = set(range(1, 10001)), set()
-i = 1
 for i in range(1, 10001):
-    gens.add(gen(i))
+    g = gen(i)
+    if g < 10001: nums[g] = True
 
-nums -= gens
-print("\n".join(map(str, sorted(nums))))
+li = []
+for i in range(1, 10001):
+    if not nums[i]: li.append(i)
+
+print("\n".join(map(str, li)))
