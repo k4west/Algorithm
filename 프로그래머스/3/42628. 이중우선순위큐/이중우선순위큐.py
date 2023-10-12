dfab2942221e81e2cd7cc72d5d7ba09f935bd123
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-12 23:57:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,45 +1,57 @@
-# 최대 힙, 최소 힙을 각각 생성해서 결과적으로 입력시 두 번 저장, 삭제시 두 번 삭제를 한다.
-# 입력할 때, 정수와 함께 주어진 입력 순번도 함께 저장
-# 삭제할 때, 앞서 다른 힙에서 삭제된 정수들을 삭제하고, 최댓값 또는 최솟값을 삭제한다.
-# 다른 힙에서 삭제한 정수의 정보는 삭제시 입력 순번을 기준으로 표시된 방문 기록으로 확인 한다
-import heapq
+# 최대 힙, 최소 힙을 각각 생성해서 결과적으로 입력시 두 번 저장, 삭제시 필요하다면 두 번 삭제를 한다.
+# 삭제할 때, 진짜 최댓값 또는 최솟값을 삭제하기 위해서 앞서 다른 힙에서 삭제된 정수들을 먼저 삭제한다.
+from heapq import heappop, heappush
 
 def solution(operations):
-    # 최대, 최소힙, 방문 기록 초기화
-    Qmax, Qmin = [], []
-    visited = [False] * 10**6
-    for i, s in enumerate(operations):  # i : 입력 순번
-        # 연산자와 정수 입력받기
-        op, n = s.split()
+    Qmax, Qmin, d = [], [], {}  # 최대, 최소힙, 정수의 개수 저장하는 딕셔너리 초기화
+    cnt = 0   # Q에 남아있는 정수의 개수 : 입력시 +1, 실제 삭제시 -1
 
-        # 최대 힙, 최소 힙에 각각 정수와 입력 순번 저장
+    for s in operations:
+        op, n = s.split()   # 연산자, 정수 
+        
+        # 최대 힙, 최소 힙에 각각 주어진 정수 저장, 딕셔너리에 저장된 주어진 정수 개수 저장
         if op == 'I':
+            cnt += 1
             n = int(n)
-            heapq.heappush(Qmax, (-n, i))
-            heapq.heappush(Qmin, (n, i))
+            c = d.get(n, 0)
+            if not c:
+                heappush(Qmax, -n)
+                heappush(Qmin, n)
+            d[n] = c + 1
 
-        # 다른 힙에서 삭제된 정수를 입력 순번을 통해 확인해서 삭제해준 뒤,
-        # Q에 정수가 남아 있으면, 입력 순번으로 방문 표시 후 1과 -1에 따라서 최댓값, 최솟값 삭제
-        else:
+        # Q에 남아있는 정수가 있으면 삭제 실행
+        elif cnt:
+            cnt -= 1
             if n == '1':
-                while Qmax and visited[Qmax[0][1]]:
-                    heapq.heappop(Qmax)
-                if Qmax:
-                    visited[Qmax[0][1]] = True
-                    heapq.heappop(Qmax)
+                # Qmin에서 삭제된 정수가 Qmax의 최댓값으로 저장되어 있다면 그런 정수는 모두 삭제
+                while not d.get(-Qmax[0], 0): heappop(Qmax)
+
+                # 최댓값이 여러 개 있으면 딕셔너리에서 개수만 줄이고, 1개만 있으면 Qmax에서도 삭제
+                m = -Qmax[0]
+                c = d.get(m, 0)
+                if c:
+                    d[m] -= 1
+                    if c == 1: heappop(Qmax)
             else:
-                while Qmin and visited[Qmin[0][1]]:
-                    heapq.heappop(Qmin)
-                if Qmin:
-                    visited[Qmin[0][1]] = True
-                    heapq.heappop(Qmin)
+                # Qmax에서 삭제된 정수가 Qmin의 최솟값으로 저장되어 있다면 그런 정수는 모두 삭제
+                while not d.get(Qmin[0], 0): heappop(Qmin)
 
-    # 다른 힙에서 삭제된 정수를 마저 삭제하는 과정
-    while Qmax and visited[Qmax[0][1]]:
-        heapq.heappop(Qmax)
-    while Qmin and visited[Qmin[0][1]]:
-        heapq.heappop(Qmin)
+                # 최솟값이 여러 개 있으면 딕셔너리에서 개수만 줄이고, 1개만 있으면 Qmin에서도 삭제
+                m = Qmin[0]
+                c = d.get(m, 0)
+                if c:
+                    d[m] -= 1
+                    if c == 1: heappop(Qmin)  
 
-    # 모든 연산 처리 후, 남아 있는 Qmax, Qmin의 최댓값, 최솟값 또는 EMPTY 반환
-    if Qmax : return [-Qmax[0][0], Qmin[0][0]]
-    return [0,0]
+    # 실제 남아있는 최댓값과 최솟값을 구하기 위해서 다른 힙에서 삭제된 정수 삭제
+    # 모든 처리 후, 남아 있는 Qmax, Qmin의 최댓값, 최솟값 또는 EMPTY 반환
+    if cnt:
+        while not d.get(-Qmax[0], 0): heappop(Qmax)
+        while not d.get(Qmin[0], 0): heappop(Qmin)
+        return [-Qmax[0], Qmin[0]]
+    return [0, 0]
+
+
+if __name__ == "__main__":
+    main()
+    
