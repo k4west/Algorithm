fb1025410269245b30264c19a71a6765c275a05d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-13 20:50:07 +0900

    [Gold IV] Title: DSLR, Time: 4572 ms, Memory: 214724 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -5,7 +5,7 @@ li = []
 for _ in range(int(input())):
     s, e = map(int, input().split())
     visited = [False]*10000
-    q = deque([[s, ""]])
+    q = deque([(s, "")])
     visited[s] = True
     while q:
         x, op = q.popleft()
@@ -15,17 +15,17 @@ for _ in range(int(input())):
         x0 = (x<<1)%10000
         if not visited[x0]:
             visited[x0] = True
-            q.append([x0, op + "D"])
+            q.append((x0, op + "D"))
         x1 = (x-1)%10000
         if not visited[x1]:
             visited[x1] = True
-            q.append([x1, op + "S"])
+            q.append((x1, op + "S"))
         x2 = (x*10)%10000 + x//1000
         if not visited[x2]:
             visited[x2] = True
-            q.append([x2, op + "L"])
+            q.append((x2, op + "L"))
         x3 = x%10*1000 + x//10
         if not visited[x3]:
             visited[x3] = True
-            q.append([x3, op + "R"])
+            q.append((x3, op + "R"))
 print("\n".join(li))
