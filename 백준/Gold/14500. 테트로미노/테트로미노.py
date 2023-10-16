06cb7c40c365b4e0433b05efc2c311aeb7c5af14
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-16 23:54:30 +0900

    [Gold IV] Title: 테트로미노, Time: 144 ms, Memory: 36412 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -6,23 +6,29 @@ def main():
     N, M = map(int, input().split())
     d = ((-1, 0), (1, 0), (0, -1), (0, 1))
     T = [list(map(int, input().split())) for _ in range(N)]
-    m = max(map(max, T))
+    m = max(map(max, T))    # 가장 큰 값
     
     def dfs(y, x, cnt, s):
         global ans
-        if cnt == 4:
-            ans = max(ans, s)
-            return
+
+        # 남은 기회로 모두 최댓값을 탐색하는 경우의 값이 
+        # 현재 정답보다 작으면 탐색의 의미가 없음
         if ans > s + m*(4-cnt):
             return
-        for dy, dx in d:
+        
+        if cnt == 4:        # 4칸 탐색 완료시
+            ans = max(ans, s)   # 정답을 최댓값으로 업데이트
+            return
+        
+        for dy, dx in d:    # 상, 하, 좌, 우
             ny, nx = y + dy, x + dx
+            # 범위 내 방문 안 한 칸으로 탐색
             if 0 <= ny < N and 0 <= nx < M and T[ny][nx]:
                 t = T[ny][nx]
-                if cnt == 2:
-                    T[ny][nx] = False
-                    dfs(y, x, cnt+1, s+t)
-                    T[ny][nx] = t
+                if cnt == 2:    # 테트로미노 모양 반영
+                    T[ny][nx] = False       # 탐색 표시
+                    dfs(y, x, cnt+1, s+t)   # 탐색한 칸 수 +1, 탐색한 칸들의 합 업데이트
+                    T[ny][nx] = t       # 다른 탐색의 경우의 수에 포함하기 위해 원래 값으로 복원
                 T[ny][nx] = False
                 dfs(ny, nx, cnt+1, s+t)
                 T[ny][nx] = t
