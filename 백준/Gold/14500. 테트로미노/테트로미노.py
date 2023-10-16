1faa5cef7601e0d2dec5221a455148c69a46a9d6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-16 23:47:18 +0900

    [Gold IV] Title: 테트로미노, Time: 148 ms, Memory: 36412 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -5,9 +5,8 @@ ans = 0
 def main():
     N, M = map(int, input().split())
     d = ((-1, 0), (1, 0), (0, -1), (0, 1))
-    visited = [[False] * M for _ in range(N)]
     T = [list(map(int, input().split())) for _ in range(N)]
-    m = max((max(li) for li in T))
+    m = max(map(max, T))
     
     def dfs(y, x, cnt, s):
         global ans
@@ -18,20 +17,22 @@ def main():
             return
         for dy, dx in d:
             ny, nx = y + dy, x + dx
-            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+            if 0 <= ny < N and 0 <= nx < M and T[ny][nx]:
+                t = T[ny][nx]
                 if cnt == 2:
-                    visited[ny][nx] = True
-                    dfs(y, x, cnt+1, s+T[ny][nx])
-                    visited[ny][nx] = False
-                visited[ny][nx] = True
-                dfs(ny, nx, cnt+1, s+T[ny][nx])
-                visited[ny][nx] = False
+                    T[ny][nx] = False
+                    dfs(y, x, cnt+1, s+t)
+                    T[ny][nx] = t
+                T[ny][nx] = False
+                dfs(ny, nx, cnt+1, s+t)
+                T[ny][nx] = t
 
     for row in range(N):
         for col in range(M):
-            visited[row][col] = True
-            dfs(row, col, 1, T[row][col])
-            visited[row][col] = False
+            t = T[row][col]
+            T[row][col] = False
+            dfs(row, col, 1, t)
+            T[row][col] = t
 
     print(ans)
 
