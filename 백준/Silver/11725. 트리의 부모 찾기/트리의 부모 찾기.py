ab69bdf6fd7cb53bf144a74a6d34d39ce01cd176
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-18 10:25:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 308 ms, Memory: 57092 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,16 +1,10 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 N = int(input())
-graph = [[] for _ in range(N+1)]
-ans = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    i, j = map(int, input().split())
-    graph[i].append(j)
-    graph[j].append(i)
 
-def bfs():
+def bfs(graph):
+    ans = [[] for _ in range(N+1)]
     q = deque([1])
     while q:
         i = q.popleft()
@@ -18,6 +12,16 @@ def bfs():
             if not ans[n]: 
                 ans[n] = i
                 q.append(n)
+    print("\n".join(map(str, ans[2:])))
 
-bfs()
-print("\n".join(map(str, ans[2:])))
+def main():
+    graph = [[] for _ in range(N+1)]
+    for _ in range(N-1):
+        i, j = map(int, input().split())
+        graph[i].append(j)
+        graph[j].append(i)
+
+    bfs(graph)
+
+if __name__ == "__main__":
+    main()
