9c363fcd2f3dfb7710b19a01ee798f6a46de6e75
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-18 19:45:06 +0900

    [Silver V] Title: 피보나치 수 4, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -1,13 +1,7 @@
 import sys
 def f(n):
-    a, b = 1, 1
-    for _ in range(n//2-1):
+    a, b = 0, 1
+    for _ in range(n):
         a, b = b, (a+b)
-    if n % 2:
-        return (pow(b,2)+pow(a,2))
-    else:
-        return (pow(b,2)-pow(b-a,2))
-
-n = int(sys.stdin.readline())
-if n in (0, 1): print(n)
-else: print(f(n))
+    return a
+print(f(int(sys.stdin.readline())))
