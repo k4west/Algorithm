c8ffaf74e4d7a637682b2e3a7bf3ff5ff3f5aea4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-18 12:52:24 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,18 +4,13 @@ input = stdin.readline
 def main():
     N = int(input())
     RGB = []
-    costs = [[] for _ in range(N)]
     for _ in range(N):
         RGB.append(list(map(int, input().split())))
     
-    costs[0] = RGB[0]
-    order = [0, 1, 2]
     for i in range(1, N):
-        cost = []
-        for j in order:
-            cost.append(min(costs[i-1][(j+1)%3], costs[i-1][(j+2)%3]) + RGB[i][j])
-        costs[i] = cost
-    print(min(costs[N-1]))
+        for j in range(3):
+            RGB[i][j] += min(RGB[i-1][(j+1)%3], RGB[i-1][(j+2)%3])
+    print(min(RGB[N-1]))
 
 
 if __name__ == "__main__":
