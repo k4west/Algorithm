cf97d5311f81b6621b88e681b6150067d1cb923e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-18 11:49:10 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,25 +1,15 @@
-from sys import stdin
-from collections import deque
-input = stdin.readline
-
 def main():
     A, B = map(int, input().split())
 
-    q = deque([A])
-    nq = deque()
     c = 1
-    while q or nq:
-        if not q:
-            q, nq = nq, q
-            c += 1
-        n = q.popleft()
-        for m in (2*n, 10*n+1):
-            if m == B:
-                print(c+1)
-                return
-            if m < B:
-                nq.append(m)
-    print("-1")
+    while A < B:
+        if not B%2: B //= 2
+        elif B%10 == 1: B //= 10
+        else: break
+        c += 1
+
+    if A == B: print(c)
+    else: print("-1")
 
 if __name__ == "__main__":
     main()
