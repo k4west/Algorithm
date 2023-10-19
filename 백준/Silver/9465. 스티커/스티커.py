6148bb21ce332ba3ec2da833d7a11d202071525b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-20 01:27:13 +0900

    [Silver I] Title: 스티커, Time: 284 ms, Memory: 163164 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -6,20 +6,24 @@ def main():
     li = []
     for _ in range(T):
         n = int(input())
-
+        
         if n > 1:
             row1 = list(map(int, input().split()))
             row2 = list(map(int, input().split()))
         
-            a = [row1[0], row2[0]] 
-            b = [row1[1] + row2[0], row2[1] + row1[0]]
+            ans = [[row1[0], row2[0]], 
+                [row1[1] + row2[0], row2[1] + row1[0]]]
             
             for i in range(2, n):
-                a, b = b, [row1[i] + max(b[1], a[1]), 
-                           row2[i] + max(b[0], a[0])]
-            li.append(max(b))
+                ans = [ans[1], 
+                    [row1[i] + max(ans[1][1], ans[0][1]), 
+                        row2[i] + max(ans[1][0], ans[0][0])]]
+
+            li.append(max(ans[1]))
         
-        else: li.append(max(int(input()), int(input())))
+        else:
+            li.append(max(int(input()), int(input())))
+
     print("\n".join(map(str, li)))
 
 if __name__ == "__main__":
