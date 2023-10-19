a02e749da5dff1e52728c338fb22c0f5195fb085
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-19 19:54:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,15 +1,9 @@
 from sys import stdin
 n = int(stdin.readline())
-graph = [int(stdin.readline())]
+graph = (int(stdin.readline()), )
 
 for i in range(n-1):
-    new = list(map(int, stdin.readline().split()))
-    new[0] += graph[0]
-    new[i+1] += graph[i]
-    
-    for j in range(1, i+1):
-        new[j] += max(graph[j-1], graph[j])
-    
-    graph = new
+    new = tuple(map(int, stdin.readline().split()))
+    graph = (new[0] + graph[0], *[new[j] + max(graph[j-1], graph[j]) for j in range(1, i+1)], new[i+1] + graph[i])
 
 print(max(graph))
