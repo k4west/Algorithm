d5ad9763ea9c885f6742a15d037ba14c9d593297
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-19 19:43:48 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 35612 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -4,7 +4,7 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, stdin.readline().split())))
 
-for i, row in enumerate(graph[1:]):
+for i in range(n-1):
     graph[i+1][0] += graph[i][0]
     graph[i+1][i+1] += graph[i][i]
     
