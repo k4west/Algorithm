40fabefc6becebf9362780dd7826b7df1fdcf56b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-23 22:51:47 +0900

    [Gold V] Title: 최소비용 구하기, Time: 160 ms, Memory: 38296 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -3,35 +3,34 @@ from heapq import heappush, heappop
 input = stdin.readline
 INF = float('INF')
 
+def dijkstra(graph, s, e, N):
+    costs = [INF]*(N+1)
+    q = []
+    heappush(q, (0, s))
+    costs[s] = 0
+    while q:
+        s_cost, node = heappop(q)
+        if costs[node] < s_cost: continue
+        if node == e: break
+        for dst, d_cost in graph[node].items():
+            d_cost += s_cost
+            if d_cost < costs[dst]:
+                costs[dst] = d_cost
+                heappush(q, (d_cost, dst))
+    print(costs[e])
+
 def main():
     N = int(input())
     M = int(input())
     graph = [{} for _ in range(N+1)]
-    costs = [INF]*(N+1)
     for _ in range(M):
         s, e, c = map(int, input().split())
         if e in graph[s]:
             graph[s][e] = min(graph[s][e], c)
         else:
             graph[s][e] = c
-            
-    def dijkstra(s, e):
-        q = []
-        heappush(q, (0, s))
-        costs[s] = 0
-        while q:
-            s_cost, node = heappop(q)
-            if costs[node] < s_cost: continue
-            if node == e: break
-            for dst, d_cost in graph[node].items():
-                d_cost += s_cost
-                if d_cost < costs[dst]:
-                    costs[dst] = d_cost
-                    heappush(q, (d_cost, dst))
-        print(costs[e])
-        
     s, e = map(int, input().split())
-    dijkstra(s, e)
+    dijkstra(graph, s, e, N)
 
 if __name__ == "__main__":
     main()
