e8b4ab2342525bc4c1b297b4b147f29ea7dcea22
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-23 22:22:42 +0900

    [Gold V] Title: 최소비용 구하기, Time: 256 ms, Memory: 57372 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,35 @@
+from sys import stdin
+from heapq import heappush, heappop
+input = stdin.readline
+
+def main():
+    INF = float('INF') # 10**5
+    N = int(input())
+    graph = [[] for _ in range(N+1)]
+    costs = [INF]*(N+1)
+
+    M = int(input())
+    for _ in range(M):
+        s, e, c = map(int, input().split())
+        graph[s].append((e, c))
+
+    def dijkstra(s, e):
+        q = []
+        heappush(q, (0, s))
+        costs[s] = 0
+        while q:
+            s_cost, node = heappop(q)
+            if costs[node] < s_cost: continue
+            for dst, d_cost in graph[node]:
+                d_cost += costs[node]
+                if d_cost < costs[dst]:
+                    costs[dst] = d_cost
+                    heappush(q, (d_cost, dst))
+
+        print(costs[e])
+
+    s, e = map(int, input().split())
+    dijkstra(s, e)
+
+if __name__ == "__main__":
+    main()
