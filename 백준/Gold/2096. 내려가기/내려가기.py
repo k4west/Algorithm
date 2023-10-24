1da9a8f30fb3a35b5e90058eb3931b8ef8c2db04
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-24 23:40:53 +0900

    [Gold V] Title: 내려가기, Time: 284 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,10 @@
+from sys import stdin
+input = stdin.readline
+N = range(int(input()))
+M = [0, 0, 0]
+m = [0, 0, 0]
+for _ in N:
+    a, b, c = map(int, input().split())
+    M = a + max(M[0], M[1]), b + max(M), c + max(M[1], M[2])
+    m = a + min(m[0], m[1]), b + min(m), c + min(m[1], m[2])
+print(max(M), min(m))
