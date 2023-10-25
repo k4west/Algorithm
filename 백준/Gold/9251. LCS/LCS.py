e558a78886318ab598f2badd348bd866df53be3c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-25 20:56:45 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -4,18 +4,18 @@ input = sys.stdin.readline
 def main():
     s1 = input().rstrip()
     s2 = input().rstrip()
-    n1, n2 = len(s1) + 1, len(s2) + 1
 
-    dp = [[0]*(n1) for _ in range(n2)]
+    dp = [0] * 1000
 
-    for i in range(1, n2):
-        s = s2[i-1]
-        for j in range(1, n1):
-            if s == s1[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else: dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    for a in s1:
+        c = 0
+        for i, b in enumerate(s2):
+            if c < dp[i]:
+                c = dp[i]
+            elif b == a:
+                dp[i] = c + 1
 
-    print(dp[n2-1][n1-1])
+    print(max(dp))
 
 if __name__ == "__main__":
     main()
