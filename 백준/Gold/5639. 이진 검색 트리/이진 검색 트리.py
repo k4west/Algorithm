5e33d80a17c84429f9da753e17140f0a9e0c6ead
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-27 00:18:26 +0900

    [Gold V] Title: 이진 검색 트리, Time: 56 ms, Memory: 32524 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,30 +1,27 @@
 import sys
 sys.setrecursionlimit(10000*2)
 
-def main():
-    li = []
-    while True:
-        try:
-            li.append(int(sys.stdin.readline()))
-        except:
+li, ans = [], []
+while True:
+    try:
+        li.append(int(sys.stdin.readline()))
+    except:
+        break
+
+def f(s, e):
+    if s >= e: 
+        ans.append(li[s])
+        return
+    if li[s] > li[e] or li[s] < li[s+1]:
+        f(s+1, e)
+        ans.append(li[s])
+        return
+    for i in range(s+1, e+1):
+        if li[s] < li[i]:
+            f(s+1, i-1)
+            f(i, e)
             break
+    ans.append(li[s])
 
-    def f(s, e):
-        if s >= e: 
-            print(li[s])
-            return
-        if li[s] > li[e] or li[s] < li[s+1]:
-            f(s+1, e)
-            print(li[s])
-            return
-        for i in range(s+1, e+1):
-            if li[s] < li[i]:
-                f(s+1, i-1)
-                f(i, e)
-                break
-        print(li[s])
-
-    f(0, len(li)-1)
-
-if __name__ == "__main__":
-    main()
+f(0, len(li)-1)
+print("\n".join(map(str, ans)))
