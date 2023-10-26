adddec9640316e8eb0d771d2eb901978d03c6aca
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-27 00:13:22 +0900

    [Gold V] Title: 이진 검색 트리, Time: 56 ms, Memory: 33708 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,17 +1,21 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10000*2)
 
 def main():
     li = [int(n) for n in open(0).readlines()]
 
     def f(s, e):
-        if s > e: return
-        for i in range(s+1, e+1):
-            if li[s] < li[i]:
-                f(s+1, i-1)
-                f(i, e)
-                break
-        else: f(s+1, e)
+        if s >= e: 
+            print(li[s])
+            return
+        if li[s] > li[e] or li[s] < li[s+1]:
+            f(s+1, e)
+        else:
+            for i in range(s+1, e+1):
+                if li[s] < li[i]:
+                    f(s+1, i-1)
+                    f(i, e)
+                    break
         print(li[s])
 
     f(0, len(li)-1)
