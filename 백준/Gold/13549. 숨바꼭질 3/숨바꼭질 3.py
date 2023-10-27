b290b9f2dc16626616d553c43ee23e5fc3ad5f00
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-27 20:39:32 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 64 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,23 +1,25 @@
+# K --> N
 from sys import stdin
 input = stdin.readline
 
 def main():
     N, K = map(int, input().split())
+    if N >= K: 
+        return N-K
     q = [(K, 0)]
     v = set()
 
     while True:
         s, t = q.pop(0)
         if s == N:
-            print(t)
-            break
+            return t
         if not s%2 and (d:=s//2) not in v and s > N:
             v.add(d)
             q.append((d, t))
         for i in (s+1, s-1):
-            if i not in v:
+            if i not in v and i < K+2:
                 v.add(i)
                 q.append((i, t+1))
 
 if __name__ == "__main__":
-    main()
+    print(main())
