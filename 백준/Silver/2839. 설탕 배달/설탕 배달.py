a377621ccaa6174a19c825de72bf29be3f8f5ed3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-27 16:35:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 N = int(input())
 c = 0
 while N >= 0:
-    if N % 5 == 0:
+    if not N % 5:
         c += (N//5)
         print(c)
         break
-    N = N - 3
-    c = c + 1
+    N -= 3
+    c += 1
 else:
     print(-1)
