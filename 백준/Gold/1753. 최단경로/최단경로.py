ba1bbab0f04448b5b130ec45964f74b257fe1a9a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-31 01:09:43 +0900

    [Gold IV] Title: 최단경로, Time: 644 ms, Memory: 68336 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,35 +1,28 @@
-from sys import stdin, stdout
+import sys
 from heapq import heappop, heappush
+input = sys.stdin.readline
 
-def main():
-    input = stdin.readline
-    print = stdout.write
+INF = float('inf')
+V, E = map(int, input().split())
+s = int(input())
+graph = [[] for _ in range(V+1)]
+distance = [INF] * (V+1)
+distance[s] = 0
 
-    INF = float('inf')
-    V, E = map(int, input().split())
-    s = int(input())
-    graph = [[] for _ in range(V+1)]
-    distance = [INF] * (V+1)
+for _ in range(E):
+    i, j, k = map(int, input().split())
+    graph[i].append((j, k))
 
-    for _ in range(E):
-        i, j, k = map(int, input().split())
-        graph[i].append((j, k))
+q = []
+heappush(q, (0, s))
+while q:
+    dist, node = heappop(q)
+    if dist > distance[node]:
+        continue
+    for next, d in graph[node]:
+        n_dist = dist + d
+        if n_dist < distance[next]:
+            distance[next] = n_dist
+            heappush(q, (n_dist, next))
 
-    q = []
-    heappush(q, (0, s))
-    while q:
-        dist, node = heappop(q)
-        if dist <= distance[node]:
-            distance[node] = dist
-        for next, d in graph[node]:
-            n_dist = dist + d
-            if n_dist < distance[next]:
-                distance[next] = n_dist
-                heappush(q, (n_dist, next))
-
-    for d in distance[1:]:
-        if d != INF: print(f'{d}\n')
-        else: print('INF\n')
-        
-if __name__ == "__main__":
-    main()
+print("\n".join(map(str, distance[1:])).upper())
