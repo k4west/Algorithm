f5ea64fc1450786a52e9f196d428f524988a0b3f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-10-31 21:48:16 +0900

    [Gold IV] Title: 트리의 지름, Time: 60 ms, Memory: 34704 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,32 +1,28 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 n = int(input())
 graph = [[] for _ in range(n+1)]
-tmp, last = set(), set()
 for _ in range(n-1):
     p, c, w = map(int, input().split())
     graph[p].append((c, w))
     graph[c].append((p, w))
-    tmp.add(p)
-    last.add(c)
-last -= tmp
 
-def dfs(n, d, v):
-    global distance
-    global s
-    for node, weight in graph[n]:
-        if node in v:
-            continue
-        n_d = d + weight
-        if node in last:
+def dfs(n):
+    distance, s = 0, 1
+    v = set()
+    stack = [(n, 0)]
+    while stack:
+        n, d = stack.pop()
+        v.add(n)
+        for node, weight in graph[n]:
+            if node in v:
+                continue
+            n_d = d + weight
             if n_d >= distance:
                 distance = n_d
                 s = node
-        else:
             v.add(node)
-            dfs(node, n_d, v)
-distance, s = 0, 1
-dfs(1, 0, {1})
-dfs(s, 0, {s})
-print(distance)
+            stack.append((node, n_d))
+    return (s, distance)
+
+print(dfs(dfs(1)[0])[1])
