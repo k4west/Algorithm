f9a61255b124ab17988438c3e3bba876e7a00da8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-01 23:40:51 +0900

    [Gold IV] Title: 플로이드, Time: 688 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,20 @@
+import sys
+INF = float('inf')
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+graph = [[INF]*(N+1) for _ in range(N+1)]
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    graph[i][j] = min(k, graph[i][j])
+
+for node in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if i == j:
+                graph[i][j] = 0
+            else:
+                graph[i][j] = min(graph[i][j], graph[i][node] + graph[node][j])
+
+print("\n".join([" ".join(map(str, row[1:])) for row in graph[1:]]).replace('inf', '0'))
