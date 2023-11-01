58bf5da7a323db9807618987f052ac1fb341562a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-01 23:54:22 +0900

    [Gold IV] Title: 플로이드, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,22 +1,27 @@
 import sys
-INF = float('inf')
-input = sys.stdin.readline
-N = int(input())
-M = int(input())
-graph = [[INF]*(N+1) for _ in range(N+1)]
 
-for _ in range(M):
-    i, j, k = map(int, input().split())
-    if k < graph[i][j]:
-        graph[i][j] = k
+def main():
+    INF = float('inf')
+    input = sys.stdin.readline
+    N = int(input())
+    M = int(input())
+    graph = [[INF]*(N+1) for _ in range(N+1)]
 
-for i in range(1, N+1):
-    graph[i][i] = 0
+    for _ in range(M):
+        i, j, k = map(int, input().split())
+        if k < graph[i][j]:
+            graph[i][j] = k
 
-for node in range(1, N+1):
     for i in range(1, N+1):
-        for j in range(1, N+1):
-            if graph[i][node] + graph[node][j] < graph[i][j]:
-                graph[i][j] = graph[i][node] + graph[node][j]
+        graph[i][i] = 0
 
-print("\n".join([" ".join(map(str, row[1:])) for row in graph[1:]]).replace('inf', '0'))
+    for node in range(1, N+1):
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                if graph[i][node] + graph[node][j] < graph[i][j]:
+                    graph[i][j] = graph[i][node] + graph[node][j]
+
+    print("\n".join([" ".join(map(str, row[1:])) for row in graph[1:]]).replace('inf', '0'))
+
+if __name__ == '__main__':
+    main()
