ac9e162688a8934d72da5964cda75b2ed0da010a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-08 23:54:21 +0900

    [Gold II] Title: 트리의 지름, Time: 460 ms, Memory: 78088 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -9,22 +9,21 @@ def main():
         for b, c in zip(li[::2], li[1::2]):
             graph[a].append((b, c))
 
-    def dfs(s):
+    def dfs(s, V):
         dist = 0
-        v = set()
+        v = [False]*(V+1)
         stack = [(s, 0)]
         while stack:
             n0, d0 = stack.pop()
             if dist < d0:
-                dist = d0
-                s = n0
-            v.add(n0)
+                s, dist = n0, d0
+            v[n0] = True
             for n1, d1 in graph[n0]:
-                if n1 not in v:
+                if not v[n1]:
                     stack.append((n1, d0 + d1))
         return s, dist
 
-    print(dfs(dfs(1)[0])[1])
+    print(dfs(dfs(1, V)[0], V)[1])
 
 if __name__ == "__main__":
     main()
