01afaec72d829f2bdf52122f4026fe69366b105a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-09 21:16:39 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,13 +1,17 @@
-d = {1:1, 2:1, 3:2, 4:3}
-n = int(input())
+def main():
+    d = {1:1, 2:1, 3:2, 4:3, 5:5, 6:8, 7:13, 8:21, 9:34, 10:55, 11:89, 12:144, 13:233, 14:377, 15:610, 16:987, 17:1597}
+    n = int(input())
 
-def f(n):
-    if d.get(n):
+    def f(n):
+        if d.get(n):
+            return d[n]
+        if n % 2:
+            d[n] = (pow(f(n//2+1), 2) + pow(f(n//2), 2)) % 1_000_000_007
+        else:
+            d[n] = (pow(f(n//2+1), 2) - pow(f(n//2-1), 2)) % 1_000_000_007
         return d[n]
-    if n % 2:
-        d[n] = (pow(f(n//2+1), 2) + pow(f(n//2), 2)) % 1_000_000_007
-    else:
-        d[n] = (pow(f(n//2+1), 2) - pow(f(n//2-1), 2)) % 1_000_000_007
-    return d[n]
 
-print(f(n))
+    print(f(n))
+
+if __name__ == "__main__":
+    main()
