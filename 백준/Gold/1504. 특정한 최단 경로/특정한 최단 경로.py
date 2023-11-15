0e85b1623ee2676c240595df81924cd2f8250f22
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-15 18:12:20 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 760 ms, Memory: 64248 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -2,28 +2,32 @@ import sys
 from heapq import heappop, heappush
 input = sys.stdin.readline
 
-INF = float('inf')
-N, E = map(int, input().split())
-graph = [[] for _ in range(N+1)]
+def main():
+    INF = float('inf')
+    N, E = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
 
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+        graph[b].append((a, c))
 
-v1, v2 = map(int, input().split())
+    v1, v2 = map(int, input().split())
 
-def f(node, li):
-    dist = [INF]*(N+1)
-    dist[node] = 0
-    q = [(0, node)]
-    while q:
-        d0, s = heappop(q)
-        for e, d1 in graph[s]:
-            if (d:= d0 + d1) < dist[e]:
-                dist[e] = d
-                heappush(q, (d, e))
-    return [dist[i] for i in li]
+    def f(node, li):
+        dist = [INF]*(N+1)
+        dist[node] = 0
+        q = [(0, node)]
+        while q:
+            d0, s = heappop(q)
+            for e, d1 in graph[s]:
+                if (d:= d0 + d1) < dist[e]:
+                    dist[e] = d
+                    heappush(q, (d, e))
+        return [dist[i] for i in li]
 
-ans = min([x+y for x, y in zip(f(1, [v1, v2]), f(N, [v2, v1]))]) + f(v1, [v2])[0]
-print(ans if ans != INF else '-1')
+    ans = min([x+y for x, y in zip(f(1, [v1, v2]), f(N, [v2, v1]))]) + f(v1, [v2])[0]
+    print(ans if ans != INF else '-1')
+
+if __name__ == "__main__":
+    main()
