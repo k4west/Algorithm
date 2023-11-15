4d6f5b40cb86fa822c039da2e063e9bf1fbf097e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-15 16:46:12 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -3,26 +3,25 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = [['0']]
-dp = [[[0]*(N+1) for _ in range(N+1)] for _ in range(3)]
+dp = [[[0, 0, 0] for _ in range(N+1)] for _ in range(N+1)]
 
 for _ in range(N):
     graph.append(['0']+input().rstrip().split()+['1'])
 graph.append(['1']*(N+2))
-
 if graph[N][N] == '1' or (graph[N-1][N] == '1' and graph[N][N-1] == '1'):
     print(0)
     exit()
 
-dp[0][1][2] = 1
+dp[1][2][0] = 1
 for c in range(3, N+1):
     if graph[1][c] == '0':
-        dp[0][1][c] = dp[0][1][c-1]
+        dp[1][c][0] = dp[1][c-1][0]
 
 for i in range(2, N+1):
     for j in range(3, N+1):
         if graph[i][j] == '0':
             if graph[i-1][j] == graph[i][j-1] == '0':
-                dp[1][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1]
-            dp[0][i][j] = dp[0][i][j-1] + dp[1][i][j-1]
-            dp[2][i][j] = dp[2][i-1][j] + dp[1][i-1][j]
-print(dp[0][N][N]+dp[1][N][N]+dp[2][N][N])
+                dp[i][j][1] = sum(dp[i-1][j-1])
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1]
+            dp[i][j][2] = dp[i-1][j][2] + dp[i-1][j][1]
+print(sum(dp[N][N]))
