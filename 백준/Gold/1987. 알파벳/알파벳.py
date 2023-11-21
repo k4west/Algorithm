d19d50b169d8a52122480f0d12fd431c55ef11c2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-22 01:27:22 +0900

    [Gold IV] Title: 알파벳, Time: 976 ms, Memory: 111892 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -8,25 +8,26 @@ for _ in range(R):
     graph.extend(list(map(lambda x:1 << ord(x)-65, input().rstrip())))
 
 ans = 1
+stack = [(1, 0, graph[0])]
 
-def dfs(d, p, v):
-    global ans
-    i, j = p//C, p%C
-          
+while stack:
+    d, p, v = stack.pop()
     if ans < d: 
         ans = d
         if ans == 26:
             print(26)
             exit()
+    
+    i, j = p//C, p%C
     d += 1
+    
     if (i+1) < R and not (w:=graph[k:=p+C])&v:
-        dfs(d, k, v|w)
+        stack.append((d, k, v|w))
     if 0 <= (i-1) and not (w:=graph[k:=p-C])&v:
-        dfs(d, k, v|w)
+        stack.append((d, k, v|w))
     if (j+1) < C and not (w:=graph[k:=p+1])&v:
-        dfs(d, k, v|w)
+        stack.append((d, k, v|w))
     if 0 <= (j-1) and not (w:=graph[k:=p-1])&v:
-        dfs(d, k, v|w)
+        stack.append((d, k, v|w))
 
-dfs(1, 0, graph[0])
 print(ans)
