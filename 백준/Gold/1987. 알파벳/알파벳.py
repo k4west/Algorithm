b63e3bb62a9aa2a34fecdc7cb9901e98e8c2c08a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-22 00:17:27 +0900

    [Gold IV] Title: 알파벳, Time: 4712 ms, Memory: 142432 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -6,23 +6,25 @@ graph = []
 for _ in range(R):
     graph.append(list(input().rstrip()))
 
-v, ans = {graph[0][0]}, 1
+v, ans = [False]*26, 1
+v[ord(graph[0][0])-65] = True
+
 def dfs(d, i, j):
     global ans
 
-    def f(a, c, x, y):
-        v.add(a)
+    def f(n, c, x, y):
+        v[n] = True
         dfs(c, x, y)
-        v.remove(a)
+        v[n] = False
         
     if ans < d: ans = d
-    if (ni:=i+1) < R and not (k:=graph[ni][j]) in v:
+    if (ni:=i+1) < R and not v[k:=ord(graph[ni][j])-65]:
         f(k, d+1, ni, j)
-    if 0 <= (ni:=i-1) and not (k:=graph[ni][j]) in v:
+    if 0 <= (ni:=i-1) and not v[k:=ord(graph[ni][j])-65]:
         f(k, d+1, ni, j)
-    if (nj:=j+1) < C and not (k:=graph[i][nj]) in v:
+    if (nj:=j+1) < C and not v[k:=ord(graph[i][nj])-65]:
         f(k, d+1, i, nj)
-    if 0 <= (nj:=j-1) and not (k:=graph[i][nj]) in v:
+    if 0 <= (nj:=j-1) and not v[k:=ord(graph[i][nj])-65]:
         f(k, d+1, i, nj)
 
 dfs(1, 0, 0)
