216cda760c3ac5312fdca0d2fdd80abcc1517126
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-22 20:09:29 +0900

    [Gold IV] Title: 문자열 폭발, Time: 172 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -2,11 +2,12 @@ def main():
     string = input()
     bomb = list(input())
     b = len(bomb)
+    x = bomb[-1]
 
     stack = []
     for s in string:
         stack.append(s)
-        if stack[-b:] == bomb:
+        if s == x and stack[-b:] == bomb:
             del stack[-b:]
 
     print("".join(stack) if stack else "FRULA")
