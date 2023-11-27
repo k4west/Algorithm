d5efbef720d7494a3cdcfd21f1343778e8913f47
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-27 21:58:18 +0900

    [Gold IV] Title: 서강그라운드, Time: 44 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,7 +1,7 @@
 import sys
 from heapq import heappop, heappush
 input = sys.stdin.readline
-INF = float('inf')
+# INF = float('inf')
 
 n, m, r = map(int, input().split())
 items = [0] + list(map(int, input().split()))
@@ -12,20 +12,27 @@ for _ in range(r):
     graph[a].append((b, l))
     graph[b].append((a, l))
 
-ans = 0
-for i in range(1, n+1):
-    dist = [INF]*(n+1)
-    q = [(0, i)]
-    dist[i] = 0
+def dijkstra(s):
+    dist = [16]*(n+1)
+    q = [(0, s)]
+    dist[s] = 0
     while q:
         d0, n0 = heappop(q)
         if dist[n0] < d0:
             continue
         for n1, d1 in graph[n0]:
-            if (d := d0 + d1) < dist[n1]:
+            if (d:=d0+d1) < dist[n1]:
                 dist[n1] = d
                 heappush(q, (d, n1))
-    if ans < (tmp:=sum([item for d, item in zip(dist, items) if d <= m])):
+    tmp = 0
+    for i, d in enumerate(dist[1:], 1):
+        if d <= m:
+            tmp += items[i]
+    return tmp
+
+ans = 0
+for i in range(1, n+1):
+    if ans < (tmp:=dijkstra(i)):
         ans = tmp
 
 print(ans)
