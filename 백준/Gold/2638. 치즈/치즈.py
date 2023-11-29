6a811a7bc50198996c746dfa28df8fe8aab1c980
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-29 22:57:58 +0900

    [Gold III] Title: 치즈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -8,21 +8,21 @@ def main():
     graph = [[-1] + list(map(int, input().split())) + [-1] for _ in range(N)]
     graph = margin + graph + margin
 
-    q, nq, t = [(1, 1)], [], -1
-    while q:
-        while q:
-            r, c = q.pop()
-            for dr, dc in d:
-                if graph[(nr:=r+dr)][(nc:=c+dc)] > 0:
-                    graph[nr][nc] += 1
-                    if graph[nr][nc] > 2:
-                        graph[nr][nc] = -1
-                        nq.append((nr, nc))
-                if not graph[(nr:=r+dr)][(nc:=c+dc)]:
+    q, nq, t = [(1, 1)], [], 0
+    while q or nq:
+        if not q:
+            q, nq = nq, []
+            t += 1
+        r, c = q.pop()
+        for dr, dc in d:
+            if graph[(nr:=r+dr)][(nc:=c+dc)] > 0:
+                graph[nr][nc] += 1
+                if graph[nr][nc] > 2:
                     graph[nr][nc] = -1
-                    q.append((nr, nc))
-        q, nq = nq, []
-        t += 1
+                    nq.append((nr, nc))
+            if not graph[(nr:=r+dr)][(nc:=c+dc)]:
+                graph[nr][nc] = -1
+                q.append((nr, nc))
     print(t)
 
 if __name__ == "__main__":
