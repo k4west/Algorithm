0ccd168b118e9b4f2eb5d400efcd68d88969d8f3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-29 22:50:05 +0900

    [Gold III] Title: 치즈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    N, M = map(int, input().split())
+    d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    margin = [[-1] * (M+2)]
+    graph = [[-1] + list(map(int, input().split())) + [-1] for _ in range(N)]
+    graph = margin + graph + margin
+
+    q, nq, t = [(1, 1)], [], -1
+    while q:
+        while q:
+            r, c = q.pop()
+            for dr, dc in d:
+                if graph[(nr:=r+dr)][(nc:=c+dc)] > 0:
+                    graph[nr][nc] += 1
+                    if graph[nr][nc] > 2:
+                        graph[nr][nc] = -1
+                        nq.append((nr, nc))
+                if not graph[(nr:=r+dr)][(nc:=c+dc)]:
+                    graph[nr][nc] = -1
+                    q.append((nr, nc))
+        q, nq = nq, []
+        t += 1
+    print(t)
+
+if __name__ == "__main__":
+    main()
