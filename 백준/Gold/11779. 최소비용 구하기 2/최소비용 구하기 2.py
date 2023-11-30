c4852991b01c6ee764a38e79e71f4e1897d1376d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-11-30 23:07:40 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 160 ms, Memory: 38416 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -2,35 +2,39 @@ import sys
 from heapq import heappop, heappush
 input = sys.stdin.readline
 
-INF = float('inf')
-n = int(input())
-m = int(input())
-buses = [{} for _ in range(n+1)]
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    if c < buses[a].get(b, INF):
-        buses[a][b] = c
-A, B = map(int, input().split())
+def main():
+    INF = float('inf')
+    n = int(input())
+    m = int(input())
+    buses = [{} for _ in range(n+1)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        if c < buses[a].get(b, INF):
+            buses[a][b] = c
+    A, B = map(int, input().split())
 
 
-dist = [INF]*(n+1)
-dist[A] = 0
-path = [0]*(n+1)
+    dist = [INF]*(n+1)
+    dist[A] = 0
+    path = [0]*(n+1)
 
-li = [(0, A)]
-while li:
-    c0, n0 = heappop(li)
-    if c0 > dist[n0]:
-        continue
-    for n1, c1 in buses[n0].items():
-        if (c:=c0+c1) < dist[n1]:
-            dist[n1] = c
-            path[n1] = n0
-            heappush(li, (c, n1))
+    li = [(0, A)]
+    while li:
+        c0, n0 = heappop(li)
+        if c0 > dist[n0]:
+            continue
+        for n1, c1 in buses[n0].items():
+            if (c:=c0+c1) < dist[n1]:
+                dist[n1] = c
+                path[n1] = n0
+                heappush(li, (c, n1))
 
-ans = [B]
-while (n:=ans[-1]) != A:
-    ans.append(path[n])
+    ans = [B]
+    while (n:=ans[-1]) != A:
+        ans.append(path[n])
 
-print(dist[B], len(ans), sep='\n')
-print(*ans[::-1])
+    print(dist[B], len(ans), sep='\n')
+    print(*ans[::-1])
+
+if __name__ == "__main__":
+    main()
