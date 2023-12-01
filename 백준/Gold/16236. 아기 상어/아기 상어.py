18ef12bccf128b8e6af0388627cce5c28e5b2e1d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-02 01:24:52 +0900

    [Gold III] Title: 아기 상어, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,71 +1,56 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-sea = [list(map(int, input().split())) for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if sea[i][j] == 9:
-            start = [i, j]
-            sea[i][j] = 0
-            break
-
-t = 0
-def search(i, j):
+def search(i, j, level):
     q, nq = [(i, j)], []
     v = {(i, j)}
     tmp = []
     cnt = 1
     while q or nq:
-        if not q:
-            if tmp:
-                break
-            q, nq = nq, []
-            cnt += 1
+        for r, c in q:
+            for dr, dc in d:
+                if 0 <= (nr:=r+dr) < N and 0 <= (nc:=c+dc) < N and (k:=sea[nr][nc]) <= level and (s:=(nr, nc)) not in v:
+                    if 0 < k < level:
+                        tmp.append((nr, nc))
+                    else: 
+                        v.add(s)
+                        nq.append((nr, nc))
 
-        r, c = q.pop(0)
-        if (nr:=r-1) >= 0 and (k:=sea[nr][c]) <= level and (s:=(nr, c)) not in v:
-            if 0 < k < level:
-                tmp.append((nr, c))
-            else: 
-                v.add(s)
-                nq.append((nr, c))
-        if (nc:=c-1) >= 0 and (k:=sea[r][nc]) <= level and (s:=(r, nc)) not in v:
-            if 0 < k < level:
-                tmp.append((r, nc))
-            else:
-                v.add(s) 
-                nq.append((r, nc))
-        if (nc:=c+1) < N and (k:=sea[r][nc]) <= level and (s:=(r, nc)) not in v:
-            if 0 < k < level:
-                tmp.append(( r, nc))
-            else:
-                v.add(s) 
-                nq.append((r, nc))
-        if (nr:=r+1) < N and (k:=sea[nr][c]) <= level and (s:=(nr, c)) not in v:
-            if 0 < k < level:
-                tmp.append((nr, c))
-            else:
-                v.add(s) 
-                nq.append((nr, c))
+        if tmp:
+            break
+        q, nq = nq, []
+        cnt += 1
+
     if tmp:
         tmp.sort()
         return cnt, tmp[0]
     return False
 
+def main():
+    t, level, exp = 0, 2, 0
+    i, j = start
+    while True:
+        if not (s:=search(i, j, level)):
+            break
+        k, (i, j) = s
+        sea[i][j] = 0
+        t += k
 
-level, exp = 2, 0
-i, j = start
-while True:
-    if not (s:=search(i, j)):
-        break
-    k, (i, j) = s
-    sea[i][j] = 0
-    t += k
+        exp += 1
+        if exp == level:
+            level += 1
+            exp = 0
 
-    exp += 1
-    if exp == level:
-        level += 1
-        exp = 0
+    print(t)
 
-print(t)
+if __name__ == '__main__':
+    N = int(input())
+    d = ((-1, 0), (0, -1), (0, 1), (1, 0))
+    sea = [list(map(int, input().split())) for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if sea[i][j] == 9:
+                start = [i, j]
+                sea[i][j] = 0
+                break
+    main()
