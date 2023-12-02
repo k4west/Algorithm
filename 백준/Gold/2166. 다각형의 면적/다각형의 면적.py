75bf4e988f7a1ca171e93dbe897416c659e6cad1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-02 13:16:08 +0900

    [Gold V] Title: 다각형의 면적, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+area = 0
+x, y = map(int, input().split())
+x0, y0 = x, y
+for i in range(N-1):
+    x1, y1 = map(int, input().split())
+    area += x0*y1 - x1*y0
+    x0, y0 = x1, y1
+area += x1*y - x*y1
+
+print(round(area/2, 1)*(2*int(area > 0)-1))
