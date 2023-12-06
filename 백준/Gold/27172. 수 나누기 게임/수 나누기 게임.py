5b1650ae3a9c1eabe0225a4dbb7d651150c8aedd
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-06 09:12:41 +0900

    [Gold V] Title: 수 나누기 게임, Time: 772 ms, Memory: 54212 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-cards = list(map(int, input().split()))
-M = max(cards)+1
-v = [False]*M
-ans = [0]*M
-for card in cards:
-    v[card] = True
-for card in cards:
-    for i in range(2*card, M, card):
-        if v[i]:
-            ans[card] += 1
-            ans[i] -= 1
-print(*(ans[card] for card in cards))
+def main():
+    N = int(input())
+    cards = list(map(int, input().split()))
+    M = max(cards)+1
+    v = [False]*M
+    ans = [0]*M
+    for card in cards:
+        v[card] = True
+    for card in cards:
+        for i in range(2*card, M, card):
+            if v[i]:
+                ans[card] += 1
+                ans[i] -= 1
+    print(*(ans[card] for card in cards))
+
+if __name__ == "__main__":
+    main()
