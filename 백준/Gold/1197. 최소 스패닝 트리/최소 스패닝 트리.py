5b396b812a34e79f4b6a0893c2955ad3bc3cf38b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-06 11:30:55 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 248 ms, Memory: 55360 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,25 +1,28 @@
 import sys
 input = sys.stdin.readline
+def main():
+    V, E = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(E)]
+    graph.sort(key=lambda x: x[2])
+    parent = [i for i in range(V+1)]
 
-V, E = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(E)]
-graph.sort(key=lambda x: x[2])
-parent = [i for i in range(V+1)]
+    def find_root(r):
+        nr = parent[r]
+        while nr != r:
+            pr, r, nr = r, nr, parent[nr]
+            parent[pr] = nr
+        return r
 
-def find_root(r):
-    nr = parent[r]
-    while nr != r:
-        pr, r, nr = r, nr, parent[nr]
-        parent[pr] = nr
-    return r
+    w, v = 0, 0
+    for a, b, c in graph:
+        if (x:=find_root(a)) == (y:=find_root(b)):
+            continue
+        parent[y] = x
+        w += c
+        v += 1
+        if v == V-1:
+            break
+    print(w)
 
-w, v = 0, 0
-for a, b, c in graph:
-    if (x:=find_root(a)) == (y:=find_root(b)):
-        continue
-    parent[y] = x
-    w += c
-    v += 1
-    if v == V-1:
-        break
-print(w)
+if __name__ == "__main__":
+    main()
