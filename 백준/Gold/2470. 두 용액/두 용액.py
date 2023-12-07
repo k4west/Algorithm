d7e0d6057a21a6727e41b212b4770c18e1c6690a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-07 20:44:31 +0900

    [Gold V] Title: 두 용액, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -5,24 +5,20 @@ def main():
     N = int(input())
     liq = sorted(list(map(int, input().split())))
 
-    x = float('inf')
     s, e = 0, N-1
     l, r = 0, -1
+    x = liq[s]+liq[e]
     while s < e:
         t = liq[s]+liq[e]
-        if t == 0:
+        if abs(t) < abs(x):
+            x = t
             l, r = s, e
+        if t == 0:
             break
         if t > 0:
-            if x > t:
-                x = t
-                l, r = s, e
             e -= 1
-        else:
-            if x > -t:
-                x = -t
-                l, r = s, e
-            s += 1
+            continue
+        s += 1
     print(liq[l], liq[r])
 
 if __name__ == "__main__":
