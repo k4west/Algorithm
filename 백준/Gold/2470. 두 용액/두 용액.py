c6cf36e1321711250596a831ea3e151ab2eb51b9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-07 20:24:38 +0900

    [Gold V] Title: 두 용액, Time: 100 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    N = int(input())
+    liq = sorted(list(map(int, input().split())))
+
+    x = float('inf')
+    s, e = 0, N-1
+    while s < e:
+        t = liq[s]+liq[e]
+        if t == 0:
+            ans = [liq[s], liq[e]]
+            break
+        if t > 0:
+            if x > t:
+                x = t
+                ans = [liq[s], liq[e]]
+            e -= 1
+        else:
+            if x > -t:
+                x = -t
+                ans = [liq[s], liq[e]]
+            s += 1
+    print(*ans, sep=' ')
+
+if __name__ == "__main__":
+    main()
