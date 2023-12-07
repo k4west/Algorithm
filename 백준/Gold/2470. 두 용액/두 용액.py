947f8ca19fe89bffd86287fb6946b5b5ab7f7494
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-07 20:33:39 +0900

    [Gold V] Title: 두 용액, Time: 92 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -7,22 +7,23 @@ def main():
 
     x = float('inf')
     s, e = 0, N-1
+    l, r = 0, -1
     while s < e:
         t = liq[s]+liq[e]
         if t == 0:
-            ans = [liq[s], liq[e]]
+            l, r = s, e
             break
         if t > 0:
             if x > t:
                 x = t
-                ans = [liq[s], liq[e]]
+                l, r = s, e
             e -= 1
         else:
             if x > -t:
                 x = -t
-                ans = [liq[s], liq[e]]
+                l, r = s, e
             s += 1
-    print(*ans, sep=' ')
+    print(liq[l], liq[r])
 
 if __name__ == "__main__":
     main()
