82419fda6061a9264c86599b7742979825f903db
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-08 15:56:42 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2636 ms, Memory: 273788 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -7,31 +7,29 @@ def find_root(root, n):
     root[n] = find_root(root, nr)
     return root[n]
 
-def union_root(root, a, b):
-    if (x:=find_root(root, a)) == (y:=find_root(root, b)):
-        return False
+def union_root(root, x, y):
     if x > y:
         root[x] = y
     else:
         root[y] = x
-    return True
+    return
 
 def main():
     N, M = map(int, input().split())
     root = list(range(N+1))
     roads = [list(map(int, input().split())) for _ in range(M)]
     roads.sort(key=lambda x: x[2])
-    ans = float('inf')
     w, n = 0, 1
     for a, b, c in roads:
-        if union_root(root, a, b):
-            w += c
-            n += 1
+        if (x:=find_root(root, a)) == (y:=find_root(root, b)):
+            continue
+        union_root(root, x, y)
+        w += c
+        n += 1
         if n == N:
-            if ans > w:
-                ans = w
+            w -= c
             break
-    print(ans-c)
+    print(w)
 
 if __name__ == "__main__":
     main()
