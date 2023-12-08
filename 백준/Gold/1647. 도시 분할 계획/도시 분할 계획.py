6d1b5fdf283f29de51daba042dea36f7ebb3425a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-08 16:14:47 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1872 ms, Memory: 211324 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,34 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-def find_root(root, n):
-    if root[n] == n:
-        return n
-    root[n] = find_root(root, root[n])
-    return root[n]
-
-def union_root(root, x, y):
-    if x > y:
-        root[x] = y
-    else:
-        root[y] = x
-    return
-
 def main():
     N, M = map(int, input().split())
     root = list(range(N+1))
-    roads = [list(map(int, input().split())) for _ in range(M)]
-    roads.sort(key=lambda x: x[2])
+    roads = []
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        roads.append((c, a, b))
+    roads.sort(key=lambda x: x[0])
+    
+    def find_root(n):
+        if root[n] == n: return n
+        root[n] = find_root(root[n])
+        return root[n]
+
+    def union_root(x, y):
+        if x > y: root[x] = y
+        else: root[y] = x
+        return
+    
     w, n = 0, 1
-    for a, b, c in roads:
-        if (x:=find_root(root, a)) == (y:=find_root(root, b)):
+    for c, a, b in roads:
+        if (x:=find_root(a)) == (y:=find_root(b)):
             continue
-        union_root(root, x, y)
+        union_root(x, y)
         w += c
         n += 1
-        if n == N:
-            print(w-c)
-            return
+        if n != N:
+            continue
+        print(w-c)
+        return
 
 if __name__ == "__main__":
     main()
