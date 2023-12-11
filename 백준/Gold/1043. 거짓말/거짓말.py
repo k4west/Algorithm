9bfa56d2d6928b4b0ef060e7457042d78f7e879a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-12 08:52:41 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -2,33 +2,17 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-nk, *stack = map(int, input().split())
-meeting, count = [], 0
-graph = {i: set() for i in range(1, N+1)}
-if not nk:
-    for _ in range(M): input()
+n, *k = map(int, input().split())
+meeting = [{*list(map(int, input().split()))[1:]} for _ in range(M)]
+if not n:
     print(M)
 else:
+    k, c = {*k}, 0
     for _ in range(M):
-        _, *m = map(int, input().split())
-        meeting.append(m)
-        for n1 in m:
-            for n2 in m:
-                graph[n1].add(n2)
-                graph[n2].add(n1)
-
-    know = set(stack)
-    while stack:
-        n = stack.pop()
-        know.add(n)
-        m = graph[n]
-        if not m - know:
-            continue
-        for p in m:
-            if p in know:
-                stack.extend(list(m - know))
+        for m in meeting:
+            if k&m:
+                k |= m
     for m in meeting:
-        if not set.intersection(know, m):
-            count += 1
-
-    print(count)
+        if not k&m:
+            c += 1
+    print(c)
