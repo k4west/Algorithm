437944f22e5391a5f8fa9a1a96db8b699f194a4e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-14 10:38:29 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1508 ms, Memory: 61960 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -5,18 +5,18 @@ def main():
     N = int(input())
     num = list(map(int, input().split()))
     M = int(input())
-    dp = [[0]*N for _ in range(N)]
+    dp = [['0']*N for _ in range(N)]
     
     for i in range(N):
         for s in range(N-i):
             e = s + i
             if s == e:
-                dp[s][e] = 1
+                dp[s][e] = '1'
             elif num[s] == num[e]:
                 if s+1 == e:
-                    dp[s][e] = 1
-                elif dp[s+1][e-1]:
-                    dp[s][e] = 1
+                    dp[s][e] = '1'
+                elif dp[s+1][e-1] == '1':
+                    dp[s][e] = '1'
     
     for _ in range(M):
         S, E = map(int, input().split())
