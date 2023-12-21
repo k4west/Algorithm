df523e093583ffd539c5eb8a0894022b6d415c9d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-22 00:38:39 +0900

    [Gold III] Title: 두 배열의 합, Time: 212 ms, Memory: 73108 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -11,14 +11,18 @@ def main():
         asum = 0
         for j in range(i, n):
             asum += A[j]
-            Tsub[asum] = Tsub.get(asum, 0) + 1
+            if asum in Tsub:
+                Tsub[asum] += 1
+            else:
+                Tsub[asum] = 1
         
     ans = 0
     for i in range(m):
         bsum = 0
         for j in range(i, m):
             bsum += B[j]
-            ans += Tsub.get(T-bsum, 0)
+            if T-bsum in Tsub:
+                ans += Tsub[T-bsum]
 
     print(ans)
 
