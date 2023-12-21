46c44e1cd7c0487f065802bf2136a41561168f23
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-22 00:18:19 +0900

    [Gold III] Title: 두 배열의 합, Time: 336 ms, Memory: 73108 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,23 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def subsum(N, L):
-    Lsub = {}
-    for i in range(N):
-        ss = 0
-        for j in range(i, N):
-            ss += L[j]
-            Lsub[ss] = Lsub.get(ss, 0) + 1
-    return Lsub
-
 T = int(input())
 n, A = int(input()), list(map(int, input().split()))
 m, B = int(input()), list(map(int, input().split()))
 
-Asub, Bsub = subsum(n, A), subsum(m, B)
+Tsub = {}
+for i in range(n):
+    asum = 0
+    for j in range(i, n):
+        asum += A[j]
+        Tsub[asum] = Tsub.get(asum, 0) + 1
+    
 ans = 0
-for asub, an in Asub.items():
-    tmp = T - asub
-    ans += an*Bsub.get(tmp, 0)
+for i in range(m):
+    bsum = 0
+    for j in range(i, m):
+        bsum += B[j]
+        ans += Tsub.get(T-bsum, 0)
 
 print(ans)
