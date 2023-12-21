cfa3efbec260fd8b482f1138d77dcc74ee51d8f8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-21 23:47:58 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,21 @@
+def main():
+    d = {1:1, 2:1}
+    n = int(input())
+    
+    if not n:
+        print(0)
+        return
+
+    def f(n):
+        if d.get(n):
+            return d[n]
+        if n % 2:
+            d[n] = (pow(f(n//2+1), 2) + pow(f(n//2), 2)) % 1_000_000_007
+        else:
+            d[n] = (pow(f(n//2+1), 2) - pow(f(n//2-1), 2)) % 1_000_000_007
+        return d[n]
+
+    print(f(n))
+
+if __name__ == "__main__":
+    main()
