4cb11eeda1bba3fb1f9e0c5d0dac494a9b0dd940
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-22 00:18:59 +0900

    [Gold III] Title: 두 배열의 합, Time: 224 ms, Memory: 73108 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,22 +1,26 @@
-import sys
-input = sys.stdin.readline
+def main():
+    import sys
+    input = sys.stdin.readline
 
-T = int(input())
-n, A = int(input()), list(map(int, input().split()))
-m, B = int(input()), list(map(int, input().split()))
+    T = int(input())
+    n, A = int(input()), list(map(int, input().split()))
+    m, B = int(input()), list(map(int, input().split()))
 
-Tsub = {}
-for i in range(n):
-    asum = 0
-    for j in range(i, n):
-        asum += A[j]
-        Tsub[asum] = Tsub.get(asum, 0) + 1
-    
-ans = 0
-for i in range(m):
-    bsum = 0
-    for j in range(i, m):
-        bsum += B[j]
-        ans += Tsub.get(T-bsum, 0)
+    Tsub = {}
+    for i in range(n):
+        asum = 0
+        for j in range(i, n):
+            asum += A[j]
+            Tsub[asum] = Tsub.get(asum, 0) + 1
+        
+    ans = 0
+    for i in range(m):
+        bsum = 0
+        for j in range(i, m):
+            bsum += B[j]
+            ans += Tsub.get(T-bsum, 0)
 
-print(ans)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
