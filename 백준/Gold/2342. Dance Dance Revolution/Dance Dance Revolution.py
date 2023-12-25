c81958fb803ab3b672007b60fae6853a068c202a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-26 00:50:51 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 204 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,31 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-INF = float('inf')
-d = [[2, 2, 2, 2, 2],
-     [0, 1, 3, 4, 3],
-     [0, 3, 1, 3, 4],
-     [0, 4, 3, 1, 3],
-     [0, 3, 4, 3, 1]]
+def main():
+    INF = float('inf')
+    c = [[2, 2, 2, 2, 2],
+         [0, 1, 3, 4, 3],
+         [0, 3, 1, 3, 4],
+         [0, 4, 3, 1, 3],
+         [0, 3, 4, 3, 1]]
 
-arr = list(map(int, input()[:-2].split()))
-n = len(arr)
-dp = [[[INF]*5 for _ in range(5)] for _ in range(n+1)]
-dp[-1][0][0] = 0 
+    arr = list(map(int, input()[:-2].split()))
+    dp = [INF]*5
+    dp[0] = 0
+    a0 = 0
 
-for i in range(n):
-    a = arr[i]
-    for j in range(5):
-        for k in range(5):
-            dp[i][a][j] = min(dp[i][a][j], dp[i-1][k][j] + d[k][a])
-    
-    for j in range(5):
-        for k in range(5):
-            dp[i][j][a] = min(dp[i][j][a], dp[i-1][j][k] + d[k][a])
+    for a1 in arr:
+        dp[a0] = min(dp[i] + c[i][a1] for i in range(5))
+        for j in range(5):
+            if j == a0:
+                continue
+            dp[j] += c[a0][a1]
+        a0 = a1
 
-ans = INF
-for i in range(5):
-    for j in range(5):
-        if ans > (tmp:=dp[n-1][i][j]):
-            ans = tmp
-print(ans)
+    print(min(dp))
+
+if __name__ == "__main__":
+    main()
