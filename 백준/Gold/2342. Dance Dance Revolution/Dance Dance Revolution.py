e336e30e6bddafe3408145bc4ca7b7daf56fe334
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-26 01:05:02 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 204 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -3,22 +3,15 @@ input = sys.stdin.readline
 
 def main():
     INF = float('inf')
-    c = [[2, 2, 2, 2, 2],
-         [0, 1, 3, 4, 3],
-         [0, 3, 1, 3, 4],
-         [0, 4, 3, 1, 3],
-         [0, 3, 4, 3, 1]]
-
+    c = [[2, 2, 2, 2, 2], [0, 1, 3, 4, 3], [0, 3, 1, 3, 4], [0, 4, 3, 1, 3], [0, 3, 4, 3, 1]]
     arr = list(map(int, input()[:-2].split()))
-    dp = [INF]*5
+    dp, a0 = [INF]*5, 0
     dp[0] = 0
-    a0 = 0
-
+    
     for a1 in arr:
         dp[a0] = min(dp[i] + c[i][a1] for i in range(5))
         for j in range(5):
-            if j == a0:
-                continue
+            if j == a0: continue
             dp[j] += c[a0][a1]
         a0 = a1
 
