8d108f626711d61f2cf4f85b77adbdb1890a9c6e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-26 18:19:44 +0900

    [Gold II] Title: 보석 도둑, Time: 1036 ms, Memory: 98164 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 
 def main():
     N, K = map(int, input().split())
-    jam, bag, q, i, ans = [], [], [], 0, 0
+    gem, bag, q, i, ans = [], [], [], 0, 0
 
-    for _ in range(N): jam.append(list(map(int, input().split())))
+    for _ in range(N): gem.append(tuple(map(int, input().split())))
     for _ in range(K): bag.append(int(input()))
-    jam.sort()
+    gem.sort()
     bag.sort()
 
     for w in bag:
-        while i < N and jam[i][0] <= w:
-            heappush(q, -jam[i][1])
+        while i < N and gem[i][0] <= w:
+            heappush(q, -gem[i][1])
             i += 1
         if q: ans -= heappop(q)
 
