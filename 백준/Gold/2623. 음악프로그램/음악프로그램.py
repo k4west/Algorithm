f2e67200524a2fe554de43b88354bfdfd50b5178
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-28 17:03:23 +0900

    [Gold III] Title: 음악프로그램, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.py
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -1,32 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-def main():
-    N, M = map(int, input().split())
-    graph = [[] for _ in range(N+1)]
-    in_num, v = [0]*(N+1), set(range(1, N+1))
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+in_num, v = [0]*(N+1), set(range(1, N+1))
 
-    for _ in range(M):
-        n, *order = map(int, input().split())
-        for i in range(0, n-1):
-            graph[order[i]].append((t:=order[i+1]))
-            in_num[t] += 1
-            v.discard(t)
+for _ in range(M):
+    order = list(map(int, input().split()))
+    for i in range(1, order[0]):
+        graph[order[i]].append((t:=order[i+1]))
+        in_num[t] += 1
+        v.discard(t)
 
-    s = list(v)
-    ans = []
-    while s:
-        n = s.pop()
-        ans.append(str(n))
-        for i in graph[n]:
-            in_num[i] -= 1
-            if not in_num[i]:
-                s.append(i)
+s = list(v)
+ans = []
+while s:
+    n = s.pop()
+    ans.append(str(n))
+    for i in graph[n]:
+        in_num[i] -= 1
+        if not in_num[i]:
+            s.append(i)
 
-    if len(ans) != N:
-        print(0)
-    else:
-        print('\n'.join(ans))
-
-if __name__ == "__main__":
-    main()
+if len(ans) != N:
+    print(0)
+else:
+    print('\n'.join(ans))
