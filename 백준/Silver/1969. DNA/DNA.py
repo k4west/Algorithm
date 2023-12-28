b2421c245c3023f6229798203c3c159e94dfded8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-28 12:43:50 +0900

    [Silver IV] Title: DNA, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -7,18 +7,16 @@ for _ in range(N):
     for i, s in enumerate(input().rstrip()):
         dna[i][s] += 1
 
-ans = ""
+ans, c0 = "", 0
 for d in dna:
-    ts, tc = '', 0
+    ts, tc, td = '', 0, []
     for s, c in d.items():
+        td.append((s, c))
         if c > tc:
             ts, tc = s, c
     ans += ts
-
-c0 = 0
-for s0, d in zip(ans, dna):
-    for s, c in d.items():
-        if s != s0:
+    for s, c in td:
+        if s != ts:
             c0 += c
 
 print(ans)
