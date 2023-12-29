81adef9c00257a0b19b31858394942f274597d37
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-29 21:22:38 +0900

    [Silver IV] Title: 바닥 장식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,8 +1,5 @@
-import sys
-input = sys.stdin.readline
-
 N, M = map(int, input().split())
-pattern = [input().rstrip() for _ in range(N)]
+pattern = [input() for _ in range(N)]
 r = sum(len([i for i in row.split('|') if i]) for row in pattern)
 c = sum(len([i for i in "".join(col).split('-') if i]) for col in zip(*pattern))
 print(r+c)
