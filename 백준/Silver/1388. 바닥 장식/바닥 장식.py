0cce39f910a51f0cb21366110ea0a29bd0f3a2d8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-29 21:21:36 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -3,33 +3,10 @@ input = sys.stdin.readline
 
 def main():
     N, M = map(int, input().split())
-    pattern = [list(input().strip()) for _ in range(N)]
-
-    def row(i, j):
-        for k in range(j, M):
-            if pattern[i][k] != '-':
-                break
-            pattern[i][k] = False
-
-    def col(i, j):
-        for k in range(i, N):
-            if pattern[k][j] != '|':
-                break
-            pattern[k][j] = False
-
-    c = 0
-    for i in range(N):
-        for j in range(M):
-            if not (p:=pattern[i][j]):
-                continue
-            if p == '-':
-                c += 1
-                row(i, j)
-            elif p == '|':
-                c += 1
-                col(i, j)
-
-    print(c)
+    pattern = [input().rstrip() for _ in range(N)]
+    r = sum(len([i for i in row.split('|') if i]) for row in pattern)
+    c = sum(len([i for i in "".join(col).split('-') if i]) for col in zip(*pattern))
+    print(r+c)
 
 if __name__ == "__main__":
     main()
