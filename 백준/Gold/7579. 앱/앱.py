f8dadfba4971a67e4d5275b212d620bef991f0a1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-29 20:38:15 +0900

    [Gold III] Title: 앱, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -8,11 +8,11 @@ def main():
     for m, c in mc:
         t = {}
         for dc, dm in d.items():
-            tm, tc = min(M, m+dm), c+dc
+            tm, tc = m+dm, c+dc
             if d.get(tc, 0) < tm:
                 t[tc] = tm
         d.update(t)
-    print(min(c for c, m in d.items() if m==M))
+    print(min(c for c, m in d.items() if m>=M))
 
 if __name__ == "__main__":
     main()
