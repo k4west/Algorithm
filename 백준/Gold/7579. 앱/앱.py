bd551a40cb2891ec727db70da14261ba160682ce
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-29 20:10:12 +0900

    [Gold III] Title: 앱, Time: 376 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-*memories, = map(int, input().split())
-*costs, = map(int, input().split())
+def main():
+    N, M = map(int, input().split())
+    *memories, = map(int, input().split())
+    *costs, = map(int, input().split())
 
-C = sum(costs)
-dp = [0]*(C+1)
-for i in range(N):
-    m, c = memories[i], costs[i]
-    for j in range(C, c-1, -1):
-        dp[j] = min(M, max(dp[j], dp[j-c]+m))
+    C = sum(costs)
+    dp = [0]*(C+1)
+    for i in range(N):
+        m, c = memories[i], costs[i]
+        for j in range(C, c-1, -1):
+            dp[j] = min(M, max(dp[j], dp[j-c]+m))
 
-li = [c for c in range(C+1) if dp[c]==M]
-print(sorted(li)[0])
+    li = [c for c in range(C+1) if dp[c]==M]
+    print(sorted(li)[0])
+
+if __name__ == "__main__":
+    main()
