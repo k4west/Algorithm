5d1c113256ab0c0c11a84a721cbcb7adac4cb3ed
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-30 21:21:28 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 728 ms, Memory: 118776 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
@@ -4,21 +4,31 @@ input = sys.stdin.readline
 def main():
     N, S = map(int, input().split())
     *arr, = map(int, input().split())
-    l, r, m = {}, {}, N//2
+    m = N//2
 
-    def f(li, s, a, d):
+    def fl(li):
+        t = [0]
+        for a in li:
+            tmp = [a+b for b in t]
+            t.extend(tmp)
+        return  t
+    
+    def fd(li, s, a, d):
         d[s] =  d.get(s, 0) + 1
         for i in range(a, len(li)):
-            f(li, s + li[i], i+1, d)
+            fd(li, s + li[i], i+1, d)
 
-    f(arr[:m], 0, 0, l)
-    f(arr[m:], 0, 0, r)
+
+    l = fl(arr[:m])
+    r = {}
+    fd(arr[m:], 0, 0, r)
+    
     c = 0
     if not S: c = -1
 
     for lk in l:
         if (rk:=S-lk) in r:
-            c += l[lk] * r[rk]
+            c += r[rk]
     print(c)
 
 if __name__ == "__main__":
