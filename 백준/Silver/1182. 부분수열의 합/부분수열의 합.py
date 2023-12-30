eeb4e291d2a2c86349b6ff46580015d21ccdefb3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2023-12-30 09:10:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,23 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-N, S = map(int, input().split())
-*arr, = map(int, input().split())
+def main():
+    N, S = map(int, input().split())
+    *arr, = map(int, input().split())
+    l, r, m = {}, {}, N//2
 
-c = 0
-if not S: c = -1
+    def f(li, s, a, d):
+        d[s] =  d.get(s, 0) + 1
+        for i in range(a, len(li)):
+            f(li, s + li[i], i+1, d)
 
-def f(s, a):
-    global c
-    
-    if s == S: 
-        c += 1
-    
-    for i in range(a+1, N):
-        s += arr[i]
-        f(s, i)
-        s -= arr[i]
+    f(arr[:m], 0, 0, l)
+    f(arr[m:], 0, 0, r)
+    c = 0
+    if not S: c = -1
 
-f(0, -1)
+    for lk in l:
+        if (rk:=S-lk) in r:
+            c += l[lk] * r[rk]
+    print(c)
 
-print(c)
+if __name__ == "__main__":
+    main()
