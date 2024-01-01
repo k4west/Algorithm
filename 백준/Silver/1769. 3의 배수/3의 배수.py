c77575fea132c138119cb339c319aa07c83dbe00
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-01 21:12:51 +0900

    [Silver V] Title: 3의 배수, Time: 188 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,12 +1,18 @@
-n, c = input(), 0
-while len(n) != 1:
-    t = 0
-    c += 1
-    for i in n:
-        t += int(i)
-    n = str(t)
-    
-print(c)
-if not int(n)%3:
-    print('YES')
-else: print('NO')
+import sys
+
+def main():
+    n, c = sys.stdin.readline().rstrip(), 0
+    while len(n) != 1:
+        t = 0
+        c += 1
+        for i in n:
+            t += int(i)
+        n = str(t)
+        
+    print(c)
+    if not int(n)%3:
+        print('YES')
+    else: print('NO')
+
+if __name__ == '__main__':
+    main()
