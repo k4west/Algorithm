07e6074ed31e2a18928119a19b4f242353d918cb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-05 16:14:47 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,15 +1,11 @@
 N = int(input())
-dp = [0] + [1]*9
+M = 1_000_000_000
+dp = [0] + [1] * 9
 
-for idx in range(2, N+1):
-    tmp = [0]*10
-    for n in range(10):
-        if not n:
-            tmp[n] = (tmp[n] + dp[1])%1_000_000_000
-        elif n == 9:
-            tmp[n] = (tmp[n] + dp[8])%1_000_000_000
-        else:
-            tmp[n] = (tmp[n] + dp[n-1] + dp[n+1])%1_000_000_000
+for idx in range(2, N + 1):
+    tmp = [dp[1]] + [0] * 8 + [dp[8]]
+    for n in range(1, 9):
+        tmp[n] = (dp[n - 1] + dp[n + 1]) % M
     dp = tmp
 
-print(sum(dp)%1_000_000_000)
+print(sum(dp) % M)
