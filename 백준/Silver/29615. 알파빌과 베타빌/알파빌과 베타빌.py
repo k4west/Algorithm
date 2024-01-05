a6d420d302492cf3d8f626becfddfdca403d9e88
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-05 13:25:12 +0900

    [Silver V] Title: 알파빌과 베타빌, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/29615. 알파빌과 베타빌/알파빌과 베타빌.py
+++ 백준/Silver/29615. 알파빌과 베타빌/알파빌과 베타빌.py
@@ -1,22 +1,6 @@
 import sys
-
 input = sys.stdin.readline
 N, M = map(int, input().split())
-order = list(map(int, input().split()))
-friends = set(map(int, input().split()))
-l, v, c = len(friends), set(), 0
-
-for i in range(N):
-    if l == len(v):
-        break
-    for j in range(N - 1, i, -1):
-        if (s := order[i]) in friends:
-            v.add(s)
-            break
-        if (t := order[j]) not in friends:
-            continue
-        order[j], order[i] = s, t
-        v.add(t)
-        c += 1
-        break
-print(c)
+order = set(input().split()[:M])
+friends = set(input().split())
+print(len(order-friends))
