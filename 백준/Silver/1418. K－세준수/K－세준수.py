ebc01748c1ab5e218b767a879d428f64716e8930
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-11 12:48:24 +0900

    [Silver V] Title: K-세준수, Time: 84 ms, Memory: 33460 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.py
+++ 백준/Silver/1418. K－세준수/K－세준수.py
@@ -1,17 +1,18 @@
-import sys
+if __name__ == "__main__":
+    import sys
 
-input = lambda: int(sys.stdin.readline())
-N, K = input(), input()
-nums = [False] + [True] * (N)
-primes = [False, False] + [True] * (N - 1)
+    input = lambda: int(sys.stdin.readline())
+    N, K = input(), input()
+    nums = [False] + [True] * (N)
+    primes = [False, False] + [True] * (N - 1)
 
-for i in range(2, int(N**0.5) + 1):
-    if primes[i]:
-        for j in range(i**2, N + 1, i):
-            primes[j] = False
+    for i in range(2, int(N**0.5) + 1):
+        if primes[i]:
+            for j in range(i**2, N + 1, i):
+                primes[j] = False
 
-for n in range(K + 1, N + 1):
-    if primes[n] and K < n:
-        for i in range(n, N + 1, n):
-            nums[i] = False
-print(sum(nums))
+    for n in range(K + 1, N + 1):
+        if primes[n] and K < n:
+            for i in range(n, N + 1, n):
+                nums[i] = False
+    print(sum(nums))
