031f32f4003ea8087fd3ef97265209d1008c5173
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-11 12:47:53 +0900

    [Silver V] Title: K-세준수, Time: 88 ms, Memory: 33460 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.py
+++ 백준/Silver/1418. K－세준수/K－세준수.py
@@ -3,24 +3,15 @@ import sys
 input = lambda: int(sys.stdin.readline())
 N, K = input(), input()
 nums = [False] + [True] * (N)
-c = K if N >= K else N
 primes = [False, False] + [True] * (N - 1)
 
-for i in range(2, int(N**.5) + 1):
+for i in range(2, int(N**0.5) + 1):
     if primes[i]:
         for j in range(i**2, N + 1, i):
             primes[j] = False
-primes = [i for i, b in enumerate(primes[2 : K + 1], 2) if b]
 
 for n in range(K + 1, N + 1):
-    if nums[n]:
-        flag = False
-        for p in primes:
-            while not n % p:
-                n //= p
-            if n <= K or n == 1:
-                c += 1
-                flag = True
-                break
-                
-print(c)
+    if primes[n] and K < n:
+        for i in range(n, N + 1, n):
+            nums[i] = False
+print(sum(nums))
