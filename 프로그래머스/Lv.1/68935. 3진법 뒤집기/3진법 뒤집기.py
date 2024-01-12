719df74061e26c96a05b40a16606773b4f25d2e3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-12 17:46:06 +0900

    [level Lv.1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/Lv.1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,9 +1,15 @@
 def solution(n):
-    tri = []
+    tri = ''
     while n:
-        tri.append(n%3)
+        tri += str(n%3)
         n //= 3
-    ans = 0
-    for i, t in enumerate(tri[::-1]):
-        ans += t*(3**i)
-    return ans
+    return int(tri, 3)
+
+    # tri = []
+    # while n:
+    #     tri.append(n%3)
+    #     n //= 3
+    # ans = 0
+    # for i, t in enumerate(tri[::-1]):
+    #     ans += t*(3**i)
+    # return ans
