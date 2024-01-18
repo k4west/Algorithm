68cb1ca3ab0344820b65e62bfbef51fe8ffc3d33
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-18 10:23:07 +0900

    [Silver V] Title: Musical Chairs, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/17857. Musical Chairs/Musical Chairs.py
+++ 백준/Silver/17857. Musical Chairs/Musical Chairs.py
@@ -1,15 +1,17 @@
 import sys
 
-input = sys.stdin.readline
-n = int(input())
-professors = list(range(1, n + 1))
-li = list(map(int, input().split()))
-idx = 0
-while n > 1:
-    idx += li[idx] - 1
-    idx %= n
-    professors.pop(idx)
-    li.pop(idx)
-    n -= 1
-    idx %= n
-print(professors[0])
+def main():
+    input = sys.stdin.readline
+    n = int(input())
+    faculties = list(range(1, n + 1))
+    opus_num = list(map(int, input().split()))
+    idx = 0
+    while n > 1:
+        faculties.pop(idx := (idx + opus_num[idx] - 1) % n)
+        opus_num.pop(idx)
+        n -= 1
+        idx %= n
+    print(faculties[0])
+
+if __name__ == "__main__":
+    main()
