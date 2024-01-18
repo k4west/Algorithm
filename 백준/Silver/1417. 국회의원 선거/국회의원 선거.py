be3d0073e72c785a439b8859bb217f61d2aed296
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-18 09:53:45 +0900

    [Silver V] Title: 국회의원 선거, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -5,16 +5,12 @@ def main():
     input = lambda: int(sys.stdin.readline())
     N = input()
     Dasom = input()
-    li = [input() for _ in range(N - 1)]
+    li = [input() for _ in range(N - 1)] + [0]
     for i in range(101):
-        m, idx = 0, 0
-        for j in range(N - 1):
-            if m < (t := li[j]):
-                m, idx = t, j
-        if m < Dasom:
+        if (m := max(li)) < Dasom:
             print(i)
             return
-        li[idx] -= 1
+        li[li.index(m)] -= 1
         Dasom += 1
 
 
