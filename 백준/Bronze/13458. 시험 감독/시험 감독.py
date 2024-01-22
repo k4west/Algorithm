e8c33296d5427d921cc7b7079c2e3651f6c88688
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-22 21:00:12 +0900

    [Bronze II] Title: 시험 감독, Time: 360 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,6 +1,2 @@
-from sys import stdin
-input = stdin.readline
-N = int(input())
-*A, = map(int, input().split())
-B, C = map(int, input().split())
+N, *A, B, C = map(int, open(0).read().split())
 print(len(A)-sum((B-a)//C for a in A if a>B))
