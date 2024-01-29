42510ad52e1d9b55e3458c3a8ca6eb48cb7f22cb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-29 16:14:16 +0900

    [Gold II] Title: 문제집, Time: 172 ms, Memory: 44420 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,25 +1,29 @@
 import sys
 from heapq import heappop, heappush
 
-input = lambda: map(int, sys.stdin.readline().split())
-N, M = input()
-graph, degs = [[] for _ in range(N + 1)], [0 for _ in range(N + 1)]
-v = set(range(1, 1 + N))
+def main():
+    input = lambda: map(int, sys.stdin.readline().split())
+    N, M = input()
+    graph, degs = [[] for _ in range(N + 1)], [0 for _ in range(N + 1)]
+    v = set(range(1, 1 + N))
 
-for _ in range(M):
-    a, b = input()
-    graph[a].append(b)
-    degs[b] += 1
-    v.discard(b)
+    for _ in range(M):
+        a, b = input()
+        graph[a].append(b)
+        degs[b] += 1
+        v.discard(b)
 
-heap, ans = list(v), []
+    heap, ans = list(v), []
 
-while heap:
-    a = heappop(heap)
-    ans.append(str(a))
-    for b in graph[a]:
-        degs[b] -= 1
-        if not degs[b]:
-            heappush(heap, b)
+    while heap:
+        a = heappop(heap)
+        ans.append(str(a))
+        for b in graph[a]:
+            degs[b] -= 1
+            if not degs[b]:
+                heappush(heap, b)
 
-print(" ".join(ans))
+    print(" ".join(ans))
+
+if __name__ == "__main__":
+    main()
