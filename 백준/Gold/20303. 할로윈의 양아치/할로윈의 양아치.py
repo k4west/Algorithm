9e755438f69f138fb6547b7020af0ac941a406b3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-29 11:29:50 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 404 ms, Memory: 117460 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def main():
     def find_root(x):
         if x == (t := roots[x]):
@@ -7,45 +8,43 @@ def main():
         roots[x] = find_root(t)
         return roots[x]
 
-
     def union_root(x, y):
         if (a := find_root(x)) == (b := find_root(y)):
             return
         if a < b:
             roots[b] = a
+            friedns[a] += friedns[b]
+            candies[a] += candies[b]
         else:
             roots[a] = b
+            friedns[b] += friedns[a]
+            candies[b] += candies[a]
         return
-
 
     input = lambda: map(int, sys.stdin.readline().split())
     N, M, K = input()
-    candies = list(input())
+    candies = [0] + list(input())
+    roots = [i for i in range(N + 1)]
+    friedns = [0] + [1 for _ in range(N)]
     roots = [i for i in range(N + 1)]
 
     for _ in range(M):
         i, j = input()
         union_root(i, j)
 
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         find_root(i)
 
-    d = {root: [0, 0] for root in roots}
-    for i, root in enumerate(roots[1:]):
-        d[root][0] += 1
-        d[root][1] += candies[i]
+    f_c = [[friedns[i], candies[i]] for i in range(1, N + 1) if i == roots[i]]
+    f_c.sort(key=lambda x: (x[0], x[1]))
 
-    dp = {0: 0}
-    for F, C in d.values():
-        tmp = {}
-        for f, c in dp.items():
-            if (f := F + f) >= K:
-                continue
-            if dp.get(f, 0) < (c := C + c):
-                tmp[f] = c
-        dp.update(tmp)
-    print(max(dp.values()))
+    dp = [0] * K
+    for F, C in f_c:
+        for i in range(K - 1, F - 1, -1):
+            if dp[i] < (t := dp[i - F] + C):
+                dp[i] = t
+    print(dp[K - 1])
+
 
 if __name__ == "__main__":
     main()
-    
