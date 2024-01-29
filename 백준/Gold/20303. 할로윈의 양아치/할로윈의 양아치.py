60760073bb5a7170cf72da1ead3ffd40346f667b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-29 12:56:48 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 384 ms, Memory: 115276 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -1,50 +1,55 @@
 import sys
 
 
-def main():
-    def find_root(x):
-        if x == (t := roots[x]):
-            return x
-        roots[x] = find_root(t)
-        return roots[x]
+class Candy:
+    def __init__(self):
+        self.input = lambda: map(int, sys.stdin.readline().split())
+        self.N, self.M, self.K = self.input()
+        self.candies = [0] + list(self.input())
+        self.friedns = [0] + [1 for _ in range(self.N)]
+        self.roots = [i for i in range(self.N + 1)]
 
-    def union_root(x, y):
-        if (a := find_root(x)) == (b := find_root(y)):
+    def find_root(self, x):
+        if x == (t := self.roots[x]):
+            return x
+        self.roots[x] = self.find_root(t)
+        return self.roots[x]
+
+    def union_root(self, x, y):
+        if (a := self.find_root(x)) == (b := self.find_root(y)):
             return
         if a < b:
-            roots[b] = a
-            friedns[a] += friedns[b]
-            candies[a] += candies[b]
+            self.roots[b] = a
+            self.friedns[a] += self.friedns[b]
+            self.candies[a] += self.candies[b]
         else:
-            roots[a] = b
-            friedns[b] += friedns[a]
-            candies[b] += candies[a]
+            self.roots[a] = b
+            self.friedns[b] += self.friedns[a]
+            self.candies[b] += self.candies[a]
         return
 
-    input = lambda: map(int, sys.stdin.readline().split())
-    N, M, K = input()
-    candies = [0] + list(input())
-    roots = [i for i in range(N + 1)]
-    friedns = [0] + [1 for _ in range(N)]
-    roots = [i for i in range(N + 1)]
+    def sol(self):
+        for _ in range(self.M):
+            i, j = self.input()
+            self.union_root(i, j)
 
-    for _ in range(M):
-        i, j = input()
-        union_root(i, j)
+        for i in range(1, self.N + 1):
+            self.find_root(i)
 
-    for i in range(1, N + 1):
-        find_root(i)
+        f_c = [
+            (self.friedns[i], self.candies[i])
+            for i in range(1, self.N + 1)
+            if i == self.roots[i]
+        ]
+        f_c.sort(key=lambda x: (x[0], x[1]))
 
-    f_c = [(friedns[i], candies[i]) for i in range(1, N + 1) if i == roots[i]]
-    f_c.sort(key=lambda x: (x[0], x[1]))
-
-    dp = [0] * K
-    for F, C in f_c:
-        for i in range(K - 1, F - 1, -1):
-            if dp[i] < (t := dp[i - F] + C):
-                dp[i] = t
-    print(dp[K - 1])
+        dp = [0] * self.K
+        for F, C in f_c:
+            for i in range(self.K - 1, F - 1, -1):
+                if dp[i] < (t := dp[i - F] + C):
+                    dp[i] = t
+        print(dp[self.K - 1])
 
 
 if __name__ == "__main__":
-    main()
+    Candy().sol()
