8207f2742e897aa237496f4d179dea6050fddb4a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-01-31 18:58:58 +0900

    [Silver III] Title: 회전하는 큐, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,25 +1,16 @@
 import sys
-from collections import deque
 
 input = lambda: map(int, sys.stdin.readline().split())
 N, M = input()
-q = deque(range(1, N + 1))
-ans = 0
+q = list(range(1, N + 1))
+ans = t = 0
 
-for i in input():
-    while True:
-        if q[0] == i:
-            q.popleft()
-            break
-        else:
-            if (n := q.index(i)) < (N) / 2:
-                while q[0] != i:
-                    q.append(q.popleft())
-                    ans += 1
-            else:
-                while q[0] != i:
-                    q.appendleft(q.pop())
-                    ans += 1
+for a in input():
+    x = q.index(a)
+    y = min((x - t) % N, (t - x) % N)
+    q.pop(x)
     N -= 1
+    ans += y
+    t = x
 
 print(ans)
