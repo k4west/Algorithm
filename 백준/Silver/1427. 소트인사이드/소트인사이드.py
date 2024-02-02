9fd8bacb46185bc730608473ee1c0e9a677cf68d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-02 10:37:53 +0900

    [Silver V] Title: 소트인사이드, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,5 +1,3 @@
 import sys
 
-n = sys.stdin.readline()
-d = {i: n.count(i) for i in map(str, range(9, -1, -1))}
-print("".join((i * j for i, j in d.items())))
+print("".join(sorted(sys.stdin.readline().rstrip(), reverse=True)))
