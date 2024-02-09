08f751a0c195a698b8273343e28b1f823f34ed9b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-09 17:59:35 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,3 +1,6 @@
-from functools import cmp_to_key as ck
+# from functools import cmp_to_key as ck
+# def solution(numbers):
+#     return str(int("".join(sorted(map(str, numbers), key=ck(lambda x,y: 2*(x+y>y+x) -1))[::-1])))
+
 def solution(numbers):
-    return str(int("".join(sorted(map(str, numbers), key=ck(lambda x,y: 2*(x+y>y+x) -1))[::-1])))
+    return str(int("".join(sorted(map(str, numbers), key=lambda x: x*4)[::-1])))
