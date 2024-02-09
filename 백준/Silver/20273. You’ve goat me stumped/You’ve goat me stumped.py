e95a42b8e86a6af0051e33afdb6319141c32e29e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-09 15:48:57 +0900

    [Silver IV] Title: You’ve goat me stumped, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20273. You’ve goat me stumped/You’ve goat me stumped.py
+++ 백준/Silver/20273. You’ve goat me stumped/You’ve goat me stumped.py
@@ -1,9 +1,5 @@
 import sys
-
-
 def area():
     l, r = map(int, sys.stdin.readline().split())
-    return str(int(round(3.141592653 * (r * (r**2 - l**2) ** 0.5) / 4, -2)))
-
-
+    return str(int(round(3.14 * (r * (r**2 - l**2) ** 0.5) / 4, -2)))
 print("\n".join(area() for _ in range(int(input()))))
