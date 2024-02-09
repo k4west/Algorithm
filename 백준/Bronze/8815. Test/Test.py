ed77397bdaf6b0c8d8720e68c0e757d828998c68
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-09 20:54:23 +0900

    [Bronze III] Title: Test, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/8815. Test/Test.py
+++ 백준/Bronze/8815. Test/Test.py
@@ -1,2 +1,1 @@
-a=[int(input()) for _ in range(int(input()))]
-print("\n".join("ABCD"[((n-2)//3+1+((n-2)%3)%2)%4] for n in a))
+print("\n".join("BABCBCDCDADA"[int(input())%12] for _ in range(int(input()))))
