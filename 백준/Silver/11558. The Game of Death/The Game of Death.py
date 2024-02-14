c3f8c89e20e1653d4d445518a862d2ba9ca2e836
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-14 10:54:47 +0900

    [Silver IV] Title: The Game of Death, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11558. The Game of Death/The Game of Death.py
+++ 백준/Silver/11558. The Game of Death/The Game of Death.py
@@ -1,22 +1,14 @@
 import sys
-
 input = lambda: int(sys.stdin.readline())
-
 ans = []
 for i in range(input()):
     n = input()
     graph = [input() for _ in range(n)]
     c, prev = 1, graph[0]
     for _ in range(n):
-        if prev == n:
-            ans.append(c)
-            break
+        if prev == n: ans.append(str(c)); break
+        if t := graph[prev - 1]: graph[prev - 1], prev = 0, t
+        else: break
         c += 1
-        if t := graph[prev - 1]:
-            graph[prev - 1] = 0
-            prev = t
-        else:
-            break
-    if len(ans) == i:
-        ans.append(0)
-print("\n".join(map(str, ans)))
+    if len(ans) == i: ans.append('0')
+print("\n".join(ans))
