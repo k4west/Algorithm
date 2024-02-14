92086c6745a66cd5a236cc3f6b134c2e92e3d5b2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-14 10:08:22 +0900

    [Bronze I] Title: Black and White, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/18156. Black and White/Black and White.py
+++ 백준/Bronze/18156. Black and White/Black and White.py
@@ -1,28 +1,20 @@
 import sys
-
-input = sys.stdin.readline
-n = int(input())
-grid = [input().rstrip() for _ in range(n)]
-ans = 1
+input=sys.stdin.readline
+n=int(input())
+grid=[input().rstrip() for _ in range(n)]
+ans=1
 
 def more_than_3(line):
-    count, prev = 0, "W"
+    count,prev=0,"W"
     for s in line:
-        if s == prev:
-            if (count := count + 1) == 3:
-                return True
-        else:
-            count, prev = 1, s
+        if s==prev: 
+            if (count:=count+1)==3:return True
+        else:count,prev=1,s
     return False
 
-for maps in (grid, zip(*grid)):
+for maps in (grid,zip(*grid)):
     for line in maps:
-        if line.count("W") != n // 2:
-            ans = 0
-            break
-        if more_than_3(line):
-            ans = 0
-            break
-    if not ans:
-        break
+        if line.count("W")!=n//2:ans=0;break
+        if more_than_3(line):ans=0;break
+    if not ans: break
 print(ans)
