3947bf05f30f358c369057f8da0af49eb7c9baa7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-14 09:55:12 +0900

    [Bronze I] Title: Black and White, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/18156. Black and White/Black and White.py
+++ 백준/Bronze/18156. Black and White/Black and White.py
@@ -1,27 +1,20 @@
 import sys
-
 input = sys.stdin.readline
 n = int(input())
 grid = [input().rstrip() for _ in range(n)]
 ans = 1
-
-
 def more_than_3(line):
-    b = w = 0
+    count = 0
+    prev = "W"
     for s in line:
-        if s == "B":
-            w = 0
-            b += 1
-            if b == 3:
+        if s == prev:
+            count += 1
+            if count == 3:
                 return True
-        if s == "W":
-            b = 0
-            w += 1
-            if w == 3:
-                return True
+        else:
+            count = 1
+            prev = s
     return False
-
-
 for row in grid:
     if row.count("W") != n // 2:
         ans = 0
@@ -29,11 +22,11 @@ for row in grid:
     if more_than_3(row):
         ans = 0
         break
-
 if ans:
     for col in zip(*grid):
         if col.count("W") != n // 2:
             ans = 0
+            break
         if more_than_3(col):
             ans = 0
             break
