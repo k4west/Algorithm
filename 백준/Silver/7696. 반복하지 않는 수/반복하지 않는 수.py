2272396d3a4fb9138e3504b637328f571495c63f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-20 19:15:21 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 548 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.py
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.py
@@ -1,22 +1,16 @@
 def sol(length, num, flag, digits):
     global idx
     if idx > M: return
-    if not length:
-        nums[idx] = num
-        idx += 1
-        return
+    if not length: nums[idx] = num; idx += 1; return
     for digit in digits:
         if flag and digit == 0: continue
-        tmp = [i for i in digits]
-        tmp.remove(digit)
-        sol(length - 1, num + digit * (10 ** (length - 1)), False, tmp)
-
+        digits.remove(digit)
+        sol(length - 1, num + digit * (10 ** (length - 1)), False, digits)
+        digits.append(digit); digits.sort()
 
 def main():
     ans = []
-    for length in range(1, 9):
-        sol(length, 0, True, list(range(10)))
-
+    for length in range(1, 9): sol(length, 0, True, list(range(10)))
     while True:  # 계속 입력을 받다가 입력이 0인 경우 종료
         if (i := int(input())) == 0: break
         ans.append(nums[i])
