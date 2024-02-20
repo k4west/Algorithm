432819bf11cc67cc16e754a1d6097514d9a70401
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-20 20:03:57 +0900

    [Bronze I] Title: Inspiration, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/16715. Inspiration/Inspiration.py
+++ 백준/Bronze/16715. Inspiration/Inspiration.py
@@ -4,13 +4,10 @@ N = int(sys.stdin.readline())
 m, no = 0, 2
 for i in range(2, N + 1):
     tmp = []
-    n, s, j = N, 0, 0
-    while n >= i ** (j + 1):
-        j += 1
+    n, s = N, 0
     while n:
-        t, n = n // (i**j), n % (i**j)
-        s += t
-        j -= 1
-        if m < s:
-            m, no = s, i
+        s += n % i
+        n = n // i
+    if m < s:
+        m, no = s, i
 print(m, no)
