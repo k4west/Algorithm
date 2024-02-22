1b26991a711836049fb906802898007e121be78f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-22 19:34:38 +0900

    [Silver IV] Title: 식당 메뉴, Time: 828 ms, Memory: 153512 KB -BaekjoonHub

--- 백준/Silver/26043. 식당 메뉴/식당 메뉴.py
+++ 백준/Silver/26043. 식당 메뉴/식당 메뉴.py
@@ -1,18 +1,22 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
-A, B, C = [], [], deque([])
-for _ in range(int(input())):
-    type_, *num = map(int, input().split())
-    if type_ == 1:
-        C.append(num)
-    else:
-        a, b = C.popleft()
-        if num[0] == b:
-            A.append(a)
+def main():
+    input = sys.stdin.readline
+    A, B, C = [], [], deque([])
+    for _ in range(int(input())):
+        type_, *num = map(int, input().split())
+        if type_ == 1:
+            C.append(num)
         else:
-            B.append(a)
-print(" ".join(map(str, sorted(A))) or "None")
-print(" ".join(map(str, sorted(B))) or "None")
-print(" ".join(map(str, sorted((a for a, _ in C)))) or "None")
+            a, b = C.popleft()
+            if num[0] == b:
+                A.append(a)
+            else:
+                B.append(a)
+    print(" ".join(map(str, sorted(A))) or "None")
+    print(" ".join(map(str, sorted(B))) or "None")
+    print(" ".join(map(str, sorted((a for a, _ in C)))) or "None")
+
+if __name__ == "__main__":
+    main()
