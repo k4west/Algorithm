8eca3416f18dd44206f36531717fab119543277e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-23 14:53:38 +0900

    [Bronze I] Title: Secret Message, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11093. Secret Message/Secret Message.py
+++ 백준/Bronze/11093. Secret Message/Secret Message.py
@@ -1,8 +1,5 @@
 import sys
 input = sys.stdin.readline
-ans = []
 for _ in range(int(input())):
-    message = input().rstrip()
-    k = int((len(message) - 1) ** 0.5) + 1
-    ans.append("".join(message[i::k][::-1] for i in range(k)))
-print("\n".join(ans))
+    k = int((len(message:= input().rstrip()) - 1) ** 0.5) + 1
+    print("".join(message[i::k][::-1] for i in range(k)))
