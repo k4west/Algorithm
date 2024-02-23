7131a4d748a65f4a4a1b885ed4052d2cbfb46953
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-23 09:48:57 +0900

    [Silver IV] Title: 식당 메뉴, Time: 716 ms, Memory: 162660 KB -BaekjoonHub

--- 백준/Silver/26043. 식당 메뉴/식당 메뉴.py
+++ 백준/Silver/26043. 식당 메뉴/식당 메뉴.py
@@ -1,26 +1,23 @@
 import sys
 from collections import deque
 
-
 def sol(li: list) -> str:
     return " ".join(map(str, sorted(li))) or "None"
-
 
 def main():
     input = sys.stdin.readline
     A, B, C = [], [], deque([])
     for _ in range(int(input())):
-        type_, *num = map(int, input().split())
-        if type_ == 1:
+        type_, *num = input().split()
+        if type_ == '1':
             C.append(num)
         else:
             a, b = C.popleft()
             if num[0] == b:
-                A.append(a)
+                A.append(int(a))
             else:
-                B.append(a)
-    print("\n".join((sol(A), sol(B), sol((a for a, _ in C)))))
-
+                B.append(int(a))
+    print("\n".join((sol(A), sol(B), sol(map(int, (a for a, _ in C))))))
 
 if __name__ == "__main__":
     main()
