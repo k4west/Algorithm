4665f3cbad67fc6e4b9ab460005bfc09c9f691de
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-25 22:16:14 +0900

    [Silver III] Title: 문자열 암호화, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6616. 문자열 암호화/문자열 암호화.py
+++ 백준/Silver/6616. 문자열 암호화/문자열 암호화.py
@@ -3,14 +3,12 @@ import sys
 input = sys.stdin.readline
 ans = []
 while True:
-    if (n := int(input())) == 0:
-        break
+    if (n := int(input())) == 0: break
     message = "".join(input().rstrip().split()).upper()
-    tmp, i, j, length = {}, 0, 0, len(message)
+    i, j, length = 0, 0, len(message)
+    tmp = [""] * length
     for k in range(length):
-        tmp[i] = message[k]
-        i += n
-        if i >= length:
-            i = (j := j + 1)
-    ans.append("".join(map(lambda x: x[1], sorted(tmp.items(), key=lambda x: x[0]))))
+        tmp[i] = message[k]; i += n
+        if i >= length: i = (j := j + 1)
+    ans.append("".join(tmp))
 print("\n".join(ans))
