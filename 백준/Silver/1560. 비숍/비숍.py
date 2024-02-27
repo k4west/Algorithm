8dc1dc80730017d485dc130219682f6230f6ad13
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-27 10:24:51 +0900

    [Silver III] Title: 비숍, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1560. 비숍/비숍.py
+++ 백준/Silver/1560. 비숍/비숍.py
@@ -1,10 +1,1 @@
-N = input()
-if N != '1':
-    carry, i, ans = 0, 1, []
-    for digit in map(int, N[::-1]):
-        n, i = (digit - i) * 2 + carry, 0
-        carry = n // 10
-        ans.append(n % 10)
-    if carry: ans.append(carry)
-    print("".join(map(str, ans[::-1])))
-else: print(N)
+print(int(input())*2-2 or 1)
