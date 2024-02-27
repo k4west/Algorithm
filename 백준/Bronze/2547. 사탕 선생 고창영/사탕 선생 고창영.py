6ad3f822746a0e5ddd0993151a372cbbe7f7ac99
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-27 10:51:22 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
@@ -1,10 +1,8 @@
 import sys
 input = sys.stdin.readline
-ans = []
 for _ in range(int(input())):
     input()
     n = int(input())
     s = sum(int(input()) for _ in range(n))
-    if s%n: ans.append("NO")
-    else: ans.append("YES")
-print("\n".join(ans))
+    if s%n: print("NO")
+    else: print("YES")
