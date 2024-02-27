2a0bf7d1ce5d4463e1964f04f70916ec7ddf51f4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-27 11:53:48 +0900

    [Bronze II] Title: 완전수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14563. 완전수/완전수.py
+++ 백준/Bronze/14563. 완전수/완전수.py
@@ -2,13 +2,14 @@ import sys
 input = sys.stdin.readline
 def Aliquot_sum(n):
     s = 0
-    for i in range(1, n):
-        if n % i == 0: s += i
-    return s
+    for i in range(1, int(n**.5)+1):
+        if n % i == 0: 
+            s += i
+            if i != (j:=n//i): s += j
+    return ('Deficient', 'Abundant')[2*n < s]
 ans = []
 input()
 for n in map(int, input().split()):
-    if n == (a := Aliquot_sum(n)): ans.append("Perfect")
-    elif n > a: ans.append("Deficient")
-    else: ans.append("Abundant")
+    if n in (6,28,496,8128): ans.append("Perfect")
+    else: ans.append(Aliquot_sum(n))
 print("\n".join(ans))
