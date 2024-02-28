533220ea30b68db99397efb3496defd6fe44ee4f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-28 16:53:22 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,8 +1,6 @@
 input()
 Y=M=0
-for n in map(int, input().split()):
-    Y+=n//30*10+10
-    M+=n//60*15+15
+for n in map(int,input().split()):Y+=n//30*10+10;M+=n//60*15+15
 if Y==M:print(f'Y M {Y}')
 elif Y<M:print(f'Y {Y}')
 else:print(f'M {M}')
