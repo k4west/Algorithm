68894f153bd88f04cec66e32981efab57f20d056
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-28 10:39:33 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,10 +1,4 @@
 import sys
-X,Y=map(int, sys.stdin.readline().split())
+X,Y=map(int,sys.stdin.readline().split())
 if(Z:=(Y*100)//X)>=99:print(-1)
-else:
-    l,r=1,1000000000
-    while l<=r:
-        m=(l+r)//2
-        if(Y+m)*100//(X+m)>Z:r=m-1
-        else:l=m+1
-    print(l)
+else:print(bool(((Y+(i:=(X *(Z+1)-100*Y)//(99-Z)))*100)//(X+i)<=Z)+i)
