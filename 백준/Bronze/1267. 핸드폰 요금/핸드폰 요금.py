fe06ea2728bcb192d672ab6e19005d755e69924f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-28 16:52:29 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,8 +1,8 @@
-d = {'Y': 0, 'M': 0}
 input()
+Y=M=0
 for n in map(int, input().split()):
-    d['Y'] += 10 * (n // 30 + 1)
-    d['M'] += 15 * (n // 60 + 1)
-if d['Y'] == d['M']: print(f'Y M {d["Y"]}')
-elif d['Y'] < d['M']: print(f'Y {d["Y"]}')
-else: print(f'M {d["M"]}')
+    Y+=n//30*10+10
+    M+=n//60*15+15
+if Y==M:print(f'Y M {Y}')
+elif Y<M:print(f'Y {Y}')
+else:print(f'M {M}')
