1265b5c563ef7dde48c9d2f00e324cd179492b69
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-02-29 12:37:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4472 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,14 +1,14 @@
 def main():
     import sys
+
     input = sys.stdin.readline
     N = int(input())
-    s = {}
-    for i in range(N):
-        if (n:= input().rstrip()) in s: s[n] += 1
-        else: s[n] = 1
-    for n in map(str, range(1, 10001)):
-        if n in s:
-            for _ in range(s[n]): sys.stdout.write(n+"\n")
+    s = [0] * 10001
+    for _ in range(N): s[n := int(input())] += 1
+    for i, n in enumerate(s):
+        if n:
+            i = str(i)
+            for _ in range(n): sys.stdout.write(i + "\n")
 
 if __name__ == "__main__":
     main()
