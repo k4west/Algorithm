a9e11fbd2df72965984a5912b5096afb7c39ad5d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-01 22:52:52 +0900

    [Bronze I] Title: Bingo!, Time: 420 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5694. Bingo！/Bingo！.py
+++ 백준/Bronze/5694. Bingo！/Bingo！.py
@@ -1,10 +1,16 @@
 import sys
+
 def main():
     input = sys.stdin.readline
     while True:
         N, B = map(int, input().split())
-        if N == 0: break
+        if N == 0:
+            break
         cards = sorted(map(int, input().split()))
-        tmp = set(cards[i] - cards[j] for i in range(B - 1, -1, -1) for j in range(i - 1, -1, -1))
+        tmp = set()
+        for i in range(B - 1, -1, -1):
+            for j in range(i - 1, -1, -1):
+                tmp.add(cards[i] - cards[j])
         print("YN"[bool(set(range(1, N + 1)) - tmp)])
+
 main()
