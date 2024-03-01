77167340bb3adc29f0d6a7e7e264460a34809265
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-01 22:50:57 +0900

    [Bronze I] Title: Bingo!, Time: 444 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5694. Bingo！/Bingo！.py
+++ 백준/Bronze/5694. Bingo！/Bingo！.py
@@ -1,8 +1,13 @@
 import sys
-input = sys.stdin.readline
-while True:
-    N, B = map(int, input().split())
-    if N == 0: break
-    cards = sorted(map(int, input().split()))
-    tmp = set(cards[i] - cards[j] for i in range(B - 1, -1, -1) for j in range(i - 1, -1, -1))
-    print("YN"[bool(set(range(1, N + 1)) - tmp)])
+
+def main():
+    input = sys.stdin.readline
+    while True:
+        N, B = map(int, input().split())
+        if N == 0: break
+        cards = sorted(map(int, input().split()))
+        tmp = set(cards[i] - cards[j] for i in range(B - 1, -1, -1) for j in range(i - 1, -1, -1))
+        print("YN"[bool(set(range(1, N + 1)) - tmp)])
+
+if __name__ == "__main__":
+    main()
