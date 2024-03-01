247544f9c9b9a900a5271df3ad76baab4e220dea
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-01 21:22:58 +0900

    [Bronze V] Title: 크냐?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.py
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -1,1 +1,5 @@
-while (s := input())[0] != "0": print(("No", "Yes")[eval(s.replace(" ", ">"))])
+import sys
+input = sys.stdin.readline
+ans = []
+while (s := input())[0] != "0": ans.append(("No", "Yes")[eval(s.replace(" ", ">"))])
+print("\n".join(ans))
