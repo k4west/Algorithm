349ee2234ae638ba9a40a329c0ade5d88fe921d5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-01 21:03:02 +0900

    [Silver III] Title: 선물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.py
+++ 백준/Silver/1166. 선물/선물.py
@@ -1,7 +1,7 @@
 import sys
 N, L, W, H = map(int, sys.stdin.readline().split())
 left, right = 0, (L * W * H / N) ** (1 / 3)
-for _ in range(50):
+for _ in range(40):
     mid = (left + right) / 2
     if (L // mid) * (W // mid) * (H // mid) < N:
         right = mid
