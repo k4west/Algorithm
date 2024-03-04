92069dbcae01c7f379dcafe4aa81e2b2b7e09644
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-04 13:49:33 +0900

    [Silver V] Title: Better Chance, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/30320. Better Chance/Better Chance.py
+++ 백준/Silver/30320. Better Chance/Better Chance.py
@@ -1,6 +1,6 @@
 import sys
 rt,rj,st,sj=map(float,sys.stdin.readline().split())
-t,j=(rt-1)*int(float(sj)*1000),(rj-1)*int(float(st)*1000)
+t,j=(rt-1)*int(sj*1000),(rj-1)*int(st*1000)
 if t<j: print("TAOYUAN")
 elif t>j: print("JAKARTA")
 else: print("SAME")
