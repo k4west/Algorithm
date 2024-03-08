293fa56ba58498172b25395c920fae5273c65855
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-08 16:51:16 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 16:31:31
+2024년 03월 08일 16:51:15
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,29 @@
+# def solution(clothes):
+#     d = {}
+#     for cloth, types in clothes:
+#         if types in d:
+#             d[types].append(cloth)
+#         else:
+#             d[types] = [cloth]
+#     s = 1
+#     for i in d.values():
+#         s *= len(i) + 1
+#     return s - 1
+
+# def solution(clothes):
+#     d = {}
+#     for _, types in clothes:
+#         d[types] = d.get(types, 0) + 1
+#     s = 1
+#     for i in d.values():
+#         s *= i + 1
+#     return s - 1
+
 def solution(clothes):
     d = {}
-    for cloth, types in clothes:
-        if types in d:
-            d[types].append(cloth)
-        else:
-            d[types] = [cloth]
+    for types in [a for a in zip(*clothes)][1]:
+        d[types] = d.get(types, 0) + 1
     s = 1
     for i in d.values():
-        s *= len(i) + 1
+        s *= i + 1
     return s - 1
