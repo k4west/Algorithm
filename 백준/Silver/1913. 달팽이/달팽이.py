11a7d4a1b6b0b4027a218be9e0c69a44d5b811ef
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-08 16:56:23 +0900

    [Silver III] Title: 달팽이, Time: 492 ms, Memory: 83552 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,26 +1,23 @@
-N = int(input())
-x = int(input())
-
-td = [[[] for _ in range(N)] for _ in range(N)]
-M = (N - 1)//2
-o = [M,M]
-num = 1
-td[M][M] = num
-for i in range(1, N, 2):
-    o[0] -= 1
-    o[1] -= 1
-    for d in ((0,1),(1,0),(0,-1),(-1,0)):
-        dx, dy = d[0], d[1]
-        for _ in range(i+1):
-            o[0] += dx
-            o[1] += dy
-            num += 1
-            td[o[0]][o[1]] = num
-for r in td:
-    print(*r)
-
-for row in range(N):
-    for col in range(N):
-        if td[row][col] == x:
-            print(*(row + 1, col + 1))
-            exit()
+import sys
+def main():
+    input=sys.stdin.readline
+    N,x=int(input()),int(input())
+    N2=N*N
+    td,M=[[N2-i]+[0]*(N-1) for i in range(N)],(N-1)//2
+    r=c=M
+    if x in range(N*(N-1)+1,N2+1): r,c=N2-x,0
+    t=1
+    for i in range(N):
+        prev=td[i][0]
+        if i==M: t=0
+        for j in range(1,N):
+            num=1
+            if j<=i+t:
+                if t or i!=N-1 and j<=M-i%M: num=(N-2*j+1)*4-1
+                num*=-1
+            elif N-j<=(i*t)-(2*M-i)*(t-1): num=(2*j-N)*4-1
+            prev=td[i][j]=prev+num
+            if prev==x: r,c=i,j
+    print("\n".join(map(lambda x:" ".join(map(str,x)),td)))
+    print(r+1,c+1)
+main()
