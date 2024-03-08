52ec5b5a47588d16b026fe6aa78fce963b8f337e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-08 16:59:38 +0900

    [level 2] Title: 전화번호 목록, Time: 145.03 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,5 +1,5 @@
 def solution(phone_book):
     phone_book.sort()
-    for i in range((n:= len(phone_book))-1):
+    for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]): return False
     return True
