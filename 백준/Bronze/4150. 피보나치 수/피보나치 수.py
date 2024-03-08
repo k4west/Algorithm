4700a7908dac323fc1ba912b5550f01d2e3c8435
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-08 16:56:36 +0900

    [Bronze I] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4150. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.py
@@ -1,18 +1,7 @@
-def mul(A: list, B: list) -> list:
-    return [
-        A[0] * B[0] + A[1] * B[1],
-        A[0] * B[1] + A[1] * B[2],
-        A[1] * B[1] + A[2] * B[2],
-    ]
+def fibo(n):
+    a = b = 1
+    for _ in range(n):
+        a, b = b, a+b
+    return a
 
-
-def sol(n, A):
-    if n == 1:
-        return A
-    elif n % 2:
-        return mul(mul(sol(n // 2, A), sol(n // 2, A)), [1, 1, 0])
-    else:
-        return mul(sol(n // 2, A), sol(n // 2, A))
-
-
-print(sol(int(input()), [1, 1, 0])[1])
+print(fibo(int(input())-1))
