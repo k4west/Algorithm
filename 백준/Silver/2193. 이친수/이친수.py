e1f13c2c532ed2756b2105c101ce2b4457c885e5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-09 10:26:42 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,4 +1,18 @@
-a, b = 0, 1
+# 답은 피보나치 수열입니다.
+# 아래 함수도 피보나치 수열을 구하는 함수로 볼 수도 있는데,
+# 문제에 맞는 주석으로 설명을 적었습니다.
+
+end_with_0, end_with_1 = 0, 1   # 초기 상태
+
+# 0으로 끝나는 수는 이후에 0과 1을 이어서 적어도 된다.
+# 1로 끝나는 수는 이후에 0만 이어 붙일 수 있다.
+
 for _ in range(int(input())-1):
-    a, b = a+b, a
-print(a+b)
+    # end_with_0, end_with_1 = end_with_0 + end_with_1, end_with_0
+    # 아래 식은 위 주석처럼 한 줄로 처리할 수 있습니다.
+     
+    tmp = end_with_0
+    end_with_0 += end_with_1
+    end_with_1 = tmp
+
+print(end_with_0+end_with_1)    # 합계 출력
