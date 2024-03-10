d5aca168f51414093e8ee9675c331ec14d498f5c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-10 16:32:12 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,17 +1,18 @@
 import sys
+def main():
+    input = sys.stdin.readline
+    n = int(input())
+    balloon = list(range(2, n+1))
+    note = list(map(int, input().split()))
+    ans = [1]
+    index = 0
 
-input = sys.stdin.readline
-n = int(input())
-balloon = list(range(2, n+1))
-note = list(map(int, input().split()))
-ans = [1]
-index = 0
-
-for i in range(1, n):
-    t = note[ans[-1]-1]
-    if t > 0:
-        t -= 1
-    index = (index + t) % (n-i)
-    ans.append(balloon.pop(index))
-ans += balloon
-print(" ".join(map(str, ans)))
+    for i in range(1, n):
+        t = note[ans[-1]-1]
+        if t > 0:
+            t -= 1
+        index = (index + t) % (n-i)
+        ans.append(balloon.pop(index))
+    ans += balloon
+    print(" ".join(map(str, ans)))
+main()
