2913e90da9d932abe266f571411846dc8195aef4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-11 13:20:34 +0900

    [Bronze I] Title: The Brave Sir Robin’s cAsE cOrReCtOr, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9160. The Brave Sir Robin’s cAsE cOrReCtOr/The Brave Sir Robin’s cAsE cOrReCtOr.py
+++ 백준/Bronze/9160. The Brave Sir Robin’s cAsE cOrReCtOr/The Brave Sir Robin’s cAsE cOrReCtOr.py
@@ -1,9 +1,9 @@
-flag = False
-a = list("".join(map(lambda x: x.lower(), open(0))))
-for i, t in enumerate(a):
-    if flag:
-        if t in " ()\n": continue
-        a[i] = t.upper()
-        flag = False
-    if t in ".!?": flag = True
+f=False
+a=list("".join(map(lambda x:x.lower(),open(0))))
+for i,t in enumerate(a):
+    if f:
+        if t in " ()\n":continue
+        a[i]=t.upper()
+        f=False
+    if t in ".!?":f=True
 print("".join(a))
