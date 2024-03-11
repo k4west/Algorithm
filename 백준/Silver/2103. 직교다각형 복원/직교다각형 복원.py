11fb6f16baa3d38b3afd306a89a5ca46768fe4d1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-11 12:32:19 +0900

    [Silver III] Title: 직교다각형 복원, Time: 128 ms, Memory: 34612 KB -BaekjoonHub

--- 백준/Silver/2103. 직교다각형 복원/직교다각형 복원.py
+++ 백준/Silver/2103. 직교다각형 복원/직교다각형 복원.py
@@ -1,11 +1,10 @@
-import sys
 def main():
-    input = sys.stdin.readline
-    n, ans = int(input()), 0
+    a = open(0)
+    n, ans = int(next(a)), 0
     rows = [[] for _ in range(10001)]
     cols = [[] for _ in range(10001)]
-    for _ in range(n):
-        i, j = map(int, input().split())
+    for p in a:
+        i, j = map(int, p.split())
         rows[i].append(j)
         cols[j].append(i)
     for row in rows:
