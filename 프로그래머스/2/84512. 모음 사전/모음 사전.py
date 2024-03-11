1e21de23477ea07e4d112bd66f5aee6ed3d987f4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-11 18:28:53 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,3 @@
-from itertools import permutations, islice
 def solution(word):
-    answer = 0
     d = {t:i for i, t in enumerate('AEIOU')}
-    for i, t in enumerate(word):
-        f = 0
-        for j in range(5-i):
-            f += 5 ** j
-        answer += f*d[t] + 1
-    return answer
+    return sum((5**(5-i)//4)*d[t] + 1 for i, t in enumerate(word))
