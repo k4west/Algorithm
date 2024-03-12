f6189724427d2117e6c3475b97d2519a85ac6ba2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-12 12:27:57 +0900

    [Silver III] Title: 세워라 반석 위에, Time: 1132 ms, Memory: 55884 KB -BaekjoonHub

--- 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.py
+++ 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.py
@@ -1,18 +1,19 @@
 import sys
-from collections import deque
-def check(num):
-    li = [i for i, j in enumerate(num) if j]
-    return li[-1] - li[0]
 def main():
     input = sys.stdin.readline
+    nums = [0] * 11
+    s = ans = tmp = 0
     input()
-    num = [0] * 11
-    q = deque([])
-    ans = 1
-    for a in map(int, input().split()):
-        q.append(a)
-        num[a] += 1
-        while check(num) > 2: num[q.popleft()] -= 1
-        if ans < (t := len(q)): ans = t
+    arr = list(map(int, input().split()))
+    for a in arr:
+        nums[a] += 1
+        tmp += 1
+        li = [i for i, j in enumerate(nums) if j]
+        if li[-1] - li[0] > 2:
+            nums[arr[s]] -= 1
+            tmp -= 1
+            s += 1
+        if ans < tmp:
+            ans = tmp
     print(ans)
 main()
