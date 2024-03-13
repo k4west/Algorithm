22a6d706d80081d08a42db497cc1699e9fc89eb2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-13 21:53:24 +0900

    [Bronze III] Title: 공, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.py
+++ 백준/Bronze/1547. 공/공.py
@@ -1,8 +1,6 @@
 k = 1
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    if a == k:
-        k = b
-    elif b == k:
-        k = a
+    if a == k: k = b
+    elif b == k: k = a
 print(k)
