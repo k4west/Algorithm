10b4ed7c6d47ef2f0ad91c62ecf0d2a74ca81fe1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-14 13:54:34 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -1,5 +1,5 @@
 n = int(input())
 m = 2*n-1
 for i in range(m):
-    if i < n: print(('*'*(2*(n-i)-1)).center(m).rstrip())
-    else: print(('*'*(2*(i-n)+3)).center(m).rstrip())
+    if i < n: print(('*'*(2*(n-i)-1)).rjust(2*n-i-1))
+    else: print(('*'*(2*(i-n)+3)).rjust(i+1))
