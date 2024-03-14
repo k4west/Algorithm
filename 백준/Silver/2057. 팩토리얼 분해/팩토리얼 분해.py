d5d714a4a7f2af57c452508aedb1ff3849b72a49
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-14 14:28:37 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
@@ -1,12 +1,9 @@
 def main():
-    if (N:= int(input())) == 0:
-        return 'NO'
-    factorials = [1]
-    for i in range(1, 21):
-        factorials.append(factorials[-1]*i)
-    for i in factorials[::-1]:
-        if N >= i:
-            N -= i
+    if (N:= int(input())) == 0: return 'NO'
+    dp = [1]
+    for i in range(1, 21): dp.append(dp[-1]*i)
+    for i in dp[::-1]:
+        if N >= i: N -= i
     if N: return 'NO'
     return 'YES'
     
