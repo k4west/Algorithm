c2556e83f2d3f4e2d89abc4f39462a277f0960b8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-14 11:11:05 +0900

    [Gold IV] Title: Manage your Energy (Large), Time: 80 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/12332. Manage your Energy （Large）/Manage your Energy （Large）.py
+++ 백준/Gold/12332. Manage your Energy （Large）/Manage your Energy （Large）.py
@@ -1,39 +1,40 @@
 import sys
+def main():
+    input = sys.stdin.readline
+    ans = []
+    for x in range(int(input())):
+        E, R, N = map(int, input().split())
+        n = (E+R-1)//R
+        current = E
+        if E < R:
+            R = E
+        arr = list(map(int, input().split())) + [0]
+        gain = 0
 
-input = sys.stdin.readline
-ans = []
-for x in range(int(input())):
-    E, R, N = map(int, input().split())
-    n = (E+R-1)//R
-    current = E
-    if E < R:
-        R = E
-    arr = list(map(int, input().split())) + [0]
-    gain = 0
+        for i in range(N):
+            v = arr[i]
+            for j in range(i+1, N+1):
+                if j == i + n + 1:
+                    j == N
+                    break
+                if arr[j] > v:
+                    break
 
-    for i in range(N):
-        v = arr[i]
-        for j in range(i+1, N+1):
-            if j == i + n + 1:
-                j == N
-                break
-            if arr[j] > v:
-                break
+            if j == N:
+                spent = current
+            else:
+                tmp = current + (j - i) * R - E
+                if tmp < 0:
+                    tmp = 0
+                elif tmp > current:
+                    tmp = current
+                spent = tmp
 
-        if j == N:
-            spent = current
-        else:
-            tmp = current + (j - i) * R - E
-            if tmp < 0:
-                tmp = 0
-            elif tmp > current:
-                tmp = current
-            spent = tmp
+            gain += v * spent
+            current += R - spent
+            if current > E:
+                current = E
 
-        gain += v * spent
-        current += R - spent
-        if current > E:
-            current = E
-
-    ans.append(f"Case #{x+1}: {gain}")
-print("\n".join(ans))
+        ans.append(f"Case #{x+1}: {gain}")
+    print("\n".join(ans))
+main()
