f4aee00c41de4e28d83211c9f5af572abf36efed
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-14 09:45:52 +0900

    [Silver IV] Title: 카드2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,9 +1,10 @@
-import sys
-n = int(sys.stdin.readline())
-li = [i for i in range(1, n+1)]
-while len(li)!=1:
-    m = len(li)%2
-    li = li[1::2]
-    if m:
-        li.insert(0,0)
-print(li[0])
+n = int(input())
+if n < 3:
+    print(n)
+else:
+    i = n - 1
+    c = -1
+    while i:
+        i //= 2
+        c += 1
+    print((n - 2**c)*2)
