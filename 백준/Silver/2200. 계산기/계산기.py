11a463f5e53a8f434639c5a46bb7106f8e8d8988
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-14 13:08:54 +0900

    [Silver III] Title: 계산기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2200. 계산기/계산기.py
+++ 백준/Silver/2200. 계산기/계산기.py
@@ -1,10 +1,6 @@
 input()
-li = input().rstrip().split()
-s = 2
-for i in li[1:-1]:
-    if i == "0":
-        s += 2
-    else:
-        s += len(i) + 3
-s += 1 + len(li[-1]) if li[-1] != "0" else 0
+s = 0
+for i in input().split()[1:]:
+    s += len(i) + 3
+    if i == "0": s -= 2
 print(s)
