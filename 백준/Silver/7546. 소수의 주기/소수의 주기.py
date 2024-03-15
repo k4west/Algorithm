e2d3d518234b8a3944c8a28a290d4369839efd17
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-15 11:28:48 +0900

    [Silver III] Title: 소수의 주기, Time: 292 ms, Memory: 47088 KB -BaekjoonHub

---
+++ 백준/Silver/7546. 소수의 주기/소수의 주기.py
@@ -0,0 +1,54 @@
+import sys
+
+input = sys.stdin.readline
+ans = []
+
+
+def gcd(x, y):
+    while x % y:
+        x, y = y, x % y
+    return y
+
+
+def finite_decimal_number(z, b):
+    prime_factors_of_b = []
+    for p in range(2, b + 1):
+        if b % p == 0:
+            prime_factors_of_b.append(p)
+            while b % p == 0:
+                b //= p
+
+    for p in prime_factors_of_b:
+        while z % p == 0:
+            z //= p
+
+    return z == 1
+
+
+def cal_period(x, y, b):
+    d, idx = {x: 0}, 1
+    current = x * b
+    while True:
+        current %= y
+        if current in d:
+            return idx - d[current]
+
+        d[current] = idx
+        idx += 1
+        current *= b
+
+
+for i in range(int(input())):
+    b, x, y = input().rstrip().split()
+    b = int(b)
+    x, y = int(x, b), int(y, b)
+    d = gcd(x, y)
+    x //= d
+    y //= d
+
+    if finite_decimal_number(y, b):
+        period = 0
+    else:
+        period = cal_period(x, y, b)
+    ans.append(f"Scenario #{i+1}:\n{period}")
+print("\n\n".join(ans))
