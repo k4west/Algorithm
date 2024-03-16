cf57ac387c37a3ae783d2fa4f7d7fe06164ee85b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-16 20:22:01 +0900

    [Silver V] Title: Perfect Game (Small), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12366. Perfect Game （Small）/Perfect Game （Small）.py
+++ 백준/Silver/12366. Perfect Game （Small）/Perfect Game （Small）.py
@@ -1,8 +1,6 @@
 import sys
-from functools import cmp_to_key
-def minimizing(L, P):
-    return L[0] * P[1] - L[1] * P[0]
 input = sys.stdin.readline
 for i in range(int(input())):
     N = int(input())
-    print(f"Case #{i+1}: {' '.join((j for _, _, j in sorted(list(zip(map(int, input().split()), map(int, input().split()), map(str, range(N)))), key=cmp_to_key(minimizing))))}")
+    input()
+    print(f"Case #{i+1}: {' '.join((j for _, j in sorted(list(zip(map(int, input().split()), map(str, range(N)))), key=lambda x: -x[0])))}")
