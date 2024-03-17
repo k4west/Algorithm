869fc908837c922d488b633b5ff76d7a47f79d81
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-17 18:43:02 +0900

    [Silver III] Title: 칩 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2236. 칩 만들기/칩 만들기.py
+++ 백준/Silver/2236. 칩 만들기/칩 만들기.py
@@ -1,12 +1,8 @@
-import sys
-
-input = sys.stdin.readline
 N, K = map(int, input().split())
 arr = sorted(zip(map(int, input().split()), range(N)))
 connected_chips = ["0"] * N
 powers = ["0"] * K
 for i, (_, j) in enumerate(arr[::-1][:K]):
-    powers[i] = str(j + 1)
-    connected_chips[j] = str(j + 1)
-print("\n".join(powers))
-print("\n".join(connected_chips))
+    powers[i] = j + 1
+    connected_chips[j] = j + 1
+print("\n".join(map(str, powers + connected_chips)))
