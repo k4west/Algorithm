ff62259e0b9a7176a7776d2638260ff0f52fa303
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-19 10:20:53 +0900

    [Bronze II] Title: 오르막, Time: 340 ms, Memory: 149492 KB -BaekjoonHub

--- 백준/Bronze/14910. 오르막/오르막.py
+++ 백준/Bronze/14910. 오르막/오르막.py
@@ -1,9 +1,10 @@
 import sys
-a, *arr, = map(int, sys.stdin.readline().split())
-for b in arr:
-    if a > b:
-        print('Bad')
-        break
-    a = b
-else:
+def main():
+    a, *arr, = map(int, sys.stdin.readline().split())
+    for b in arr:
+        if a > b:
+            print('Bad')
+            return
+        a = b
     print('Good')
+main()
