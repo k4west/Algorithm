d44785beb20f0745a59507cf4cc7797a23016252
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-20 11:12:30 +0900

    [Bronze III] Title: 소금 폭탄, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.py
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.py
@@ -1,9 +1,5 @@
-def time2sec(h, m, s):
-    return h*3600 + m*60 + s
-now = time2sec(*map(int, input().split(':')))
-time = time2sec(*map(int, input().split(':')))
-time -= now
-if time <= 0:
-    time += 86400
-h, m, s = time//3600, (time:=time%3600)//60, time%60
+def time2sec(h, m, s): return h*3600 + m*60 + s
+time = -time2sec(*map(int, input().split(':'))) + time2sec(*map(int, input().split(':')))
+if time <= 0: time += 86400
+h, m, s = time//3600, (time%3600)//60, time%60
 print(f'{h:02d}:{m:02d}:{s:02d}')
