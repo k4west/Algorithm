bd0264187c1764f19bae261ce32137bafb7b0138
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-25 17:33:21 +0900

    [Silver V] Title: 김인천씨의 식료품가게 (Large), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/12034. 김인천씨의 식료품가게 （Large）/김인천씨의 식료품가게 （Large）.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+for i in range(int(input())):
+    discounted = []
+    n = int(input())
+    prices = list(map(int, input().split()))
+    for j in range(n):
+        if (org := prices[j] * 4 // 3) in prices:
+            discounted.append(prices[j])
+            prices.remove(org)
+    print(f"Case #{i+1}:", *discounted)
