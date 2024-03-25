df557130db44d56a2869fd2ecf48cd73b4059b7c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-25 17:53:56 +0900

    [Silver IV] Title: 팰린드롬 애너그램, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    s = input()
+    d = {chr(i): 0 for i in range(97, 123)}
+    if N % 2:
+        d[s[N // 2]] -= 1
+    for t in s:
+        d[t] += 1
+    for v in d.values():
+        if v % 2:
+            print("No")
+            return
+    print("Yes")
+main()
