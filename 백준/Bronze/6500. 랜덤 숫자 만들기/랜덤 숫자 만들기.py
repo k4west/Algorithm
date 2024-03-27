91214cbbbc204157d72a5676b820fd3b55445b00
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-27 20:34:36 +0900

    [Bronze II] Title: 랜덤 숫자 만들기, Time: 316 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6500. 랜덤 숫자 만들기/랜덤 숫자 만들기.py
@@ -0,0 +1,12 @@
+n = 4
+ans = []
+for a in map(int, open(0)):
+    if a == 0:
+        break
+    visit = set()
+    while a not in visit:
+        visit.add(a)
+        a *= a
+        a = int(str(a).zfill(8)[2:6])
+    ans.append(len(visit))
+print("\n".join(map(str, ans)))
