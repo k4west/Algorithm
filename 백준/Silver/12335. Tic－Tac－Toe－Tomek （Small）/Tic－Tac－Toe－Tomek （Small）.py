50890513e143d372a5944a77abf545457fe2b1f1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-03-29 12:50:15 +0900

    [Silver V] Title: Tic-Tac-Toe-Tomek (Small), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12335. Tic－Tac－Toe－Tomek （Small）/Tic－Tac－Toe－Tomek （Small）.py
+++ 백준/Silver/12335. Tic－Tac－Toe－Tomek （Small）/Tic－Tac－Toe－Tomek （Small）.py
@@ -1,5 +1,3 @@
-import sys
-
 def check(board):
     a, b, c = [board, zip(*board), [[board[i][i] for i in range(4)], [board[i][3 - i] for i in range(4)]]]
     for d in (a, b, c):
@@ -24,7 +22,7 @@ def check(board):
 
 results = ["X won", "O won", "Draw", "Game has not completed"]
 ans = []
-lines = sys.stdin.readlines()
+lines = open(0).readlines()
 n = len(lines)
 for idx, board in enumerate((lines[5 * i : 5 * (i + 1)] for i in range(n // 5)), 1):
     ans.append(f"Case #{idx}: {results[check(board[1:])]}")
