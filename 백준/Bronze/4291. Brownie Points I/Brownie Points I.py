e7a4a67f047dc2f883ac998cc52e709cebe0d325
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-01 16:01:48 +0900

    [Bronze I] Title: Brownie Points I, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4291. Brownie Points I/Brownie Points I.py
+++ 백준/Bronze/4291. Brownie Points I/Brownie Points I.py
@@ -5,13 +5,13 @@ ans = []
 while n := int(input()):
     li = [[*map(int, input().split())] for _ in range(n)]
     x0, y0 = li[n // 2]
-    stan, ollie = 0, 0
+    s = [0, 0]
     for x, y in li:
         if x == x0 or y == y0:
             continue
         if (x > x0 and y > y0) or (x < x0 and y < y0):
-            stan += 1
+            s[0] += 1
         else:
-            ollie += 1
-    ans.append(f"{stan} {ollie}")
+            s[1] += 1
+    ans.append(" ".join(map(str, s)))
 print("\n".join(ans))
