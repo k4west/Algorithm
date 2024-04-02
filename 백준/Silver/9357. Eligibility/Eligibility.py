6953aef9caf1aca73a4da7b15f4e0177619445fa
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-02 17:00:51 +0900

    [Silver IV] Title: Eligibility, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9357. Eligibility/Eligibility.py
+++ 백준/Silver/9357. Eligibility/Eligibility.py
@@ -1,18 +1,17 @@
+import sys
+input = sys.stdin.readline
+ans = []
 for t in range(int(input())):
     d = {}
     contestants = []
     for _ in range(int(input())):
-        name_year = input()
+        name_year = input().rstrip()
         name = name_year[:-5]
         year = name_year[-4:]
-
-        if name in d:
-            d[name].add(year)
-        else:
-            d[name] = {year}
+        if name in d: d[name].add(year)
+        else: d[name] = {year}
     for name, years in d.items():
-        if len(years) < 5:
-            contestants.append(name)
-    print(f"Case #{t+1}:")
-    for name in sorted(contestants):
-        print(name)
+        if len(years) < 5: contestants.append(name)
+    tmp = [f"Case #{t+1}:"] + sorted(contestants)
+    ans.append("\n".join(tmp))
+print("\n".join(ans))
