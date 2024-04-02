51b4430d2c07a8791305ca1d0dcb38e0fd176976
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-02 11:40:12 +0900

    [Silver V] Title: Bukmacherzy, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/8827. Bukmacherzy/Bukmacherzy.py
+++ 백준/Silver/8827. Bukmacherzy/Bukmacherzy.py
@@ -1,6 +1,3 @@
-ans = []
-tmp = ["NIE", "TAK"]
 for _ in range(int(input())):
     X, Y = map(float, input().split())
-    ans.append(tmp[(X - 1) * (Y - 1) > 1])
-print("\n".join(ans))
+    print(["NIE", "TAK"][(X - 1) * (Y - 1) > 1])
