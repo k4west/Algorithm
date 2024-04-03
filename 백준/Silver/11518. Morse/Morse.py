3434ec8b91a9d09a7d24c4778683bec8dd29896e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-03 09:38:02 +0900

    [Silver V] Title: Morse, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11518. Morse/Morse.py
+++ 백준/Silver/11518. Morse/Morse.py
@@ -1,18 +1,14 @@
 import sys
-
 input = sys.stdin.readline
-alpha2code = {}
-code2alpha = {}
+a2c = {}
 for _ in range(26):
-    alpha, code = input().rstrip().split()
-    alpha2code[alpha] = code
-    code2alpha[code] = alpha
-dictionary = {}
+    a, c = input().rstrip().split()
+    a2c[a] = c
+d = {}
 for _ in range(int(input())):
     word = input().rstrip()
-    morse = "".join(alpha2code[w] for w in word)
-    dictionary[morse] = word
-
+    m = "".join(a2c[w] for w in word)
+    d[m] = word
 ans = []
 while n := int(input()):
     tmp = []
@@ -21,10 +17,10 @@ while n := int(input()):
         if not flag:
             input()
             continue
-        if (morse := input().rstrip()) in dictionary:
-            tmp.append(dictionary[morse])
+        if (m := input().rstrip()) in d:
+            tmp.append(d[m])
         else:
-            tmp = [morse, "not in dictionary."]
+            tmp = [m, "not in dictionary."]
             flag = False
     ans.append(" ".join(tmp))
 print("\n".join(ans))
