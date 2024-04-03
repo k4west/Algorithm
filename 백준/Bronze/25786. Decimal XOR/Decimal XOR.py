bbd37a8892ca2a5e851e2b868493846c6694dfa1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-03 11:01:19 +0900

    [Bronze III] Title: Decimal XOR, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25786. Decimal XOR/Decimal XOR.py
+++ 백준/Bronze/25786. Decimal XOR/Decimal XOR.py
@@ -1,2 +1,3 @@
-n = max(len(a:=input()), len(b:=input()))
-print("".join(['0' if (x<=2 and y<=2) or (x>=7 and y>=7) else '9' for x, y in map(lambda x: map(int, x), zip(a.zfill(n), b.zfill(n)))]))
+a = input()
+b = input()
+print("".join('0' if (x<=2 and y<=2) or (x>=7 and y>=7) else '9' for x, y in map(lambda x: map(int, x), zip(a.zfill(len(b)), b.zfill(len(a))))))
