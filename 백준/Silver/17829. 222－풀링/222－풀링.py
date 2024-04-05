d4a72faadfb7049de776c27658aaa7e07a940fee
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-05 11:36:57 +0900

    [Silver II] Title: 222-풀링, Time: 492 ms, Memory: 75180 KB -BaekjoonHub

--- 백준/Silver/17829. 222－풀링/222－풀링.py
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -1,21 +1,22 @@
 import sys
 
-
 def pooling(board, n):
     new = []
     for i in range(n):
         a, b = 2 * i, 2 * i + 1
         tmp = []
         for j in range(n):
-            t = sorted(board[a][2 * j : 2 * (j + 1)] + board[b][2 * j : 2 * (j + 1)])[2] 
-            tmp.append(t)
+            t = board[a][2 * j : 2 * (j + 1)] + board[b][2 * j : 2 * (j + 1)]
+            t.sort()
+            tmp.append(t[2])
         new.append(tmp)
     return new
 
-
 input = sys.stdin.readline
 n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
+board = []
+for _ in range(n):
+    board.append([*map(int, input().split())])
 
 while n > 1:
     n //= 2
