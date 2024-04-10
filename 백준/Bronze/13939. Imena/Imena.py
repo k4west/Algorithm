c30198389436be2fef453e4563c898ca44d7d852
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-10 14:32:55 +0900

    [Bronze II] Title: Imena, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13939. Imena/Imena.py
+++ 백준/Bronze/13939. Imena/Imena.py
@@ -1,10 +1,10 @@
 input()
-for s in input().replace("?", ".").replace("!", ".").split("."):
-    if s:
-        s = s.replace(".", "")
-        cnt = 0
-        for t in s.split():
-            t = t.strip()
-            if t.isalpha() and t[0] == t[0].upper() and t[1:] == t[1:].lower():
-                cnt += 1
-        print(cnt)
+ans = []
+for s in input().replace("?", ".").replace("!", ".").split(".")[:-1]:
+    s = s.replace(".", "")
+    cnt = 0
+    for t in s.split():
+        t = t.strip()
+        if t.isalpha() and t[0] == t[0].upper() and t[1:] == t[1:].lower():
+            cnt += 1
+    print(cnt)
