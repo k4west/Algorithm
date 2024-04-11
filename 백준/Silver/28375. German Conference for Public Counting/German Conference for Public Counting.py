06a49ddfffdb9a66d8019442ebdc3839dac39cb7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-11 17:53:38 +0900

    [Silver III] Title: German Conference for Public Counting, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28375. German Conference for Public Counting/German Conference for Public Counting.py
+++ 백준/Silver/28375. German Conference for Public Counting/German Conference for Public Counting.py
@@ -1,7 +1,4 @@
-l = len(n := input())
-a = (l - 1) * 10 + int(t := n[0])
-if n < t * l:
-    a -= 1
-if l == 1:
-    a += 1
+a=((l:=len(n:=input()))-1)*10+int(t:=n[0])
+if n<t*l:a-=1
+if l==1:a+=1
 print(a)
