6891accec2889deece7496ced23f5231953d03cc
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-11 15:32:03 +0900

    [Bronze II] Title: nmマス計算, Time: 788 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/22361. nmマス計算/nmマス計算.py
+++ 백준/Bronze/22361. nmマス計算/nmマス計算.py
@@ -1,19 +1,18 @@
 import sys
-
-input = sys.stdin.readline
-ans = []
-
-while True:
-    n, m = map(int, input().split())
-    if n == 0:
-        break
-    a = map(int, input().split())
-    b = list(map(int, input().split()))
-    c = [0] * 10
-    for i in a:
-        for j in b:
-            for k in map(int, str(i * j)):
-                c[k] += 1
-    ans.append(" ".join(map(str, c)))
-
-print("\n".join(ans))
+def main():
+    input = sys.stdin.readline
+    ans = []
+    while True:
+        n, m = map(int, input().split())
+        if n == 0:
+            break
+        a = map(int, input().split())
+        b = list(map(int, input().split()))
+        c = [0] * 10
+        for i in a:
+            for j in b:
+                for k in map(int, str(i * j)):
+                    c[k] += 1
+        ans.append(" ".join(map(str, c)))
+    print("\n".join(ans))
+main()
