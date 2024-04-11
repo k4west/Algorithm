505f33c011c2c596a283c126aa5158f9394bdbab
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-11 12:46:48 +0900

    [Bronze I] Title: 피타고라스 기댓값, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11070. 피타고라스 기댓값/피타고라스 기댓값.py
+++ 백준/Bronze/11070. 피타고라스 기댓값/피타고라스 기댓값.py
@@ -5,7 +5,6 @@ ans = []
 for _ in range(int(input())):
     n, m = map(int, input().split())
     scores = [[0, 0] for _ in range(n + 1)]
-    teams = [0] * (n + 1)
     for _ in range(m):
         a, b, p, q = map(int, input().split())
         scores[a][0] += p
@@ -13,6 +12,12 @@ for _ in range(int(input())):
         scores[b][0] += q
         scores[b][1] += p
     ws = [score[0] ** 2 / (score[0] ** 2 + score[1] ** 2) if score != [0, 0] else 0 for score in scores[1:]]
-    ans.append(int(1000*max(ws)))
-    ans.append(int(1000*min(ws)))
+    ma, mi = 0, 1
+    for w in ws:
+        if ma < w:
+            ma = w
+        if mi > w:
+            mi = w
+    ans.append(int(1000*ma))
+    ans.append(int(1000*mi))
 print(*ans, sep='\n')
