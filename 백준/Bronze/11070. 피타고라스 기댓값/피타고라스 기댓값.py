f530ae121a63fb06bdeae7849c2f1eec06cfcab9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-11 12:50:54 +0900

    [Bronze I] Title: 피타고라스 기댓값, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11070. 피타고라스 기댓값/피타고라스 기댓값.py
+++ 백준/Bronze/11070. 피타고라스 기댓값/피타고라스 기댓값.py
@@ -1,23 +1,28 @@
 import sys
 
-input = sys.stdin.readline
-ans = []
-for _ in range(int(input())):
-    n, m = map(int, input().split())
-    scores = [[0, 0] for _ in range(n + 1)]
-    for _ in range(m):
-        a, b, p, q = map(int, input().split())
-        scores[a][0] += p
-        scores[a][1] += q
-        scores[b][0] += q
-        scores[b][1] += p
-    ws = [score[0] ** 2 / (score[0] ** 2 + score[1] ** 2) if score != [0, 0] else 0 for score in scores[1:]]
-    ma, mi = 0, 1
-    for w in ws:
-        if ma < w:
-            ma = w
-        if mi > w:
-            mi = w
-    ans.append(int(1000*ma))
-    ans.append(int(1000*mi))
-print(*ans, sep='\n')
+
+def main():
+    input = sys.stdin.readline
+    ans = []
+    for _ in range(int(input())):
+        n, m = map(int, input().split())
+        ss = [[0, 0] for _ in range(n + 1)]
+        for _ in range(m):
+            a, b, p, q = map(int, input().split())
+            ss[a][0] += p
+            ss[a][1] += q
+            ss[b][0] += q
+            ss[b][1] += p
+        W = [s[0] ** 2 / (s[0] ** 2 + s[1] ** 2) if s != [0, 0] else 0 for s in ss[1:]]
+        ma, mi = 0, 1
+        for w in W:
+            if ma < w:
+                ma = w
+            if mi > w:
+                mi = w
+        ans.append(int(1000 * ma))
+        ans.append(int(1000 * mi))
+    print(*ans, sep="\n")
+
+
+main()
