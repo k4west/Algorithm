151a66621f2cf8b86049f47d3b58fc756fcd7bac
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-12 13:40:20 +0900

    [Bronze II] Title: Make America Grade Again, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17850. Make America Grade Again/Make America Grade Again.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+    *per, n = map(int, input().split())
+    d = {"Lab": [0, 0], "Hw": [0, 0], "Proj": [0, 0], "Exam": [0, 0]}
+    for _ in range(n):
+        assign, _, score = input().split()
+        p, q = map(int, score.split("/"))
+        d[assign][0] += p
+        d[assign][1] += q
+    print(int(sum(pp * p / q for pp, (p, q) in zip(per, d.values()))))
+
+if __name__ == "__main__":
+    main()
