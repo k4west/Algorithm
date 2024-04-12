0ccf6a400fa9e4d1cd4582ec8932119e33852847
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-12 12:54:01 +0900

    [Bronze II] Title: Number Game, Time: 96 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Bronze/1975. Number Game/Number Game.py
+++ 백준/Bronze/1975. Number Game/Number Game.py
@@ -1,19 +1,19 @@
 import sys
 
 def main():
-    ans = []
+    ans=[]
+    d={}
     for n in sys.stdin.readlines()[1:]:
         if not (n:=int(n)) in d:
-            cnt = 0
-            for b in range(2, n + 2):
-                s = n
-                while not s % b:
-                    cnt += 1
-                    s //= b
-            d[n] = str(cnt)
+            cnt=0
+            for b in range(2, n+1):
+                s=n
+                while not s%b:
+                    cnt+=1
+                    s//=b
+            d[n]=str(cnt)
         ans.append(d[n])
     print("\n".join(ans))
-
-if __name__ == "__main__":
-    d = {}
+    
+if __name__=="__main__":
     main()
