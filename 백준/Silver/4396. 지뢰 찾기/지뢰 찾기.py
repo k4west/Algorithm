473e4613810536beb4557cba6a2b366aa62e1a07
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 17:10:03 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
@@ -3,24 +3,22 @@ input = sys.stdin.readline
 
 n = int(input())
 d = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-
-bombs = [input().rstrip() for _ in range(n)]
-board = [list(input().rstrip()) for _ in range(n)]
+board = [list(input().rstrip()) for _ in range(2*n)]
 flag = False
 for i in range(n):
     for j in range(n):
-        if board[i][j] == 'x':
-            if bombs[i][j] == '*':
+        if board[i+n][j] == 'x':
+            if board[i][j] == '*':
                 flag = True
             cnt = 0
             for di, dj in d:
-                if 0<=(ni:=i+di)<n and 0<=(nj:=j+dj)<n and bombs[ni][nj]=='*':
+                if 0<=(ni:=i+di)<n and 0<=(nj:=j+dj)<n and board[ni][nj]=='*':
                     cnt += 1
-            board[i][j] = str(cnt)
+            board[i+n][j] = str(cnt)
 if flag:
     for i in range(n):
         for j in range(n):
-            if bombs[i][j] == '*':
-                board[i][j] = '*'
-for line in board:
+            if board[i][j] == '*':
+                board[i+n][j] = '*'
+for line in board[n:]:
     print("".join(line))
