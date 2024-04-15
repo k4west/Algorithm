736e488b5fc1318c1d864b8216e87757d82d6d72
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 16:06:21 +0900

    [Silver V] Title: 팰린드롬, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/8892. 팰린드롬/팰린드롬.py
+++ 백준/Silver/8892. 팰린드롬/팰린드롬.py
@@ -1,27 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+def check():
+    k = int(input())
+    words = [input().rstrip() for _ in range(k)]
+    for i in range(k):
+        a = words[i]
+        for j in range(k):
+            if i == j:
+                continue
+            b = words[j]
+            s = a+b
+            if s == s[::-1]:
+                return s
+    return '0'
+
 def main():
-    ans = []
-    for _ in range(int(input())):
-        k = int(input())
-        words = [input().rstrip() for _ in range(k)]
-        flag = False
-        for i in range(k):
-            a = words[i]
-            for j in range(k):
-                if i == j:
-                    continue
-                b = words[j]
-                s = a+b
-                if s == s[::-1]:
-                    ans.append(s)
-                    flag = True
-                    break
-            if flag:
-                break
-        if not flag:
-            ans.append('0')
-    print("\n".join(ans))
+    print("\n".join([check() for _ in range(int(input()))]))
 
 main()
