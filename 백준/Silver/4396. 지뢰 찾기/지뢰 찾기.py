298a58fad2999f944623afa693065266b7fe01ef
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 17:19:21 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
@@ -1,22 +1,25 @@
 d=((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1))
-n, *board = open(0).read().split()
-n = int(n)
-board = list(map(list, board))
-flag=False
+n,*b=open(0).read().split()
+n=int(n)
+b=list(map(list,b))
+f=0
 for i in range(n):
     for j in range(n):
-        if board[i+n][j]=='x':
-            if board[i][j]=='*':
-                flag=True
+        if b[i+n][j]=='x':
+            if b[i][j]=='*':
+                f=1
             cnt=0
             for di,dj in d:
-                if 0<=(ni:=i+di)<n and 0<=(nj:=j+dj)<n and board[ni][nj]=='*':
-                    cnt += 1
-            board[i+n][j]=str(cnt)
-if flag:
+                ni=i+di
+                nj=j+dj
+                if 0<=ni<n and 0<=nj<n:
+                    if b[ni][nj]=='*':
+                        cnt += 1
+            b[i+n][j]=str(cnt)
+if f:
     for i in range(n):
         for j in range(n):
-            if board[i][j]=='*':
-                board[i+n][j]='*'
-for line in board[n:]:
-    print("".join(line))
+            if b[i][j]=='*':
+                b[i+n][j]='*'
+for l in b[n:]:
+    print("".join(l))
