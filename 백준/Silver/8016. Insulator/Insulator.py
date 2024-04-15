d7fa3555b4dca3a26ec2478fc45a432df4853f4a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 10:03:44 +0900

    [Silver V] Title: Insulator, Time: 80 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/8016. Insulator/Insulator.py
+++ 백준/Silver/8016. Insulator/Insulator.py
@@ -1,9 +1,6 @@
-import sys
-
 def main():
-    input = sys.stdin.readline
-    n = int(input())
-    arr = sorted([int(input()) for _ in range(n)])
+    n, *arr = map(int, open(0).read().split())
+    arr.sort()
     s = sum(arr) + sum(arr[(n+1)//2:]) - sum(arr[:n//2])
     print(s)
 
