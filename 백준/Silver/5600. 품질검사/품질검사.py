ee00064f374303c67c7b2780ed48064f4c0158f3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 12:38:25 +0900

    [Silver II] Title: 품질검사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5600. 품질검사/품질검사.py
+++ 백준/Silver/5600. 품질검사/품질검사.py
@@ -3,9 +3,9 @@ ans = [2]*(a+b+c+1)
 test_0 = []
 for s in range(n):
     i, j, k, r = test[s*4:s*4+4]
-    if r: ans[i] = ans[j] = ans[k] = 1
+    if r: ans[i]=ans[j]=ans[k]=1
     else: test_0.append((i, j, k))
 for parts in test_0:
     for s in range(3):
-        if ans[parts[s-1]] * ans[parts[(s+1)%3]] == 1: ans[parts[s]] = 0
+        if ans[parts[s-1]]*ans[parts[(s+1)%3]]==1: ans[parts[s]]=0
 print(*ans[1:], sep="\n")
