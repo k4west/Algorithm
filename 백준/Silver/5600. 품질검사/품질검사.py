478fc931a048b5fdd86533d4668c99c571f5b016
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-15 12:36:24 +0900

    [Silver II] Title: 품질검사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5600. 품질검사/품질검사.py
+++ 백준/Silver/5600. 품질검사/품질검사.py
@@ -1,19 +1,16 @@
-import sys
-
-input = sys.stdin.readline
-a, b, c = map(int, input().split())
+a, b, c, n, *test = open(0).read().split()
+a, b, c = map(int, (a, b, c))
 ans = [2]*(a+b+c+1)
 test_0 = []
-for _ in range(int(input())):
-    *parts, r = map(int, input().split())
+for s in range(int(n)):
+    i, j, k, r = map(int, test[s*4:s*4+4])
     if r:
-        for part in parts:
-            ans[part] = 1
+        ans[i] = ans[j] = ans[k] = 1
     else:
-        test_0.append(parts)
+        test_0.append((i, j, k))
 for parts in test_0:
-    for i in range(3):
-        if ans[parts[i - 1]] * ans[parts[(i+1) % 3]] == 1:
-            ans[parts[i]] = 0
+    for s in range(3):
+        if ans[parts[s-1]] * ans[parts[(s+1) % 3]] == 1:
+            ans[parts[s]] = 0
 
 print(*ans[1:], sep="\n")
