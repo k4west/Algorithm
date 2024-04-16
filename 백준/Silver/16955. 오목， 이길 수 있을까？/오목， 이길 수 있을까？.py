1fb8720be4943c2fd2a7b83d49b4a5f07dcaa478
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-16 14:29:38 +0900

    [Silver III] Title: 오목, 이길 수 있을까?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
@@ -1,16 +1,15 @@
 def main():
-    d = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-    board = [s for s in open(0)]
-    for i in range(10):
-        for j in range(10):
-            if board[i][j] == "X":
-                for di, dj in d:
-                    cnt, flag, ti, tj = 1, True, i, j
-                    while 0 <= (ni := ti + di) < 10 and 0 <= (nj := tj + dj) < 10:
-                        if board[ni][nj] == "X": cnt += 1
-                        elif board[ni][nj] == "." and flag: cnt += 1; flag = False
-                        else: break
-                        ti, tj = ni, nj
-                    if cnt == 5 or (flag and cnt == 4 and 0<=(ni:=i-di)<10 and 0<=(nj:=j-dj)<10 and board[ni][nj]=='.'): print(1); exit(0)
+    d = (-11, -10, -9, -1, 1, 9, 10, 11)
+    board = "".join(s.rstrip() for s in open(0))
+    for i in range(100):
+        if board[i] == "X":
+            for di in d:
+                cnt, flag, ti, ni = 1, True, i, i+di
+                while 0 <= ni < 100 and abs(ti//10 - ni//10) < 2 and abs(ti%10 - ni%10) < 2:
+                    if board[ni] == "X": cnt += 1
+                    elif board[ni] == "." and flag: cnt += 1; flag = False
+                    else: break
+                    ti, ni = ni, ni+di
+                if cnt == 5 or (flag and cnt == 4 and 0<=(ni:=i-di)<10 and board[ni]=='.'): print(1); exit(0)
     print(0)
 main()
