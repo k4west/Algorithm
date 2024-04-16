2fe95e7dd747a329a4656a59f2d417841af38563
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-16 14:13:44 +0900

    [Silver III] Title: 오목, 이길 수 있을까?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
@@ -5,20 +5,12 @@ def main():
         for j in range(10):
             if board[i][j] == "X":
                 for di, dj in d:
-                    cnt = 1
-                    flag = True
-                    ti, tj = i, j
+                    cnt, flag, ti, tj = 1, True, i, j
                     while 0 <= (ni := ti + di) < 10 and 0 <= (nj := tj + dj) < 10:
-                        if board[ni][nj] == "X":
-                            cnt += 1
-                        elif board[ni][nj] == "." and flag:
-                            cnt += 1
-                            flag = False
-                        else:
-                            break
+                        if board[ni][nj] == "X": cnt += 1
+                        elif board[ni][nj] == "." and flag: cnt += 1; flag = False
+                        else: break
                         ti, tj = ni, nj
-                    if cnt == 5 or (flag and cnt == 4 and 0<=(ni:=i-di)<10 and 0<=(nj:=j-dj)<10 and board[ni][nj]=='.'):
-                        print(1)
-                        exit(0)
+                    if cnt == 5 or (flag and cnt == 4 and 0<=(ni:=i-di)<10 and 0<=(nj:=j-dj)<10 and board[ni][nj]=='.'): print(1); exit(0)
     print(0)
 main()
