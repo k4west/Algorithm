d1994cfb7b332436579aabefbf819800de0400bb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-18 18:18:02 +0900

    [Silver IV] Title: Klingon Levels, Time: 3768 ms, Memory: 109972 KB -BaekjoonHub

--- 백준/Silver/5716. Klingon Levels/Klingon Levels.py
+++ 백준/Silver/5716. Klingon Levels/Klingon Levels.py
@@ -1,10 +1,9 @@
 import sys
-INF = float('inf')
+
 def main():
     input = sys.stdin.readline
     ans = []
     while N:=int(input()):
-        cnt = INF
         tmp = [[0]*1001 for _ in range(N)]
         for i in range(N):
             input()
@@ -12,9 +11,7 @@ def main():
                 tmp[i][k] += 1
             for k in range(1000):
                 tmp[i][k+1] += tmp[i][k]
-        for j in range(1001):
-            if cnt > (t:=sum(abs(tmp[i][1000] - 2*tmp[i][j]) for i in range(N))):
-                cnt = t
-        ans.append(cnt)
+        ans.append(min(sum(abs(tmp[i][1000] - 2*tmp[i][j]) for i in range(N)) for j in range(1001)))
     print("\n".join(map(str, ans)))
+
 main()
