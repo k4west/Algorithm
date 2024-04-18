37d05ef08382aa58d04adb64b09bdc287dcae2a3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-18 17:48:31 +0900

    [Bronze II] Title: Prerequisites?, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4285. Prerequisites？/Prerequisites？.py
+++ 백준/Bronze/4285. Prerequisites？/Prerequisites？.py
@@ -1,15 +1,18 @@
 import sys
-input = sys.stdin.readline
-ans = []
-while True:
-    k, *m = list(map(int, input().split()))
-    if k == 0:
-        break
-    chosen = set(map(int, input().split()))
-    flag = True
-    for _ in range(*m):
-        _, r, *courses = list(map(int, input().split()))
-        if flag and sum(course in chosen for course in courses) < r:
-            flag = False
-    ans.append("yes" if flag else "no")
-print("\n".join(ans))
+def main():
+    input = sys.stdin.readline
+    ans = []
+    while True:
+        km = input().rstrip()
+        if km == '0':
+            break
+        _, m = map(int, km.split())
+        chosen = set(input().split())
+        flag = True
+        for _ in range(m):
+            _, r, *courses = input().split()
+            if flag and sum(course in chosen for course in courses) < int(r):
+                flag = False
+        ans.append("yes" if flag else "no")
+    print("\n".join(ans))
+main()
