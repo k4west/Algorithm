fd620b3fad0eedf616fd048d01df8eeb5fac3402
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-24 18:10:18 +0900

    [Silver V] Title: Get a Rectangular Field, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/22820. Get a Rectangular Field/Get a Rectangular Field.py
+++ 백준/Silver/22820. Get a Rectangular Field/Get a Rectangular Field.py
@@ -1,22 +1,18 @@
 import sys
+
 def main():
-    d = ((1, 0), (0, 1))
-    n, *list = sys.stdin.readlines()
+    n, *li = sys.stdin.readlines()
     ans = []
     for i in range(int(n)):
-        maps = [line.rstrip().split() for line in list[i * 6 : (i + 1) * 6]]
+        maps = [list(map(int, line.rstrip().split())) for line in li[i*6:(i+1)*6]]
         m = 0
         for j in range(5):
             for k in range(5):
                 for s in range(1, 6 - j):
                     for t in range(1, 6 - k):
-                        if all(
-                            maps[p][q] == "1"
-                            for p in range(j, j + s)
-                            for q in range(k, k + t)
-                        ):
-                            if m < s * t:
-                                m = s * t
+                        if all(maps[p][q] for p in range(j, j+s) for q in range(k, k+t)):
+                            if m < s*t:
+                                m = s*t
         ans.append(m)
-    print(*ans, sep="\n")
+    print("\n".join(map(str, ans)))
 main()
