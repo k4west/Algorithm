77a5d2b7c2434d3e230d696c43054011f66850ab
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-25 17:12:26 +0900

    [Silver IV] Title: Un-bear-able Zoo, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/24771. Un－bear－able Zoo/Un－bear－able Zoo.py
+++ 백준/Silver/24771. Un－bear－able Zoo/Un－bear－able Zoo.py
@@ -1,16 +1,12 @@
 import sys
-def main():
-    input = sys.stdin.readline
-    ans = []
-    i = 1
-    while n:=int(input()):
-        animals = {}
-        for _ in range(n):
-            animal = input().split()[-1].rstrip().lower()
-            animals[animal] = animals.get(animal, 0) + 1
-        animals = [f"{animal} | {num}" for animal, num in animals.items()]
-        animals.sort()
-        ans.append(f"List {i}:\n"+"\n".join(animals))
-        i += 1
-    print(*ans, sep="\n")
-main()
+input = sys.stdin.readline
+ans = []
+i = 1
+while n:=int(input()):
+    animals = {}
+    for _ in range(n):
+        animal = input().split()[-1].rstrip().lower()
+        animals[animal] = animals.get(animal, 0) + 1
+    ans.append(f"List {i}:\n"+"\n".join(sorted([f"{animal} | {num}" for animal, num in animals.items()])))
+    i += 1
+print(*ans, sep="\n")
