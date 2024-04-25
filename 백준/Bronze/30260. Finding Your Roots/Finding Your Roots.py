56bc36d8d0fc7b3dc43eb208b44eb15c6fb4b15e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-25 16:57:08 +0900

    [Bronze III] Title: Finding Your Roots, Time: 76 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Bronze/30260. Finding Your Roots/Finding Your Roots.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+ans = []
+for i in range(int(input())):
+    L, n = map(int, input().split())
+    roots = [0] + list(map(int, input().split()))
+    m = 0
+    while L > 0:
+        L = roots[L]
+        m += 1
+
+    ans.append(f"Data Set {i+1}:\n{m}\n")
+print(*ans, sep="\n")
