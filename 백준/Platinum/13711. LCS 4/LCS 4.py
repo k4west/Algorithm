439a60047dcfeac122353fc5ef2483a803e2ff34
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-26 17:54:01 +0900

    [Platinum V] Title: LCS 4, Time: 184 ms, Memory: 47020 KB -BaekjoonHub

--- 백준/Platinum/13711. LCS 4/LCS 4.py
+++ 백준/Platinum/13711. LCS 4/LCS 4.py
@@ -1,27 +1,29 @@
 import sys
-input = sys.stdin.readline
-n = int(input())
-n1 = list(map(int, input().split()))
-n2 = list(map(int, input().split()))
+def main():
+    input = sys.stdin.readline
+    n = int(input())
+    n1 = list(map(int, input().split()))
+    n2 = list(map(int, input().split()))
 
-n1_idx = [0] * (n)
-for i in range(n):
-    n1_idx[n1[i] - 1] = i # n1의 숫자 순서를 dp에 저장
-n2_idx_in_n1 = [n1_idx[n2[i] - 1] for i in range(n)] # n2의 숫자가 n1에서 몇번째 인덱스에 해당하는지 저장
+    n1_idx = [0] * (n+1)
+    for i in range(n):
+        n1_idx[n1[i]] = i+1 # n1의 숫자 순서를 dp에 저장
 
-ans = [n2_idx_in_n1[0]]
-for num in n2_idx_in_n1[1:]:
-    if ans[-1] < num:
-        ans.append(num)
-    else:
-        s = 0
-        e = len(ans)
-        while s < e:
-            m = (s + e) // 2
-            if ans[m] < num:
-                s = m + 1
-            else:
-                e = m
-        ans[s] = num
+    ans = [n1_idx[n2[0]]]
+    for num in n2[1:]:
+        idx = n1_idx[num]
+        if ans[-1] < idx:
+            ans.append(idx)
+        else:
+            s = 0
+            e = len(ans)
+            while s < e:
+                m = (s + e) // 2
+                if ans[m] < idx:
+                    s = m + 1
+                else:
+                    e = m
+            ans[s] = idx
 
-print(len(ans))
+    print(len(ans))
+main()
