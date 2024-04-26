dd9abc98732d756d2bd991cbbf986f32457f32c6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-26 17:56:25 +0900

    [Platinum V] Title: LCS 4, Time: 180 ms, Memory: 46324 KB -BaekjoonHub

--- 백준/Platinum/13711. LCS 4/LCS 4.py
+++ 백준/Platinum/13711. LCS 4/LCS 4.py
@@ -3,14 +3,14 @@ def main():
     input = sys.stdin.readline
     n = int(input())
     n1 = list(map(int, input().split()))
-    n2 = list(map(int, input().split()))
+    n2 = map(int, input().split())
 
     n1_idx = [0] * (n+1)
     for i in range(n):
-        n1_idx[n1[i]] = i+1 # n1의 숫자 순서를 dp에 저장
+        n1_idx[n1[i]] = i+1
 
-    ans = [n1_idx[n2[0]]]
-    for num in n2[1:]:
+    ans = [n1_idx[next(n2)]]
+    for num in n2:
         idx = n1_idx[num]
         if ans[-1] < idx:
             ans.append(idx)
