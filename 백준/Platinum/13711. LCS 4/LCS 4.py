c9a14286b12243a2af45ecf33fecf7d90f79c328
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-26 18:01:55 +0900

    [Platinum V] Title: LCS 4, Time: 148 ms, Memory: 44680 KB -BaekjoonHub

--- 백준/Platinum/13711. LCS 4/LCS 4.py
+++ 백준/Platinum/13711. LCS 4/LCS 4.py
@@ -1,29 +1,18 @@
 import sys
+from bisect import bisect_left
 def main():
     input = sys.stdin.readline
     n = int(input())
-    n1 = list(map(int, input().split()))
+    n1_idx = [0] * n
+    for i, n in enumerate(map(int, input().split())):
+        n1_idx[n-1] = i
     n2 = map(int, input().split())
-
-    n1_idx = [0] * (n+1)
-    for i in range(n):
-        n1_idx[n1[i]] = i+1
-
-    ans = [n1_idx[next(n2)]]
+    ans = [n1_idx[next(n2)-1]]
     for num in n2:
-        idx = n1_idx[num]
+        idx = n1_idx[num-1]
         if ans[-1] < idx:
             ans.append(idx)
         else:
-            s = 0
-            e = len(ans)
-            while s < e:
-                m = (s + e) // 2
-                if ans[m] < idx:
-                    s = m + 1
-                else:
-                    e = m
-            ans[s] = idx
-
+            ans[bisect_left(ans, idx)] = idx
     print(len(ans))
 main()
