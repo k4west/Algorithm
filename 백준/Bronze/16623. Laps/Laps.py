cf9dc070c38e7b0021a3ec2a390b3bb7501bbabb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-29 13:09:38 +0900

    [Bronze II] Title: Laps, Time: 72 ms, Memory: 39012 KB -BaekjoonHub

--- 백준/Bronze/16623. Laps/Laps.py
+++ 백준/Bronze/16623. Laps/Laps.py
@@ -1,9 +1,12 @@
 import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-c = prev = 0
-for d in map(int, input().split()):
-    if prev > d:
-        c += 1
-    prev = d
-print(c)
+def main():
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    c = 0
+    prev = 0
+    for d in map(int, input().split()):
+        if prev > d:
+            c += 1
+        prev = d
+    print(c)
+main()
