67e0fa89e741926f309b51e443d3824add3f22ab
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-29 17:53:47 +0900

    [Silver III] Title: Soccer League, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5741. Soccer League/Soccer League.py
+++ 백준/Silver/5741. Soccer League/Soccer League.py
@@ -1,26 +1,19 @@
 import sys
-
 input = sys.stdin.readline
-
 ans = []
 while n := int(input()):
     d = {}
     for _ in range(n):
         A, a, _, b, B = input().rstrip().split()
-        if (a := int(a)) < (b := int(b)):
-            A, a, b, B = B, b, a, A
-        if not A in d:
-            d[A] = [0, 0]
-        if not B in d:
-            d[B] = [0, 0]
-        if a != b:
-            c = a - b
-            d[A][0] += 3
-            d[A][1] += c
-            d[B][1] -= c
-        else:
-            d[A][0] += 1
-            d[B][0] += 1
+        pa = pb = 1
+        if (a := int(a)) < (b := int(b)): A, a, b, B = B, b, a, A
+        if not A in d: d[A] = [0, 0]
+        if not B in d: d[B] = [0, 0]
+        if c := a - b: pa, pb = 3, 0
+        d[A][0] += pa
+        d[B][0] += pb
+        d[A][1] += c
+        d[B][1] -= c
     li = [[points, goal, name] for name, [points, goal] in d.items()]
     li.sort(key=lambda x: [-x[0], -x[1]])
     ans.append("\n".join(f"{x[0]} {x[2]}" for x in li))
