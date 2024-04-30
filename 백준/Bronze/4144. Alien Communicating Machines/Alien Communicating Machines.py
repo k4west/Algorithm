796494789b7af515b8863c902d07d8d254ba64ee
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-04-30 10:39:20 +0900

    [Bronze I] Title: Alien Communicating Machines, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4144. Alien Communicating Machines/Alien Communicating Machines.py
+++ 백준/Bronze/4144. Alien Communicating Machines/Alien Communicating Machines.py
@@ -1,13 +1,10 @@
-import sys
-input = sys.stdin.readline
 d = {i: str(i) for i in range(10)}
-for i in range(65, 91):
-    d[i - 55] = chr(i)
-for _ in range(int(input())):
-    x, y, z = input().split()
+for i in range(65, 91): d[i - 55] = chr(i)
+a = open(0)
+for _ in range(int(next(a))):
+    x, y, z = next(a).split()
     z = int(z, int(x))
-    if not z:
-        print(0)
+    if not z: print(0)
     else:
         y = int(y)
         tmp = ""
