3e70f9cc0c10d233f7cb6fc04b0304bc657c7a15
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-02 22:34:33 +0900

    [Silver I] Title: A Rational Sequence, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11526. A Rational Sequence/A Rational Sequence.py
+++ 백준/Silver/11526. A Rational Sequence/A Rational Sequence.py
@@ -4,26 +4,19 @@ def main():
     for _ in range(int(next(a))):
         i, pq = next(a).rstrip().split()
         p, q = map(int, pq.split('/'))
-        line = 0
+        j = 1
         r = []
         while p != 1 or q != 1:
-            if p < q:
-                q -= p
-                r.append(False)
-            else:
-                p -= q
-                r.append(True)
-            line += 1
-        j = 1
-        if p-1:
-            r += [True]*(p-1)
-        if q-1:
-            r += [False]*(q-1)
-        for t in r[::-1]:
+            if p < q: q -= p; r.append(False)
+            else: p -= q; r.append(True)
             j *= 2
-            if not t:
-                j -= 1
-        n = 2**line - 1 + j
+        k = 1
+        if p-1: r += [True]*(p-1)
+        if q-1: r += [False]*(q-1)
+        for t in r[::-1]:
+            k *= 2
+            if not t: k -= 1
+        n = j - 1 + k
         ans.append(f"{i} {n}")
     print("\n".join(ans))
 main()
