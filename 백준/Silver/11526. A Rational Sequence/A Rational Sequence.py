d52526accac6c145e8575e0a57022db45c9dbc3a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-02 22:31:50 +0900

    [Silver I] Title: A Rational Sequence, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11526. A Rational Sequence/A Rational Sequence.py
+++ 백준/Silver/11526. A Rational Sequence/A Rational Sequence.py
@@ -1,27 +1,29 @@
 a = open(0)
-ans = []
-for _ in range(int(next(a))):
-    i, pq = next(a).rstrip().split()
-    p, q = map(int, pq.split('/'))
-    line = 0
-    r = []
-    while p != 1 or q != 1:
-        if p < q:
-            q -= p
-            r.append(False)
-        else:
-            p -= q
-            r.append(True)
-        line += 1
-    j = 1
-    if p-1:
-        r += [True]*(p-1)
-    if q-1:
-        r += [False]*(q-1)
-    for t in r[::-1]:
-        j *= 2
-        if not t:
-            j -= 1
-    n = 2**line - 1 + j
-    ans.append(f"{i} {n}")
-print("\n".join(ans))
+def main():
+    ans = []
+    for _ in range(int(next(a))):
+        i, pq = next(a).rstrip().split()
+        p, q = map(int, pq.split('/'))
+        line = 0
+        r = []
+        while p != 1 or q != 1:
+            if p < q:
+                q -= p
+                r.append(False)
+            else:
+                p -= q
+                r.append(True)
+            line += 1
+        j = 1
+        if p-1:
+            r += [True]*(p-1)
+        if q-1:
+            r += [False]*(q-1)
+        for t in r[::-1]:
+            j *= 2
+            if not t:
+                j -= 1
+        n = 2**line - 1 + j
+        ans.append(f"{i} {n}")
+    print("\n".join(ans))
+main()
