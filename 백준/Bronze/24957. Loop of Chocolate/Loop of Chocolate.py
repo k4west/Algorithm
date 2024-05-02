8b0c39f2d24b50115f5ee6b840ff15dc9f387144
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-02 21:40:52 +0900

    [Bronze I] Title: Loop of Chocolate, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24957. Loop of Chocolate/Loop of Chocolate.py
+++ 백준/Bronze/24957. Loop of Chocolate/Loop of Chocolate.py
@@ -1,14 +1,14 @@
-from math import pi
+pi=3.141592653589793
 a = open(0)
 n, r = map(int, next(a).split())
 s = 4*n*pi*r**3/3
 x0, y0, z0 = map(int, next(a).split())
 x1, y1, z1 = x0, y0, z0
-ds = []
 for _ in range(n-1):
     x, y, z = map(int, next(a).split())
     d = ((x-x0)**2+(y-y0)**2+(z-z0)**2)**.5
     s -= 2/3*pi*(r - d/2)**2*(2*r+d/2)
     x0, y0, z0 = x, y, z
 d = ((x1-x)**2+(y1-y)**2+(z1-z)**2)**.5
-print(s - 2/3*pi*(r - d/2)**2*(2*r+d/2))
+s -= 2/3*pi*(r - d/2)**2*(2*r+d/2)
+print(s)
