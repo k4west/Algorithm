46907ba7a8a566f36706fc7c5e420c9036a1cb94
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-03 22:23:04 +0900

    [Bronze II] Title: Letterle, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24747. Letterle/Letterle.py
+++ 백준/Bronze/24747. Letterle/Letterle.py
@@ -1,23 +1,17 @@
 a = open(0)
 letter = next(a).rstrip()
-ans = []
 flag = False
-for guess in a:
+for i, guess in enumerate(a):
     if letter == guess.rstrip():
         flag = True
         break
     else:
         tmp = ''
         for l, g in zip(letter, guess):
-            if l == g:
-                tmp += 'G'
-            elif g in letter:
-                tmp += 'Y'
-            else:
-                tmp += 'X'
-        ans.append(tmp)
+            if l == g: tmp += 'G'
+            elif g in letter: tmp += 'Y'
+            else: tmp += 'X'
+        if i < 6: print(tmp)
+        else: print('LOSER')
 if flag:
-    ans.append("WINNER")
-else:
-    ans[6] = 'LOSER'
-print("\n".join(ans))
+    print("WINNER")
