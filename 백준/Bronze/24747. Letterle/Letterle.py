28d28ecf1d050d009c2eb12d38b8d4207cae64ff
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-03 22:37:29 +0900

    [Bronze II] Title: Letterle, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24747. Letterle/Letterle.py
+++ 백준/Bronze/24747. Letterle/Letterle.py
@@ -1,11 +1,10 @@
-a = open(0)
-L = next(a).rstrip()
+L=next(a:=open(0)).strip()
 for i, G in enumerate(a):
-    t = ''
+    t=''
     for l, g in zip(L, G):
-        if l == g: t += 'G'
-        elif g in L: t += 'Y'
-        else: t += 'X'
-    if t == 'G'*5: print('WINNER'); break
-    elif i < 6: print(t)
+        if l==g: t+='G'
+        elif g in L: t+='Y'
+        else: t+='X'
+    if t=='G'*5: print('WINNER'); exit()
+    elif i<6: print(t)
     else: print('LOSER')
