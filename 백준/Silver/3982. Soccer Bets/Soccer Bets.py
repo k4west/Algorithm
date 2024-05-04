c3ceccdb66dead75f6e5d1b22d840d8894045db8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-04 12:13:24 +0900

    [Silver V] Title: Soccer Bets, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3982. Soccer Bets/Soccer Bets.py
+++ 백준/Silver/3982. Soccer Bets/Soccer Bets.py
@@ -1,12 +1,12 @@
 a = open(0)
 for _ in range(int(next(a))):
-    candi, dump = set(), set()
+    c, d = set(), set()
     for _ in range(16):
         t1, t2, g1, g2 = next(a).split()
         if int(g1) < int(g2):
             t1, t2 = t2, t1
-        if not t1 in dump:
-            candi.add(t1)
-        candi.discard(t2)
-        dump.add(t2)
-    print(*candi)
+        if not t1 in d:
+            c.add(t1)
+        c.discard(t2)
+        d.add(t2)
+    print(*c)
