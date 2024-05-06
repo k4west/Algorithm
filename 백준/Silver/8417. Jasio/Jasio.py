33c7142e14303efefd2f6ad7fb476d93991f9aea
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-06 19:33:54 +0900

    [Silver V] Title: Jasio, Time: 260 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/8417. Jasio/Jasio.py
+++ 백준/Silver/8417. Jasio/Jasio.py
@@ -1,19 +1,10 @@
 def is_palindrome(s):
-    n = len(s)
-    for i in range(n-1):
-        if s[i] == s[i+1] or (i+2 != n and s[i] == s[i+2]):
-            return True
+    for i in range((n:=len(s))-1): 
+        if s[i] == s[i+1] or (i+2 != n and s[i] == s[i+2]): return True
     return False
-
-def jasio(s):
-    return is_palindrome(s.replace('j', 'i').replace('b', 'p').replace('d', 'p'))
-
 p = q = 0
 for i in range(int(next(a:= open(0)))):
     s = next(a)
-    if is_palindrome(s):
-        p += 1
-    elif jasio(s):
-        q += 1
-
+    if is_palindrome(s): p += 1
+    elif is_palindrome(s.replace('j', 'i').replace('b', 'p').replace('d', 'p')): q += 1
 print(f'{p}\n{p+q}')
