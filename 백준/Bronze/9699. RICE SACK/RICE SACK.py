b66505fc65eb431bf0b41b5e3707485ce60c49e8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-06 19:11:52 +0900

    [Bronze IV] Title: RICE SACK, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9699. RICE SACK/RICE SACK.py
+++ 백준/Bronze/9699. RICE SACK/RICE SACK.py
@@ -1,8 +1,1 @@
-ans = []
-for i in range(int(next(a:= open(0)))):
-    m = 0
-    for s in map(int, next(a).split()):
-        if m < s:
-            m = s
-    ans.append(f'Case #{i+1}: {m}')
-print('\n'.join(ans))
+for i in range(int(next(a:= open(0)))): print(f'Case #{i+1}: {max(map(int, next(a).split()))}')
