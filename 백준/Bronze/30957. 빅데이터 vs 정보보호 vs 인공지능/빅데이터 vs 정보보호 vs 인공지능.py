41aaa1e6e0894eb149852ed661f98c292c1eef70
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-08 22:08:31 +0900

    [Bronze III] Title: 빅데이터 vs 정보보호 vs 인공지능, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30957. 빅데이터 vs 정보보호 vs 인공지능/빅데이터 vs 정보보호 vs 인공지능.py
+++ 백준/Bronze/30957. 빅데이터 vs 정보보호 vs 인공지능/빅데이터 vs 정보보호 vs 인공지능.py
@@ -1,13 +1,9 @@
 d={}
-a=open(0)
-next(a)
-s=next(a)
-for k in 'BSA':
-    d[k] = s.count(k)
+input()
+s=input()
+for k in 'BSA': d[k] = s.count(k)
 li=sorted([(k, v) for k, v in d.items()], key=lambda x: -x[1])
 ans, m = li[0]
 for k, v in li[1:]:
     if m==v: ans+=k
-if ans=='BSA':
-    ans='SCU'
-print(ans)
+print(ans if ans!='BSA' else 'SCU')
