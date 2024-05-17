9a980419ee316ea0dee7ff41c82daa00c73791b3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-17 21:40:49 +0900

    [Silver II] Title: It’s All About Three, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4937. It’s All About Three/It’s All About Three.py
+++ 백준/Silver/4937. It’s All About Three/It’s All About Three.py
@@ -1,10 +1,9 @@
-import sys
 def is_prime(n):
     for i in range(2, int(n**.5)+1):
         if n%i==0:return False
     return True
 ans=[]
-for n in map(int, sys.stdin):
+for n in map(int, open(0)):
     if n==-1:print('\n'.join(ans))
     elif is_prime(n) and n%10==3:ans.append(f'{n} YES')
     else:
