35600e29b129719022a39d63fd0f094db0177351
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-17 21:49:28 +0900

    [Silver IV] Title: Final Score, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15233. Final Score/Final Score.py
@@ -0,0 +1,9 @@
+a=open(0)
+next(a)
+dic={}
+s=0
+for name in next(a).strip().split():dic[name]=1
+for name in next(a).strip().split():dic[name]=-1
+for name in next(a).strip().split():s+=dic[name]
+if s:s//=abs(s)
+print(['TIE','A','B'][s])
