c79cf55a36491e366d30ba15ca8fd5bd6c99e3f7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-17 20:39:57 +0900

    [Silver II] Title: It’s All About Three, Time: 1836 ms, Memory: 46748 KB -BaekjoonHub

--- 백준/Silver/4937. It’s All About Three/It’s All About Three.py
+++ 백준/Silver/4937. It’s All About Three/It’s All About Three.py
@@ -1,23 +1,21 @@
-n = 10**6
-p = [False, False] + [True] * (n-1)
-for i in range(2, int(n**.5)+1):
-    if p[i]:
-        for j in range(i*i, n+1, i):
-            p[j] = False
-a=open(0)
-ans=[]
-while (i:=int(next(a))) != -1:
-    j = i
-    tmp = set()
-    for k in range(2, i+1):
-        if p[k]:
-            while j%k == 0:
-                j //= k
-                tmp.add(k%10)
-        if j == 1:
-            break
-    if tmp == {3}:
-        ans.append(f'{i} YES')
-    else:
-        ans.append(f'{i} NO')
-print('\n'.join(ans))
+def main():
+    n=10**6
+    p=[False, False]+[True]*(n-1)
+    for i in range(2,int(n**.5)+1):
+        if p[i]:
+            for j in range(i*i,n+1,i):p[j]=False
+    a=open(0)
+    ans=[]
+    while (i:=int(next(a))) != -1:
+        j=i
+        tmp=set()
+        for k in range(2, i+1):
+            if p[k]:
+                while j%k==0:
+                    j//=k
+                    tmp.add(k%10)
+            if j==1:break
+        if tmp=={3}:ans.append(f'{i} YES')
+        else:ans.append(f'{i} NO')
+    print('\n'.join(ans))
+main()
