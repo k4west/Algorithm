a0b49b6f5594fb6f0de2b8614b87857443b349d8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-17 21:37:54 +0900

    [Silver II] Title: It’s All About Three, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4937. It’s All About Three/It’s All About Three.py
+++ 백준/Silver/4937. It’s All About Three/It’s All About Three.py
@@ -3,18 +3,16 @@ def is_prime(n):
     for i in range(2, int(n**.5)+1):
         if n%i==0:return False
     return True
-
-def main():
-    ans=[]
-    for n in map(int, sys.stdin):
-        if n==-1:print('\n'.join(ans))
-        elif is_prime(n) and n%10==3:ans.append(f'{n} YES')
-        else:
-            i,m=2,n
-            while m!=1:
-                if m%i==0:
-                    if i%10!=3:ans.append(f'{n} NO');break
-                    while m%i==0:m//=i
-                i+=1
-            if m==1:ans.append(f'{n} YES')
-main()
+ans=[]
+for n in map(int, sys.stdin):
+    if n==-1:print('\n'.join(ans))
+    elif is_prime(n) and n%10==3:ans.append(f'{n} YES')
+    else:
+        m,f=n,True
+        for i in range(2, int(n**.5)+1):
+            if m%i==0:
+                if i%10!=3:ans.append(f'{n} NO');f=False;break
+                while m%i==0:m//=i
+        if f:
+            if m==1 or m%10==3:ans.append(f'{n} YES')
+            else: ans.append(f'{n} NO')
