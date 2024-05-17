c637ab0790fc39d802dfd281b65f6ce1dc32acb4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-17 21:23:35 +0900

    [Silver II] Title: It’s All About Three, Time: 560 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4937. It’s All About Three/It’s All About Three.py
+++ 백준/Silver/4937. It’s All About Three/It’s All About Three.py
@@ -1,22 +1,20 @@
+def is_prime(n):
+    for i in range(2, int(n**.5)+1):
+        if n%i==0:return False
+    return True
+
 def main():
-    n=10**6
-    p=[False, False]+[True]*(n-1)
-    for i in range(2,int(n**.5)+1):
-        if p[i]:
-            for j in range(i*i,n+1,i):p[j]=False
     a=open(0)
     ans=[]
-    while (i:=int(next(a))) != -1:
-        j,f=i,True
-        for k in range(2, i+1):
-            if p[k] and j%k==0:
-                if k%10 != 3:
-                    ans.append(f'{i} NO')
-                    f=False
-                    break
-                while j%k==0:
-                    j//=k
-            if j==1:break
-        if f:ans.append(f'{i} YES')
+    while (n:=int(next(a))) != -1:
+        if is_prime(n) and n%10==3:ans.append(f'{n} YES')
+        else:
+            i,m=2,n
+            while m!=1:
+                if m%i==0:
+                    if i%10!=3:ans.append(f'{n} NO');break
+                    while m%i==0:m//=i
+                i+=1
+            if m==1:ans.append(f'{n} YES')
     print('\n'.join(ans))
 main()
