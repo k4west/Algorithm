a9e40848751581cd71e3e05df3c445ecf15fa1ad
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-18 11:24:03 +0900

    [Silver II] Title: 별 가두기, Time: 252 ms, Memory: 32656 KB -BaekjoonHub

--- 백준/Silver/30679. 별 가두기/별 가두기.py
+++ 백준/Silver/30679. 별 가두기/별 가두기.py
@@ -1,18 +1,20 @@
-a=open(0)
-f=lambda:map(int,next(a).split())
-d=((0,1),(1,0),(0,-1),(-1,0))
-n,m=f()
-g=[[*f()] for _ in range(n)]
-h=[]
-for r in range(n-1):
-    i,j,c=r,0,0
-    v={(i,j,c)}
-    while 0<=i<n and 0<=j<m:
-        k,e=g[i][j],d[c]
-        i+=k*e[0]
-        j+=k*e[1]
-        if (t:=(i,j,c)) in v:h.append(r+1);break
-        else:v.add(t)
-        c=(c+1)%4
-print(len(h))
-if h:print(*h)
+def main():
+    a=open(0)
+    f=lambda:map(int,next(a).split())
+    d=((0,1),(1,0),(0,-1),(-1,0))
+    n,m=f()
+    g=[[*f()] for _ in range(n)]
+    h=[]
+    for r in range(n-1):
+        i,j,c=r,0,0
+        v={(i,j,c)}
+        while 0<=i<n and 0<=j<m:
+            k,e=g[i][j],d[c]
+            i+=k*e[0]
+            j+=k*e[1]
+            if (t:=(i,j,c)) in v:h.append(r+1);break
+            else:v.add(t)
+            c=(c+1)%4
+    print(len(h))
+    if h:print(*h)
+main()
