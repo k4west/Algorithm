b09b351a1e58185cea696c1576cb104dd58140d4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-18 13:34:12 +0900

    [Silver II] Title: 별 가두기, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/30679. 별 가두기/별 가두기.py
+++ 백준/Silver/30679. 별 가두기/별 가두기.py
@@ -1,20 +1,9 @@
 def main():
-    a=open(0)
-    f=lambda:map(int,next(a).split())
-    d=((0,1),(1,0),(0,-1),(-1,0))
-    n,m=f()
-    g=[[*f()] for _ in range(n)]
-    h=[]
+    a=open(0);f=lambda:map(int,next(a).split());d=(0,1,0,-1);n,m=f();g=[[*f()] for _ in range(n)];h=[];v=[[[-1 for _ in range(4)] for _ in range(m)] for _ in range(n)]
     for r in range(n-1):
         i,j,c=r,0,0
-        v={(i,j,c)}
-        while 0<=i<n and 0<=j<m:
-            k,e=g[i][j],d[c]
-            i+=k*e[0]
-            j+=k*e[1]
-            if (t:=(i,j,c)) in v:h.append(r+1);break
-            else:v.add(t)
-            c=(c+1)%4
+        while 0<=i<n and 0<=j<m and v[i][j][c]==-1:v[i][j][c]=r;k,p,q=g[i][j],d[c],d[(c+1)%4];i+=k*p;j+=k*q;c=(c+1)%4
+        if 0<=i<n and 0<=j<m and (v[i][j][c]==r or v[i][j][c]+1 in h):h.append(r+1)
     print(len(h))
     if h:print(*h)
 main()
