60a1300ec7ca6eccae48d64276c50c2c5d7133b6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-20 23:05:12 +0900

    [Silver II] Title: Wieża 2, Time: 340 ms, Memory: 199936 KB -BaekjoonHub

--- 백준/Silver/8736. Wieża 2/Wieża 2.py
+++ 백준/Silver/8736. Wieża 2/Wieża 2.py
@@ -1,26 +1,15 @@
 def main():
-    u=open(0)
-    f=lambda:map(int,next(u).split())
-    n,m=f()
-    a=f()
-    b=f()
-    q=[]
-    p=next(a)
-    c=[p]
+    u=open(0);f=lambda:map(int,next(u).split());n,m=f();a=f();b=f();q=[];p=next(a);c=[p];r=t=10**9
     for i in a:
         if p<i:p=i
         c.append(p)
-    r=t=10**9
     for h in b:
         s,e=0,n
         while s<e:
             m=(s+e+1)//2
             if h>c[m-1]:s=m
             else:e=m-1
-        if t-1>e:r=e
-        elif t>1:r=t-1
-        else:r=0
-        q.append(r)
+        q.append(r:=max(0,min(t-1,e)))
         if t>r:t=r
     print(" ".join(map(str, q)))
 main()
