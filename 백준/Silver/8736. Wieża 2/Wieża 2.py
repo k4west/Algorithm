1a6e1d37c28d04ba93a080758ff2ebb156cf698f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-20 22:46:24 +0900

    [Silver II] Title: Wieża 2, Time: 1040 ms, Memory: 107132 KB -BaekjoonHub

--- 백준/Silver/8736. Wieża 2/Wieża 2.py
+++ 백준/Silver/8736. Wieża 2/Wieża 2.py
@@ -1,26 +1,28 @@
-def bi(E,h):
-    s,e=0,E
-    while s<e:
-        m=(s+e+1)//2
-        if h>c[m-1]:s=m
-        else:e=m-1
-    return e
-u=open(0)
-f=lambda:map(int,next(u).split())
-n,m=f()
-a=f()
-b=f()
-q=[]
-p=next(a)
-c=[p]
-for i in a:
-    if p<i:p=i
-    c.append(p)
-r=s=10**9
-for h in b:
-    if s-1>(t:=bi(n,h)):r=t
-    elif s>1:r=s-1
-    else:r=0
-    q.append(r)
-    if s>r:s=r
-print(*q)
+def main():
+    def bi(E,h):
+        s,e=0,E
+        while s<e:
+            m=(s+e+1)//2
+            if h>c[m-1]:s=m
+            else:e=m-1
+        return e
+    u=open(0)
+    f=lambda:map(int,next(u).split())
+    n,m=f()
+    a=f()
+    b=f()
+    q=[]
+    p=next(a)
+    c=[p]
+    for i in a:
+        if p<i:p=i
+        c.append(p)
+    r=s=10**9
+    for h in b:
+        if s-1>(t:=bi(n,h)):r=t
+        elif s>1:r=s-1
+        else:r=0
+        q.append(r)
+        if s>r:s=r
+    print(*q)
+main()
