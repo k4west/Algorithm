35198149a1d19acf79091e6f308a24ab3afac734
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-22 22:27:04 +0900

    [Bronze I] Title: Alarm Clock, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10372. Alarm Clock/Alarm Clock.py
+++ 백준/Bronze/10372. Alarm Clock/Alarm Clock.py
@@ -1,28 +1,6 @@
-h1={2:[1], 5:[2], 6:[0]}
-h2={2:[1], 3:[7], 4:[4], 5:[2,3,5], 6:[0,6,9], 7:[8]}
-h={}
-for n1 in h1:
-    for n2 in h2:
-        for s in h1[n1]:
-            for t in h2[n2]:
-                if not (s==2 and t>=4):
-                    if n1+n2 not in h: h[n1+n2]=[]
-                    h[n1+n2].append(s*10+t)                  
-m1={2:[1], 4:[4], 5:[2,3,5], 6:[0]}
-m2={2:[1], 3:[7], 4:[4], 5:[2,3,5], 6:[0,6,9], 7:[8]}
-m={n1+n2:[s*10+t for s in m1[n1] for t in m2[n2]] for n1 in m1 for n2 in m2}
-m={}
-for n1 in m1:
-    for n2 in m2:
-        for s in m1[n1]:
-            for t in m2[n2]:
-                if n1+n2 not in m: m[n1+n2]=[]
-                m[n1+n2].append(s*10+t) 
+d={4:[11],5:[17],6:[14],7:[12],8:[1],9:[7],10:[4],11:[2],12:[0],13:[8]}
 n = int(input())
 if 8<=n<=26:
-    for i in h:
-        if n-i in m:
-            print(f'{h[i][0]:02d}:{m[n-i][0]:02d}')
-            break
-else:
-    print('Impossible')
+    for i in d:
+        if n-i in d:print(f'{d[i][0]:02d}:{d[n-i][0]:02d}');break
+else:print('Impossible')
