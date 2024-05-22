2dacdbe68b97e284a76725a2b6b2d46f2b51e979
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-22 23:08:01 +0900

    [Silver II] Title: Kaleidoscopic Palindromes, Time: 1216 ms, Memory: 110744 KB -BaekjoonHub

--- 백준/Silver/16520. Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
+++ 백준/Silver/16520. Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
@@ -1,18 +1,17 @@
-def check(n, b):
-    tmp=[]
-    while n:
-        tmp.append(n%b)
-        n//=b
-    for i in range(len(tmp)//2):
-        if tmp[i] != tmp[-i-1]:
-            return False
-    return True
-def main():
-    a,b,k=map(int,input().split())
-    s=0
-    for i in range(a,b+1):
-        for j in range(2,k+1):
-            if not check(i,j):break
-        else: s+=1
-    print(s)
-main()
+a,b,k=map(int,input().split())
+s=0
+for i in range(a,b+1):
+    f=False
+    for j in range(2,k+1):
+        t=i
+        tmp=[]
+        while t:
+            tmp.append(t%j)
+            t//=j
+        for t in range(len(tmp)//2):
+            if tmp[t]!=tmp[-t-1]:
+                f=True
+                break
+        if f:break
+    if not f:s+=1
+print(s)
