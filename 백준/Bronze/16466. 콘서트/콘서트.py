f7ff9ad1e75a81124875a35c4c79c7081f6df777
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-23 20:35:41 +0900

    [Bronze I] Title: 콘서트, Time: 324 ms, Memory: 210024 KB -BaekjoonHub

--- 백준/Bronze/16466. 콘서트/콘서트.py
+++ 백준/Bronze/16466. 콘서트/콘서트.py
@@ -1,5 +1,8 @@
-import sys
-input=sys.stdin.readline
-a=int(input())+1
-b=set(range(1,a))-set(map(int,input().split()))
-print(min(b) if b else a)
+def main():
+    a,b=open(0).readlines()
+    s=1
+    for i in sorted(map(int,b.split())):
+        if s==i:s+=1
+        else:print(s);return
+    print(s)
+main()
