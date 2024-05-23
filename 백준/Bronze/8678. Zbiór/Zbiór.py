ad1eed5fcbeeead2a86c24e31000fbed57c19f45
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-23 20:12:34 +0900

    [Bronze III] Title: Zbiór, Time: 936 ms, Memory: 126428 KB -BaekjoonHub

--- 백준/Bronze/8678. Zbiór/Zbiór.py
+++ 백준/Bronze/8678. Zbiór/Zbiór.py
@@ -1,5 +1,7 @@
-n=[]
-for l in open(0).readlines()[1:]:
-    a,b=map(int, l.split())
-    n.append(['TAK','NIE'][bool(b%a)])
-print('\n'.join(n))
+def main():
+    n=[]
+    for l in open(0).readlines()[1:]:
+        a,b=map(int, l.split())
+        n.append(['TAK','NIE'][bool(b%a)])
+    print('\n'.join(n))
+main()
