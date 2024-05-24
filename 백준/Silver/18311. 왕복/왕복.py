6b414026b80c10c596c5a1e31131becc00837940
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-24 21:29:44 +0900

    [Silver V] Title: 왕복, Time: 80 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Silver/18311. 왕복/왕복.py
@@ -0,0 +1,11 @@
+a=open(0)
+N,K=map(int,next(a).split())
+d=[*map(int,next(a).split())]
+s=sum(d)
+q,r=divmod(K,s)
+q%=2
+if q:d=d[::-1]
+for i,j in enumerate(d):
+    if r>=j:r-=j
+    else:break
+print([i+1,N-i][q])
