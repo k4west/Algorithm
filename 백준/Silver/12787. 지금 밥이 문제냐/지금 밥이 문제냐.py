5d98bf603c016951823f8b784ca331cf0249fab8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-27 21:49:04 +0900

    [Silver V] Title: 지금 밥이 문제냐, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12787. 지금 밥이 문제냐/지금 밥이 문제냐.py
+++ 백준/Silver/12787. 지금 밥이 문제냐/지금 밥이 문제냐.py
@@ -1,15 +1,11 @@
 def main():
-    a=open(0)
-    T=[]
+    a=open(0);T=[]
     for _ in range(int(next(a))):
         M,N=next(a).split()
-        if M=='1':
-            T.append(sum(n<<(8*(7-i)) for i,n in enumerate(map(int,N.split('.')))))
+        if M=='1':T.append(sum(n<<(56-8*i) for i,n in enumerate(map(int,N.split('.')))))
         else:
             t,N=[],int(N)
-            for _ in range(8):
-                t.append(N%256)
-                N//=256
+            for _ in range(8):t.append(N%256);N>>=8
             T.append('.'.join(map(str,t[::-1])))
-    print(*T,sep='\n')
+    print('\n'.join(map(str,T)))
 main()
