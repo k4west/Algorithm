3d18f8a5e7a32ae69b1107d276c7c9d0217ad526
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-29 22:04:08 +0900

    [Bronze IV] Title: Population, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26561. Population/Population.py
+++ 백준/Bronze/26561. Population/Population.py
@@ -1,3 +1,1 @@
-T=[]
-for _ in range(int(input())):p,t=map(int,input().split());T.append(p+t//4-t//7)
-print(*T,sep='\n')
+for _ in range(int(input())):p,t=map(int,input().split());print(p+t//4-t//7)
