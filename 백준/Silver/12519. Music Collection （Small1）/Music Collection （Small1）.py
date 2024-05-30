00c666d9ccfaba86d973cc00c47f86cae8af3bce
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-30 23:30:55 +0900

    [Silver II] Title: Music Collection (Small1), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12519. Music Collection （Small1）/Music Collection （Small1）.py
+++ 백준/Silver/12519. Music Collection （Small1）/Music Collection （Small1）.py
@@ -1,33 +1,20 @@
-a=open(0)
-t=[]
-for i in range(int(next(a))):
-    t.append(f'Case #{i+1}:')
-    n=int(next(a))
-    s=[next(a).strip().upper() for _ in range(n)]
-    if n==1:
-        t.append('""')
-        continue
-    for j in range(n):
-        b=''
-        p=s[j]
-        for k in range(1, len(p)+1):
-            for l in range(len(p)-k+1):
-                c=p[l:l+k]
-                f=False
-                for m in range(n):
-                    if j==m:continue
-                    if c in s[m]:
-                        f=True
-                        break
-                if not f:
-                    if b:
-                        b=min(b,c) #
-                    else:
-                        b=c
-            if b:
-                break
-        if b:
-            t.append(f'"{b}"')
-        else:
-            t.append(':(')
-print('\n'.join(t))
+def main():
+    a=open(0)
+    for i in range(int(next(a))):
+        print(f'Case #{i+1}:');n=int(next(a));s=[next(a).strip().upper() for _ in range(n)]
+        if n==1:print('""');continue
+        for j in range(n):
+            b='';p=s[j]
+            for k in range(1,len(p)+1):
+                for l in range(len(p)-k+1):
+                    c=p[l:l+k];f=False
+                    for m in range(n):
+                        if j==m:continue
+                        if c in s[m]:f=True;break
+                    if not f:
+                        if b:b=min(b,c)
+                        else:b=c
+                if b:break
+            if b:print(f'"{b}"')
+            else:print(':(')
+main()
