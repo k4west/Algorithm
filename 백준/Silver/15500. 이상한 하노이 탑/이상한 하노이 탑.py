8ba25368dcd0aa796900a9bc3ca79726a7ef74e9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-05-30 22:33:11 +0900

    [Silver II] Title: 이상한 하노이 탑, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.py
+++ 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.py
@@ -5,8 +5,8 @@ v=2
 a=[]
 while n:
     f=False
-    for _ in range(n):
-        if not p[u]:u,v=v,u;break
+    if not p[u]:u,v=v,u
+    for _ in range(len(p[u])):
         if (j:=p[u].pop())==n:a.append(f'{u} 3');n-=1;break
         if j == n-1:f=True
         p[v].append(j)
