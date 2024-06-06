808f20ce9c8b05df893e1553fc59b776979a5927
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-06 09:38:59 +0900

    [Silver V] Title: The Pleasant Walk, Time: 64 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/19829. The Pleasant Walk/The Pleasant Walk.py
+++ 백준/Silver/19829. The Pleasant Walk/The Pleasant Walk.py
@@ -1,8 +1,11 @@
-n,k=map(int,input().split())
-c=input().split()
-v=[i for i in range(n)]
-for i in range(n-1):
-    if c[i]!=c[i+1]:v[i+1]=v[i]
-d={}
-for i in v:d[i]=d.get(i,0)+1
-print(max(d.values()))
+a=open(0)
+def main():
+    n,k=map(int,next(a).split())
+    c=next(a).split()
+    v=[i for i in range(n)]
+    for i in range(n-1):
+        if c[i]!=c[i+1]:v[i+1]=v[i]
+    d={}
+    for i in v:d[i]=d.get(i,0)+1
+    print(max(d.values()))
+main()
