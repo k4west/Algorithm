ef9946b991e545b311d52a31ed767d66d5c4aaaa
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-07 22:55:23 +0900

    [level 2] Title: 땅따먹기, Time: 199.93 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,8 +1,5 @@
 def solution(land):
-    dp=land[0]
-    for row in land[1:]:
-        tmp=[0,0,0,0]
-        for i in range(4): 
-            tmp[i] = max(dp[:i]+dp[i+1:])+row[i]
-        dp=tmp
-    return max(dp)
+    for i in range(len(land)-1):
+        for j in range(4): 
+            land[i+1][j] += max(land[i][:j]+land[i][j+1:])
+    return max(land[-1])
