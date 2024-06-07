06aee3832614d4f8c99b3991bf27857d62360680
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-07 22:47:38 +0900

    [level 2] Title: 롤케이크 자르기, Time: 680.50 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -5,6 +5,4 @@ def solution(topping):
         w.add(topping[-i-1])
         l.append(len(v))
         r[-i-1]=len(w)
-    for i in range(1,n-1):
-        if l[i]==r[i+1]:t+=1
-    return t
+    return sum(l[i]==r[i+1] for i in range(1,n-1))
