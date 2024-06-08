623dbc07a45087ff28afce6f4df2d5df4adbeed4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-08 20:09:57 +0900

    [Silver III] Title: 엑셀, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2757. 엑셀/엑셀.py
@@ -0,0 +1,13 @@
+t=[]
+for i in open(0).readlines():
+    r,c=i.strip()[1:].split('C')
+    if r==c=='0':break
+    c=int(c)
+    s=''
+    while c:
+        c-=1
+        j=c%26
+        s+=chr(j+65)
+        c//=26
+    t.append(s[::-1]+r)
+print('\n'.join(t))
