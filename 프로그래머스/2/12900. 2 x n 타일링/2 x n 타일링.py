9e6783cd1a5627bc0bf241be48715861466649f3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-08 21:53:29 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,6 +1,9 @@
 m=10**9+7
-def solution(n):
-    dp = [1,1]
-    for _ in range(n-1):
-        dp.append((dp[-1]+dp[-2])%m)
-    return dp[-1]
+dp={1:1,2:1,3:2,4:3,5:5}
+def f(n):
+    if n in dp:return dp[n]
+    t=n//2
+    if n%2:dp[n]=(f(t)**2+f(t+1)**2)%m
+    else:dp[n]=(f(t)*(f(t-1)+f(t+1)))%m
+    return dp[n]
+def solution(n):return f(n+1)
