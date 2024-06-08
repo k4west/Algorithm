bbd0f0a93b67e46a59a9de005a588db74ce65e6c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-08 11:40:04 +0900

    [level 3] Title: 야근 지수, Time: 3.94 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,21 +1,24 @@
 def solution(n, works):
-    if n >= (W:=sum(works)):
+    if n >= sum(works):
         return 0
-    d={}
-    for w in works:d[w]=d.get(w,0)+1
-    k=[*d.keys()]
-    k.sort()
-    while n:
-        if n >= d[(t:=k[-1])]:
-            n -= d[t]
-            if t>1:
-                d[t-1]=d.get(t-1,0)+d[t]
-                if t-1 not in k: k.append(t-1);k.sort()
-            del d[k.pop()]
-        else:
-            if t>1:
-                d[t-1]=d.get(t-1,0)+n
-            d[t]-=n
-            break
     
-    return sum(k*k*v for k, v in d.items())
+    d={}
+    for w in works:
+        d[w] = d.get(w,0)+1
+        
+    li = [0]*((m:=max(d))+1)
+    for k,v in d.items():
+        li[k] = v
+    
+    for i in range(1, m+1):
+        if (t:=li[-i]):
+            if t < n:
+                n -= t
+                li[-i] = 0
+                li[-i-1] += t
+            else:
+                li[-i] -= n
+                li[-i-1] += n
+                break
+                
+    return sum(k*k*v for k, v in enumerate(li) if v)
