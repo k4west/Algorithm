956c5db8b4458f934db3ae4c4a2e14dde95e40e4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-08 20:18:20 +0900

    [Silver III] Title: 엑셀, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2757. 엑셀/엑셀.py
+++ 백준/Silver/2757. 엑셀/엑셀.py
@@ -1,13 +1,12 @@
 t=[]
 for i in open(0).readlines():
-    r,c=i.strip()[1:].split('C')
-    if r==c=='0':break
-    c=int(c)
+    r,c=map(int,i.strip()[1:].split('C'))
+    if r==c==0:break
     s=''
     while c:
         c-=1
         j=c%26
         s+=chr(j+65)
         c//=26
-    t.append(s[::-1]+r)
+    t.append(f'{s[::-1]}{r}')
 print('\n'.join(t))
