310862c43f05f0ce16febb7c9cb92dc8d76e825c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-08 22:14:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,9 +1,4 @@
 m=10007
-d={1:1,2:1,3:2,4:3,5:5}
-def f(n):
-    if n in d:return d[n]
-    t=n//2
-    if n%2:d[n]=(f(t)**2+f(t+1)**2)%m
-    else:d[n]=(f(t)*(f(t-1)+f(t+1)))%m
-    return d[n]
-print(f(int(input())+1))
+l=[1,1]
+for _ in range(int(input())-1):l.append((l[-1]+l[-2])%m)
+print(l[-1])
