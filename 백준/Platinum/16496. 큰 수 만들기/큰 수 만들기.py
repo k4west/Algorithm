1410cbede2d7076b850bd7519eb7f0d7630cf71a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-12 22:58:55 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,4 +1,2 @@
-from functools import cmp_to_key as ck
 input()
-def main():print(int("".join(sorted(input().split(),key=ck(lambda x,y:2*(x+y>y+x)-1))[::-1])))
-main()
+print(int("".join(sorted(input().split(), key=lambda x: x*9, reverse=True))))
