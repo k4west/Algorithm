8ae74df23f8923c55f2b9b10cce2c4e9a14c7568
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-20 13:54:25 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 272 ms, Memory: 46788 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,17 +1,18 @@
+m = 1003001
+primes = [False, False, True] + [True, False]*(m//2)
+for i in range(3, int(m**.5)+1):
+    if primes[i]:
+        for j in range(i*i, m, i):
+            primes[j] = False
+
+def f(s):
+    for i in range(len(s)//2):
+        if s[i]!=s[-i-1]: return False
+    return True
+
 n=int(input())
-if n==1 or n==2:
-    print(2)
-else:
-    while True:
-        if n%2:
-            for i in range(3, int(n**.5)+1):
-                if n%i==0:
-                    break
-            else:
-                for i in range(len(s:=str(n))//2):
-                    if s[i] != s[-i-1]:
-                        break
-                else:
-                    print(n)
-                    exit()
-        n+=1
+while True:
+    if primes[n] and f(str(n)):
+        print(n)
+        break 
+    n+=1
