3a257509f176dbff9320ce039c6a46a00b1c89b8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-22 23:50:26 +0900

    [Bronze I] Title: Game, Time: 1060 ms, Memory: 68732 KB -BaekjoonHub

--- 백준/Bronze/3432. Game/Game.py
+++ 백준/Bronze/3432. Game/Game.py
@@ -3,20 +3,17 @@ def main():
     n,*m=open(0)
     for i in range(int(n)):
         t=m[i*5:(i+1)*5]
-        p=[False,False]
-
+        p=[0,0]
         for j in range(5):
             r,c=t[j][2],t[2][j]
-            if r*3 in t[j]: p[r=='B']=True
-            if c*3 in ''.join([k[j] for k in t]): p[c=='B']=True
-
-        for r in range(3):
-            for c in range(3):
-                s = t[r+1][c+1]
-                if s*2==t[r][c]+t[r+2][c+2]: p[s=='B']=True
-                if s*2==t[r][c+2]+t[r+2][c]: p[s=='B']=True
-        
-        a,b=map(int,p)
+            if r*3 in t[j]: p[r=='B']=1
+            if c*3 in ''.join([k[j] for k in t]): p[c=='B']=1
+            if j<3:
+                for k in range(3):
+                    s = t[j+1][k+1]
+                    if s*2==t[j][k]+t[j+2][k+2]: p[s=='B']=1
+                    if s*2==t[j][k+2]+t[j+2][k]: p[s=='B']=1
+        a,b=p
         w.append(['draw','A wins','B wins'][(a>b)+2*(a<b)])
     print('\n'.join(w))
 main()
