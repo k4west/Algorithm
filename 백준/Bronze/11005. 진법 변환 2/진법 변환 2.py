b940aecedcf4dfe394a506b109cab9731b0d24eb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-06-27 13:48:54 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,7 +1,7 @@
-N, B = map(int, input().split())
-s = []
-while N > 0:
-    n = N % B
-    s.append(chr(n+[55, 48][n < 10]))
-    N //= B
-print(("".join(s))[::-1])
+N,B=map(int,input().split())
+t=[]
+while N:
+    i=N%B
+    t.append(chr(i+55) if i>9 else i)
+    N//=B
+print(*t[::-1],sep='')
