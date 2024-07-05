130a426dd89c0564ddb57f5fa8eba33b8bb0e1c6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-05 13:40:16 +0900

    [Bronze I] Title: Vaccine Efficacy, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/21205. Vaccine Efficacy/Vaccine Efficacy.py
+++ 백준/Bronze/21205. Vaccine Efficacy/Vaccine Efficacy.py
@@ -1,14 +1,12 @@
+a=open(0).readlines()[1:]
 d=[[0,0,0],[0,0,0]]
 c=[0,0]
-t=[]
-for _ in range(int(input())):
-    i=input()
+for i in a:
+    i=i.strip()
     j=i[0]=='Y'
     c[j]+=1
     for k in range(3):
         if i[k+1]=='Y':d[j][k] += 1
 for i, j in zip(*d):
-    if i/c[0] <= j/c[1]:
-        t.append('Not Effective')
-    else:t.append(f'{100-(100*j*c[0])/(i*c[1]):.6f}')
-print(*t,sep='\n')
+    if i/c[0] <= j/c[1]:print('Not Effective')
+    else:print(f'{100-(100*j*c[0])/(i*c[1]):.6f}')
