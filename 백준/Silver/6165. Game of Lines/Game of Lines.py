c13bc1eed8789359f60dd4fa63b24baada7c0b3f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-05 13:54:54 +0900

    [Silver IV] Title: Game of Lines, Time: 44 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/6165. Game of Lines/Game of Lines.py
+++ 백준/Silver/6165. Game of Lines/Game of Lines.py
@@ -1,13 +1,11 @@
-n,*a=open(0)
+n,*a=map(float,open(0).read().split())
 n=int(n)
-a=[[*map(float,i.split())] for i in a]
 f=False
 s=set()
 for i in range(n):
-    x0,y0=a[i]
+    x0,y0=a[2*i:2*(i+1)]
     for j in range(i+1,n):
-        x1,y1=a[j]
+        x1,y1=a[2*j:2*(j+1)]
         if x1==x0:f=True
-        else:
-            s.add((y1-y0)/(x1-x0))
+        else:s.add((y1-y0)/(x1-x0))
 print(len(s)+f)
