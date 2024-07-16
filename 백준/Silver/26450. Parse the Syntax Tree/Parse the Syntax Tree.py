7230aa76e7ddb4706da48ab6aad247e1a2a66d37
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-17 00:17:41 +0900

    [Silver II] Title: Parse the Syntax Tree, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26450. Parse the Syntax Tree/Parse the Syntax Tree.py
+++ 백준/Silver/26450. Parse the Syntax Tree/Parse the Syntax Tree.py
@@ -1,17 +1,8 @@
-def parse(a, x, y, w):
-    if w == 1: return a[x][y]
-    for i in range(w):
-        if a[x][y + i] in '+-*':
-            op = a[x][y + i]
-            break
-    left = parse(a, x + 1, y, i)
-    right = parse(a, x + 1, y + i + 1, w - i - 1)
-    return str(eval(''.join((left,op,right))))
-
-def main():
-    n,*a=open(0)
-    _,W=map(int,n.split())
-    print(parse(a, 0, 0, W))
-
-if __name__ == "__main__":
-    main()
+_,*a=open(0)
+a=list(''.join(a).replace('.','').replace(' ','').replace('\n','')[::-1])
+i=0
+for j,s in enumerate(a):
+    if s in '+-*':
+        a[j]="".join(('(',a[i+1],s,a[i],')'))
+        i+=2
+print(eval(a[-1]))
