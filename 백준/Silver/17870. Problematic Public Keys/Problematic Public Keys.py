fb1a658b87e545d136f1197f7408c77a74467385
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-19 13:27:56 +0900

    [Silver IV] Title: Problematic Public Keys, Time: 156 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/17870. Problematic Public Keys/Problematic Public Keys.py
+++ 백준/Silver/17870. Problematic Public Keys/Problematic Public Keys.py
@@ -1,10 +1,14 @@
-m,*a=open(0)
-p=set()
-for n in map(int, a):
+def get_p(n):
+    n=int(n)
     for i in range(2, int(n*.5)+1):
         if n%i==0:
-            break
-    p.add(i)
-    p.add(n//i)
-p=sorted(p)
-print('\n'.join([' '.join(map(str, p[5*i:5*i+5])) for i in range(len(p)//5+1)]))
+            return i, n//i
+def main():
+    m,*a=open(0)
+    p=set()
+    for i, j in map(get_p, a):
+        p.add(i)
+        p.add(j)
+    p=sorted(p)
+    print('\n'.join([' '.join(map(str, p[5*i:5*i+5])) for i in range(len(p)//5+1)]))
+main()
