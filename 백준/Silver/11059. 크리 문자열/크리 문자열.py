a9cd89179b15afbea9e8c673f67b2e8c11bc8a46
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-22 13:25:56 +0900

    [Silver V] Title: 크리 문자열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11059. 크리 문자열/크리 문자열.py
+++ 백준/Silver/11059. 크리 문자열/크리 문자열.py
@@ -1,19 +1,18 @@
-def check(li, n):
-    return sum(li[:n]) == sum(li[n:])
-
 def main():
-    S=[*map(float, list(input()))]
-    n=len(S)
+    s=[*map(int, list(input()))]
+    n=len(s)
+    S=[0]*(n+1)
+    for i in range(n):
+        S[i+1]=S[i]+s[i]
     i=n-n%2
     flag = False
     while i:
         for j in range(n-i+1):
-            if check(S[j:j+i], i//2):
+            if S[j+i//2]-S[j] == S[j+i]-S[j+i//2]:
                 print(i)
                 return
         if flag:
             break
         i -= 2
     print(i)
-
 main()
