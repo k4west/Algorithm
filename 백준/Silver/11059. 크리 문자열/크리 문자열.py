0179804f807cbec79f2da8847260359c853471b8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-07-22 13:27:44 +0900

    [Silver V] Title: 크리 문자열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11059. 크리 문자열/크리 문자열.py
+++ 백준/Silver/11059. 크리 문자열/크리 문자열.py
@@ -5,14 +5,11 @@ def main():
     for i in range(n):
         S[i+1]=S[i]+s[i]
     i=n-n%2
-    flag = False
     while i:
         for j in range(n-i+1):
             if S[j+i//2]-S[j] == S[j+i]-S[j+i//2]:
                 print(i)
                 return
-        if flag:
-            break
         i -= 2
     print(i)
 main()
