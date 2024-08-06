e02fcae47a50f6eceda9f34249bc7c43787d9692
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-06 21:33:14 +0900

    [Silver IV] Title: 판화, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.py
+++ 백준/Silver/1730. 판화/판화.py
@@ -1,13 +1,8 @@
 d={'U':(0,-1,1),'D':(0,1,1),'L':(-1,0,2),'R':(1,0,2)}
-N,a=open(0)
-N=int(N)
-a=a.strip()
+N=int(input())
 G=[[0]*N+[-1] for _ in range(N)]
 x=y=0
-for i in a:
+for i in input():
     dx,dy,t=d[i]
-    if 0<=(nx:=x+dx)<N and 0<=(ny:=y+dy)<N:
-        G[y][x]|=t
-        G[ny][nx]|=t
-        x,y=nx,ny
+    if 0<=(nx:=x+dx)<N and 0<=(ny:=y+dy)<N:G[y][x]|=t;G[ny][nx]|=t;x,y=nx,ny
 print(''.join('.|-+\n'[g] for g in sum(G,[])))
