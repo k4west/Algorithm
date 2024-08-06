0332e3eba3c2e7658af555a5237167de7b5fdce9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-06 21:23:02 +0900

    [Silver IV] Title: 판화, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1730. 판화/판화.py
@@ -0,0 +1,13 @@
+d={'U':(0,-1,1),'D':(0,1,1),'L':(-1,0,2),'R':(1,0,2)}
+N,a=open(0)
+N=int(N)
+a=a.strip()
+G=[[0]*N for _ in range(N)]
+x=y=0
+for i in a:
+    dx,dy,t=d[i]
+    if 0<=(nx:=x+dx)<N and 0<=(ny:=y+dy)<N:
+        G[y][x]|=t
+        G[ny][nx]|=t
+        x,y=nx,ny
+print('\n'.join(''.join(map(lambda x:'.|-+'[x],g)) for g in G))
