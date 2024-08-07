5e5453e970501025dab0dd419fa694e8e01a5d70
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-07 13:22:20 +0900

    [Silver V] Title: Jarvis, Time: 112 ms, Memory: 49420 KB -BaekjoonHub

--- 백준/Silver/17048. Jarvis/Jarvis.py
+++ 백준/Silver/17048. Jarvis/Jarvis.py
@@ -1,9 +1,4 @@
+from collections import Counter
 N,A,B=open(0)
-d={}
-for a,b in zip(A.strip().split(),B.strip().split()):
-    c=int(a)-int(b)
-    if c in d:
-        d[c]+=1
-    else:
-        d[c]=1
+d=Counter(int(a)-int(b) for a,b in zip(A.strip().split(),B.strip().split()))
 print(max(d.values()))
