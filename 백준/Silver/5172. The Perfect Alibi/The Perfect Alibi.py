e9698c95ead0bac1f2a24bffe2663dd2b9f08d58
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-09 22:48:44 +0900

    [Silver IV] Title: The Perfect Alibi, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5172. The Perfect Alibi/The Perfect Alibi.py
+++ 백준/Silver/5172. The Perfect Alibi/The Perfect Alibi.py
@@ -1,40 +1,28 @@
+def g(p,i,o,P):
+    for b, f in i:
+        fg=False
+        for P1 in range(1,p+1):
+            if P==P1:continue
+            for b1, f1 in o[P1]: 
+                if b<=b1<=f or b<=f1<=f or b1<=b<=f1 or b1<=f<=f1:fg=True;break
+            if fg:break
+        if not fg:return False
+    return True
 a=open(0)
 r=[]
 for i in range(int(next(a))):
     s,w,p,t=map(int,next(a).split())
-    suspects=[]
-    places=[set() for _ in range(s+1)]
-    outtime=[[[] for _ in range(p+1)] for _ in range(s+1)]
-    intime=[[[] for _ in range(p+1)] for _ in range(s+1)]
+    ss=[]
+    ps=[set() for _ in range(s+1)]
+    ot=[[[] for _ in range(p+1)] for _ in range(s+1)]
+    it=[[[] for _ in range(p+1)] for _ in range(s+1)]
     for _ in range(w):
         S,P,b,f=map(int,next(a).split())
-        if b<=t<=f:
-            places[S].add(P)
-            intime[S][P].append([b, f])
-        else:
-            outtime[S][P].append([b, f])
-    for S in range(1, s+1):
-        if len(P:=places[S])!=1:
-            suspects.append(S)
-        else:
-            P0=[*P][0]
-            check=True
-            for b0, f0 in intime[S][P0]:
-                flag=False
-                for P1 in range(1,p+1):
-                    if P0==P1:
-                        continue
-                    for b1, f1 in outtime[S][P1]: 
-                        if b0<=b1<=f0 or b0<=f1<=f0 or b1<=b0<=f1 or b1<=f0<=f1:
-                            flag=True
-                            break
-                    if flag:
-                        break
-                if not flag:
-                    check=False
-                    break
-            if check:
-                suspects.append(S)
-    suspects='\n'.join(map(str,suspects)) or 'No suspect.'
-    r.append(f'Data Set {i+1}:\n{suspects}')
+        if b<=t<=f:ps[S].add(P);it[S][P].append([b,f])
+        else:ot[S][P].append([b,f])
+    for S in range(1,s+1):
+        if len(P:=ps[S])!=1:ss.append(S)
+        elif g(p,it[S][[*P][0]],ot[S],P):ss.append(S)
+    ss='\n'.join(map(str,ss)) or 'No suspect.'
+    r.append(f'Data Set {i+1}:\n{ss}')
 print('\n\n'.join(r))
