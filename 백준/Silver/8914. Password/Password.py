292bd292211bae81e122413dc1dbbfb29bfda876
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-16 15:40:19 +0900

    [Silver II] Title: Password, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/8914. Password/Password.py
+++ 백준/Silver/8914. Password/Password.py
@@ -5,22 +5,15 @@ for i in range(int(T)):
     K=int(K)
     w=[sorted(set(col[:6])&set(col[6:12])) for col in zip(*grids)]
     c=[len(j) for j in w]
-    n=1
-    for j in c:
-        n*=j
-    if n==0 or n<K:
-        t='NO'
+    n=1;t=''
+    for j in c:n*=j
+    if n==0 or n<K:t='NO'
     else:
-        t=''
         for j in range(5):
-            n//=c[j]
-            idx=0
+            n//=c[j];k=0
             while True:
-                if n*(idx+1)<K:
-                    idx+=1
-                else:
-                    break
-            t+=w[j][idx]
-            K-=n*idx
+                if n*(k+1)<K:k+=1
+                else:break
+            t+=w[j][k];K-=n*k
     r.append(t)
 print('\n'.join(r))
