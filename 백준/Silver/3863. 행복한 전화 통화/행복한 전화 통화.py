76b111988ecb6627080836157d77cd537f79ea21
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-19 14:24:59 +0900

    [Silver V] Title: 행복한 전화 통화, Time: 1120 ms, Memory: 34188 KB -BaekjoonHub

---
+++ 백준/Silver/3863. 행복한 전화 통화/행복한 전화 통화.py
@@ -0,0 +1,15 @@
+a=open(0)
+t=[]
+while True:
+    n,m=map(int,next(a).split())
+    if n==0:
+        break
+    c=[[*map(int,next(a).split()[2:])] for _ in range(n)]
+    d=[[*map(int,next(a).split())] for _ in range(m)]
+    for i,j in d:
+        s=0
+        for p,q in c:
+            if i<=p<i+j or p<=i<p+q:
+                s+=1
+        t.append(s)
+print('\n'.join(map(str,t)))
