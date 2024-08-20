6ba36859318ca3dd434b8e3526628fd76dce5284
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-20 15:55:48 +0900

    [Bronze II] Title: Congestion Charging Zone, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16735. Congestion Charging Zone/Congestion Charging Zone.py
@@ -0,0 +1,12 @@
+n,*a=open(0)
+t=[]
+p=0
+for i in a:
+    h,m=map(int,i.split(':'))
+    if 390<=(j:=h*60+m)<=1140:
+        t.append(j)
+if t:
+    t.sort()
+    s,e=t[0],t[-1]
+    p=[24000,36000,16800,24000][2*(s>600)+(e>960)]
+print(p)
