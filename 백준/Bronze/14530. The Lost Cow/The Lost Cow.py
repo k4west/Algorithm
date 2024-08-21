fdad28d681a9b3fd8b4ff8e39b6c929f31819b67
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-21 09:48:51 +0900

    [Bronze II] Title: The Lost Cow, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14530. The Lost Cow/The Lost Cow.py
+++ 백준/Bronze/14530. The Lost Cow/The Lost Cow.py
@@ -1,9 +1,7 @@
 x,y=map(int,input().split())
 d=y-x
 f=x>y
-if d==1:print(1)
-else:
-    if f:d*=-1
-    i=1+f
-    while i<d:i*=4
-    print(i*2-2+d)
+if f:d*=-1
+i=1+f
+while i<d:i*=4
+print(i*2-2+d)
