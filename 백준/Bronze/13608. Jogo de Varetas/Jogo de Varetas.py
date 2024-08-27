22aaad9eb392c3a4bd1ce337cf2891df71218252
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-27 12:47:24 +0900

    [Bronze I] Title: Jogo de Varetas, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13608. Jogo de Varetas/Jogo de Varetas.py
+++ 백준/Bronze/13608. Jogo de Varetas/Jogo de Varetas.py
@@ -1,9 +1,2 @@
 a=open(0)
-s=[]
-while n:=int(next(a)):
-    t=0
-    for _ in range(n):
-        c,v=map(int,next(a).split())
-        t+=v//2
-    s.append(t//2)
-print('\n'.join(map(str,s)))
+while n:=int(next(a)):print(sum((int(next(a).split()[1])//2 for _ in range(n)))//2)
