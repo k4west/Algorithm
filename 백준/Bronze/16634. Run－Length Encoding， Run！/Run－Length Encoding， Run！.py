af9a394623165a5256ee17bc5e5decb1cdca601e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-29 13:35:44 +0900

    [Bronze II] Title: Run-Length Encoding, Run!, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16634. Run－Length Encoding， Run！/Run－Length Encoding， Run！.py
@@ -0,0 +1,17 @@
+a,s=input().split()
+r=''
+if a=='E':
+    t=''
+    c=0
+    for i in s:
+        if i!=t:
+            if c:
+                r+=f'{t}{c}'
+                c=0
+            t=i
+        c+=1
+    r+=f'{t}{c}'
+else:
+    for i in range(0,len(s),2):
+        r+=s[i]*int(s[i+1])
+print(r)
