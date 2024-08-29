52720bd51314c3a4240a01d332e78f74c2a73b76
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-29 13:28:10 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -1,6 +1,4 @@
 n,*a=open(0)
-t=[]
 for i in a:
     c,v=map(int,i.split())
-    t.append(f'You get {c//v} piece(s) and your dad gets {c%v} piece(s).')
-print('\n'.join(t))
+    print(f'You get {c//v} piece(s) and your dad gets {c%v} piece(s).')
