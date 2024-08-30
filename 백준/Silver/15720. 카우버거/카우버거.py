ccac47ffe9a8537e4d937534a200d914cd8a57fa
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-08-30 12:51:00 +0900

    [Silver V] Title: 카우버거, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15720. 카우버거/카우버거.py
+++ 백준/Silver/15720. 카우버거/카우버거.py
@@ -1,10 +1,8 @@
-a,b,c,d=open(0)
-a = [*map(int,a.split())]
-b = sorted(map(int,b.split()))
-c = sorted(map(int,c.split()))
-d = sorted(map(int,d.split()))
-n = min(a)
-t=sum(sum([b,c,d],[]))
-k=sum([sum(j[:i-n]) for i, j in zip(a,[b,c,d])])
+a,*b=open(0)
+a=[*map(int,a.split())]
+b=[*map(lambda x:sorted(map(int,x.split())), b)]
+n=min(a)
+t=sum(sum(b,[]))
+k=sum([sum(j[:i-n]) for i, j in zip(a,b)])
 print(t)
 print((t-k)//10*9+k)
