4f7afadd10fec9ed2b2789946d852d520224a79e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-04 20:29:43 +0900

    [Silver V] Title: Mini Sudoku X, Time: 2088 ms, Memory: 82444 KB -BaekjoonHub

--- 백준/Silver/9727. Mini Sudoku X/Mini Sudoku X.py
+++ 백준/Silver/9727. Mini Sudoku X/Mini Sudoku X.py
@@ -1,29 +1,26 @@
 m=set(range(1,7))
 def check(li):
     return set(li)==m
-t=[]
-n,*a=open(0)
-for i in range(int(n)):
-    p=1
+def sol(rows):
     col=[[] for _ in range(6)]
     dia=[[] for _ in range(2)]
     squ=[[] for _ in range(2)]
-    for r,row in enumerate(map(lambda x:[*map(int,x.split())],a[6*i:6*(i+1)])):
-        if not check(row):p=0;break
+    for r,row in enumerate(map(lambda x:[*map(int,x.split())],rows)):
+        if not check(row):return 0
         squ[0].extend(row[:3])
         squ[1].extend(row[3:])
         if len(squ[0])==6:
             for s in squ:
-                if not check(s):p=0;break
-            if not p:break
+                if not check(s):return 0
             squ=[[] for _ in range(2)]
         for j,k in enumerate(row):
             col[j].append(k)
             if r==j:dia[0].append(k)
             if r+j==5:dia[1].append(k)
     for c in col:
-        if not check(c):p=0;break
+        if not check(c):return 0
     for d in dia:
-        if not check(d):p=0;break
-    t.append(f'Case#{i+1}: {p}')
-print('\n'.join(t))
+        if not check(d):return 0
+    return 1
+n,*a=open(0)
+print('\n'.join([f'Case#{i+1}: {sol(a[6*i:6*(i+1)])}' for i in range(int(n))]))
