cf1b32f11b2625d1d52e32f71951e021853365dd
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-04 21:10:44 +0900

    [Silver V] Title: Mini Sudoku X, Time: 736 ms, Memory: 197972 KB -BaekjoonHub

--- 백준/Silver/9727. Mini Sudoku X/Mini Sudoku X.py
+++ 백준/Silver/9727. Mini Sudoku X/Mini Sudoku X.py
@@ -1,25 +1,19 @@
-def check(li):
-    return len(set(li))==6
+m=set(range(1,7))
 def sol(rows):
-    col=[[] for _ in range(6)]
-    dia=[[] for _ in range(2)]
+    rows = [*map(lambda x:[*map(int,x.split())],rows)]
     squ=[[] for _ in range(2)]
-    for r,row in enumerate(map(lambda x:[*map(int,x.split())],rows)):
-        if len(set(row))!=6:return 0
+    for row in rows:
+        if 6!=len(set(row)):return 0
         squ[0].extend(row[:3])
         squ[1].extend(row[3:])
         if len(squ[0])==6:
             for s in squ:
-                if len(set(s))!=6:return 0
+                if 6!=len(set(s)):return 0
             squ=[[] for _ in range(2)]
-        for j,k in enumerate(row):
-            col[j].append(k)
-            if r==j:dia[0].append(k)
-            if r+j==5:dia[1].append(k)
-    for c in col:
-        if len(set(c))!=6:return 0
-    for d in dia:
-        if len(set(d))!=6:return 0
+    for c in zip(*rows):
+        if 6!=len(c):return 0
+    if 6!=len(set([rows[i][i] for i in range(6)])):return 0
+    if 6!=len(set([rows[i][5-i] for i in range(6)])):return 0
     return 1
 n,*a=open(0)
 print('\n'.join([f'Case#{i+1}: {sol(a[6*i:6*(i+1)])}' for i in range(int(n))]))
