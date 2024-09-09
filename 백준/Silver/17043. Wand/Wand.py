a39362b97ac7193f0ad5318258146ebf4a17b6d7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-09 11:31:40 +0900

    [Silver I] Title: Wand, Time: 148 ms, Memory: 68588 KB -BaekjoonHub

--- 백준/Silver/17043. Wand/Wand.py
+++ 백준/Silver/17043. Wand/Wand.py
@@ -1,17 +1,23 @@
-def dfs(i):
-    if v[i]:return
-    v[i]=True
-    for j in g[i]:
-        t[j]='1'
-        dfs(j)
-n,*a=open(0)
-N,M=map(int,n.split())
-v=[False]*(N+1)
-t=['0']*(N+1)
-g=[set() for i in range(N+1)]
-for i in a:
-    x,y=map(int,i.split())
-    g[y].add(x)
-if g[1]:dfs(1)
-else: t[1]='1'
-print(''.join(t[1:]))
+def main():
+    n,*a=open(0)
+    N,M=map(int,n.split())
+    g=[set() for i in range(N+1)]
+    for i in a:
+        x,y=map(int,i.split())
+        g[y].add(x)
+    v=[False]*(N+1)
+    t=['0']*(N+1)
+    o=g[1].copy()
+    while o:
+        oo=set()
+        for i in o:
+            if not v[i]:
+                v[i]=True
+                n=g[i]-g[1]
+                g[1]|=n
+                oo|=n
+        o,oo=oo,set()
+    for i in g[1]:t[i]='1'
+    if not g[1]:t[1]='1'
+    print(''.join(map(str,t[1:])))
+main()
