1d10006200b21f3b4bfc6bafa7f7f065672bcc72
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-13 13:30:44 +0900

    [Bronze I] Title: Hammasrattad, Time: 232 ms, Memory: 39000 KB -BaekjoonHub

--- 백준/Bronze/7132. Hammasrattad/Hammasrattad.py
+++ 백준/Bronze/7132. Hammasrattad/Hammasrattad.py
@@ -2,8 +2,11 @@ def gcd(a,b):
     while b:a,b=b,a%b
     return a
 def main():
-    M,N=map(int,input().split());v=set()
+    M,N=map(int,input().split())
+    v=[[False for _ in range(N+1)] for _ in range(N+1)]
+    t=0
     for i in range(M,N):
-        for j in range(i+1,N+1):v.add((i//(k:=gcd(j,i)),j//k))
-    print(2*len(v)+1)
+        for j in range(i+1,N+1):
+            if not v[i//(k:=gcd(j,i))][j//k]:v[i//k][j//k]=True;t+=1
+    print(2*t+1)
 main()
