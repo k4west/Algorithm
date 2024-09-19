a697b240ad8c582e7477cfc93ca7c79708250138
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-19 13:39:01 +0900

    [Silver IV] Title: Rhyming Slang, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13527. Rhyming Slang/Rhyming Slang.py
+++ 백준/Silver/13527. Rhyming Slang/Rhyming Slang.py
@@ -1,8 +1,5 @@
 def contains(sl, s):
-    for v in sl:
-        if s.endswith(v):
-            return True
-    return False
+    return any(s.endswith(v) for v in sl)
 
 def main():
     a = open(0)
@@ -10,21 +7,13 @@ def main():
     e = int(next(a))
     sl = []
     for _ in range(e):
-        if contains((wl:=next(a).strip().split()), w):
-            sl.append(wl)
+        if contains((wl:=next(a).strip().split()), w): sl.append(wl)
     t = []
     p = int(next(a))
     for _ in range(p):
         s = next(a).strip()
-        flag = False
-        for group in sl:
-            if contains(group, s):
-                flag = True
-                break
-        if flag:
-            t.append("YES")
-        else:
-            t.append("NO")
+        if any(map(lambda x: contains(x, s), sl)): t.append("YES")
+        else: t.append("NO")
     print('\n'.join(t))
 
 if __name__ == "__main__":
