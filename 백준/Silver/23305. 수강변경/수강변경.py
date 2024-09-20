a61d87628661e59c6fba2eb4ad9666bff1fc4f54
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-20 14:06:22 +0900

    [Silver IV] Title: 수강변경, Time: 448 ms, Memory: 256068 KB -BaekjoonHub

--- 백준/Silver/23305. 수강변경/수강변경.py
+++ 백준/Silver/23305. 수강변경/수강변경.py
@@ -1,10 +1,8 @@
 def main():
-    n,*a=map(int,open(0).read().split())
+    _,a,b=open(0)
     d=[0]*(10**6+1)
-    for i in a[:n]:
-        d[i]+=1
-    for i in a[n:]:
-        if d[i]:
-            d[i]-=1
+    for i in map(int,b.split()):d[i]+=1
+    for i in map(int,a.split()):
+        if d[i]:d[i]-=1
     print(sum(d))
 main()
