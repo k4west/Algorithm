05a509ec42baad5297866787b6ea2ac645ce7df5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-23 11:24:36 +0900

    [Silver IV] Title: Ordered Fractions, Time: 172 ms, Memory: 53396 KB -BaekjoonHub

--- 백준/Silver/27108. Ordered Fractions/Ordered Fractions.py
+++ 백준/Silver/27108. Ordered Fractions/Ordered Fractions.py
@@ -3,15 +3,15 @@ def gcd(a,b):
         a,b=b,a%b
     return a
 
-n = int(input())
-t = {(0, 1), (1, 1)}
-for i in range(1, n+1):
-    for j in range(1, i):
-        d = gcd(i, j)
-        b, a = i//d, j//d  
-        t.add((a, b))
-
-s = [(j, i) for j, i in t]
-s.sort(key=lambda x: x[0]*n/x[1])
-print(len(s))
-print('\n'.join(map(lambda x: f'{x[0]}/{x[1]}', s)))
+def main():
+    n = int(input())
+    t = {(0, 1), (1, 1)}
+    for i in range(1, n+1):
+        for j in range(1, i):
+            d = gcd(i, j)  
+            t.add((j//d, i//d))
+    s = [(j, i) for j, i in t]
+    s.sort(key=lambda x: x[0]*n/x[1])
+    print(len(s))
+    print('\n'.join(map(lambda x: f'{x[0]}/{x[1]}', s)))
+main()
