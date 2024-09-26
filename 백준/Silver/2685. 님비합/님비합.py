685e3ae4b0f2acc89cba7f73818ca603dd2150ce
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-26 22:46:38 +0900

    [Silver III] Title: 님비합, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2685. 님비합/님비합.py
+++ 백준/Silver/2685. 님비합/님비합.py
@@ -1,13 +1,4 @@
-t = []
 for _ in range(int(input())):
-    b, x, y = map(int, input().split())
-    n = 0
-    e = 1
-    bt = []
-    while x or y:
-        n += (x%b+y%b)%b*e
-        x //= b
-        y //= b
-        e *= b
-    t.append(n)
-print('\n'.join(map(str, t)))
+    b, x, y = map(int, input().split()); n, e = 0, 1
+    while x or y: n += (x%b+y%b)%b*e; x //= b; y //= b; e *= b
+    print(n)
