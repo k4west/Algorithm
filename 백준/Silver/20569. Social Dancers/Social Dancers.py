83864ca3b1e020ee0403b5818c8de9dd1090dc6b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-30 17:27:28 +0900

    [Silver IV] Title: Social Dancers, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20569. Social Dancers/Social Dancers.py
+++ 백준/Silver/20569. Social Dancers/Social Dancers.py
@@ -1,16 +1,16 @@
 a = open(0)
-L, F, M = map(int, input().split())
-lead = {}
-follow = {}
+L, F, M = map(int, next(a).split())
+lead = [0]*7
+follow = [0]*7
+nums = [1,1,1,2,2,2,3]
+dances = {("bachata",):0, ("country",):1, ("swing",):2, ("bachata", "country"):3, ("bachata", "swing"):4, ("country", "swing"):5, ("bachata", "country", "swing"):6}
 for _ in range(L):
-    k, *d = input().split()
-    d = tuple(sorted(d))
-    lead[d] = lead.get(d, 0) + 1
+    k, *d = sorted(next(a).strip().split())
+    lead[dances[tuple(d)]] += 1
 for _ in range(F):
-    k, *d = input().split()
-    d = tuple(sorted(d))
-    follow[d] = follow.get(d, 0) + 1
+    k, *d = sorted(next(a).strip().split())
+    follow[dances[tuple(d)]] += 1
 s = 0
-for k, v in lead.items():
-    s += min(v, follow.get(k, 0)) * len(k) / 3
+for i, j, k in zip(lead, follow, nums):
+    s += min(i, j) * k / 3
 print(s*M)
