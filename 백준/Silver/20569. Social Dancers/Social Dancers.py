5267d4899e826d3e93699431cbeadf35704c00e2
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-09-30 17:31:45 +0900

    [Silver IV] Title: Social Dancers, Time: 120 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20569. Social Dancers/Social Dancers.py
+++ 백준/Silver/20569. Social Dancers/Social Dancers.py
@@ -1,16 +1,10 @@
 a = open(0)
-L, F, M = map(int, next(a).split())
-lead = [0]*7
-follow = [0]*7
-nums = [1,1,1,2,2,2,3]
 dances = {("bachata",):0, ("country",):1, ("swing",):2, ("bachata", "country"):3, ("bachata", "swing"):4, ("country", "swing"):5, ("bachata", "country", "swing"):6}
-for _ in range(L):
-    k, *d = sorted(next(a).strip().split())
-    lead[dances[tuple(d)]] += 1
-for _ in range(F):
-    k, *d = sorted(next(a).strip().split())
-    follow[dances[tuple(d)]] += 1
+def f(n):
+    li = [0]*7
+    for _ in range(n):_, *d = sorted(next(a).strip().split());li[dances[tuple(d)]] += 1
+    return li
+L, F, M = map(int, next(a).split())
 s = 0
-for i, j, k in zip(lead, follow, nums):
-    s += min(i, j) * k / 3
+for i, j, k in zip(f(L), f(F), [1,1,1,2,2,2,3]):s += min(i, j) * k / 3
 print(s*M)
