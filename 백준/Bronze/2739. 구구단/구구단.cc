d5083ca8fcc5dad9c58533b25b84dd28fceaa452
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-01 12:47:42 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, i;
+    cin >> N;
+    for (i=1; i<=9; i++){
+        cout << N << " * " << i << " = " << N*i << endl;
+    }
+}
