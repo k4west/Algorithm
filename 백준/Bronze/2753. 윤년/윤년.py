b581f773cbd39e3dcdb1deec350c4cf463125a68
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-03 22:01:13 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,5 +1,1 @@
-year = int(input())
-if (year % 4 == 0 and year % 25 != 0) or year % 16 == 0:
-    print(1)
-else: 
-    print(0)
+print(int(((year:= int(input())) % 4 == 0 and year % 25 != 0) or year % 16 == 0))
