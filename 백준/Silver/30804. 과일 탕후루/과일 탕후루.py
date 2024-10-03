c0687415dc5998e15bd8e7fb1cdb2593b82bbf9c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-03 22:30:59 +0900

    [Silver II] Title: 과일 탕후루, Time: 92 ms, Memory: 34600 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -1,13 +1,15 @@
-N, *a = map(int, open(0).read().split())
-s = m = 0
-counts = [0]*10
-num_fruit = 0
-for e in range(N):
-    if not counts[a[e]]: num_fruit += 1
-    counts[a[e]] += 1
-    while num_fruit > 2:
-        counts[a[s]] -= 1
-        if not counts[a[s]]: num_fruit -= 1
-        s += 1
-    if m < e-s+1: m = e-s+1
-print(m)
+def main():
+    N, *a = map(int, open(0).read().split())
+    s = m = 0
+    counts = [0]*10
+    num_fruit = 0
+    for e in range(N):
+        if not counts[a[e]]: num_fruit += 1
+        counts[a[e]] += 1
+        while num_fruit > 2:
+            counts[a[s]] -= 1
+            if not counts[a[s]]: num_fruit -= 1
+            s += 1
+        if m < e-s+1: m = e-s+1
+    print(m)
+main()
