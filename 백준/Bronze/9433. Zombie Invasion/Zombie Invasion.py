c77f838d5aab9e1ded99b83579c5163692ff47d7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-07 23:56:15 +0900

    [Bronze II] Title: Zombie Invasion, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9433. Zombie Invasion/Zombie Invasion.py
+++ 백준/Bronze/9433. Zombie Invasion/Zombie Invasion.py
@@ -1,6 +1,4 @@
 for _ in range(int(input())):
-    p = [*map(int, input().split())]
-    r = 0
-    for i in range(19): p[19-i], r = (p[19-i] + r) % 2, (p[19-i] + r) // 2
-    p[0] += r
-    print(*p)
+    p = [*map(int, input().split())]; r = 0
+    for i in range(19, 0, -1): r, p[i] = divmod(p[i] + r, 2)
+    p[0] += r; print(*p)
