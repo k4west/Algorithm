70134fff5761595fa4428d55f1a9fedde2bd5cbb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-10 22:32:25 +0900

    [Silver II] Title: Horror Film Night, Time: 796 ms, Memory: 225092 KB -BaekjoonHub

--- 백준/Silver/14995. Horror Film Night/Horror Film Night.py
+++ 백준/Silver/14995. Horror Film Night/Horror Film Night.py
@@ -1,16 +1,18 @@
-a = set([int(x) for x in input().split()][1:])
-b = set([int(x) for x in input().split()][1:])
-n, m, t = 0, 0, 0
-for i in range(1000000):
-    if i in a and i in b:
-        t += 1
-        n, m = 0, 0
-    elif i in a and m == 0:
-        t += 1
-        m += 1
-        n = 0
-    elif i in b and n == 0:
-        t += 1
-        n += 1
-        m = 0
-print(t)
+def main():
+    a = set([int(x) for x in input().split()][1:])
+    b = set([int(x) for x in input().split()][1:])
+    n, m, t = 0, 0, 0
+    for i in range(1000000):
+        if i in a and i in b:
+            t += 1
+            n, m = 0, 0
+        elif i in a and m == 0:
+            t += 1
+            m += 1
+            n = 0
+        elif i in b and n == 0:
+            t += 1
+            n += 1
+            m = 0
+    print(t)
+main()
