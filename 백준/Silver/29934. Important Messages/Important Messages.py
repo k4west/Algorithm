aafb1972ab0d6c060a1d70e39fa20ef3082d38fd
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-18 21:55:35 +0900

    [Silver V] Title: Important Messages, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/29934. Important Messages/Important Messages.py
+++ 백준/Silver/29934. Important Messages/Important Messages.py
@@ -1,3 +1,5 @@
-a = open(0)
-t = [next(a).strip() for _ in range(int(next(a)))]
-print(sum([next(a).strip() in t for _ in range(int(next(a)))]))
+def main():
+    a = open(0)
+    t = [next(a).strip() for _ in range(int(next(a)))]
+    print(sum([next(a).strip() in t for _ in range(int(next(a)))]))
+main()
