fd3502a05ffe2d787cba4c492a41ad549cb2c9aa
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-21 10:20:16 +0900

    [Bronze I] Title: 스노우볼, Time: 292 ms, Memory: 108228 KB -BaekjoonHub

--- 백준/Bronze/17950. 스노우볼/스노우볼.py
+++ 백준/Bronze/17950. 스노우볼/스노우볼.py
@@ -1,6 +1,8 @@
-m = 10**9 + 7
-s = 0
-H, x, *a = map(int, open(0).read().split())
-for i in a[::-1]:
-    s = ((s+i)*x)%m
-print(s)
+def main():
+    m = 10**9 + 7
+    s = 0
+    H, x, *a = map(int, open(0).read().split())
+    for i in a[::-1]:
+        s = (s+i)*x%m
+    print(s)
+main()
