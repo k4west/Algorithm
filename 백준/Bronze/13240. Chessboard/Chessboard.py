8b3cdb107ac0d04d9cfce520038f9ecd4538900c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-24 22:57:35 +0900

    [Bronze III] Title: Chessboard, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13240. Chessboard/Chessboard.py
+++ 백준/Bronze/13240. Chessboard/Chessboard.py
@@ -1,3 +1,2 @@
-s='*.'
 n,m=map(int,input().split())
-print('\n'.join(''.join(s[(i+j)%2] for i in range(m)) for j in range(n)))
+print('\n'.join(''.join('*.'[(i+j)%2] for i in range(m)) for j in range(n)))
