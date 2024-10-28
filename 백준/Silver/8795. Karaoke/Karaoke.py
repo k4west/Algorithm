35bc992a02ae750e0c9bb1ce35f7d1f0c614e92b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-28 12:05:12 +0900

    [Silver IV] Title: Karaoke, Time: 452 ms, Memory: 133896 KB -BaekjoonHub

--- 백준/Silver/8795. Karaoke/Karaoke.py
+++ 백준/Silver/8795. Karaoke/Karaoke.py
@@ -1,16 +1,18 @@
-n, *a = open(0)
-t = []
-v = set('aeiouy')
-for i in a:
-    k, s = i.rstrip().split()
-    k = int(k)
-    c = p = 0
-    for j in s:
-        if j in v:
-            c += 1
-        else:
-            c = 0
-        if c >= k:
-            p += 1
-    t.append(p)
-print('\n'.join(map(str, t)))
+def main():
+    n, *a = open(0)
+    t = []
+    v = set('aeiouy')
+    for i in a:
+        k, s = i.rstrip().split()
+        k = int(k)
+        c = p = 0
+        for j in s:
+            if j in v:
+                c += 1
+            else:
+                c = 0
+            if c >= k:
+                p += 1
+        t.append(p)
+    print('\n'.join(map(str, t)))
+main()
