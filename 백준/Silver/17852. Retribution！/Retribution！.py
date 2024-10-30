2e59b42b1e4027c20d3c90eb640e1cfd8aa2a74f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-10-30 16:44:02 +0900

    [Silver II] Title: Retribution!, Time: 5456 ms, Memory: 337332 KB -BaekjoonHub

--- 백준/Silver/17852. Retribution！/Retribution！.py
+++ 백준/Silver/17852. Retribution！/Retribution！.py
@@ -2,17 +2,17 @@ def dist(i, loc):
     x0, y0 = loc
     return [(((x1-x0)**2+(y1-y0)**2)**.5, j, i) for j, (x1, y1) in enumerate(judges)]
 
-def sum_d(n):
+def sum_d(t):
     s = 0
-    locs = sum([dist(i, [*map(int, next(a).split())]) for i in range(n)], [])
+    locs = sum([dist(i, [*map(int, next(a).split())]) for i in range(t)], [])
     locs.sort()
-    jud_visit = set()
-    loc_visit = set()
+    jud_visit = [0]*n
+    loc_visit = [0]*t
     for d, i, j in locs:
-        if i in jud_visit or j in loc_visit:
+        if jud_visit[i] or loc_visit[j]:
             continue
-        jud_visit.add(i)
-        loc_visit.add(j)
+        jud_visit[i] = 1
+        loc_visit[j] = 1
         s += d
     return s
 
