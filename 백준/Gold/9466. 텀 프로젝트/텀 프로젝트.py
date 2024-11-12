f8a744d8abe31242f32277e2a522fb317b90512f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-12 23:41:55 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1236 ms, Memory: 48104 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,21 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 def main():
     ans = []
-    for _ in range(int(input())):
-        n = int(input())
-        li = [0] + list(map(int, input().split()))
+    a = open(0)
+    for _ in range(int(next(a))):
+        n = int(next(a))
+        li = [0] + list(map(int, next(a).split()))
         d = [0]*(n+1)
         for i in li: d[i] += 1
-        
         left = [i for i in range(1, n+1) if not d[i]]
         for i in left:
             d[(j:=li[i])] -= 1
             if not d[j]: left.append(j)
-    
-        ans.append(str(len(left)))
-    print("\n".join(ans))
+        ans.append(len(left))
+    print("\n".join(map(str, ans)))
 
 if __name__ == "__main__":
     main()
