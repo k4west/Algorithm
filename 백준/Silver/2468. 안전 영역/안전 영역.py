c7616322679eade807931deab243c510d83e21ad
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-12 22:25:18 +0900

    [Silver I] Title: 안전 영역, Time: 216 ms, Memory: 113464 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,9 +1,16 @@
+def dfs(q, nq, v, level):
+    while q or nq:
+        for j in q:
+            r, c = j//n, j%n
+            for dr, dc in d:
+                if 0<=(nr:=r+dr)<n and 0<=(nc:=c+dc)<n and p[(nj:=n*nr+nc)] > level and not v[nj]:
+                    v[nj] = 1
+                    nq.append(nj)
+        q, nq = nq, []
+    return v
+    
 def main():
     t = []
-    n, *p = map(int,open(0).read().split())
-    d = ((-1, 0), (1, 0), (0, -1), (0, 1))
-    max_level = max(p)
-    graph = []
     for level in range(max_level):
         v = [0 for _ in range(n*n)]
         m = 0
@@ -11,15 +18,11 @@ def main():
             if p[i] > level and not v[i]:
                 m += 1
                 v[i] = 1
-                q, nq = [i], []
-                while q or nq:
-                    for j in q:
-                        r, c = j//n, j%n
-                        for dr, dc in d:
-                            if 0<=(nr:=r+dr)<n and 0<=(nc:=c+dc)<n and p[(nj:=n*nr+nc)] > level and not v[nj]:
-                                v[nj] = 1
-                                nq.append(nj)
-                    q, nq = nq, []
+                v = dfs([i], [], v, level)
         t.append(m)
     print(max(t))
+    
+d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+n, *p = map(int,open(0).read().split())
+max_level = max(p)
 main()
