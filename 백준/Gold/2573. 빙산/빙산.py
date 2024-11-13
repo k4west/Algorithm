48e0ed92458071b290448b49f5c9779a152a862b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-13 23:41:05 +0900

    [Gold IV] Title: 빙산, Time: 712 ms, Memory: 209900 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -5,21 +5,19 @@ def dfs(r, c):
     while q:
         r, c = q.popleft()
         for dr, dc in d:
-            if b[(nr:=r+dr)][(nc:=c+dc)] > 0 and not v[m*nr+nc]:
+            if b[m*(nr:=r+dr)+(nc:=c+dc)] > 0 and not v[m*nr+nc]:
                 v[m*nr+nc] = 1
                 q.append((nr, nc))
         
 d = ((-1, 0), (1, 0), (0, -1), (0, 1))
-nm, *b = open(0).read().split('\n')
-n, m = map(int, nm.split())
-b = [[*map(int, i.split())] for i in b]
+n, m, *b = map(int, open(0).read().split())
 y = 0
 while True:
     v = [0]*(n*m)
     flag = False
     for r in range(1, n-1):
         for c in range(1, m-1):
-            if not v[m*r+c] and b[r][c] > 0:
+            if not v[m*r+c] and b[m*r+c] > 0:
                 if flag:
                     exit(print(y))
                 dfs(r, c)
@@ -28,7 +26,7 @@ while True:
         break
     for r in range(1, n-1):
         for c in range(1, m-1):
-            if b[r][c] > 0:
-                b[r][c] -= sum([not v[m*(r+dr)+c+dc] for dr, dc in d])
+            if b[m*r+c] > 0:
+                b[m*r+c] -= sum([not v[m*(r+dr)+c+dc] for dr, dc in d])
     y += 1
 print(0)
