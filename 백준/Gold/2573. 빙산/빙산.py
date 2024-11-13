25ebf57371152490f1a5dbdbeadded29a33b437a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-13 23:48:39 +0900

    [Gold IV] Title: 빙산, Time: 632 ms, Memory: 210788 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,32 +1,30 @@
 from collections import deque
-def dfs(r, c):
-    v[m*r+c] = 1
-    q = deque([(r, c)])
+def dfs(i):
+    v[i] = 1
+    q = deque([i])
     while q:
-        r, c = q.popleft()
-        for dr, dc in d:
-            if b[m*(nr:=r+dr)+(nc:=c+dc)] > 0 and not v[m*nr+nc]:
-                v[m*nr+nc] = 1
-                q.append((nr, nc))
+        i = q.popleft()
+        for di in d:
+            if b[(ni:=i+di)] > 0 and not v[ni]:
+                v[ni] = 1
+                q.append(ni)
         
-d = ((-1, 0), (1, 0), (0, -1), (0, 1))
 n, m, *b = map(int, open(0).read().split())
+d = (-1, 1, m, -m)
 y = 0
 while True:
-    v = [0]*(n*m)
+    v = [0 for _ in range(n*m)]
     flag = False
-    for r in range(1, n-1):
-        for c in range(1, m-1):
-            if not v[m*r+c] and b[m*r+c] > 0:
-                if flag:
-                    exit(print(y))
-                dfs(r, c)
-                flag = True
+    for i in range(1, n*m-1):
+        if not v[i] and b[i] > 0:
+            if flag:
+                exit(print(y))
+            dfs(i)
+            flag = True
     if not flag:
         break
-    for r in range(1, n-1):
-        for c in range(1, m-1):
-            if b[m*r+c] > 0:
-                b[m*r+c] -= sum([not v[m*(r+dr)+c+dc] for dr, dc in d])
+    for i in range(1, n*m-1):
+        if b[i] > 0:
+            b[i] -= sum([not v[i+di] for di in d])
     y += 1
 print(0)
