0ce886d03f0fed638efbfda41bbbaf9bfa7225e7
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-15 21:57:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 56 ms, Memory: 32372 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,41 @@
 import sys
-from collections import deque
 
-def dfs(A, S, v):
-    global dfs_li
+def dfs(S):
     v[S] = True
-    dfs_li.append(S)
+    li.append(S)
     for a in A[S]:
         if not v[a]:
-            dfs(A, a, v)
+            dfs(a)
 
-def bfs(A, S, N):
-    li = []
-    v = [False] * (N+1)
-    q = deque([S])
+def bfs(S):
     v[S] = True
+    q = [S]
     while q:
-        node = q.popleft()
+        node = q.pop(0)
         li.append(node)
         for a in A[node]:
             if not v[a]:
                 v[a] = True
                 q.append(a)
-    print(*li)
 
-if __name__ == "__main__":
-    N, M, S = map(int, sys.stdin.readline().split())
-    A = [[] for _ in range(N + 1)]
+N, M, S = map(int, sys.stdin.readline().split())
+A = [[] for _ in range(N + 1)]
 
-    for _ in range(M):
-        i, j = map(int, sys.stdin.readline().split())
-        A[i].append(j)
-        A[j].append(i)
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    A[i].append(j)
+    A[j].append(i)
 
-    for a in A:
-        if a:
-            a.sort()
-    dfs_li = []
-    dfs_v = [False] * (N+1)
+for a in A:
+    if a:
+        a.sort()
+        
+li = []
+v = [False] * (N+1)
+dfs(S)
+print(" ".join(map(str, li)))
 
-    dfs(A, S, dfs_v)
-    print(*dfs_li)
-    bfs(A, S, N)
+li = []
+v = [False] * (N+1)
+bfs(S)
+print(" ".join(map(str, li)))
