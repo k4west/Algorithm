afa2ff3351282b0184fb480b7897c8a8106db529
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-15 22:40:05 +0900

    [Gold II] Title: 빵집, Time: 1120 ms, Memory: 345432 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,8 +1,5 @@
-def check(r, c):
-    return 0 > r or r >= R or a[r][c]!='.'
-
 def dfs(r, c):
-    if check(r, c):
+    if 0 > r or r >= R or a[r][c]!='.':
         return False
     a[r][c] = 'x'
     if c == C-1:
@@ -12,9 +9,8 @@ def dfs(r, c):
             return True
     return False
 
-if __name__ == '__main__':
-    d = (-1, 0, 1)
-    a = open(0)
-    R, C = map(int, next(a).split())
-    a = [*map(list, a.read().split())]
-    print(sum(dfs(r, 1) for r in range(R)))
+d = (-1, 0, 1)
+a = open(0)
+R, C = map(int, next(a).split())
+a = [*map(list, a.read().split())]
+print(sum(dfs(r, 1) for r in range(R)))
