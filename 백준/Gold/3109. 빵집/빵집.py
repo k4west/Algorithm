f081fa3c82e573ebb252252a3191cbdaf757362f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-15 22:55:37 +0900

    [Gold II] Title: 빵집, Time: 956 ms, Memory: 314196 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,16 +1,22 @@
+from collections import deque
 def dfs(r, c):
-    if 0 > r or r >= R or a[r][c]!='.':
-        return False
-    a[r][c] = 'x'
-    if c == C-1:
-        return True
-    for dr in d:
-        if dfs((nr:=r+dr), c+1):
-            return True
+    q = deque([(r, c)])
+    while q:
+        r, c = q.popleft()
+        a[r][c] = ''
+        t = []
+        c += 1
+        for dr in d:
+            if 0 > (nr:=r+dr) or nr >= R or a[nr][c]!='.':
+                continue
+            if c == C-1:
+                return True
+            t.append((nr, c))
+        q.extendleft(t)
     return False
 
-d = (-1, 0, 1)
+d = (1, 0, -1)
 a = open(0)
 R, C = map(int, next(a).split())
 a = [*map(list, a.read().split())]
-print(sum(dfs(r, 1) for r in range(R)))
+print(sum(dfs(r, 0) for r in range(R)))
