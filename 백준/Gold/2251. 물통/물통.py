59cd6ea9a61b8f4e803c6d1c9246b02decdb7ec6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-18 22:29:56 +0900

    [Gold IV] Title: 물통, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.py
+++ 백준/Gold/2251. 물통/물통.py
@@ -1,32 +1,24 @@
-def check(i, j, liters):
-    if buckets[j]!=liters[j]:
-        t = min(liters[i], buckets[j]-liters[j])
-        liters[i] -= t
-        liters[j] += t
-        if not v[liters[0]][liters[1]][liters[2]]:
-            v[liters[0]][liters[1]][liters[2]] = 1
-            return liters
-    return False
+def candi(a, b, c):
+    A, B, C = buckets
+    for i, j, k in [(max(a - (B - b), 0), min(B, a + b), c), (max(a - (C - c), 0), b, min(C, a + c)), (min(A, a + b), max(b - (A - a), 0), c), (a, max(b - (C - c), 0), min(C, b + c)), (min(A, a + c), b, max(c - (A - a), 0)), (a, min(B, b + c), max(c - (B - b), 0))]:
+        if not (i, j, k) in v:
+            v.add((i, j, k))
+            yield (i, j, k)
     
 def bfs(a, b, c):
     q = [[a, b, c]]
     while q:
-        liters = q.pop()
-        for i in range(3):
-            if liters[i]:
-                for j in range(3):
-                    if i==j:
-                        continue
-                    if (tmp:=check(i, j, [*liters])):
-                        q.append(tmp)
-            elif not i:
-                r[liters[2]] = 1
+        a, b, c = q.pop()
+        if not a:
+            r[c] = 1
+        for na, nb, nc in candi(a, b, c):
+            q.append((na, nb, nc))
 
 if __name__=="__main__":
     buckets = [*map(int, input().split())]
     C = buckets[2]
     r = [0 for _ in range(C+1)]
-    v = [[[0 for _ in range(C+1)] for _ in range(buckets[1]+1)]for _ in range(buckets[0]+1)]
-    v[0][0][C] = 1
+    v = set()
+    v.add((0, 0, C))
     bfs(0, 0, C)
     print(*[i for i in range(C+1) if r[i]])
