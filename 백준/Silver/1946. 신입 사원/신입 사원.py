a76544855457739bd7f49f838dabec1024caaa77
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-19 21:59:52 +0900

    [Silver I] Title: 신입 사원, Time: 628 ms, Memory: 126216 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,19 +1,21 @@
-import sys
-input = sys.stdin.readline
 def main():
+    a = open(0)
     t = []
-    for _ in range(int(input())):
+    for _ in range(int(next(a))):
         c = 1
-        n = int(input())
+        n = int(next(a))
         li = [0] * (n+1)
         for _ in range(n):
-            i, j = map(int, input().split())
+            i, j = map(int, next(a).split())
             li[i] = j
         m = li[1]
         for i in range(2, n+1):
             if m > (j:=li[i]):
                 c += 1
                 m = j
-        t.append(c)
-    print('\n'.join(map(str, t)))
-main()
+        yield(c)
+
+def sol():
+    for i in main():
+        print(i)
+sol()
