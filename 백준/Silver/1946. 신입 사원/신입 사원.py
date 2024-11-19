54934e4071d2ee3ae1c5307ef97a106b587cefa5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-19 21:47:59 +0900

    [Silver I] Title: 신입 사원, Time: 3132 ms, Memory: 59452 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,13 @@
+a = open(0)
+t = []
+for _ in range(int(next(a))):
+    c = 0
+    n = int(next(a))
+    scores = sorted([tuple(map(int, next(a).split())) for _ in range(n)])
+    m = n+1
+    for _, i in scores:
+        if i < m:
+            c += 1
+            m = i
+    t.append(c)
+print('\n'.join(map(str, t)))
