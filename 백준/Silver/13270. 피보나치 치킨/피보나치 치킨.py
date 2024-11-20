a3010ac7d6c71d39a017edce0762476d19abfbb5
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-20 16:43:00 +0900

    [Silver I] Title: 피보나치 치킨, Time: 520 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13270. 피보나치 치킨/피보나치 치킨.py
+++ 백준/Silver/13270. 피보나치 치킨/피보나치 치킨.py
@@ -1,20 +1,22 @@
-N = int(input())
-chicken = [0] * (N+1)
-a = 2; b = 1
-while a <= N:
-    chicken[a] = b
-    a, b = a+b, a
-min_dp = [i for i in chicken]
-max_dp = [i for i in chicken]
-
-for i in range(2, N+1):
-    mn = min_dp[i] or N
-    mx = max_dp[i]
-    for j in range(2, i//2+1):
-        if mn > (t:=min_dp[j]+min_dp[i-j]):
-            mn = t
-        if mx < (t:=max_dp[j]+max_dp[i-j]):
-            mx = t
-    min_dp[i] = mn
-    max_dp[i] = mx
-print(min_dp[N], max_dp[N])
+def main():
+    N = int(input())
+    chicken = [0] * (N+1)
+    a = 2; b = 1
+    while a <= N:
+        chicken[a] = b
+        a, b = a+b, a
+    min_dp = [i for i in chicken]
+    max_dp = [i for i in chicken]
+    
+    for i in range(2, N+1):
+        mn = min_dp[i] or N
+        mx = max_dp[i]
+        for j in range(2, i//2+1):
+            if mn > (t:=min_dp[j]+min_dp[i-j]):
+                mn = t
+            if mx < (t:=max_dp[j]+max_dp[i-j]):
+                mx = t
+        min_dp[i] = mn
+        max_dp[i] = mx
+    print(min_dp[N], max_dp[N])
+main()
