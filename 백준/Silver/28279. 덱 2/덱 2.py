d2e03a48aa7d1789779baecea939a83aaf3654f8
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-28 22:26:52 +0900

    [Silver IV] Title: 덱 2, Time: 564 ms, Memory: 102424 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -8,31 +8,31 @@ def main():
     for _ in range(int(next(a))):
         i, *n = next(a).strip().split()
         if i == '1':
-            arr.appendleft(int(n[0]))
+            arr.appendleft(n[0])
             count += 1
         elif i == '2':
-            arr.append(int(n[0]))
+            arr.append(n[0])
             count += 1
         elif i == '3':
             if count:
                 t.append(arr.popleft())
                 count -= 1
             else:
-                t.append(-1)
+                t.append('-1')
         elif i == '4':
             if count:
                 t.append(arr.pop())
                 count -= 1
             else:
-                t.append(-1)
+                t.append('-1')
         elif i == '5':
-            t.append(count)
+            t.append(str(count))
         elif i == '6':
-            t.append(int(not bool(count)))
+            t.append('10'[bool(count)])
         elif i == '7':
-            t.append(arr[0] if count else -1)
+            t.append(arr[0] if count else '-1')
         elif i == '8':
-            t.append(arr[-1] if count else -1)
-    print('\n'.join(map(str, t)))
+            t.append(arr[-1] if count else '-1')
+    print('\n'.join(t))
 
 main()
