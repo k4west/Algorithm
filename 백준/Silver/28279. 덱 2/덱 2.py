75e6cf4bab2c9ecd0bad2db44e5f9746cf3d1e79
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-11-28 22:30:45 +0900

    [Silver IV] Title: 덱 2, Time: 300 ms, Memory: 131528 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -1,38 +1,21 @@
 from collections import deque
 
 def main():
-    a = open(0)
-    arr = deque([])
-    count = 0
-    t = []
-    for _ in range(int(next(a))):
-        i, *n = next(a).strip().split()
-        if i == '1':
-            arr.appendleft(n[0])
-            count += 1
-        elif i == '2':
-            arr.append(n[0])
-            count += 1
+    a = iter(open(0).read().split()); next(a)
+    arr = deque([]); count = 0; t = []
+    for i in a:
+        if (i:=i.strip()) == '1': arr.appendleft(next(a).strip()); count += 1
+        elif i == '2': arr.append(next(a).strip()); count += 1
         elif i == '3':
-            if count:
-                t.append(arr.popleft())
-                count -= 1
-            else:
-                t.append('-1')
+            if count: t.append(arr.popleft()); count -= 1
+            else: t.append('-1')
         elif i == '4':
-            if count:
-                t.append(arr.pop())
-                count -= 1
-            else:
-                t.append('-1')
-        elif i == '5':
-            t.append(str(count))
-        elif i == '6':
-            t.append('10'[bool(count)])
-        elif i == '7':
-            t.append(arr[0] if count else '-1')
-        elif i == '8':
-            t.append(arr[-1] if count else '-1')
+            if count: t.append(arr.pop()); count -= 1
+            else: t.append('-1')
+        elif i == '5': t.append(str(count))
+        elif i == '6': t.append('10'[bool(count)])
+        elif i == '7': t.append(arr[0] if count else '-1')
+        elif i == '8': t.append(arr[-1] if count else '-1')
     print('\n'.join(t))
 
 main()
