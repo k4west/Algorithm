995ee4c08fa0c30be7f331e24f85abe1e4b1c1ad
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-12-19 20:48:38 +0900

    [Bronze II] Title: Nine Knights, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31040. Nine Knights/Nine Knights.py
+++ 백준/Bronze/31040. Nine Knights/Nine Knights.py
@@ -1,11 +1,12 @@
+a = open(0)
 d = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
 t = []
-n = int(input())
+n = int(next(a))
 for _ in range(n):
     knights = []
     flag=False
     for i in range(5):
-        for j, k in enumerate(input()):
+        for j, k in enumerate(next(a).strip()):
             if k=='k':
                 knights.append((i, j))
     for i, j in knights:
@@ -15,5 +16,5 @@ for _ in range(n):
                 break
         if flag: break
     t.append('in'*flag+'valid')
-    if _: input()
+    if _: next(a)
 print('\n'.join(t))
