a8997551e242a81a81a180a6bcc887cafb425561
Yosep Kim <125423305+k4west@users.noreply.github.com>
2024-12-25 10:50:09 +0900

    [Silver IV] Title: 비 오는 날, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/31066. 비 오는 날/비 오는 날.py
+++ 백준/Silver/31066. 비 오는 날/비 오는 날.py
@@ -1,21 +1,4 @@
-def main():
-    a = open(0)
-    t = []
-    for _ in range(int(next(a))):
-        N, M, K = map(int, next(a).split())
-        M *= K
-        if N<=M:
-            t.append(1)
-        elif M==1:
-            t.append(-1)
-        else:
-            c = 0
-            while N > 0:
-                c += 1
-                N -= M
-                if N > 0:
-                    c += 1
-                    N += 1
-            t.append(c)
-    print('\n'.join(map(str, t)))
-main()
+a = open(0)
+for _ in range(int(next(a))):
+    N, M, K = map(int, next(a).split())
+    print(2*max(0, (N-2)//M)+1 if (M:=M*K-1) else 1-2*(N!=1))
