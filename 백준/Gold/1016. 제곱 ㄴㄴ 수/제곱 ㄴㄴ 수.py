4bdf5452db96439e192c412b7f7c68a1b1091e8f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-10 19:46:03 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 156 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,9 +1,9 @@
 def main():
     x, y = map(int, input().split())
     arr = [True for _ in range(y-x+1)]
-    for i in map(lambda x:x**2, range(2, int(y**.5)+1)):
-        s = i-x%i if x%i else 0
-        for j in range(s, y-x+1, i):
+    for i in range(2, int(y**.5)+1):
+        i *= i
+        for j in range(i-x%i if x%i else 0, y-x+1, i):
             arr[j] = False
     print(sum(arr))
 if __name__ == "__main__":
