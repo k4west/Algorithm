4766126117dfec5621297cd7e3994b3d257a575c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-10 20:31:40 +0900

    [Gold III] Title: 나머지 합, Time: 376 ms, Memory: 259004 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -0,0 +1,11 @@
+def main():
+    N, M = map(int, input().split())
+    arr = [*map(int, input().split())]+[0]
+    remain = [0 for _ in range(M)]
+    for i in range(N):
+        arr[i] = (arr[i] + arr[i-1])%M
+        remain[arr[i]] += 1
+    t = remain[0]*(remain[0]+1)//2 + sum(remain[i]*(remain[i]-1)//2 for i in range(1, M))
+    print(t)
+if __name__ == "__main__":
+    main()
