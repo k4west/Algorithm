08590eaa024b13802fc6cc92cb92463a85bf6379
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-10 19:11:37 +0900

    [Gold II] Title: 피보나치 수 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,17 @@
+def main():
+    d = {1:1, 2:1}
+    n = int(input())
+
+    def f(n):
+        if d.get(n):
+            return d[n]
+        if n % 2:
+            d[n] = (pow(f(n//2+1), 2) + pow(f(n//2), 2)) % 1_000_000
+        else:
+            d[n] = (pow(f(n//2+1), 2) - pow(f(n//2-1), 2)) % 1_000_000
+        return d[n]
+
+    print(f(n))
+
+if __name__ == "__main__":
+    main()
