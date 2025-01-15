bbc41d38e3877ef8c73d129387983386a936a9e6
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-15 23:08:46 +0900

    [Platinum I] Title: 아파트 임대, Time: 4996 ms, Memory: 40944 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.py
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.py
@@ -1,24 +1,26 @@
-primes = [2, 3, 5, 7, 11]
-cnt = 0
-for n in map(int, open(0).read().split()[1:]):
-    flag = True
-    m = 2*n+1
-    s, d = 0, m-1
-    while d%2 == 0:
-        s += 1
-        d //= 2
-    for a in primes:
-        x, y = 1, d
-        while y > 0:
-            if y%2: x = (x*a)%m
-            a = (a*a)%m
-            y //= 2
-        if x == 1 or x == m-1: continue
-        for _ in range(s-1):
-            x = x*x%m
-            if x == m-1: break
-        else:
-            flag = False
-            break
-    cnt += flag or m in primes
-print(cnt)
+def main():
+    primes = [2, 3, 5, 7, 11]
+    cnt = 0
+    for n in map(int, open(0).read().split()[1:]):
+        flag = True
+        m = 2*n+1
+        s, d = 0, m-1
+        while d%2 == 0:
+            s += 1
+            d //= 2
+        for a in primes:
+            x, y = 1, d
+            while y > 0:
+                if y%2: x = (x*a)%m
+                a = (a*a)%m
+                y //= 2
+            if x == 1 or x == m-1: continue
+            for _ in range(s-1):
+                x = x*x%m
+                if x == m-1: break
+            else:
+                flag = False
+                break
+        cnt += flag or m in primes
+    print(cnt)
+main()
