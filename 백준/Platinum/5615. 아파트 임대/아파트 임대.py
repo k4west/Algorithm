e04e26fb7948666db0ae559f88ae60f54ab1aa25
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-15 22:31:18 +0900

    [Platinum I] Title: 아파트 임대, Time: 668 ms, Memory: 125956 KB -BaekjoonHub

---
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.py
@@ -0,0 +1,28 @@
+primes = [2, 3, 5, 7, 11]
+
+def powmod(a, d, n):
+    result = 1
+    while d > 0:
+        if d%2: result = (result*a)%n
+        a = (a*a)%n
+        d //= 2
+    return result
+
+def miller_rabin(n):
+    s, d = 0, n-1
+    while d%2 == 0:
+        s += 1
+        d //= 2
+    for a in primes:
+        if a >= n: break
+        x = powmod(a, d, n)
+        if x == 1 or x == n-1: continue
+        for _ in range(s-1):
+            x = powmod(x, 2, n)
+            if x == n-1: break
+        else: return False
+    return True
+    
+
+if __name__ == "__main__":
+    print(sum(miller_rabin(2*n+1) for n in map(int, open(0).read().split()[1:])))
