30ab0151ec52bd04028623e6826cb6f57caf1797
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-15 23:14:59 +0900

    [Platinum I] Title: 아파트 임대, Time: 468 ms, Memory: 125952 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.py
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.py
@@ -1,28 +1,32 @@
+primes = [2, 7, 61]
+
+def powmod(a, d, n):
+    result = 1
+    while d > 0:
+        if d%2: result = (result*a)%n
+        a = (a*a)%n
+        d //= 2
+    return result
+
+def miller_rabin(m):
+    s, d = 0, m-1
+    while d%2 == 0:
+        s += 1
+        d //= 2
+    for a in primes:
+        x = powmod(a, d, m)
+        if x == 1 or x == m-1: continue
+        for _ in range(s-1):
+            x = powmod(x, 2, m)
+            if x == m-1: break
+        else: return False
+    return True
+    
 def main():
-    primes = [2, 7, 61]
     cnt = 0
     for n in map(int, open(0).read().split()[1:]):
-        if (m:=2*n+1) in primes or n < 4:
-            cnt += 1
-            continue
-        flag = True
-        s, d = 0, m-1
-        while d%2 == 0:
-            s += 1
-            d //= 2
-        for a in primes:
-            x, y = 1, d
-            while y > 0:
-                if y%2: x = (x*a)%m
-                a = (a*a)%m
-                y //= 2
-            if x == 1 or x == m-1: continue
-            for _ in range(s-1):
-                x = x*x%m
-                if x == m-1: break
-            else:
-                flag = False
-                break
-        cnt += flag
+        cnt += miller_rabin((m:=2*n+1)) or m in primes
     print(cnt)
-main()
+
+if __name__ == "__main__":
+    main()
