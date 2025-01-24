52f6d1bfe9b00bda8299b9a05faae080f14abb27
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-01-24 10:58:11 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.py
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.py
@@ -1,19 +1,17 @@
 a = open(0)
 N, M = map(int, next(a).split())
 arr = [[] for _ in range(M)]
-t = 2*N*M
+t = N*M
 for _ in range(N):
     *ms, = map(int, next(a).split())
     p = 0
     for i, m in enumerate(ms):
-        t += abs(m-p)
+        if m > p: t += m-p
         arr[i].append(m)
         p = m
-    t += p
 for ns in arr:
     p = 0
     for i, n in enumerate(ns):
-        t += abs(n-p)
+        if n > p: t += n-p
         p = n
-    t += p
-print(t)
+print(2*t)
