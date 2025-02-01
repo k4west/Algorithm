6cea6a834d3ade4580d4e39b0eee3b601d409d8a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-02-01 10:20:43 +0900

    [Silver II] Title: 연속합, Time: 72 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,9 @@
-n, *arr = map(int, open(0).read().split())
-dp = [0 for _ in range(n+1)]
-
-for i in range(n):
-    dp[i] = max(arr[i], dp[i-1] + arr[i])
-dp[-1] = dp[0]
-print(max(dp))
+def main():
+    n, *arr = map(int, open(0).read().split())
+    dp = [0 for _ in range(n+1)]
+    
+    for i in range(n):
+        dp[i] = max(arr[i], dp[i-1] + arr[i])
+    dp[-1] = dp[0]
+    print(max(dp))
+main()
