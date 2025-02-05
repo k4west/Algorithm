f61e136c4f8718f040624a1649a3a8a0e2debec1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-02-05 21:10:53 +0900

    [Bronze II] Title: 카드 역배치, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.py
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.py
@@ -1,5 +1,8 @@
-cards = [i for i in range(21)]
-for _ in range(10):
-    a, b = map(int, input().split())
-    cards[a:b+1] = cards[b:a-1:-1]
-print(*cards[1:])
+def main():
+    cards = [i for i in range(21)]
+    for i in open(0):
+        a, b = map(int, i.split())
+        cards[a:b+1] = cards[b:a-1:-1]
+    print(" ".join(map(str, cards[1:])))
+
+main()
