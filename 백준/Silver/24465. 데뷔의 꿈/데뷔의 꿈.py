c51a9cfe8eca33da467c2432303253cba796bcca
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-02-15 21:23:52 +0900

    [Silver IV] Title: 데뷔의 꿈, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/24465. 데뷔의 꿈/데뷔의 꿈.py
+++ 백준/Silver/24465. 데뷔의 꿈/데뷔의 꿈.py
@@ -4,15 +4,18 @@ def f(n):
         if n <= d[i]:
             return i
     return 0
-a = open(0)
-v = set()
-t = []
-for _ in range(7):
-    k = int("".join(map(lambda x: x.strip().zfill(2), next(a).split())))
-    v.add(f(k))
-for _ in range(int(next(a))):
-    k = int("".join(map(lambda x: x.strip().zfill(2), next(a).split())))
-    if f(k) not in v:
-        t.append(k)
-t.sort()
-print('\n'.join(map(lambda x: f'{x//100} {x%100}', t)) if t else "ALL FAILED")
+
+def main():
+    a = open(0)
+    v = set()
+    t = []
+    for _ in range(7):
+        k = int("".join(map(lambda x: x.strip().zfill(2), next(a).split())))
+        v.add(f(k))
+    for _ in range(int(next(a))):
+        k = int("".join(map(lambda x: x.strip().zfill(2), next(a).split())))
+        if f(k) not in v:
+            t.append(k)
+    t.sort()
+    print('\n'.join(map(lambda x: f'{x//100} {x%100}', t)) if t else "ALL FAILED")
+main()
