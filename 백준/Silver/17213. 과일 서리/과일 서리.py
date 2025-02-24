e4652b2770b194433dc968009bc519964c68331f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-02-25 08:35:57 +0900

    [Silver II] Title: 과일 서리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17213. 과일 서리/과일 서리.py
+++ 백준/Silver/17213. 과일 서리/과일 서리.py
@@ -1,7 +1,7 @@
 N, M = map(int, open(0).read().split())
-dp = [[1] for _ in range(M)]
-for i in range(M):
-    for j in range(i):
-        dp[i].append(dp[i-1][j]+dp[i-1][j+1])
-    dp[i].append(0)
-print(dp[M-1][N-1])
+t = s = 1
+for i in range(N, M):
+    t *= i
+for i in range(2, M-N+1):
+    s *= i
+print(t//s)
