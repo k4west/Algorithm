8fd74fa98333ce449d85e324f08ad5e78d6b901f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-03-27 22:41:30 +0900

    [Silver IV] Title: Time Zone, Time: 632 ms, Memory: 59840 KB -BaekjoonHub

--- 백준/Silver/18987. Time Zone/Time Zone.py
+++ 백준/Silver/18987. Time Zone/Time Zone.py
@@ -1,7 +1,9 @@
 def main():
+    t = []
     i = open(0)
     for _ in range(int(next(i))):
         a, b, s = next(i).split()
         o = (int(a)*60 + round((float(s[3:]) - 8) * 60) + int(b)) % 1440
-        print(f"{o//60%24:02d}:{o%60:02d}")
+        t.append(f"{o//60%24:02d}:{o%60:02d}")
+    print('\n'.join(t))
 main()
