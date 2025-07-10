548ac7c6556167ef4fab226e7182f7e9ad35581e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 13:32:42 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,12 +1,1 @@
-import sys
-from itertools import zip_longest
-
-input = sys.stdin.readline
-
-li = [[] for _ in range(5)]
-for n in range(5):
-    li[n] = list(input().strip())
-
-# Transpose the input list
-answer = [''.join(row) for row in zip_longest(*li, fillvalue='')]
-print(*answer, sep="")
+print(''.join(''.join(i).replace(' ', '') for i in zip(*[input().ljust(15) for _ in range(5)])))
