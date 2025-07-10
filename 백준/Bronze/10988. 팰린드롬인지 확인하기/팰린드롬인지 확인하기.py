4821fc45b5082d6f8d433141365a640373ae8359
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 12:25:39 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,6 +1,7 @@
 s = input()
+p = 1
 for i in range(len(s)//2):
-    if s[i] != s[-(1+i)]:
-        print(0)
-        exit()
-print(1)
+    if s[i] != s[-i-1]:
+        p = 0
+        break
+print(p)
