1fee56ebb3d563867ea755a829f3f4545cd1300d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 11:24:07 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,7 +1,7 @@
-import sys
-m = [0,0]
+M = 0
 for i in range(1, 10):
-    n = int(sys.stdin.readline())
-    if n > m[0]:
-        m = [n,i]
-print(*m, sep="\n")
+    n = int(input())
+    if n > M:
+        M = n
+        idx = i
+print(M, idx, sep='\n')
