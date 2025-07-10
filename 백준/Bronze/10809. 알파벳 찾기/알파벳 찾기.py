a17fde339b2b8fd59058dcafd3c015585d5a7c73
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 12:07:48 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,3 +1,6 @@
-s = input()
-for a in range(97, 123):
-    print(s.find(chr(a)), end=" ")
+S = input()
+li = [-1 for _ in range(26)]
+for i, s in enumerate(S):
+    if li[(j:=ord(s)-97)] == -1:
+        li[j] = i
+print(*li)
