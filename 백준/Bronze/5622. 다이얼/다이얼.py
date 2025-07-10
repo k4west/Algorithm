6e1463e9bb521e5d19049def2ae0f2efa68bebec
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 12:23:32 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,8 +1,9 @@
-d = input()
-r = 0
-for s in d:
-    n = ord(s)
-    if n <= 83:
-        r += min((n-56)//3, 8)
-    else: r += min((n-57)//3, 10)
-print(r)
+t = 0
+for s in input():
+    n = ord(s) - 56
+    if n >= 27:
+        n -= 1
+    if n == 33:
+        n -= 1
+    t += n // 3
+print(t)
