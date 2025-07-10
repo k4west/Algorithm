d9efbb8dd0bc933d71f692855e9b1bf4aac7c811
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 11:28:25 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,8 +1,7 @@
-import sys
-N, M = map(int, sys.stdin.readline().split())
-B = [0]*N
+N, M = map(int, input().split())
+arr = [0 for _ in range(N)]
 for _ in range(M):
-    i, j, k = map(int, sys.stdin.readline().split())
-    for p in range(i-1,j):
-        B[p] = k
-print(*B)
+    i, j, k = map(int, input().split())
+    for idx in range(i-1, j):
+        arr[idx] = k
+print(*arr)
