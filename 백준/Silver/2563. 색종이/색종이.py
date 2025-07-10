d1cdf9fceb90a0b658812d5a954e65e0a8bc6043
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 13:37:37 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,8 +1,7 @@
-n = int(input())
-a = [[0 for i in range(100)] for j in range(100)]
-for i in range(n):
-    x, y = map(int, input().split())
-    for s in range(x, x + 10):
-        for t in range(y, y + 10):
-            a[s][t] = 1
-print(sum(sum(a,[])))
+paper = [[0 for _ in range(101)] for _ in range(101)]
+for i in range(int(input())):
+    i, j = map(int, input().split())
+    for p in range(i, i+10):
+        for q in range(j, j+10):
+            paper[p][q] = 1
+print(sum(sum(paper, [])))
