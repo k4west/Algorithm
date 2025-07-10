da553d0d5170f8c8d1b8d0a786403c2b4f37f2df
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 13:11:01 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,5 +1,4 @@
 N, M = map(int, input().split())
 A = [[*map(int, input().split())] for _ in range(N)]
 B = [[*map(int, input().split())] for _ in range(N)]
-for i in range(N):
-    print(*[A[i][j] + B[i][j] for j in range(M)])
+print('\n'.join([' '.join(map(str, [A[i][j] + B[i][j] for j in range(M)])) for i in range(N)]))
