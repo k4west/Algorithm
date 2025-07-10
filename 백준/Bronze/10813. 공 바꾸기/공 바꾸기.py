149fae841c79fdafd5a3b3921e643facee3993cb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 11:31:31 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,7 +1,6 @@
-import sys
-N, M = map(int, sys.stdin.readline().split())
-B = list(range(1,N+1))
+N, M = map(int, input().split())
+arr = [i for i in range(N+1)]
 for _ in range(M):
-    i, j = map(int, sys.stdin.readline().split())
-    B[i-1], B[j-1] = B[j-1], B[i-1]
-print(*B)
+    i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i]
+print(*arr[1:])
