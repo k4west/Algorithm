b43dc3ae905a88d8b823f8451203c5de98984fc3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-10 11:45:17 +0900

    [Bronze I] Title: 단어 공부, Time: 64 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,9 @@
-import sys
-input = sys.stdin.readline
-
-str1 = input().replace('\n', '').upper()
-maxn = 0
-for i in set(str1):
-    count = str1.count(i)
-    if maxn < count:
-        maxn = count
-        answer = [i]
-    elif maxn == count:
-        answer.append(i)
-print('?' if len(answer) > 1 else answer[0])
+s = input().lower()
+M, c = '', 0
+for i in range(26):
+    if c < (t:=s.count(chr(i+97))):
+        M = chr(i+65)
+        c = t
+    elif c == t:
+        M = '?'
+print(M)
