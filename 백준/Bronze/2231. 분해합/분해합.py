defc24861c2721c0b8ce04df9f4ebd9b6cb0211d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-12 14:46:18 +0900

    [Bronze II] Title: 분해합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,9 +1,11 @@
 N = int(input())
-s = max(N - 63, 1)
-for n in range(s,N):
-    t = n + sum(map(int, str(n)))
-    if t == N:
-        print(n)
+g = 0
+for n in range(max(1, N-54), N):
+    x = t = n
+    while n:
+        x += n%10
+        n //= 10
+    if x == N:
+        g = t
         break
-else:
-    print(0)
+print(g)
