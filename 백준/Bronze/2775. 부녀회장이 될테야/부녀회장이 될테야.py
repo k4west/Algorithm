fefc07ac2f07ea98fab10c7215ff0474163ed16a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-14 13:50:37 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,10 +1,10 @@
-import sys
-T = int(sys.stdin.readline())
-for _ in range(T):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
-    A = list(range(1,n+1))
-    for _ in range(k):
-        for i in range(1, n):
-            A[i] += A[i-1]
-    print(A[n-1])
+answer = []
+apt = [[i for i in range(15)]] + [[0 for _ in range(15)] for _ in range(15)]
+for i in range(1, 15):
+    for j in range(1, 15):
+        apt[i][j] = apt[i-1][j] + apt[i][j-1]
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    answer.append(apt[k][n])
+print(*answer, sep='\n')
