633c65ece9e34cdedbe369e3e3e896b9af52c949
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-14 13:39:56 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,16 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 N, K = map(int, input().split())
-C = []
-for i in range(N):
-    c = int(input())
-    if c <= K:
-        C.append(c)
-
-answer = 0
-while K > 0:
-    answer += K // C[-1]
-    K %= C[-1]
-    C.pop()
-print(answer)
+coins = [int(input()) for _ in range(N)]
+count = 0
+for coin in coins[::-1]:
+    if coin <= K:
+        m = K//coin
+        K -= coin * m
+        count += m
+print(count)
