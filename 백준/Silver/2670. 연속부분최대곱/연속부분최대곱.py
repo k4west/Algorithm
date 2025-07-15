af1f2553747cc2b287164ae080d58183af865b9d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-15 09:52:17 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 292 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -1,12 +1,6 @@
 N = int(input())
-fs = [float(input()) for _ in range(N)]
-dp = [1 for _ in range(N)]
-m = 0
-
+fs = [float(input()) for _ in range(N)] + [0]
 for i in range(N):
-    dp[i] = max(dp[i-1]*fs[i], fs[i], dp[i])
-    m = max(m, fs[i])
-if m < 1:
-    print(f"{m:.3f}")
-else:
-    print(f"{max(dp):.3f}")
+    if fs[i] < (t:=fs[i] * fs[i-1]):
+        fs[i] = t
+print(f"{max(fs):.3f}")
