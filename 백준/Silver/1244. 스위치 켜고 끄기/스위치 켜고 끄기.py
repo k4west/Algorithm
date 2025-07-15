9b4a78e41a5be2cad4125e750631bfaa8e614823
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-15 10:09:24 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,31 +1,16 @@
-def main():
-    def change(i):
-        switches[i] ^= 1
-    
-    
-    def multiple(n, m):
-        for i in range(n-1, m, n):
-            change(i)
-
-
-    def symmetry(n, m):
-        count = 1
-        switches[n] ^= 1
-        while count <= n < m-count and switches[n+count] == switches[n-count]:
-            change(n-count)
-            change(n+count)
-            count += 1
-    
-    a = open(0)
-    m = int(next(a))
-    *switches, = map(int, next(a).split())
-    
-    for _ in range(int(next(a))):
-        gender, number = map(int, next(a).split())
-        if gender == 1: multiple(number, m)
-        else: symmetry(number-1, m)
-    
-    print('\n'.join(' '.join(map(str, switches[20*i:20*i+20])) for i in range((m+19)//20)))
-
-    
-main()
+N = int(input())
+switch = [*map(int, input().split())]
+for _ in range(int(input())):
+    gender, num = map(int, input().split())
+    if gender == 1:
+        for i in range(num-1, N, num):
+            switch[i] ^= 1
+    else:
+        t = 0
+        for i in range(1, min(num, N-num+1)):
+            if switch[num-i-1] != switch[num+i-1]:
+                break
+            t = i
+        for i in range(num-t-1, num+t):
+            switch[i] ^= 1
+print('\n'.join(' '.join(map(str, switch[20*i:20*(i+1)])) for i in range(N//20+bool(N%20))))
