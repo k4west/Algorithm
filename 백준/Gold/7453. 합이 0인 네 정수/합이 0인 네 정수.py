4230ca4ec8839d5def285a647200f7460801db60
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-23 17:19:54 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7188 ms, Memory: 642196 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,24 +1,46 @@
-def sum4(A, B, C, D):
-    """
-    A와 B에 있는 원소로 만들 수 있는 합의 모든 경우를 생성
-    C와 D도 똑같이 합해서 AB에 상응 하는 값이 있는지 확인
-    """
-    dct = {}
-    count = 0
-    for a in A:
-        for b in B:
-            dct[ab] = dct.get((ab := a+b), 0) + 1
-    for c in C:
-        for d in D:
-            count += dct.get(-c-d, 0)
+def sum2(A, B, C, D):
+    AB = [a+b for a in A for b in B]
+    CD = [-c-d for c in C for d in D]
+    AB.sort()
+    CD.sort()
+    return AB, CD
+
+def sum4(AB, CD, n):
+    count = i = j = 0
+    N = n*n
+    while i < N and j < N:
+        if (ab := AB[i]) == (cd := CD[j]):
+            ti = tj = 1
+            while i < N-1 and AB[i+1] == ab:
+                i += 1
+                ti += 1
+            while j < N-1 and CD[j+1] == cd:
+                j += 1
+                tj += 1
+            count += ti*tj
+            i += 1
+        elif ab < cd:
+            i += 1
+        else:
+            j += 1
+
     return count
 
 
-if __name__ == "__main__":
-    Arr = [[], [], [], []]
-    for row in open(0).read().split('\n'):
-        if ' ' not in row:
-            continue
+def main():
+    n, *arr = open(0).read().split('\n')
+    n = int(n)
+    Arr = [[], [], [], []]  # A, B, C, D
+
+    for row in arr:
+        if not row.strip('\n'):
+            break
         for i, num in enumerate(map(int, row.split(' '))):
             Arr[i].append(num)
-    print(sum4(*Arr))
+
+    AB, CD = sum2(*Arr)
+    print(sum4(AB, CD, n))
+
+
+if __name__ == "__main__":
+    main()
