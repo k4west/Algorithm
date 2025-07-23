29731c700e3354cecfbc1eb7179d198411fb345b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-23 17:32:37 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6868 ms, Memory: 650112 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,46 +1,34 @@
-def sum2(A, B, C, D):
-    AB = [a+b for a in A for b in B]
-    CD = [-c-d for c in C for d in D]
-    AB.sort()
-    CD.sort()
-    return AB, CD
+n, *arr = open(0).read().split('\n')
+n = int(n)
+Arr = [[], [], [], []]  # A, B, C, D
 
-def sum4(AB, CD, n):
-    count = i = j = 0
-    N = n*n
-    while i < N and j < N:
-        if (ab := AB[i]) == (cd := CD[j]):
-            ti = tj = 1
-            while i < N-1 and AB[i+1] == ab:
-                i += 1
-                ti += 1
-            while j < N-1 and CD[j+1] == cd:
-                j += 1
-                tj += 1
-            count += ti*tj
+for row in arr:
+    if not row.strip('\n'):
+        break
+    for i, num in enumerate(map(int, row.split(' '))):
+        Arr[i].append(num)
+
+AB = [a + b for a in Arr[0] for b in Arr[1]]
+CD = [c + d for c in Arr[2] for d in Arr[3]]
+AB.sort()
+CD.sort()
+
+count = i = 0
+N = j = n * n - 1
+while i <= N and j >= 0:
+    if (ab := AB[i]) == -(cd := CD[j]):
+        ti = tj = 1
+        while i < N and AB[i + 1] == ab:
             i += 1
-        elif ab < cd:
-            i += 1
-        else:
-            j += 1
+            ti += 1
+        while j > 0 and CD[j - 1] == cd:
+            j -= 1
+            tj += 1
+        count += ti * tj
+        i += 1
+    elif ab < -cd:
+        i += 1
+    else:
+        j -= 1
 
-    return count
-
-
-def main():
-    n, *arr = open(0).read().split('\n')
-    n = int(n)
-    Arr = [[], [], [], []]  # A, B, C, D
-
-    for row in arr:
-        if not row.strip('\n'):
-            break
-        for i, num in enumerate(map(int, row.split(' '))):
-            Arr[i].append(num)
-
-    AB, CD = sum2(*Arr)
-    print(sum4(AB, CD, n))
-
-
-if __name__ == "__main__":
-    main()
+print(count)
