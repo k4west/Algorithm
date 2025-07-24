942f82c1bc8af90e5174daa2bb9015592af4450c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-24 17:20:09 +0900

    [Gold III] Title: 고기잡이, Time: 420 ms, Memory: 110900 KB -BaekjoonHub

--- 백준/Gold/7573. 고기잡이/고기잡이.py
+++ 백준/Gold/7573. 고기잡이/고기잡이.py
@@ -1,18 +1,14 @@
 N, I, M = map(int, input().split())
-fishes = {}
+fishes = []
 xs = set()
 ys = set()
 for _ in range(M):
     r, c = map(int, input().split())
+    fishes.append((r, c))
     xs.add(r)
     ys.add(c)
-    if r in fishes:
-        fishes[r].append(c)
-    else:
-        fishes[r] = [c]
 
-for r in fishes.keys():
-    fishes[r].sort()
+fishes.sort()
 xs = sorted(xs)
 ys = sorted(ys)
 
@@ -22,10 +18,9 @@ for width in range(1, I//2):
     for x in xs:
         for y in ys:
             count = 0
-            for x1 in range(x, x+width+1):
-                for y1 in fishes.get(x1, []):
-                    if 0 <= y1 - y <= height:
-                        count += 1
+            for i, j in fishes:
+                if 0 <= i - x <= width and 0 <= j - y <= height:
+                    count += 1
             if max_count < count:
                 max_count = count
 print(max_count)
