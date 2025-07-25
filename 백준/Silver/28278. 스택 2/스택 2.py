f76e389f30d6e926f35392ad46aa6df3cb919c59
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-25 10:18:28 +0900

    [Silver IV] Title: 스택 2, Time: 632 ms, Memory: 194864 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,27 +1,16 @@
-def main():
-    n = int(input())
-    stack = []
-    ans = []
-    cnt = 0
-    for _ in range(n):
-        a, *b = map(int, input().split())
-        if a == 1:
-            stack.append(b[0])
-            cnt += 1
-        elif a == 2:
-            if cnt:
-                ans.append(stack.pop())
-                cnt -= 1
-            else:
-                ans.append(-1)
-        elif a == 3:
-            ans.append(cnt)
-        elif a == 4:
-            ans.append(1-bool(cnt))
-        elif a == 5:
-            ans.append(stack[-1] if cnt else -1)
-    print("\n".join(map(str, ans)))
-
-
-if __name__ == "__main__":
-    main()
+N = int(input())
+stack = []
+ans = []
+for _ in range(N):
+    a, *b = map(int, input().split())
+    if a == 1:
+        stack.append(b[0])
+    elif a == 2:
+        ans.append(stack.pop() if stack else -1)
+    elif a == 3:
+        ans.append(len(stack))
+    elif a == 4:
+        ans.append(1-bool(stack))
+    elif a == 5:
+        ans.append(stack[-1] if stack else -1)
+print('\n'.join(map(str, ans)))
