94c220c49bb82a85dc6ae94c32e86882e8b2a117
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-28 17:01:43 +0900

    [Silver II] Title: 과일 탕후루, Time: 144 ms, Memory: 143720 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -1,15 +1,25 @@
-def main():
-    N, *a = map(int, open(0).read().split())
-    s = m = 0
-    counts = [0]*10
-    num_fruit = 0
-    for e in range(N):
-        if not counts[a[e]]: num_fruit += 1
-        counts[a[e]] += 1
-        while num_fruit > 2:
-            counts[a[s]] -= 1
-            if not counts[a[s]]: num_fruit -= 1
-            s += 1
-        if m < e-s+1: m = e-s+1
-    print(m)
-main()
+N = int(input())
+*tang, = map(int, input().split())
+fruits = [0] * 10   # 과일 종류별 개수를 저장하는 리스트
+max_cnt = s = num_f = 0
+
+for e, f in enumerate(tang):
+    # 처음 보는 과일 -> num_f 증가
+    if not fruits[f]:
+        num_f += 1
+    fruits[f] += 1
+
+    # 과일 종류 개수 > 2 -> 과일 빼기
+    while num_f > 2:
+        fruits[(old_f := tang[s])] -= 1
+
+        # 과일 종류 개수, 없앨 과일 인덱스 반영
+        if not fruits[old_f]:
+            num_f -= 1
+        s += 1
+
+    # 과일의 개수가 가장 많은 탕후루의 과일 개수
+    if max_cnt < e-s+1:
+        max_cnt = e-s+1
+
+print(max_cnt)
