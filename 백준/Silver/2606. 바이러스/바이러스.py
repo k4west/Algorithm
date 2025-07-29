fb7c23b26e066f8979f7167790a8d06108545855
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-29 13:56:36 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,17 @@
-import sys
-input = sys.stdin.readline
+def dfs(c):
+    for nc in graph[c]:
+        if not visited[nc]:
+            visited[nc] = 1
+            dfs(nc)
 
-A = [0] * int(input())
-A[0] = 1
 
-M = int(input())
-ab = []
-for i in range(M):
-    ab.append(list(map(int,input().split())))
-    
-for _ in A:
-    for i in range(M):
-        if A[ab[i][0] - 1] != A[ab[i][1] - 1]:
-            A[ab[i][0] - 1] = 1
-            A[ab[i][1] - 1] = 1
-
-print(sum(A)-1)
+C = int(input())
+graph = [[] for _ in range(C+1)]
+visited = [0]*(C+1)
+visited[1] = 1
+for _ in range(int(input())):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+dfs(1)
+print(sum(visited)-1)
