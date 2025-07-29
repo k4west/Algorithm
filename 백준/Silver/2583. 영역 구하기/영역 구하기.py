a2cfc6eeea00e44bc30c529aa308b29b63e3a9f9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-29 16:01:16 +0900

    [Silver I] Title: 영역 구하기, Time: 104 ms, Memory: 111040 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,38 +1,32 @@
-def dfs(r, c):
-    cnt = 1
-    stack = []
-    while True:
+def bfs(p):
+    area = 1
+    q, nq = [p], []
+    while q or nq:
+        if not q:
+            q, nq = nq, []
+        r, c = q.pop()
         for dr, dc in d:
             nr, nc = r+dr, c+dc
             if 0 <= nr < N and 0 <= nc < M and not graph[nr][nc]:
                 graph[nr][nc] = 1
-                cnt += 1
-                stack.append((r, c))
-                r, c = nr, nc
-                break
-        else:
-            if stack:
-                r, c = stack.pop()
-            else:
-                break
-    return cnt
+                area += 1
+                nq.append((nr, nc))
+    return area
 
 
 ans = []
 d = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
 M, N, K = map(int, input().split())
-graph = [[0]*(M) for _ in range(N)]
+graph = [[0]*M for _ in range(N)]
 for _ in range(K):
-    x0, y0, x1, y1 = map(int, input().split())
-    for x in range(x0, x1):
-        for y in range(y0, y1):
+    x1, y1, x2, y2 = map(int, input().split())
+    for x in range(x1, x2):
+        for y in range(y1, y2):
             graph[x][y] = 1
-
 for x in range(N):
     for y in range(M):
         if not graph[x][y]:
             graph[x][y] = 1
-            ans.append(dfs(x, y))
+            ans.append(bfs((x, y)))
 print(len(ans))
-print(*sorted(ans))
+print(' '.join(map(str, sorted(ans))))
