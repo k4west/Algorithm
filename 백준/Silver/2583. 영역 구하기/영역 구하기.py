a94a3958a684d780ec30fb228f9b189279fb5165
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-29 14:21:39 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 129572 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10000)
+
+
+def dfs(r, c):
+    cnt = 1
+    for dr, dc in d:
+        nr, nc = r+dr, c+dc
+        if 0 <= nr < N and 0 <= nc < M and not graph[nr][nc]:
+            graph[nr][nc] = 1
+            cnt += dfs(nr, nc)
+    return cnt
+
+
+ans = []
+d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+M, N, K = map(int, input().split())
+graph = [[0]*(M) for _ in range(N)]
+for _ in range(K):
+    x0, y0, x1, y1 = map(int, input().split())
+    for x in range(x0, x1):
+        for y in range(y0, y1):
+            graph[x][y] = 1
+
+for x in range(N):
+    for y in range(M):
+        if not graph[x][y]:
+            graph[x][y] = 1
+            ans.append(dfs(x, y))
+print(len(ans))
+print(*sorted(ans))
