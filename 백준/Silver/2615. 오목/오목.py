5ebdbd3f1421b55441fae330178b5095414ee8ef
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-31 20:22:05 +0900

    [Silver I] Title: 오목, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,23 +1,29 @@
-def main():
-    d = ((1, 0), (0, 1), (1, 1), (-1, 1))
-    graph = [[*map(int, input().split())] for _ in range(19)]
-    for i in range(19):
-        for j in range(19):
-            if t := graph[i][j]:
-                for di, dj in d:
-                    if 0 <= i-di < 19 and 0 <= j-dj < 19 and graph[i-di][j-dj] == t:
-                        continue
-                    cnt = 1
-                    ni, nj = i, j
-                    while 0 <= (ni := ni+di) < 19 and 0 <= (nj := nj+dj) < 19 and graph[ni][nj] == t:
-                        cnt += 1
-                        if cnt > 5:
-                            break
-                    if cnt == 5:
-                        print(t)
-                        print(i+1, j+1)
-                        return
+d = (19, 1, 20, -18)
+*graph, = map(int, open(0).read().split())
+flag = True
+
+for i in range(361):
+    if not (t := graph[i]):
+        continue
+
+    for di in d:
+        if i % 19 >= (i - di) % 19 and 0 <= i - di < 361 and graph[i - di] == t:
+            continue
+
+        cnt, ni = 1, i
+        while i % 19 <= (ni + di) % 19 and 0 <= (ni := ni + di) < 361 and graph[ni] == t:
+            cnt += 1
+            if cnt > 5:
+                break
+
+        if cnt == 5:
+            print(t)
+            print(i // 19 + 1, i % 19 + 1)
+            flag = False
+
+        if not flag:
+            break
+    if not flag:
+        break
+if flag:
     print(0)
-
-
-main()
