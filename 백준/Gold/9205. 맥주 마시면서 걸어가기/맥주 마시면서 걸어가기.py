1b62c309faa941bdc2f6f1f16c8254ef7c3d1358
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-31 13:55:02 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,23 +1,19 @@
 def get_dist(a, b):
-    return abs(b[0]-a[0]) + abs(b[1]-a[1])
+    return abs(b[0] - a[0]) + abs(b[1] - a[1])
 
 
 def bfs():
-    q, nq = [0], []
+    q = [0]
     visited[0] = 1
 
-    while q or nq:
-        if not q:
-            q, nq = nq, []
-
+    while q:
         c = q.pop()
         for n in graph[c]:
             if n == N+1:
                 return True
-
             if not visited[n]:
                 visited[n] = 1
-                nq.append(n)
+                q.append(n)
     return False
 
 
@@ -28,6 +24,11 @@ for _ in range(int(input())):
     N = int(input())
     pos = [[*map(int, input().split())] for _ in range(N+2)]
 
+    # 한 번에 축제로 갈 수 있는지 확인
+    if get_dist(pos[0], pos[-1]) < m:
+        ans.append("happy")
+        continue
+
     # 이동 가능성 확인
     graph = [[] for _ in range(N+2)]
     visited = [0] * (N+2)
@@ -37,5 +38,5 @@ for _ in range(int(input())):
                 graph[i].append(j)
                 graph[j].append(i)
 
-    ans.append("happy" if graph[0] and graph[-1] and bfs() else "sad")
+    ans.append("happy" if bfs() else "sad")
 print('\n'.join(ans))
