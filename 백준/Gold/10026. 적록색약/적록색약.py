ac99a576a3e05fe1398f70fcca61cfe1a1dae5f1
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-07-31 11:56:42 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 111968 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,33 +1,53 @@
-import sys
+def bfs1(r, c):
+    q = [(r, c)]
+    visited[r][c] = 1
 
-N = int(sys.stdin.readline())
-graph = []
-for _ in range(N):
-    s = sys.stdin.readline().rstrip()
-    graph.append(list(s))
+    while q:
+        r, c = q.pop()
+        for dr, dc in d:
+            nr, nc = r+dr, c+dc
+            if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and paint[nr][nc] == color:
+                visited[nr][nc] = 1
+                q.append((nr, nc))
 
-c1, c2 = 0, 0
-three_dic = {"R": ("G", "B"), "G": ("R", "B"), "B": ("R", "G")}
-two_dic = {"R": ("B"), "G": ("B"), "B": ("R", "G")}
-d = ((-1, 0), (1, 0), (0, -1), (0, 1)) # 상, 하, 좌, 우
 
-def bfs(dic):
-    visited = [[False] * N for _ in range(N)]
-    c = 0
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j]:
-                c += 1
-                color = graph[i][j]
-                visited[i][j] = True
-                q = [(i, j)]
-                while q:
-                    y, x = q.pop()
-                    for dy, dx in d:
-                        ny, nx = y + dy, x + dx
-                        if 0 <= ny < N and 0 <= nx < N and not visited[ny][nx] and graph[ny][nx] not in dic[color]:
-                            q.append((ny, nx))
-                            visited[ny][nx] = True
-    return c
+def bfs2(r, c):
+    q = [(r, c)]
+    visited[r][c] = 1
 
-print(bfs(three_dic), bfs(two_dic))
+    while q:
+        r, c = q.pop()
+        for dr, dc in d:
+            nr, nc = r+dr, c+dc
+            if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and paint[nr][nc] != color:
+                visited[nr][nc] = 1
+                q.append((nr, nc))
+
+
+c1 = c2 = 0
+d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+N = int(input())
+paint = [input() for _ in range(N)]
+
+
+visited = [[0]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            color = paint[i][j]
+            bfs1(i, j)
+            c1 += 1
+
+visited = [[0]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            color = paint[i][j]
+            if color == 'B':
+                bfs1(i, j)
+            else:
+                color = 'B'
+                bfs2(i, j)
+            c2 += 1
+
+print(c1, c2)
