26a95cefd6f350afb2660f548469fc937e1b8dff
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-01 20:10:12 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 172 ms, Memory: 139404 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,33 +1,27 @@
-import sys
-from collections import deque
-def f(x, y, A, N, M):
-    d = ((0, 1), (0, -1), (1, 0), (-1, 0))
-    v = [[False] * M for _ in range(N)]
-    v[x][y] = True
-    c = 0
-    queue = deque([(x, y)])
-    while queue:
-        i, j = queue.popleft()
-        for dx, dy in d:
-            ni, nj = i + dx, j + dy
-            if 0 > ni or ni >= N or 0 > nj or nj >= M: continue
-            if v[ni][nj]: continue
-            if A[ni][nj] == 'X': continue
-            if A[ni][nj] == 'P': c += 1
-            v[ni][nj] = True
-            queue.append((ni, nj))
-    if c: print(c)
-    else: print('TT')
+d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+N, M = map(int, input().split())
+campus = []
+people = []
 
-def m():
-    N, M = map(int, sys.stdin.readline().split())
-    A = []
-    for i in range(N):
-        li = list(sys.stdin.readline().rstrip())
-        A.append(li)
-        if 'I' in li:
-            x, y = (i, li.index('I'))
-    f(x, y, A, N, M)
+for i in range(N):
+    row = input()
+    campus.append(list(row))
     
-if __name__ == "__main__":
-    m()
+    for j, k in enumerate(row):
+        if k == 'I':
+            start = i, j
+            campus[i][j] = 'X'
+
+cnt = 0
+q = [start]
+while q:
+    r, c = q.pop()
+    for dr, dc in d:
+        if 0 <= (nr := r+dr) < N and 0 <= (nc := c+dc) < M and campus[nr][nc] != 'X':
+            if campus[nr][nc] == 'P':
+                cnt += 1
+            
+            campus[nr][nc] = 'X'
+            q.append((nr, nc))
+
+print(cnt or 'TT')
