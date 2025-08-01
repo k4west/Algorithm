c2c9d758883c93edbefc0fd0749dd97e8593f387
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-01 10:10:23 +0900

    [Silver I] Title: 스타트링크, Time: 144 ms, Memory: 126208 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,35 +1,28 @@
-INF = float('inf')
-
 F, S, G, U, D = map(int, input().split())
-floors = [INF]*(F+1)
-q = []
 
-if S == G:                                      # 이동할 필요가 없음
-    floors[G] = 0
-elif (S < G and U) or (S > G and D):  # bfs 초기화
-    floors[S] = 0               # 각 층다마 눌러야 하는 버튼 최소 횟수를 기록할 리스트
-    visited = [0] * (F + 1)     # 방문 기록; `bfs`라서 처음 도착한 층의 횟수가 최소임
-    visited[S] = 1
-    d = (U, -D)
-    q = [S]
+if S == G or (S > G and D) or (S < G and U):
+    INF = float("inf")
+    floors = [INF]*(F+1)
 
-while q:
-    cur = q.pop(0)
+    q = [S] if S != G else []
+    buttons = (U, -D)
+    floors[S] = 0
 
-    for move in d:
-        nxt = cur + move
+    while q:
+        cf = q.pop(0)
+        for move in buttons:
+            nf = cf + move
+            if nf == G:
+                floors[nf] = floors[cf] + 1
+                q = []
+                break
+            if 0 < nf <= F and floors[nf] == INF:
+                floors[nf] = floors[cf] + 1
+                q.append(nf)
 
-        if nxt == G:
-            floors[nxt] = floors[cur] + 1
-            break
-
-        if 0 < nxt <= F and not visited[nxt]:
-            visited[nxt] = 1
-            floors[nxt] = floors[cur] + 1
-            q.append(nxt)
-
-ans = floors[G]
-if ans == INF:
-    print("use the stairs")
+    if floors[G] != INF:
+        print(floors[G])
+    else:
+        print("use the stairs")
 else:
-    print(floors[G])
+    print("use the stairs")
