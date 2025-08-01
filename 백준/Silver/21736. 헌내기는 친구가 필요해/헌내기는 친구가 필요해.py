3994759ba60719d203329ccbf49b8b4cd3635d03
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-01 19:14:25 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 300 ms, Memory: 39468 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,26 +1,33 @@
 import sys
+from collections import deque
+def f(x, y, A, N, M):
+    d = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    v = [[False] * M for _ in range(N)]
+    v[x][y] = True
+    c = 0
+    queue = deque([(x, y)])
+    while queue:
+        i, j = queue.popleft()
+        for dx, dy in d:
+            ni, nj = i + dx, j + dy
+            if 0 > ni or ni >= N or 0 > nj or nj >= M: continue
+            if v[ni][nj]: continue
+            if A[ni][nj] == 'X': continue
+            if A[ni][nj] == 'P': c += 1
+            v[ni][nj] = True
+            queue.append((ni, nj))
+    if c: print(c)
+    else: print('TT')
+
 def m():
     N, M = map(int, sys.stdin.readline().split())
     A = []
-    v = [[False] * M for _ in range(N)]
-    d = ((0, 1), (0, -1), (1, 0), (-1, 0))
     for i in range(N):
         li = list(sys.stdin.readline().rstrip())
         A.append(li)
         if 'I' in li:
             x, y = (i, li.index('I'))
-    v[x][y] = True
-    c = 0
-    q = [(x, y)]
-    while q:
-        i, j = q.pop(0)
-        if A[i][j] == 'P':
-            c += 1
-        for dx, dy in d:
-            ni, nj = i + dx, j + dy
-            if 0 <= ni < N and 0 <= nj < M and A[ni][nj] != 'X' and not v[ni][nj]:
-                v[ni][nj] = True
-                q.append((ni, nj))
-    print(c if c else 'TT')
+    f(x, y, A, N, M)
+    
 if __name__ == "__main__":
     m()
