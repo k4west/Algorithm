a727817e2f887a61ec865eb72bab8888f4e193a3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-02 08:11:18 +0900

    [Silver I] Title: 그래프 탐색 2, Time: 180 ms, Memory: 113752 KB -BaekjoonHub

--- 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
+++ 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
@@ -1,31 +1,6 @@
 # 도로 정비
 # 도시가 수도에 방문 -> 몇 개의 도시 방문?(최소)
 
-def bfs(start):
-    cnt = 0
-    q, nq = [], []
-    
-    visited[start] = 1
-    q.append(start)
-    
-    while q or nq:
-        if not q:
-            q, nq = nq, []
-            cnt += 1
-        
-        c = q.pop()
-        for nxt in graph[c]:
-            if nxt == E:
-                li[start-1] = cnt + 1
-                return
-            
-            if not visited[nxt]:
-                visited[nxt] = 1
-                nq.append(nxt)
-
-    li[start-1] = -1
-  
-  
 E = 1
 n, m = map(int, input().split())  # n: 도시 수, m 도로 수
 graph = [[] for _ in range(n+1)]
@@ -42,10 +17,19 @@ for _ in range(int(input())):  # 정비할 도로 수
     graph[s].append(e)
     graph[e].append(s)
     
-    li = [0]*n
-    for city in range(2, n+1):
-        visited = [0]*(n+1)
-        bfs(city)
+    li = [-1]*n
+    li[0] = 0
+    cnt = 0
+    q, nq = [1], []
+    while q:
+        cnt += 1
+        for cur in q:
+            for nxt in graph[cur]:
+                if li[nxt-1] != -1:
+                    continue
+                li[nxt-1] = cnt
+                nq.append(nxt)
+        q, nq = nq, []
     ans.append(li)
 
 print('\n'.join((' '.join(map(str, i)) for i in ans)))
