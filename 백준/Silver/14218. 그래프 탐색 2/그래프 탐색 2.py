3481db5ef92f16e506781bc53cd8562a50609b1b
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-02 08:12:57 +0900

    [Silver I] Title: 그래프 탐색 2, Time: 164 ms, Memory: 113628 KB -BaekjoonHub

--- 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
+++ 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
@@ -1,19 +1,19 @@
 # 도로 정비
 # 도시가 수도에 방문 -> 몇 개의 도시 방문?(최소)
 
-E = 1
-n, m = map(int, input().split())  # n: 도시 수, m 도로 수
+a = open(0)
+n, m = map(int, next(a).split())  # n: 도시 수, m 도로 수
 graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
-    s, e = map(int, input().split()) # 연결된 두 도시
+    s, e = map(int, next(a).split()) # 연결된 두 도시
     graph[s].append(e)
     graph[e].append(s)
 
 ans = []
 # 한 도로가 정비될 때마다 각 도시별로 수도를 방문하는 데 최소 방문 도시
-for _ in range(int(input())):  # 정비할 도로 수
-    s, e = map(int, input().split()) # 연결할 두 도시
+for _ in range(int(next(a))):  # 정비할 도로 수
+    s, e = map(int, next(a).split()) # 연결할 두 도시
     graph[s].append(e)
     graph[e].append(s)
     
