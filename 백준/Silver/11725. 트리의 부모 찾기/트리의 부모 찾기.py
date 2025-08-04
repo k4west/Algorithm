5e1e7fabf9baffc73093bff88a864518511d664c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-04 14:12:48 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 180 ms, Memory: 146308 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,19 +1,19 @@
-N = int(input())
+N, *li = map(int, open(0).read().split())
 
 graph = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    a, b = map(int, input().split())
+for i in range(N-1):
+    a, b = li[2*i:2*i+2]
     graph[a].append(b)
     graph[b].append(a)
 
 trees = [0]*(N+1)
 trees[1] = -1
-q = [1]
-while q:
-    parent = q.pop()
+s = [1]
+while s:
+    parent = s.pop()
     for child in graph[parent]:
         if not trees[child]:
             trees[child] = parent
-            q.append(child)
+            s.append(child)
 
 print('\n'.join(map(str, trees[2:])))
