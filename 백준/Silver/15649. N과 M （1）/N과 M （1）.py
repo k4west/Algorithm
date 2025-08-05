c330edf98093dab2213ac5720c4d73d005fc3c1e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-05 11:58:47 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 115920 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,3 +1,17 @@
-from itertools import permutations
+def dfs(depth):
+    if depth == M:
+        ans.append(' '.join(map(str, li)))
+        return
+
+    for i in range(1, N+1):
+        if not i in li:
+            li.append(i)
+            dfs(depth+1)
+            li.pop()
+
+
+ans = []
 N, M = map(int, input().split())
-print("\n".join(list(map(' '.join,permutations(map(str, range(1,N + 1)),M)))))
+li = []
+dfs(0)
+print('\n'.join(ans))
