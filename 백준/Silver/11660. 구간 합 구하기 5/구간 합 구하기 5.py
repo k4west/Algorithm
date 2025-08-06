db11c7eb9bb59d1c6e79726723d7bf0837db6fac
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-06 20:02:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 320 ms, Memory: 136128 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,18 +1,16 @@
-import sys
-input = sys.stdin.readline
-def main():
-    N, M = map(int, input().split())
-    li = [[0]*(N+1)]
-    ans = []
-    for _ in range(N):
-        li.append([0] + list(map(int, input().split())))
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            li[i][j] += li[i][j-1] + li[i-1][j] - li[i-1][j-1]
-    for _ in range(M):
-        x1, y1, x2, y2 = map(int, input().split())
-        ans.append(li[x2][y2] - li[x2][y1-1] - li[x1-1][y2] + li[x1-1][y1-1])
-    print("\n".join(map(str, ans)))
+ans = []
+N, M = map(int, input().split())
+arr = [[*map(int, input().split())] for _ in range(N)]
 
-if __name__ == "__main__":
-    main()
+acc = [[0]*(N+1) for _ in range(N+1)]
+for i in range(N):
+    for j in range(N):
+        acc[i][j] = acc[i][j-1] + arr[i][j]
+    for j in range(N):
+        acc[i][j] += acc[i-1][j]
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    ans.append(acc[x1-2][y1-2] + acc[x2-1][y2-1] - acc[x1-2][y2-1] - acc[x2-1][y1-2])
+
+print('\n'.join(map(str, ans)))
