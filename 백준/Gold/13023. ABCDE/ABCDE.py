633e37c04d687879cb3748b063ad17cca899e028
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-07 23:17:21 +0900

    [Gold V] Title: ABCDE, Time: 284 ms, Memory: 111392 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,3 +1,33 @@
+def dfs():
+    for cur in range(1, N+1):
+        stack = [(cur, 0)]
+        visited[cur] = 1
+        cnt = 0
+    
+        while stack:
+            if cnt == 4:
+                return 1
+            
+            cur, idx = stack.pop()
+            if idx == len(graph[cur]):
+                visited[cur] = 0
+                cnt -= 1
+                continue
+            
+            nxt = graph[cur][idx]
+            stack.append((cur, idx+1))
+            
+            if not visited[nxt]:
+                visited[nxt] = 1
+                cnt += 1
+                stack.append((nxt, 0))
+        
+        if ans:
+            break
+        visited[cur] = 0
+    return 0
+
+
 ans = 0
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
@@ -8,33 +38,4 @@ for _ in range(M):
     graph[a].append(b)
     graph[b].append(a)
 
-
-for cur in range(1, N+1):
-    stack = [(cur, 0)]
-    visited[cur] = 1
-    cnt = 0
-
-    while stack:
-        if cnt == 4:
-            ans = 1
-            break
-        
-        cur, idx = stack.pop()
-        if idx == len(graph[cur]):
-            visited[cur] = 0
-            cnt -= 1
-            continue
-        
-        nxt = graph[cur][idx]
-        stack.append((cur, idx+1))
-        
-        if not visited[nxt]:
-            visited[nxt] = 1
-            cnt += 1
-            stack.append((nxt, 0))
-    
-    if ans:
-        break
-    visited[cur] = 0
-
-print(ans)
+print(dfs())
