a058aac38455887cfe3b23edf0de228997dc3974
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-07 23:14:21 +0900

    [Gold V] Title: ABCDE, Time: 360 ms, Memory: 111908 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,17 +1,3 @@
-def dfs(cur, cnt):
-    global ans
-    
-    if cnt == 4:
-        ans = 1
-        return
-    
-    for nxt in graph[cur]:
-        if not visited[nxt]:
-            visited[nxt] = 1
-            dfs(nxt, cnt+1)
-            visited[nxt] = 0
-
-
 ans = 0
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
@@ -22,11 +8,33 @@ for _ in range(M):
     graph[a].append(b)
     graph[b].append(a)
 
-for start in range(1, N+1):
-    visited[start] = 1
-    dfs(start, 0)
+
+for cur in range(1, N+1):
+    stack = [(cur, 0, len(graph[cur]))]
+    visited[cur] = 1
+    cnt = 0
+
+    while stack:
+        if cnt == 4:
+            ans = 1
+            break
+        
+        cur, idx, n = stack.pop()
+        if idx == n:
+            visited[cur] = 0
+            cnt -= 1
+            continue
+        
+        nxt = graph[cur][idx]
+        stack.append((cur, idx+1, n))
+        
+        if not visited[nxt]:
+            visited[nxt] = 1
+            cnt += 1
+            stack.append((nxt, 0, len(graph[nxt])))
+    
     if ans:
         break
-    visited[start] = 0
+    visited[cur] = 0
 
 print(ans)
