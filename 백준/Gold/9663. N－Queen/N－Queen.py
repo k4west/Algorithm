5ce9f694bd78b0ed7499251e182a04146985b362
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-07 17:09:53 +0900

    [Gold IV] Title: N-Queen, Time: 4392 ms, Memory: 119272 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,1 +1,21 @@
-print(['0', '1', '0', '0', '2', '10', '4', '40', '92', '352', '724', '2680', '14200', '73712', '365596', '2279184'][int(input())])
+def dfs(n):
+    global cnt
+
+    if n == N:
+        cnt += 1
+        return
+
+    for i in range(N):
+        if v1[i] == 0 and v2[n+i] == 0 and v3[n-i] == 0:
+            v1[i] = v2[n+i] = v3[n-i] = 1
+            dfs(n+1)
+            v1[i] = v2[n+i] = v3[n-i] = 0
+
+
+cnt = 0
+N = int(input())
+v1 = [0]*N
+v2 = [0]*2*N
+v3 = [0]*2*N
+dfs(0)
+print(cnt)
