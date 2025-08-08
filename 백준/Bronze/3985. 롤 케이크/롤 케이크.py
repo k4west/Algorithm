a544f641dc5a779874015661cce7f7861000c3f3
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-08 11:38:58 +0900

    [Bronze I] Title: 롤 케이크, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Bronze/3985. 롤 케이크/롤 케이크.py
+++ 백준/Bronze/3985. 롤 케이크/롤 케이크.py
@@ -1,18 +1,23 @@
 L = int(input())
-N = int(input())
-cake = [0 for _ in range(L+1)]
-expected_i = real_i = real = M = 0
-for i in range(N):
-    t = 0
-    p, k = map(int, input().split())
-    if M < (e := k-p+1):
-        M = e
-        expected_i = i+1
-    for j in range(p, k+1):
-        if not cake[j]:
-            cake[j] = i+1
-            t += 1
-    if real < t:
-        real = t
-        real_i = i+1
-print(expected_i, real_i, sep='\n')
+cake = [0]*(L+1)
+m1 = m2 = i1 = i2 = 0
+
+for idx in range(1, int(input())+1):
+    s, e = map(int, input().split())
+    expected, cnt = e-s, 0
+    
+    for i in range(s, e+1):
+        if not cake[i]:
+            cake[i] = idx
+            cnt += 1
+
+    if m1 < expected:
+        m1 = expected
+        i1 = idx
+
+    if m2 < cnt:
+        m2 = cnt
+        i2 = idx
+
+print(i1)
+print(i2)
