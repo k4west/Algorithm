5514742891b22a55d11509492d92e06c1198d51c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-08 11:27:42 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -1,8 +1,18 @@
+def f(i, s):
+    global score
+
+    if i == 10:
+        a, b = abs(score - 100), abs(s - 100)
+        if a > b or a == b and score < s:
+            score = s
+        return
+
+    f(10, s)
+    f(i+1, s+mush[i])
+
+
 score = 0
-for _ in range(10):
-    t = int(input())
-    if abs(100-score) >= abs(100-score-t):
-        score += t
-    else:
-        break
+*mush, = map(int, (input() for _ in range(10)))
+
+f(0, 0)
 print(score)
