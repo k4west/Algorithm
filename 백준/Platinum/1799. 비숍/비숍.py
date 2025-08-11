221e1b4bc26ce58c212b161ec31b491d673ec8cc
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 17:15:20 +0900

    [Platinum V] Title: 비숍, Time: 116 ms, Memory: 111548 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.py
+++ 백준/Platinum/1799. 비숍/비숍.py
@@ -1,46 +1,47 @@
-def bt(n, cnt, li):
+def bt(n, cnt):
     global max_b
 
-    if cnt + M - n <= max_b:    # 더 해도 안 됨(가지치기)
+    # 가지 치기
+    if cnt + (M-n+1)//2 <= max_b:    # 더 해도 안 됨; candidates 절반만 탐색해서 (M-n+1)//2
         return
 
-    if n == M:                  # 모두 확인 후
+    # 종료 조건
+    # n+2로 두칸씩 이동하기 때문에 n이 M이 넘어갈 수 있음
+    # ==가 아니라 >=
+    if n >= M:                  # 모두 확인 후
         if max_b < cnt:         # 최대 값 갱신
             max_b = cnt
         return
 
-    r, c = li[n]
-    if not v1[r+c] and not v2[r-c]:     # 두 대각선에 겹치는 게 없으면 비숍 두기
-        v1[r+c] = v2[r-c] = 1           # 방문 표시
-        bt(n+1, cnt+1, li)              # 다음 탐색
-        v1[r+c] = v2[r-c] = 0           # 백트레킹
+    for r, c in candidates[n]:
+        if not visited[r-c]:            # 우하향 대각선에 겹치는 게 없으면 비숍 두기
+            visited[r-c] = 1            # 방문 표시
+            bt(n+2, cnt+1)              # 다음 탐색
+            visited[r-c] = 0            # 백트레킹
 
-    bt(n+1, cnt, li)                    # 이번 좌표에는 비숍을 두지 않는 경우
+    bt(n+2, cnt)                    # 이번 좌표에는 비숍을 두지 않는 경우
 
 
-# 대각선 층?이 다르면(45도 각도로 보면;;) 서로 겹치치 않아서 
-# 좌표를 따로 저장하고, 놓을 수 있는 수도 따로 구함
+# 대각선 층?이 다르면(45도 각도로 보면;;) 서로 겹치치 않아서 놓을 수 있는 수도 따로 구해서 더할 수 있음
 N = int(input())
-b1, b2 = [], []
-v1, v2 = [0]*2*N, [0]*2*N   # 두 대각선 방향에 겹치는 게 있는지 확인하는 방문표시용 리스트
+M = 2*N-1
+candidates = [[] for _ in range(M)]
+visited = [0]*M
 
 # 비숍을 둘 수 있는(1) 좌표 저장
 for r in range(N):
     for c, k in enumerate(map(int, input().split())):
         if k:
-            # 대각선 층?에 따라 나눠서 저장
-            if (r+c) % 2:
-                b1.append((r, c))
-            else:
-                b2.append((r, c))
+            # 방향(우상향)이 겹치는 좌표를 모아서 저장
+            candidates[r+c].append((r, c))
 
 # 대각선 층?별로 따로 놓을 수 있는 최대 비숍 수 구하기
+# 대각선 층?별로 -> bt의 첫번째 매개 변수(cnt)는 cnt + 2로 두칸씩 이동
+# -> bt(0, 0)과 bt(1, 0)으로 각각 실행
 max_b = 0
-M = len(b1)
-bt(0, 0, b1)
+bt(0, 0)
 
 t, max_b = max_b, 0
-M = len(b2)
-bt(0, 0, b2)
+bt(1, 0)
 
 print(max_b + t)
