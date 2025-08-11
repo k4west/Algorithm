8c052bffa82868dd850a4a162557aec6a8ef0e6d
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 13:33:02 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 132984 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,5 +1,6 @@
 X = int(input())
-dp = [0, 0] + [X]*(X-1)
-for i in range(2, X+1):
-    dp[i] = min(dp[i//3] if not i % 3 else X, dp[i//2] if not i % 2 else X, dp[i-1]) + 1
-print(dp[X])
+dp = [X]*3*X
+dp[X] = 0
+for i in range(X-1, 0, -1):
+    dp[i] = min(dp[i+1], dp[i*2], dp[i*3]) + 1
+print(dp[1])
