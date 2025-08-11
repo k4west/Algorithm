faa7f6efcb928c59481174de717db9f469a9ac84
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 16:56:10 +0900

    [Platinum V] Title: 비숍, Time: 328 ms, Memory: 115388 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.py
+++ 백준/Platinum/1799. 비숍/비숍.py
@@ -1,38 +1,42 @@
-def bt(s, cnt, arr):
-    global ans
-    
-    if M-s+cnt < ans:
+def bt(n, cnt, li):
+    global max_b
+
+    if cnt + M - n <= max_b:
         return
-    
-    if ans < cnt:
-        ans = cnt
-    
-    for i in range(s, M):
-        r, c = arr[i]
-        if not v1[r+c] and not v2[r-c]:
-            v1[r+c] = v2[r-c] = 1
-            bt(i+1, cnt+1, arr)
-            v1[r+c] = v2[r-c] = 0
+
+    if n == M:
+        if max_b < cnt:
+            max_b = cnt
+        return
+
+    r, c = li[n]
+    if not v1[r+c] and not v2[r-c]:
+        v1[r+c] = 1
+        v2[r-c] = 1
+        bt(n+1, cnt+1, li)
+        v1[r+c] = 0
+        v2[r-c] = 0
+    bt(n+1, cnt, li)
 
 
 N = int(input())
-w, b = [], []
+b1, b2 = [], []
+v1, v2 = [0]*2*N, [0]*2*N
+
 for r in range(N):
-    for c, k in enumerate(input().split()):
-        if k == "1":
+    for c, k in enumerate(map(int, input().split())):
+        if k:
             if (r+c) % 2:
-                w.append((r, c))
+                b1.append((r, c))
             else:
-                b.append((r, c))
+                b2.append((r, c))
 
-ans = 0
-M = len(w)
-v1, v2 = [0]*2*N, [0]*2*N
-bt(0, 0, w)
-ans_w = ans
+max_b = 0
+M = len(b1)
+bt(0, 0, b1)
 
-ans = 0
-M = len(b)
-v1, v2 = [0]*2*N, [0]*2*N
-bt(0, 0, b)
-print(ans_w + ans)
+t, max_b = max_b, 0
+M = len(b2)
+bt(0, 0, b2)
+
+print(max_b + t)
