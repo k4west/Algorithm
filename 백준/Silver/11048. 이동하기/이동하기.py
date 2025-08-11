b0b7fe9e2a34173e0aa86017eab77fed40cd15ab
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 17:30:13 +0900

    [Silver II] Title: 이동하기, Time: 184 ms, Memory: 111376 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,8 +1,8 @@
 N, M = map(int, input().split())
-maze = [[0]*(M+1)] + [[0] + [*map(int, input().split())] for _ in range(N)]
+dp = [0]*(M+1)
 
-for r in range(1, N+1):
-    for c in range(1, M+1):
-        maze[r][c] += max(maze[r][c-1], maze[r-1][c])
+for _ in range(N):
+    for i, j in enumerate(map(int, input().split()), 1):
+        dp[i] = max(dp[i], dp[i-1]) + j
 
-print(maze[N][M])
+print(dp[M])
