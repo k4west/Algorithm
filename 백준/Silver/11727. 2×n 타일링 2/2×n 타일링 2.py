68e4b2d363b359c2b44304b68537de360e97febb
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 11:51:53 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,5 +1,5 @@
 n = int(input())
-dp = [1, 1] + [0]*(n-1)
-for i in range(2, n+1):
-    dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
-print(dp[n])
+i = j = 1
+for _ in range(n-1):
+    i, j = j, (2*i + j) % 10007
+print(j)
