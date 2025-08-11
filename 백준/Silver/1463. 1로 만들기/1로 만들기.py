2912e737133cfcde5f213f36feb8ef54d62cb47a
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-11 12:23:46 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 126392 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,21 +1,10 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
-n = int(input())
-q = deque([n])
-c = [0] * (n+1)
-while q:
-    p = q.popleft()
-    if p == 1:
-        print(c[1])
-        break
-    if p % 3 == 0 and not c[p//3]:
-        c[p//3] = c[p]+1
-        q.append(p//3)
-    if p % 2 == 0 and not c[p//2]:
-        c[p//2] = c[p]+1
-        q.append(p//2)
-    if not c[p-1]:
-        c[p-1] = c[p]+1
-        q.append(p-1)
+X = int(input())
+dp = [0, 0] + [X]*(X-1)
+for i in range(2, X+1):
+    if not i % 3 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+    if not i % 2 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+    if dp[i] > dp[i-1] + 1:
+        dp[i] = dp[i-1] + 1
+print(dp[X])
