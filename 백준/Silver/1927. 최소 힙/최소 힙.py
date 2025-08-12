dfc0180b4533ca4f507e366a38b302626dfd00b9
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-12 13:50:25 +0900

    [Silver II] Title: 최소 힙, Time: 180 ms, Memory: 114568 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,9 +1,16 @@
-import heapq
-from sys import stdin
-li = []
-for _ in range(int(stdin.readline())):
-    n = int(stdin.readline())
-    if n: heapq.heappush(li, n)
+from heapq import heappush, heappop
+
+ans = []
+hq = []
+N = int(input())
+
+for _ in range(N):
+    x = int(input())
+    if x:
+        heappush(hq, x)
+    elif hq:
+        ans.append(heappop(hq))
     else:
-        if li: print(heapq.heappop(li))
-        else: print(0)
+        ans.append(0)
+
+print("\n".join(map(str, ans)))
