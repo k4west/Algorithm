31795043482e17d878b06bed273d4c591c95c890
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-13 13:19:04 +0900

    [Gold IV] Title: 최단경로, Time: 484 ms, Memory: 143320 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,35 +1,37 @@
-from sys import stdin, stdout
 from heapq import heappop, heappush
 
-def main():
-    input = stdin.readline
-    print = stdout.write
 
-    INF = float('inf')
-    V, E = map(int, input().split())
-    s = int(input())
-    graph = [[] for _ in range(V+1)]
-    distance = [INF] * (V+1)
+def dijkstra(s):
+    visited = [0]*(V+1)
+
+    hq = [(0, s)]
     distance[s] = 0
+    visited[s] = 1
 
-    for _ in range(E):
-        i, j, k = map(int, input().split())
-        graph[i].append((j, k))
+    while hq:
+        dist_c, cur = heappop(hq)
+        if distance[cur] != dist_c:
+            continue
 
-    def f(s):
-        q = [(0, s)]
-        while q:
-            dist, node = heappop(q)
-            if dist > distance[node]:
-                continue
-            for n, d in graph[node]:
-                n_dist = dist + d
-                if n_dist < distance[n]:
-                    distance[n] = n_dist
-                    heappush(q, (n_dist, n))
-    
-    f(s)
-    print("\n".join(map(str, [d if d != INF else 'INF' for d in distance[1:]])))
+        for nxt, dist_n in graph[cur].items():
+            dist = dist_c + dist_n
+            if distance[nxt] > dist:
+                distance[nxt] = dist
+                heappush(hq, (dist, nxt))
 
-if __name__ == "__main__":
-    main()
+
+# 시작점에서 다른 모든 정점으로의 최단 경로
+INF = float("inf")
+V, E = map(int, input().split())    # (1 ≤ V ≤ 20,000, 1 ≤ E ≤ 300,000)
+K = int(input())                    # 시작점
+
+distance = [INF]*(V+1)              # 1부터 V까지
+graph = [{} for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    if graph[u].get(v, INF) > w:    #  여러 개의 간선이 존재할 수도 있음
+        graph[u][v] = w
+
+dijkstra(K)
+
+print("\n".join(map(str, distance[1:])).upper())
