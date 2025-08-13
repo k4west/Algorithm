e35772dad9564e0bbd1e280d8c95a4d535b88dff
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-13 16:20:40 +0900

    [Gold V] Title: 집합의 표현, Time: 7560 ms, Memory: 157836 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,30 @@
+def find(x):
+    while x != roots[x]:
+        x = roots[x]
+
+    return x
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a > b:
+        a, b = b, a
+
+    roots[b] = a
+
+
+ans = []
+n, m = map(int, input().split())
+*roots, = range(n+1)
+options = ["NO", "YES"]
+
+for _ in range(m):
+    op, a, b = map(int, input().split())
+    if not op:
+        union(a, b)
+    else:
+        ans.append(options[find(a) == find(b)])
+
+print("\n".join(ans))
