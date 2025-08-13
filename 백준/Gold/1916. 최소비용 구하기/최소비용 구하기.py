faa9c39ac59d3e5ea4521e9b001c3a2e45543a8f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-13 10:55:02 +0900

    [Gold V] Title: 최소비용 구하기, Time: 196 ms, Memory: 116608 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,36 +1,53 @@
-from sys import stdin
-from heapq import heappush, heappop
-input = stdin.readline
-INF = float('INF')
+def get_closest_city():
+    city = 0
+    cost = INF
 
-def dijkstra(graph, s, e, N):
-    costs = [INF]*(N+1)
-    q = []
-    heappush(q, (0, s))
-    costs[s] = 0
-    while q:
-        s_cost, node = heappop(q)
-        if costs[node] < s_cost: continue
-        if node == e: break
-        for dst, d_cost in graph[node].items():
-            d_cost += costs[node]
-            if d_cost < costs[dst]:
-                costs[dst] = d_cost
-                heappush(q, (d_cost, dst))
-    print(costs[e])
+    # 방문 하지 않은 도시들 중에서 가장 비용이 적은 도시 구하기
+    for idx in range(1, N+1):
+        if visited[idx]:
+            continue
 
-def main():
-    N = int(input())
-    M = int(input())
-    graph = [{} for _ in range(N+1)]
-    for _ in range(M):
-        s, e, c = map(int, input().split())
-        if e in graph[s]:
-            graph[s][e] = min(graph[s][e], c)
-        else:
-            graph[s][e] = c
-    s, e = map(int, input().split())
-    dijkstra(graph, s, e, N)
+        tmp = costs[idx]
+        if cost > tmp:
+            city = idx
+            cost = tmp
 
-if __name__ == "__main__":
-    main()
+    visited[city] = 1
+    return city
+
+
+def dijkstra(start):
+    # 초기화
+    for city, cost in graph[start]:
+        if costs[city] > cost:
+            costs[city] = cost
+
+    # start를 제외한 나머지 노드를 가까운 순서로 탐색
+    for _ in range(N-1):
+        cur = get_closest_city()
+        cost_c = costs[cur]
+
+        # 더 저렴한 비용이 있다면 반영하기
+        for nxt, cost_n in graph[cur]:
+            cost = cost_c + cost_n
+            if costs[nxt] > cost:
+                costs[nxt] = cost
+
+
+INF = float("INF")
+N = int(input())    # 도시의 개수 N(1 ≤ N ≤ 1,000)
+M = int(input())    # 버스의 개수 M(1 ≤ M ≤ 100,000)
+
+graph = [[] for _ in range(N+1)]    # 도시의 번호: 1 ~ N
+costs = [INF]*(N+1)
+visited = [0]*(N+1)
+for _ in range(M):                  # 출발, 도착, 비용; 1 ≤ cost ≤ 100,000
+    s, e, c = map(int, input().split())
+    graph[s].append((e, c))
+
+S, E = map(int, input().split())    # 출발, 도착 -> 최소 비용
+costs[S] = 0
+visited[S] = 1
+dijkstra(S)
+
+print(costs[E])
