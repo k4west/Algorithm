e4f9f75711a91c3246dfddce8cee5f78887b4545
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-13 17:24:01 +0900

    [Gold II] Title: 대운하, Time: 284 ms, Memory: 140276 KB -BaekjoonHub

--- 백준/Gold/2350. 대운하/대운하.py
+++ 백준/Gold/2350. 대운하/대운하.py
@@ -11,7 +11,7 @@ def prim(s):
         cw, prev, cur = heappop(hq)
         if visited[cur]:
             continue
-        
+
         visited[cur] = 1
         width[prev][cur] = -cw
         width[cur][prev] = -cw
@@ -27,25 +27,9 @@ def prim(s):
                 heappush(hq, (nw, cur, nxt))
 
 
-ans = []
-# 도시의 수 N, 운하의 수 M, 노선의 수 K (N ≤ 1000, M ≤ 100000, K ≤ 10000)
-N, M, K = map(int, input().split())
-
-graph = [{} for _ in range(N+1)]
-for _ in range(M):
-    i, j, w = map(int, input().split())     # i와 j 사이에 폭이 w인 운하 (1 ≤ i, j ≤ N, w ≤ 200)
-    if graph[i].get(j, 0) > -w:          # 경로는 여러 개가 존재할 수 있다
-        graph[i][j] = -w
-        graph[j][i] = -w
-
-
-width = [[0]*(N+1) for _ in range(N+1)]
-*path, = range(N+1)
-prim(1)
-
-for _ in range(K):
+def lca(i, j):
     max_w = 200
-    i, j = map(int, input().split())        # 연결하는 도시 i, j
+
     if path[i][1] > path[j][1]:
         i, j = j, i
 
@@ -66,6 +50,28 @@ for _ in range(K):
 
         i, j = ni, nj
 
-    ans.append(max_w)
+    return max_w
+
+
+ans = []
+a = open(0)
+# 도시의 수 N, 운하의 수 M, 노선의 수 K (N ≤ 1000, M ≤ 100000, K ≤ 10000)
+N, M, K = map(int, next(a).split())
+
+graph = [{} for _ in range(N+1)]
+for _ in range(M):
+    i, j, w = map(int, next(a).split())     # i와 j 사이에 폭이 w인 운하 (1 ≤ i, j ≤ N, w ≤ 200)
+    if graph[i].get(j, 0) > -w:          # 경로는 여러 개가 존재할 수 있다
+        graph[i][j] = -w
+        graph[j][i] = -w
+
+
+width = [[0]*(N+1) for _ in range(N+1)]
+*path, = range(N+1)
+prim(1)
+
+for _ in range(K):
+    i, j = map(int, next(a).split())        # 연결하는 도시 i, j
+    ans.append(lca(i, j))
 
 print("\n".join(map(str, ans)))
