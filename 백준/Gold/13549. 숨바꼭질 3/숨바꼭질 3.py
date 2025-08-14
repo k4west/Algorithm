874a1ff03d8b74ead1fe24581d9728369b591f88
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-14 12:06:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,12 +1,13 @@
-import sys
-input = sys.stdin.readline
+def dfs(n, k):
+    if n >= k:
+        return n-k
+    elif not n:
+        return 1 + dfs(n+1, k)
+    elif not k % 2:
+        return min(k-n, dfs(n, k//2))
+    else:
+        return 1 + min(dfs(n, k-1), dfs(n, k+1))
 
-def dfs(N, K):
-    if N >= K: return N-K
-    if not N: return 1 + dfs(N+1, K)
-    if not K%2: return min(K-N, dfs(N, K//2))
-    return 1 + min(dfs(N, K-1), dfs(N, K+1))
 
-if __name__ == "__main__":
-    N, K = map(int, input().split())
-    print(dfs(N, K))
+N, K = map(int, input().split())
+print(dfs(N, K))
