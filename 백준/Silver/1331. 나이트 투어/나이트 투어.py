4f8142ee87a09309a6fc23b3543e74b9a4df11dc
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-18 14:41:25 +0900

    [Silver IV] Title: 나이트 투어, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.py
@@ -0,0 +1,19 @@
+def check(a, b):
+    return {abs(a[0] - b[0]), abs(a[1] - b[1])} == {1, 2}
+
+
+ans = "Invalid"
+d = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}  # 상하좌우
+p = {a: i for a, i in zip("ABCDEF123456", [*range(1, 7)]*2)}
+
+N = 6   # 6×6 체스판
+chess = [list('.'*N) for _ in range(N)]
+trace = [tuple(map(lambda x: p[x], input())) for _ in range(36)]
+
+if len(set(trace)) == 36 and check(trace[0], trace[-1]):
+    for i in range(35):
+        if not check(trace[i], trace[i+1]):
+            break
+    else:
+        ans = "Valid"
+print(ans)
