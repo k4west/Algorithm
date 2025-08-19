56c267cb8428ba0def1711f26daa1d6e7bbe4b8f
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-20 08:41:44 +0900

    [Gold V] Title: 보이저 1호, Time: 196 ms, Memory: 154256 KB -BaekjoonHub

---
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.py
@@ -0,0 +1,56 @@
+# \: 위 <-> 왼; 아래 <-> 오른
+# /: 위 <-> 오른; 아래 <-> 왼
+
+d = {"U": (-1, 0), "R": (0, 1), "D": (1, 0), "L": (0, -1)}
+N, M = map(int, input().split())
+graph = [list(input()) for _ in range(N)]
+r0, c0 = map(int, input().split())
+
+time = {}
+flag = False
+
+for k, (dr, dc) in d.items():
+    r, c = r0-1, c0-1
+    v = [[set() for _ in range(M)] for _ in range(N)]
+    v[r][c].add((dr, dc))
+
+    t = 0
+    while True:
+        nr, nc = r+dr, c+dc
+        t += 1
+        if 0 <= nr < N and 0 <= nc < M:
+            if (dr, dc) in v[nr][nc]:
+                flag = True
+                break
+
+            if graph[nr][nc] == 'C':
+                break
+
+            if graph[nr][nc] == '\\':
+                v[nr][nc].add((dr, dc))
+                dr, dc = dc, dr
+            elif graph[nr][nc] == '/':
+                v[nr][nc].add((dr, dc))
+                dr, dc = -dc, -dr
+
+            r, c = nr, nc
+        else:
+            break
+
+    if flag:
+        break
+    else:
+        time[k] = t
+
+if flag:
+    print(k)
+    print("Voyager")
+else:
+    ans = ''
+    m = 0
+    for k, v in time.items():
+        if m < v:
+            m = v
+            ans = k
+    print(ans)
+    print(m)
