62ac7a0e88b88093104bf5653b4ee10c4de61432
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-25 23:11:47 +0900

    [Gold III] Title: 다리 만들기, Time: 264 ms, Memory: 112172 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,35 +1,4 @@
 D = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
-
-def get_boundary(r, c):
-    boundaries = []
-    q = [(r, c)]
-    v[r][c] = 1
-
-    while q:
-        r, c = q.pop()
-        flag = False
-    
-        for dr, dc in D:
-            nr, nc = r+dr, c+dc
-    
-            if 0 <= nr < N and 0 <= nc < N:
-                if not MAP[nr][nc]:
-                    flag = True
-                    continue
-
-                if v[nr][nc]:
-                    continue
-    
-                v[nr][nc] = 1
-                q.append((nr, nc))
-        
-        if flag:
-            boundaries.append((r, c))
-    
-    return boundaries
-
-
 ans = 200
 N = int(input())
 MAP = [[*map(int, input().split())] for _ in range(N)]
@@ -39,7 +8,28 @@ v = [[0]*N for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if MAP[i][j] and not v[i][j]:
-            islands.append(get_boundary(i, j))
+            boundaries = []
+            q = [(i, j)]
+            v[i][j] = 1
+        
+            while q:
+                r, c = q.pop()
+                flag = False
+            
+                for dr, dc in D:
+                    nr, nc = r+dr, c+dc
+                    if 0 <= nr < N and 0 <= nc < N:
+                        if not MAP[nr][nc]:
+                            flag = True
+                            continue
+                        if v[nr][nc]:
+                            continue
+                        v[nr][nc] = 1
+                        q.append((nr, nc))
+                
+                if flag:
+                    boundaries.append((r, c))
+            islands.append(boundaries)
 
 I = len(islands)
 for i in range(I):
