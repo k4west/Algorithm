0a7a042c61f673387ec58f8279ceb70ac5c1ba37
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-08-30 22:30:53 +0900

    [Platinum V] Title: 회전 미로 탐색, Time: 376 ms, Memory: 144504 KB -BaekjoonHub

--- 백준/Platinum/22949. 회전 미로 탐색/회전 미로 탐색.py
+++ 백준/Platinum/22949. 회전 미로 탐색/회전 미로 탐색.py
@@ -1,6 +1,4 @@
-def rotate(r, c, div):
-    diff_r, diff_c = 4*((div-1)//k), 4*((div-1)%k)
-    return c - diff_c + diff_r, 3 - r + diff_r + diff_c
+from collections import deque
 
 
 def seen(arr, idx):
@@ -11,51 +9,57 @@ def mark(arr, idx):
     arr[idx >> 3] |= (1 << (idx & 7))
 
 
-def bfs(r, c, div):
-    t = 1
+def get_div(r, c):
+    return k*(r >> 2)+(c >> 2)+1
+
+
+def rotate_pos(r, c):
+    diff_r, diff_c = (r >> 2) << 2, (c >> 2) << 2
+    return (c & 3) + diff_r, 3 - (r & 3) + diff_c
+
+
+def rotate_maze(maze):
+    rotated = [row[:] for row in maze]
+    for r in range(K):
+        for c in range(K):
+            nr, nc = rotate_pos(r, c)
+            rotated[nr][nc] = maze[r][c]
+    return rotated
+
+
+def bfs(r, c, arr):
     v = [bytearray(BYTES) for _ in range(4)]
-    mark(v[0], r*K + c)
-    q, nq = [(r, c, r, c, div, 0)], []
+    mark(v[0], r*K+c)
+    q = deque([(r, c, 0, 0)])
     
     while q:
-        for r, c, y, x, div, offset in q:
-            for i in range(5):
-                if i:
-                    dr, dc = D[i-1]
-                    dy, dx = D[(i+offset-1)&3]
+        r, c, offset, t = q.popleft()
+        if arr[offset][r][c] == 'E':
+            return t
+        
+        div = get_div(r, c)
+        for dr, dc in D:
+            nr, nc = r+dr, c+dc
+
+            if 0 <= nr < K and 0 <= nc < K:
+                nxt_div = get_div(nr, nc)
+                if div == nxt_div:
+                    nxt_offset = (offset + 1) & 3
                 else:
-                    dr = dc = dy = dx = 0
-                nr, nc = r + dr,  c + dc
-                ny, nx = y + dy, x + dx
-
-                if 0 <= nr < K and 0 <= nc < K:
-                    nxt_div = k * (nr >> 2) + (nc >> 2) + 1
-                    if div == nxt_div:
-                        nxt_offset = (offset + 1) & 3
-                    else:
-                        nxt_offset = 1
-                        ny, nx = nr, nc
-
-                    if MAZE[ny][nx] == '#':
-                        continue
-                    if MAZE[ny][nx] == 'E':
-                        return t
-
-                    nr, nc = rotate(nr, nc, nxt_div)
-                    idx = nr*K + nc
-                    
-                    if seen(v[nxt_offset], idx):
-                        continue
-                    
-                    mark(v[nxt_offset], idx)
-                    nq.append((nr, nc, ny, nx, nxt_div, nxt_offset))
-
-        q, nq = nq, []
-        t += 1
+                    nxt_offset = 1
+                
+                nr, nc = rotate_pos(nr, nc)
+                idx = nr*K + nc
+                
+                if seen(v[nxt_offset], idx) or arr[nxt_offset][nr][nc] == '#':
+                    continue
+                
+                mark(v[nxt_offset], idx)
+                q.append((nr, nc, nxt_offset, t+1))
     return -1
 
 
-D = ((-1, 0), (0, -1), (1, 0), (0, 1))  # ↑ ← ↓ →
+D = ((0, 0), (-1, 0), (0, -1), (1, 0), (0, 1))  # ↑ ← ↓ →
 k = int(input())
 K = 4*k
 BYTES = (K*K+7) >> 3
@@ -66,9 +70,13 @@ for i in range(K):
     for j in range(K):
         if MAZE[i][j] == 'S':
             si, sj = i, j
-            sdiv = k*(i >> 2)+(j >> 2)+1
             break
     if sdiv:
         break
 
-print(bfs(si, sj, sdiv))
+ROT_MAZE = [[row[:] for row in MAZE]]
+for _ in range(3):
+    MAZE = rotate_maze(MAZE)
+    ROT_MAZE.append(MAZE)
+
+print(bfs(si, sj, ROT_MAZE))
