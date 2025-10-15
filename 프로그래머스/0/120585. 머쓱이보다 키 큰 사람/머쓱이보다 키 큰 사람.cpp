955f539082ae5ee7bd7d013550d76c2737a3191c
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-15 20:19:07 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    for ( int i = 0; i < array.size(); i++ ) {
+        if (array[i] > height) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
