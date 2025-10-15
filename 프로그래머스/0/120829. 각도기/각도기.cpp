65b0bb649402bd87e542f5c133de2c154b6d6e0e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-15 19:38:21 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer;
+    if(angle < 90) {
+        answer = 1;
+    } else if(angle == 90) {
+        answer = 2;
+    } else if(angle < 180) {
+        answer = 3;
+    } else {
+        answer = 4;
+    }
+    return answer;
+}
