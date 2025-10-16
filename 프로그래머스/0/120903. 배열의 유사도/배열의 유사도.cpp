3a1a5fd13fbe1bbeba39c607438073dd54cd2d50
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-16 14:06:18 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -1,17 +1,14 @@
+#include <unordered_set>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<string> s1, vector<string> s2) {
+    unordered_set<string> set1 = unordered_set(s1.begin(), s1.end());
     int answer = 0;
-    for (string a1: s1) {
-        for (string a2: s2) {
-            if (a1 == a2) {
-                answer += 1;
-                break;
-            }
-        }
+    for (string a: s2) {
+        if (set1.count(a)) answer++;
     }
     return answer;
 }
