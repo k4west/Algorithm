533ceac81fdb10c740dba068dfae278db956d81e
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-17 10:20:29 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    for (int num: array) {
+        if (num == n) answer++;
+    }
+    return answer;
+}
