ddd28bc1cfe99777a2014d490e7fe97c1a4024c4
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-28 15:33:07 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/README.md
+++ 프로그래머스/0/120852. 소인수분해/README.md
@@ -1,10 +1,10 @@
 # [level 0] 소인수분해 - 120852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852?language=cpp#) 
 
 ### 성능 요약
 
-메모리: 3.63 MB, 시간: 0.06 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 28일 10:31:49
+2025년 10월 28일 15:33:16
 
 ### 문제 설명
 
--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -7,14 +7,14 @@ vector<int> solution(int n) {
     vector<int> answer;
     if (n%2 == 0) {
         answer.push_back(2);
+        do n/=2;
+        while (n>0 and n%2==0);
     }
-    vector<bool> isnotprime(n+1);
     for (int i=3; i<=n; i+=2) {
-        if (not isnotprime[i] and (n%i == 0)) {
+        if (n%i==0) {
             answer.push_back(i);
-            for (int j=i*i; j<=n; j+=i) {
-                isnotprime[j] = true;
-            }
+            do n/=i;
+            while (n>0 and n%i==0);
         }
     }
     return answer;
