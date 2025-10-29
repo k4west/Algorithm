2420568d8668597d508647b5aa84e6854e078679
Yosep Kim <125423305+k4west@users.noreply.github.com>
2025-10-29 11:46:21 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -1,25 +1,17 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
 
 long long solution(string numbers) {
     long long answer = 0;
-    unordered_map<string, int> d = {
-        {"one", 1}, {"two", 2}, {"three" , 3}, 
-        {"four" , 4}, {"five" , 5}, {"six" , 6}, 
-        {"seven" , 7}, {"eight" , 8}, {"nine", 9},
-        {"zero", 0}
-    };
-    string tmp;
-    for (char s: numbers) {
-        tmp += s;
-        auto it = d.find(tmp);
-        if (it != d.end()) {
-            tmp = "";
-            answer = answer*10 + it->second;
+    vector<string> num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int index;
+    for (int i=0; i<10; i++) {
+        while ((index = numbers.find(num[i])) != string::npos) {
+            numbers.replace(index, num[i].size(), to_string(i));
         }
     }
+    answer = stoll(numbers);
     return answer;
 }
